8c1b4324e0e189d5ca43449485b0662ae5064f9a
Per Åsberg <per.asberg@op5.se>
2009-03-03 09:37:30 +0100

    helpers/widget: A simple helper to easily add widgets to a page (template)
    This helpers makes it really easy to add a new widget to a page.
    
    Example:
    $widget = widget::add('netw_health', array('index'));
    The first argument is the name of the widget we want to include and the
    second argument is an array of options passed to the widget controller.
    The first parameter in the second argument ('index' above) is the method
    that will be called in the widget controller.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/helpers/widget.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * widget helper class.
+ *
+ * @package    NINJA
+ * @author     op5 AB
+ * @license    GPL
+ */
+
+class widget_Core {
+
+	/**
+	*	@name	add
+	*	@desc	Add a new widget
+	* 	@param  string $name
+	*
+	*/
+	public function add($name=false, $arguments=false)
+	{
+		$path = Kohana::find_file('widgets/'.$name, $name, true);
+		require_once($path);
+		$classname = $name.'_Controller';
+		$obj = new $classname;
+		# if we have a requested widget method - let's call it
+		if (!empty($arguments) & is_array($arguments)) {
+			$widget_method = $arguments[0];
+			if (method_exists($obj, $widget_method)) {
+				array_shift($arguments);
+				return $obj->$widget_method($arguments);
+			}
+		}
+
+		# return false if no method defined
+		return false;
+	}
+
+}
