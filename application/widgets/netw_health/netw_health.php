c47eacb0eb2e27a515503cb31b87903c23ab21f5
Per Åsberg <per.asberg@op5.se>
2009-03-05 15:12:59 +0100

    widgets/netw_health: Updated netw_help widget to use updated widget class
    Added some logic to fetch data from the Current_status_Model and
    also extended the class with a helper method to format the fetched data.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/netw_health/netw_health.php
+++ application/widgets/netw_health/netw_health.php
@@ -7,21 +7,88 @@
  * @license    GPL
  */
 class Netw_health_Controller extends Widget_Controller {
+	# define warning/critical limit
+	private $health_warning_percentage = 90;
+	private $health_critical_percentage = 75;
+	private $host_img = false;
+	private $service_img = false;
+	private $crit_img = '/images/thermcrit.png';
+	private $warn_img = '/images/thermwarn.png';
+	private $ok_img = '/images/thermok.png';
+	private $host_val = false;
+	private $service_val = false;
+	private $data = false;
 
-	public function index($arguments=false)
+	public function __construct()
 	{
+		parent::__construct();
+		$this->set_widget_name(__CLASS__);
+	}
+
+	public function index($arguments=false, $master=false)
+	{
+		$this->master_obj = $master;
+
 		# fetch widget view path
-		$path = $this->view_path(__CLASS__, 'view');
+		$view_path = $this->view_path('view');
+
+		# create new model instance
+		$current_status = new Current_status_Model();
+
+		# fetch network health data
+		$this->data = $current_status->get_network_health();
+
+		# format data according to current values
+		$this->format_health_data();
 
 		# assign variables to widget
-		$test = 'tjobba';
+		$host_value 	= $this->host_val;
+		$service_value 	= $this->service_val;
+		$host_image 	= $this->widget_full_path.$this->host_img;
+		$service_image 	= $this->widget_full_path.$this->service_img;
+
+		# set required extra resources
+		$this->js = array('/js/netw_health');
+		$this->css = array('/css/netw_health');
 
 		# fetch widget content
-		require_once($path);
+		require_once($view_path);
 
-		# pass required extra resources to master template
-		$this->js = array($this->widget_path.$this->widget_name(__CLASS__).'/js/netw_health');
-
+		# call parent helper to assign all
+		# variables to master controller
 		return $this->fetch();
+	}
+
+	/**
+	*	@name format_health_data
+	*	@desc Decide how to present network health
+	*
+	*/
+	private function format_health_data()
+	{
+		$data = $this->data;
+		if (empty($data)) {
+			return false;
+		}
+
+		$this->host_val = $data['host_status'];
+		$this->service_val = $data['service_status'];
+
+		# host bar color
+		if ($this->host_val < $this->health_critical_percentage)
+			$this->host_img = $this->crit_img;
+		elseif ($this->host_val < $this->health_warning_percentage)
+			$this->host_img = $this->warn_img;
+		else
+			$this->host_img = $this->ok_img;
+
+		# service bar color
+		if ($this->service_val < $this->health_critical_percentage)
+			$this->service_img = $this->crit_img;
+		elseif ($this->service_val < $this->health_warning_percentage)
+			$this->service_img = $this->warn_img;
+		else
+			$this->service_img = $this->ok_img;
+		return true;
 	}
 }
