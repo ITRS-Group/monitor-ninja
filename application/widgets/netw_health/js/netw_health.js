8c3c9c7f01f533386e369d20c06cb7791799ff2b
Per Åsberg <per.asberg@op5.se>
2009-03-05 15:10:57 +0100

    widgets/netw_health: Added some new resources
    Added images and a css file. This is only to prove that we are able to
    load images and css dynamically.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/netw_health/js/netw_health.js
+++ application/widgets/netw_health/js/netw_health.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
-	doBlink('#widget-network_health', 1, 4);
+	//doBlink('#widget-network_health', 1, 4);
+	$('#widget-network_health').toggle('drop');
+	$('#widget-network_health').toggle('drop');
 });
 var doBlink = function(obj,start,finish) { jQuery(obj).fadeOut(300).fadeIn(300); if(start!=finish) { start=start+1; doBlink(obj,start,finish); } }
