c3b12c80c571231fa1c00a478318af66713054ca
Per Åsberg <per.asberg@op5.se>
2009-03-05 15:01:46 +0100

    config/widget: Add configuration options for widgets
    We need to be able to define where our widgets live in one single
    place and a widget config file seems to be the place to do it.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/config/widget.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Name of widgets directory
+ */
+$config['widget_dirname'] = 'widgets/';
+
+/**
+ * define base path for widgets
+ */
+$config['widget_path'] = 'application/';
