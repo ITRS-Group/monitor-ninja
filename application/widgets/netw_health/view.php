93667651a7bd5e9171c9dfc150dd65a7a397dded
Per Åsberg <per.asberg@op5.se>
2009-03-05 15:21:10 +0100

    widgets/netw_health/view: Updated view to present data from controller
    The netw_health widget uses this view to present the data in the calling
    controller.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -4,12 +4,27 @@
 		<strong>Network health</strong>
 	</div>
 	<div class="widget-editbox">
-		Edit the widget here
+		<!--Edit the widget here-->
 	</div>
 	<div class="widget-content">
-		This is widget content:<br />
-		<?php echo $test ?><br />
+		<!--This is widget content:<br /><br />-->
+
+		<table cellpadding="0" cellspacing="0" id="netw_health">
+		<tr>
+			<td valign="bottom" id="host_health" align="center">
+				<div class="netw_health_label"><?php echo $host_value ?> %</div>
+				<div><?php echo html::image($host_image, array('height' => $host_value, 'width' => '100%')) ?></div>
+				<div class="netw_health_label">HOSTS</div>
+			</td>
+			<td valign="bottom" id="service_health" align="center">
+				<div class="netw_health_label"><?php echo $service_value ?> %</div>
+				<div><?php echo html::image($service_image, array('height' => $service_value, 'width' => '100%')) ?></div>
+				<div class="netw_health_label">SERVICES</div>
+			</td>
+		</tr>
+		</table>
 <?php
+
 	if (!empty($arguments)) {
 		foreach ($arguments as $arg) {
 			echo $arg."<br />";
