4d084fcb9b1488eb69d707a2534ecc709e49fde4
Per Åsberg <per.asberg@op5.se>
2009-03-21 17:34:04 +0100

    models/program_status: Model to fetch program status information from db
    This is a minimal class due to the fact that it instantiates the Kohana
    ORM class. We don't need more than this to fetch the data from it for now.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/models/program_status.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Program_status_Model extends ORM {
+	protected $table_names_plural = false;
+	protected $primary_key = 'instance_id';
+}
