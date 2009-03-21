386ba4ac712e494478d2e1710602b0b6709d297d
Per Åsberg <per.asberg@op5.se>
2009-03-21 17:36:10 +0100

    models/host: Class for specific host info
    Sometimes it's easier just to query the host table than having to do
    other stuff first. This class has some helper methods to make it easy
    to fetch data about specific hosts and at the same time make sure that
    the user has access to it.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/models/host.php
@@ -0,0 +1,80 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Host_Model extends Model {
+	private $auth = false;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->auth = new Nagios_auth_Model();
+	}
+
+	/**
+	*	@name 	get_host_with_services
+	*	@desc	Fetch all onfo on a host. The returned object
+	* 			will contain all database fields for the host object.
+	* 	@param  int $id
+	* 	@param	str $name
+	* 	@return object
+	*
+	*/
+	public function get_hostinfo($id=false, $name=false)
+	{
+
+		$id = (int)$id;
+		$name = trim($name);
+
+		$auth_hosts = $this->auth->get_authorized_hosts();
+		$host_info = false;
+
+		if (!empty($id)) {
+			if (!array_key_exists($id, $auth_hosts)) {
+				return false;
+			} else {
+				$host_info = $this->db->getwhere('host', array('id' => $id));
+			}
+		} elseif (!empty($name)) {
+			if (!array_key_exists($name, $auth->hosts_r)) {
+				return false;
+			} else {
+				$host_info = $this->db->getwhere('host', array('host_name' => $name));
+			}
+		} else {
+			return false;
+		}
+		return $host_info !== false ? $host_info->current() : false;
+	}
+
+	/**
+	*	@name 	authorized_for
+	*	@desc 	Determine if userr is authorized to view info
+	* 			on a specific host.
+	* 			Accepts either hostID or host_name as input
+	* 	@param  int $id
+	* 	@param	str $name
+	* 	@return bool
+	*
+	*/
+	public function authorized_for($id=false, $name=false)
+	{
+		$id = (int)$id;
+		$name = trim($name);
+		$is_auth = false;
+
+		$auth = new Nagios_auth_Model();
+		$auth_hosts = $auth->get_authorized_hosts();
+
+		if (!empty($id)) {
+			if (!array_key_exists($id, $auth_hosts)) {
+				return false;
+			}
+		} elseif (!empty($name)) {
+			if (!array_key_exists($name, $auth->hosts_r)) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+		return true;
+	}
+}
