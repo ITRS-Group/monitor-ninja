15df727d6f25d67bdc1ecdf5aed0eeeecd2b9094
Per Åsberg <per.asberg@op5.se>
2009-04-06 14:53:27 +0200

    widgets/tac_services: Updated links for services
    The links contained 'detail' as the second argument to the services
    method in the status controller. This was bogus as it should point
    to a host. Passing 0 as host means all hosts in this case.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/tac_services/tac_services.php
+++ application/widgets/tac_services/tac_services.php
@@ -38,34 +38,34 @@ class Tac_services_Widget extends widget_Core {
 		$title = $this->translate->_('Services');
 
 		$header_links = array(
-			'status/service/all/detail/'.nagstat::SERVICE_CRITICAL => $current_status->services_critical.' '.$this->translate->_('Critical'),
-			'status/service/all/detail/'.nagstat::SERVICE_WARNING  => $current_status->services_warning.' '.$this->translate->_('Warning'),
-			'status/service/all/detail/'.nagstat::SERVICE_UNKNOWN  => $current_status->services_unknown.' '.$this->translate->_('Unknown'),
-			'status/service/all/detail/'.nagstat::SERVICE_OK  => $current_status->services_ok.' '.$this->translate->_('Ok'),
-			'status/service/all/detail/'.nagstat::SERVICE_PENDING   => $current_status->services_pending.' '.$this->translate->_('Pending')
+			'status/service/all/0/'.nagstat::SERVICE_CRITICAL => $current_status->services_critical.' '.$this->translate->_('Critical'),
+			'status/service/all/0/'.nagstat::SERVICE_WARNING  => $current_status->services_warning.' '.$this->translate->_('Warning'),
+			'status/service/all/0/'.nagstat::SERVICE_UNKNOWN  => $current_status->services_unknown.' '.$this->translate->_('Unknown'),
+			'status/service/all/0/'.nagstat::SERVICE_OK  => $current_status->services_ok.' '.$this->translate->_('Ok'),
+			'status/service/all/0/'.nagstat::SERVICE_PENDING   => $current_status->services_pending.' '.$this->translate->_('Pending')
 		);
 
 		# SERVICES CRITICAL
 		$services_critical = array();
 		if ($current_status->services_critical_unacknowledged) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_CRITICAL.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
 				$current_status->services_critical_unacknowledged.' '.$this->translate->_('Unhandled Problems');
 		}
 
 		if ($current_status->services_critical_host_problem) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_CRITICAL.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_critical_host_problem.' '.$this->translate->_('on Problem Hosts');
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_critical_host_problem.' '.$this->translate->_('on Problem Hosts');
 		}
 
 		if ($current_status->services_critical_scheduled) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_critical_scheduled.' '.$this->translate->_('Scheduled');
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_critical_scheduled.' '.$this->translate->_('Scheduled');
 		}
 
 		if ($current_status->services_critical_acknowledged) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_critical_acknowledged.' '.$this->translate->_('Acknowledged');
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_critical_acknowledged.' '.$this->translate->_('Acknowledged');
 		}
 
 		if ($current_status->services_critical_disabled) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_critical_disabled.' '.$this->translate->_('Disabled');
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_critical_disabled.' '.$this->translate->_('Disabled');
 		}
 
 
@@ -74,61 +74,61 @@ class Tac_services_Widget extends widget_Core {
 		# HOST_UP|HOST_PENDING
 		# SERVICE_NO_SCHEDULED_DOWNTIME|SERVICE_STATE_UNACKNOWLEDGED|SERVICE_CHECKS_ENABLED
 		if ($current_status->services_warning_unacknowledged) {
-			$services_warning['status/service/all/detail/'.nagstat::SERVICE_WARNING.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
+			$services_warning['status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
 				$current_status->services_warning_unacknowledged.' '.$this->translate->_('Unhandled Problems');
 		}
 
 		if ($current_status->services_warning_host_problem) {
-			$services_warning['status/service/all/detail/'.nagstat::SERVICE_WARNING.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_warning_host_problem.' '.$this->translate->_('on Problem Hosts');
+			$services_warning['status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_warning_host_problem.' '.$this->translate->_('on Problem Hosts');
 		}
 
 		if ($current_status->services_warning_scheduled) {
-			$services_critical['status/service/all/detail/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_warning_scheduled.' '.$this->translate->_('Scheduled');
+			$services_critical['status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_warning_scheduled.' '.$this->translate->_('Scheduled');
 		}
 
 		if ($current_status->services_warning_acknowledged) {
-			$services_warning['status/service/all/detail/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_warning_acknowledged.' '.$this->translate->_('Acknowledged');
+			$services_warning['status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_warning_acknowledged.' '.$this->translate->_('Acknowledged');
 		}
 
 		if ($current_status->services_warning_disabled) {
-			$services_warning['status/service/all/detail/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_warning_disabled.' '.$this->translate->_('Disabled');
+			$services_warning['status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_warning_disabled.' '.$this->translate->_('Disabled');
 		}
 
 
 		# SERVICES UNKNOWN
 		$services_unknown = array();
 		if ($current_status->services_unknown_unacknowledged) {
-			$services_unknown['status/service/all/hostdetail/'.nagstat::SERVICE_UNKNOWN.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
+			$services_unknown['status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.(nagstat::HOST_UP|nagstat::HOST_PENDING).'/'.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED)] =
 				$current_status->services_unknown_unacknowledged.' '.$this->translate->_('Unhandled Problems');
 		}
 
 		if ($current_status->services_unknown_host_problem) {
-			$services_unknown['status/service/all/detail/'.nagstat::SERVICE_UNKNOWN.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_unknown_host_problem.' '.$this->translate->_('on Problem Hosts');
+			$services_unknown['status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE)] = $current_status->services_unknown_host_problem.' '.$this->translate->_('on Problem Hosts');
 		}
 
 		if ($current_status->services_unknown_scheduled) {
-			$services_unknown['status/service/all/detail/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_unknown_scheduled.' '.$this->translate->_('Scheduled');
+			$services_unknown['status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME] = $current_status->services_unknown_scheduled.' '.$this->translate->_('Scheduled');
 		}
 
 		if ($current_status->services_unknown_acknowledged) {
-			$services_unknown['status/service/all/detail/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_unknown_acknowledged.' '.$this->translate->_('Acknowledged');
+			$services_unknown['status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_STATE_ACKNOWLEDGED] = $current_status->services_unknown_acknowledged.' '.$this->translate->_('Acknowledged');
 		}
 
 		if ($current_status->services_unknown_disabled) {
-			$services_unknown['status/service/all/detail/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_unknown_disabled.' '.$this->translate->_('Disabled');
+			$services_unknown['status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_CHECKS_DISABLED ] = $current_status->services_unknown_disabled.' '.$this->translate->_('Disabled');
 		}
 
 
 		# SERVICES OK DISABLED
 		$services_ok_disabled = array();
 		if ($current_status->services_ok_disabled) {
-			$services_ok_disabled['status/service/all/detail/'.nagstat::SERVICE_OK.'/'.nagstat::SERVICE_CHECKS_DISABLED] = $current_status->services_ok_disabled.' '.$this->translate->_('Disabled');
+			$services_ok_disabled['status/service/all/0/'.nagstat::SERVICE_OK.'/'.nagstat::SERVICE_CHECKS_DISABLED] = $current_status->services_ok_disabled.' '.$this->translate->_('Disabled');
 		}
 
 		# SERVICES PENDING DISABLED
 		$services_pending_disabled = array();
 		if ($current_status->services_pending_disabled) {
-			$services_pending_disabled['status/service/all/detail/'.nagstat::SERVICE_PENDING .'/'.nagstat::SERVICE_CHECKS_DISABLED] = $current_status->services_pending_disabled.' '.$this->translate->_('Disabled');
+			$services_pending_disabled['status/service/all/0/'.nagstat::SERVICE_PENDING .'/'.nagstat::SERVICE_CHECKS_DISABLED] = $current_status->services_pending_disabled.' '.$this->translate->_('Disabled');
 		}
 
 		# fetch widget content
