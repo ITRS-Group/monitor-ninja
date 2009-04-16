829c79ff521a92165572cf7ef51eef7be0a7a022
My Appelgren <my@op5.com>
2009-04-16 10:29:55 +0200

    just removed some <p>-tag.
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/ninja_start.php
+++ application/views/themes/default/ninja_start.php
@@ -1,12 +1,11 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <div class="box">
-	<p>
-		<?php echo $info ?><br />
-	</p>
+	<?php echo $info ?>
 </div>
 
+
 <ul>
-<?php foreach ($links as $title => $url): ?>
-	<li><?php echo html::anchor($url, html::specialchars($title)) ?></li>
-<?php endforeach ?>
+	<?php foreach ($links as $title => $url): ?>
+		<li><?php echo html::anchor($url, html::specialchars($title)) ?></li>
+	<?php endforeach ?>
 </ul>
