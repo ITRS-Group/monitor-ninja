eccbe4cf5f7748d3459ca2c096f4d8cba2626499
My Appelgren <my@op5.com>
2009-04-16 10:27:00 +0200

    updated layout for the extinfo template
    basically just cleaned up the code and applied the common styles,
    ie. not done at all.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/extinfo/index.php
+++ application/views/themes/default/extinfo/index.php
@@ -8,119 +8,113 @@ if (!empty($widgets)) {
 }
 ?>
 
-<div id="extinfo_object_main_info">
-	<span class='data'><?php echo ucfirst($lable_type) ?><br /></span>
-	<?php echo $main_object ?><br />
-	<?php echo !empty($main_object_alias) ? '('.$main_object_alias.')' : '' ?><br />
-	<?php
-		if ($type == 'service') { ?>
-			<?php echo $lable_on_host ?><br />
-			<?php echo isset($host) ? ucfirst($host) : ''; ?><br />
-			<?php echo isset($host_alias) ? '('.$host_alias.')' : '' ?>
-			<?php echo !empty($host_link) ? '('.$host_link.')' : '' ?>
-			<?php
-		}
-	?>
-	<br />
-	<?php echo $lable_member_of ?><br />
-
-	<?php echo !empty($groups) ? implode(', ', $groups) : $no_group_lable ?><br />
-	<?php echo isset($host_address) ? $host_address : ''; ?>
+<div class="widget left w98" id="extinfo_info">
+	<div id="extinfo_object_main_info">
+		<strong><?php echo ucfirst($lable_type) ?></strong>:
+		<?php echo $main_object ?><br />
+		<?php echo !empty($main_object_alias) ? '<strong>Alias</strong>: '.$main_object_alias : '' ?><br />
+		<strong><?php echo $this->translate->_('IP address');?></strong> <?php echo isset($host_address) ? $host_address : ''; ?><br />
+		<?php
+			if ($type == 'service') {
+				echo $lable_on_host.'<br />';
+				echo (isset($host) ? ucfirst($host) : '').'<br />';
+				echo isset($host_alias) ? '('.$host_alias.')' : '';
+				echo !empty($host_link) ? '('.$host_link.')' : '';
+			}
+		?>
+		<strong><?php echo $lable_member_of ?></strong>:
+		<?php echo !empty($groups) ? implode(', ', $groups) : $no_group_lable ?><br />
+	</div>
 </div>
-
-<table border=1 cellpadding="0" cellspacing="0" id="extinfo_status_table">
-	<tr>
-		<td class='dataVar'><?php echo $lable_current_status ?>:</td>
-		<td class='dataVal'>
-			<div class='service<?php echo $current_status_str ?>'><?php echo $current_status_str ?></div>
-			<?php echo $duration ? '('.$lable_for.' '.$duration.')' : '' ?>
-		</td>
-	</tr>
-
-	<tr>
-		<td class='dataVar' valign='top'><?php echo $lable_status_information ?>:</td>
-		<td class='dataVal'><?php echo $status_info ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar' valign='top'><?php echo $lable_perf_data ?>:</td>
-		<td class='dataVal'><?php echo $perf_data ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_current_attempt ?>:</td>
-		<td class='dataVal'><?php echo $current_attempt ?>/<?php echo $max_attempts ?>&nbsp;&nbsp;(<?php echo $state_type ?>)</td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_last_check ?>:</td>
-		<td class='dataVal'><?php echo $last_check ? date($date_format_str, $last_check) : $na_str ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_check_type ?>:</td>
-		<td class='dataVal'><?php echo $check_type ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar' nowrap="nowrap"><?php echo $lable_check_latency_duration ?>:</td>
-		<td class='dataVal'><?php echo $check_latency ?>&nbsp;/&nbsp;<?php echo $execution_time ?> <?php echo $lable_seconds ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_next_scheduled_check ?>:&nbsp;&nbsp;</td>
-		<td class='dataVal'><?php echo $next_check ? date($date_format_str, $next_check) : $na_str ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_last_state_change ?>:</td>
-		<td class='dataVal'><?php echo $last_state_change ? date($date_format_str, $last_state_change) : $na_str ?></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_last_notification ?>:</td>
-		<td class='dataVal'><?php echo $last_notification ?>&nbsp;(<?php echo $lable_notifications ?> <?php echo $current_notification_number ?>)</td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_flapping ?></td>
-		<td class='dataVal'>
-			<div class='notflapping'>&nbsp;&nbsp;<?php echo $flap_value ?>&nbsp;&nbsp;</div>&nbsp;<?php echo $percent_state_change_str ?>
-		</td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_in_scheduled_dt ?></td>
-		<td class='dataVal'>
-			<div class='downtimeINACTIVE'>&nbsp;&nbsp;<?php echo $scheduled_downtime_depth ?>&nbsp;&nbsp;</div>
-		</td>
-	</tr>
-
-	<tr>
-		<td class='dataVar'><?php echo $lable_last_update ?>:</td>
-		<td class='dataVal'><?php echo $last_update ? date($date_format_str, $last_update) : $na_str ?>&nbsp;&nbsp;<?php echo $last_update_ago ?></td>
-	</tr>
-</table>
-<br />
-
-<table border=1 cellpadding="0" cellspacing="0" id="extinfo_stateinfo_table">
-	<tr>
-		<td class='dataVar'><?php echo $lable_active_checks ?>:</td>
-		<td class='dataVal'><div class='checks<?php echo $active_checks_enabled ?>'>&nbsp;&nbsp;<?php echo $active_checks_enabled ?>&nbsp;&nbsp;</div></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_passive_checks ?>:</td>
-		<td class='dataVal'><div class='checks<?php echo $passive_checks_enabled ?>'>&nbsp;&nbsp;<?php echo $passive_checks_enabled ?>&nbsp;&nbsp;</div></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_obsessing ?>:</td>
-		<td class='dataVal'><div class='checks<?php echo $obsessing ?>'>&nbsp;&nbsp;<?php echo $obsessing ?>&nbsp;&nbsp;</div></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_notifications ?>:</td>
-		<td class='dataVal'><div class='notifications<?php echo $notifications_enabled ?>'>&nbsp;&nbsp;<?php echo $notifications_enabled ?>&nbsp;&nbsp;</div></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_event_handler ?>:</td>
-		<td class='dataVal'><div class='eventhandlers<?php echo $event_handler_enabled ?>'>&nbsp;&nbsp;<?php echo $event_handler_enabled ?>&nbsp;&nbsp;</div></td>
-	</tr>
-	<tr>
-		<td class='dataVar'><?php echo $lable_flap_detection ?>:</td>
-		<td class='dataVal'><div class='flapdetection<?php echo $flap_detection_enabled ?>'>&nbsp;&nbsp;<?php echo $flap_detection_enabled ?>&nbsp;&nbsp;</div></td>
-	</tr>
-</table>
-
 
 <?php
 if (!empty($commands))
 	echo $commands;
+?>
+<div class="widget collapsable left w48" id="extinfo_current">
+	<table style="border-spacing: 1px">
+		<tr class="odd">
+			<td><?php echo $lable_current_status ?></td>
+			<td class="<?php echo $current_status_str ?>"><?php echo $current_status_str ?>
+				(<?php echo $lable_for ?> <?php echo $duration ? $duration : $na_str ?>)
+			</td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_status_information ?></td>
+			<td><?php echo $status_info ?></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_perf_data ?></td>
+			<td><?php echo $perf_data ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_current_attempt ?></td>
+			<td><?php echo $current_attempt ?>/<?php echo $max_attempts ?>(<?php echo $state_type ?>)</td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_last_check ?></td>
+			<td><?php echo $last_check ? date($date_format_str, $last_check) : $na_str ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_check_type ?></td>
+			<td><?php echo $check_type ?></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_check_latency_duration ?></td>
+			<td><?php echo $check_latency ?> / <?php echo $execution_time ?> <?php echo $lable_seconds ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_next_scheduled_check ?></td>
+			<td><?php echo $next_check ? date($date_format_str, $next_check) : $na_str ?></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_last_state_change ?></td>
+			<td><?php echo $last_state_change ? date($date_format_str, $last_state_change) : $na_str ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_last_notification ?></td>
+			<td><?php echo $last_notification ?>&nbsp;(<?php echo $lable_notifications ?> <?php echo $current_notification_number ?>)</td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_flapping ?></td>
+			<td class="notflapping"><?php echo "$flap_value &nbsp; $percent_state_change_str"; ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_in_scheduled_dt ?></td>
+			<td><div class="downtime inactive"><?php echo $scheduled_downtime_depth ?></div></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_last_update ?></td>
+			<td><?php echo $last_update ? date($date_format_str, $last_update) : $na_str ?> <?php echo $last_update_ago ?></td>
+		</tr>
+	</table>
+</div>
+
+<div class="widget collapsable left w49" id="extinfo_checks">
+	<table style="border-spacing: 1px">
+		<tr class="odd">
+			<td><?php echo $lable_active_checks ?></td>
+			<td class="<?php echo $active_checks_enabled ?>"><?php echo $active_checks_enabled ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_passive_checks ?></td>
+			<td class="<?php echo $passive_checks_enabled ?>"><?php echo $passive_checks_enabled ?></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_obsessing ?></td>
+			<td class="<?php echo $obsessing ?>"><?php echo $obsessing ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_notifications ?></td>
+			<td class="<?php echo $notifications_enabled ?>"><?php echo $notifications_enabled ?></td>
+		</tr>
+		<tr class="odd">
+			<td><?php echo $lable_event_handler ?></td>
+			<td class="<?php echo $event_handler_enabled ?>"><?php echo $event_handler_enabled ?></td>
+		</tr>
+		<tr class="even">
+			<td><?php echo $lable_flap_detection ?></td>
+			<td class="<?php echo $flap_detection_enabled ?>"><?php echo $flap_detection_enabled ?></td>
+		</tr>
+	</table>
+</div>
--- application/views/themes/default/extinfo/process_info.php
+++ application/views/themes/default/extinfo/process_info.php
@@ -1,24 +1,68 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<table border=1>
-	<tr><td class='dataVar'><?php echo $lable_program_version ?>:</td><td class='dataVal'><?php echo $program_version ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_program_start_time ?>:</td><td class='dataVal'><?php echo $program_start ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_total_run_time ?>:</td><td class='dataVal'><?php echo $run_time ?></td></tr>
-
-	<tr><td class='dataVar'><?php echo $lable_last_external_cmd_check ?>:</td><td class='dataVal'><?php echo $last_command_check ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_last_logfile_rotation ?>:</td><td class='dataVal'><?php echo $last_log_rotation ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_pid ?></td><td class='dataval'><?php echo $nagios_pid ?></td></tr>
-
-	<tr><td class='dataVar'><?php echo $lable_notifications_enabled ?></td><td class='dataVal'><div class='<?php echo $notifications_class ?>'>&nbsp;&nbsp;<?php echo $notifications_str ?>&nbsp;&nbsp;</div></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_service_checks ?> </td><td class='dataVal'><div class='<?php echo $servicechecks_class ?>'>&nbsp;&nbsp;<?php echo $servicechecks_str ?>&nbsp;&nbsp;</div></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_service_checks_passive ?></td><td class='dataVal'><div class='<?php echo $passive_servicechecks_class ?>'>&nbsp;&nbsp;<?php echo $passive_servicechecks_str ?>&nbsp;&nbsp;</div></td></tr>
-
-	<tr><td class='dataVar'><?php echo $lable_host_checks ?></td><td class='dataVal'><div class='<?php echo $hostchecks_class?>'>&nbsp;&nbsp;<?php echo $hostchecks_str ?>&nbsp;&nbsp;</div></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_host_checks_passive ?></td><td class='dataVal'><div class='<?php echo $passive_hostchecks_class ?>'>&nbsp;&nbsp;<?php echo $passive_hostchecks_str ?>&nbsp;&nbsp;</div></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_event_handlers ?></td><td class='dataVal'><?php echo $eventhandler_str ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_obsess_services ?></td><td class='dataVal'><?php echo $obsess_services_str ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_obsess_hosts ?></td><td class='dataVal'><?php echo $obsess_hosts_str ?></td></tr>
-	<tr><td class='dataVar'><?php echo $lable_flap_enabled ?></td><td class='dataVal'><?php echo $flap_detection_str ?></td></tr>
-
-	<tr><td class='dataVar'><?php echo $lable_performance_data ?></td><td class='dataVal'><?php echo $performance_data_str ?></td></tr>
+<table>
+	<tr>
+		<td><?php echo $lable_program_version ?>:</td>
+		<td><?php echo $program_version ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_program_start_time ?>:</td>
+		<td><?php echo $program_start ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_total_run_time ?>:</td>
+		<td><?php echo $run_time ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_last_external_cmd_check ?>:</td>
+		<td><?php echo $last_command_check ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_last_logfile_rotation ?>:</td>
+		<td><?php echo $last_log_rotation ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_pid ?></td>
+		<td><?php echo $nagios_pid ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_notifications_enabled ?></td>
+		<td class="<?php echo $notifications_class ?>"><?php echo $notifications_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_service_checks ?> </td>
+		<td class="<?php echo $servicechecks_class ?>"><?php echo $servicechecks_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_service_checks_passive ?></td>
+		<td class="<?php echo $passive_servicechecks_class ?>"><?php echo $passive_servicechecks_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_host_checks ?></td>
+		<td class="<?php echo $hostchecks_class?>"><?php echo $hostchecks_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_host_checks_passive ?></td>
+		<td class="<?php echo $passive_hostchecks_class ?>"><?php echo $passive_hostchecks_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_event_handlers ?></td>
+		<td><?php echo $eventhandler_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_obsess_services ?></td>
+		<td><?php echo $obsess_services_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_obsess_hosts ?></td>
+		<td><?php echo $obsess_hosts_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_flap_enabled ?></td>
+		<td><?php echo $flap_detection_str ?></td>
+	</tr>
+	<tr>
+		<td><?php echo $lable_performance_data ?></td>
+		<td><?php echo $performance_data_str ?></td>
+	</tr>
 </table>
