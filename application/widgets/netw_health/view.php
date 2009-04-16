6ad474026c60758edfb8019fd4a47173195d3cc6
My Appelgren <my@op5.com>
2009-04-16 10:23:21 +0200

    updated template for network_health
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -1,36 +1,33 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class="widget movable collapsable removable closeconfirm" id="widget-network_health">
-	<div class="widget-header">
-		<strong><?php echo $title ?></strong>
-	</div>
-	<div class="widget-editbox">
-		<!--Edit the widget here-->
-	</div>
+<div class="widget movable collapsable removable closeconfirm w32 left " id="widget-network_health">
+	<div class="widget-header"><?php echo $title ?></div>
+	<div class="widget-editbox"><!--Edit the widget here--></div>
 	<div class="widget-content">
-		<!--This is widget content:<br /><br />-->
-
-		<table cellpadding="0" cellspacing="0" id="netw_health">
-		<tr>
-			<td valign="bottom" id="host_health" align="center">
-				<div class="netw_health_label"><?php echo $host_value ?> %</div>
-				<div><?php echo html::image($host_image, array('height' => $host_value, 'width' => '100%')) ?></div>
-				<div class="netw_health_label"><?php echo $host_label ?></div>
-			</td>
-			<td valign="bottom" id="service_health" align="center">
-				<div class="netw_health_label"><?php echo $service_value ?> %</div>
-				<div><?php echo html::image($service_image, array('height' => $service_value, 'width' => '100%')) ?></div>
-				<div class="netw_health_label"><?php echo $service_label ?></div>
-			</td>
-		</tr>
-		</table>
-<?php
-
-	if (!empty($arguments)) {
-		foreach ($arguments as $arg) {
-			echo $arg."<br />";
-		}
-	}
-?>
+		<table summary="Network healt" class="healt">
+				<tr>
+					<td style="text-align: center">
+						<div style="color: #ffffff; font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px"><?php echo $host_value ?> %</div>
+						<div style="color: #ffffff; font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px"><?php echo $host_label ?></div>
+						<div class="border">
+							<?php echo html::image($host_image, array('style' => 'height:'.$host_value.'px; width: 100%', 'alt' => $host_label)) ?>
+						</div>
+					</td>
+					<td style="text-align: center">
+						<div style="color: #ffffff; font-size: 22px; position: absolute;padding-top: 62px; padding-left: 10px"><?php echo $service_value ?> %</div>
+						<div style="color: #ffffff; font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px;"><?php echo $service_label ?></div>
+						<div class="border">
+							<?php echo html::image($service_image, array('style' => 'height:'.$service_value.'px; width: 100%', 'alt' => $service_label)) ?>
+						</div>
+					</td>
+				</tr>
+			</table>
+		<?php
+			if (!empty($arguments)) {
+				foreach ($arguments as $arg) {
+					echo $arg.'<br />';
+				}
+			}
+		?>
 	</div>
 </div>
 
