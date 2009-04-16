373911a31f9b3fb911736f1176585eef50ba3ffa
My Appelgren <my@op5.com>
2009-04-16 10:30:56 +0200

    removed some testform from the tac-page
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/tac/index.php
+++ application/views/themes/default/tac/index.php
@@ -1,25 +1,16 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<?php
-	# Testform to try ajax calls
-	# not translated
-	echo form::open(url::site('tac/ajax_host_lookup'), array('id' => 'test_form', 'name' => 'test_form'));
-	echo form::input('host_info')."\n";
-	echo form::submit('s1', 'Host lookup')."\n";
-	echo form::input(array('type' => 'button', 'value' => 'Clear', 'onclick' => 'clearInput();' ))."\n";
-	echo form::close();
-?>
 <div id="response"></div>
-	<?php
+<?php
 	if (!empty($widgets)) {
 		foreach ($widgets as $widget) {
 			echo $widget;
 		}
 	}
-	?>
+?>
 
-<ul>
+<!--<ul>
 <?php foreach ($links as $title => $url): ?>
 	<li><?php echo html::anchor($url, html::specialchars($title)) ?></li>
 <?php endforeach ?>
-</ul>
+</ul>--->
