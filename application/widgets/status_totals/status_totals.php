f33c2384c3eac8b5cd988f33f2f1b192202e6c3f
Per Åsberg <per.asberg@op5.se>
2009-04-17 11:02:03 +0200

    widget/status_totals: Add support for host and servicegroups
    Added support to fetch status_totals data for host- and servicegroups.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/status_totals/status_totals.php
+++ application/widgets/status_totals/status_totals.php
@@ -44,16 +44,134 @@ class Status_totals_Widget extends widget_Core {
 		$label_all_types = $this->translate->_('All Types');
 
 		$host_title = $this->translate->_('Host Status Totals');
-		$total_up = $current_status->hosts_up;
-		$total_down = $current_status->hosts_down;
-		$total_unreachable = $current_status->hosts_unreachable;
-		$total_pending = $current_status->hosts_pending;
-		$total_hosts = $current_status->total_hosts;
-		$total_problems = $current_status->hosts_down + $current_status->hosts_unreachable;
+
 		$host = isset($arguments[0]) ? trim($arguments[0]) : 'all';
 		$host_state = isset($arguments[1]) ? $arguments[1] : nagstat::HOST_UP;
 		$service_state = isset($arguments[2]) ? $arguments[2] : false;
 		$target_method = $service_state === false ? 'host' : 'service';
+
+		# Check if we are dealing with host or servicegroup
+		$grouptype = false;
+		$groupname = false;
+		if (isset($arguments[3]) && !empty($arguments[3])) {
+			$groupname = $host; # groupname is passed at same position in arguments as host
+			$grouptype = trim($arguments[3]);
+			$target_method = $grouptype;
+			$group_info = $current_status->get_servicegroup_hoststatus($groupname);
+		}
+
+		$total_hosts = 0;
+		$total_up = 0;
+		$total_down = 0;
+		$total_unreachable = 0;
+		$total_pending = 0;
+		$svc_total_ok = 0;
+		$svc_total_warning = 0;
+		$svc_total_critical = 0;
+		$svc_total_unknown = 0;
+		$svc_total_pending = 0;
+		$svc_total_services = 0;
+		$prev_host = false;
+
+		if (empty($groupname) && $host != 'all') {
+			# fetch info on selected host
+			$host_model = new Host_Model();
+			$host_info = $host_model->get_hostinfo($host);
+			$total_up = $host_info->current_state == Current_status_Model::HOST_UP ? 1:0;
+			$total_down = $host_info->current_state == Current_status_Model::HOST_DOWN ? 1:0;
+			$total_unreachable = $host_info->current_state == Current_status_Model::HOST_UNREACHABLE ? 1:0;
+			$total_pending = $host_info->current_state == Current_status_Model::HOST_PENDING ? 1:0;
+			$total_hosts = 1;
+			$total_problems = $total_down + $total_unreachable;
+
+			# service data
+			$service_info = $host_model->service_states($host);
+			foreach ($service_info as $service) {
+				switch ($service->current_state) {
+					case Current_status_Model::SERVICE_OK :
+						$svc_total_ok += $service->cnt;
+						break;
+					case Current_status_Model::SERVICE_WARNING:
+						$svc_total_warning += $service->cnt;
+						break;
+					case Current_status_Model::SERVICE_CRITICAL:
+						$svc_total_critical += $service->cnt;
+						break;
+					case Current_status_Model::SERVICE_UNKNOWN:
+						$svc_total_unknown += $service->cnt;
+						break;
+					case Current_status_Model::SERVICE_PENDING :
+						$svc_total_pending += $service->cnt;
+						break;
+				}
+			}
+			$svc_total_services += $service->cnt;
+			$svc_total_problems = $svc_total_unknown + $svc_total_warning + $svc_total_critical;
+
+		} elseif (!empty($groupname)) {
+			foreach ($group_info as $info) {
+				$service_states[$info->host_name][$info->service_state] = $info->service_state;
+				if ($info->id != $prev_host) {
+					switch ($info->current_state) {
+						case Current_status_Model::HOST_UP:
+							$total_up++;
+							break;
+						case Current_status_Model::HOST_DOWN:
+							$total_down++;
+							break;
+						case Current_status_Model::HOST_UNREACHABLE:
+							$total_unreachable++;
+							break;
+						case Current_status_Model::HOST_PENDING:
+							$total_pending++;
+							break;
+					}
+				}
+
+				# service data
+				switch ($info->service_state) {
+					case Current_status_Model::SERVICE_OK :
+						$svc_total_ok += $info->state_count;
+						break;
+					case Current_status_Model::SERVICE_WARNING:
+						$svc_total_warning += $info->state_count;
+						break;
+					case Current_status_Model::SERVICE_CRITICAL:
+						$svc_total_critical += $info->state_count;
+						break;
+					case Current_status_Model::SERVICE_UNKNOWN:
+						$svc_total_unknown += $info->state_count;
+						break;
+					case Current_status_Model::SERVICE_PENDING :
+						$svc_total_pending += $info->state_count;
+						break;
+				}
+				$svc_total_services += $info->state_count;
+				$prev_host = $info->id;
+			}
+
+			$total_hosts = sizeof($service_states);
+			$total_problems = $total_down + $total_unreachable;
+
+			$svc_total_problems = $svc_total_unknown + $svc_total_warning + $svc_total_critical;
+		} else {
+			# host data
+			$total_up = $current_status->hosts_up;
+			$total_down = $current_status->hosts_down;
+			$total_unreachable = $current_status->hosts_unreachable;
+			$total_pending = $current_status->hosts_pending;
+			$total_hosts = $current_status->total_hosts;
+			$total_problems = $current_status->hosts_down + $current_status->hosts_unreachable;
+
+			# service data
+			$svc_total_ok = $current_status->services_ok;
+			$svc_total_warning = $current_status->services_warning;
+			$svc_total_unknown = $current_status->services_unknown;
+			$svc_total_critical = $current_status->services_critical;
+			$svc_total_pending = $current_status->services_pending;
+			$svc_total_services = $current_status->total_services;
+			$svc_total_problems = $svc_total_unknown + $svc_total_warning + $svc_total_critical;
+		}
 
 		$host_header = array(
 			array('th_class' =>'hostTotals', 'link_class' => 'hostTotals',  'url' => 'status/'.$target_method.'/'.$host.'/'.nagstat::HOST_UP, 'lable' => $label_up),
@@ -67,14 +185,6 @@ class Status_totals_Widget extends widget_Core {
 		$svc_label_unknown = $this->translate->_('Unknown');
 		$svc_label_critical = $this->translate->_('Critical');
 		$svc_label_pending = $this->translate->_('Pending');
-
-		$svc_total_ok = $current_status->services_ok;
-		$svc_total_warning = $current_status->services_warning;
-		$svc_total_unknown = $current_status->services_unknown;
-		$svc_total_critical = $current_status->services_critical;
-		$svc_total_pending = $current_status->services_pending;
-		$svc_total_services = $current_status->total_services;
-		$svc_total_problems = $svc_total_unknown + $svc_total_warning + $svc_total_critical;
 
 		$service_header = array(
 			array('th_class' =>'serviceTotals', 'link_class' => 'serviceTotals',  'url' => 'status/service/'.$host.'/'.$host_state.'/'.nagstat::SERVICE_OK, 'lable' => $svc_label_ok),
