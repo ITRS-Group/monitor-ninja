d56f91a7f7e5efb027f7b91764a5a3619757d38a
Per Åsberg <per.asberg@op5.se>
2009-04-17 10:41:49 +0200

    views/status/group_overview: Template for servicegroup overview
    It is far from finished but we are now able to print the overview
    for servicegroups. Not tested with hostgroups yet.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/views/themes/default/status/group_overview.php
@@ -0,0 +1,84 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+if (!empty($widgets)) {
+	foreach ($widgets as $widget) {
+		echo $widget;
+	}
+}
+?>
+
+<div align="center" class='statusTitle'>
+	<?php echo $lable_header ?>
+</div>
+
+<div align="center">
+	<table border="0" cellpadding="0" cellspacing="0">
+		<tr>
+			<td align="center">
+				<div class='status'>
+					<a href='status.cgi?servicegroup=<?php echo $groupname ?>&amp;style=detail'><?php echo $group_alias ?></a>
+					(<a href='extinfo.cgi?type=8&amp;servicegroup=<?php echo $groupname ?>'><?php echo $groupname ?></a>)
+				</div>
+
+				<div class='status'>
+					<table border="1" cellpadding="2" cellspacing="0" class='status'>
+						<tr>
+							<th class='status'>
+								<?php echo $lable_host ?>
+							</th>
+							<th class='status'>
+								<?php echo $lable_status ?>
+							</th>
+							<th class='status'>
+								<?php echo $lable_services ?>
+							</th>
+							<th class='status'>
+								<?php echo $lable_actions ?>
+							</th>
+						</tr>
+				<?php if (!empty($hostinfo))
+						foreach ($hostinfo as $host => $details) { ?>
+						<tr class='statusEven'>
+							<td class='statusEven'>
+								<table border="0" width="100%" cellpadding="0" cellspacing="0">
+									<tr class='statusEven'>
+										<td class='statusEven'>
+											<?php echo $details['status_link'] ?>
+											<?php echo !empty($details['host_icon']) ? $details['host_icon'] : '' ?>
+										</td>
+									</tr>
+								</table>
+							</td>
+							<td class='<?php echo $details['class_name'] ?>'>
+								<?php echo $details['state_str'] ?>
+							</td>
+							<td class='statusEven'>
+								<table border="0" width="100%">
+							<?php if (!empty($service_states[$host]))
+									foreach ($service_states[$host] as $svc_state) {	?>
+									<tr>
+										<td class='<?php echo $svc_state['class_name'] ?>'>
+											<?php echo $svc_state['status_link'] ?>
+										</td>
+									</tr>
+								<?php } ?>
+								</table>
+							</td>
+							<td valign="center" class='statusEven'>
+								<?php echo $svc_state['extinfo_link'] ?>
+								<?php echo !empty($details['notes_link']) ? $details['notes_link'] : '' ?>
+								<?php echo !empty($details['action_link']) ? $details['action_link'] : '' ?>
+								<?php echo $svc_state['svc_status_link'] ?>
+								<?php echo $svc_state['statusmap_link'] ?>
+								<?php echo !empty($svc_state['nacoma_link']) ? $svc_state['nacoma_link'] : '' ?>
+								<?php echo !empty($svc_state['pnp_link']) ? $svc_state['pnp_link'] : '' ?>
+								</a>
+							</td>
+						</tr>
+				<?php 	} ?>
+					</table>
+				</div>
+			</td>
+		</tr>
+	</table>
+</div>
