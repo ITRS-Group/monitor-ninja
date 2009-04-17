c3586c725164a3ff22871a9d06bd29d0676da653
Per Åsberg <per.asberg@op5.se>
2009-04-17 10:21:36 +0200

    controllers/status: Working example of status::servicegroup()
    It supports the 'overview' part but 'details' is still left undone.
    It also has the status_totals widget incorporated.
    Some work is still left to be added though.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/status.php
+++ application/controllers/status.php
@@ -143,16 +143,136 @@ class Status_Controller extends Authenticated_Controller {
 	/**
 	*	@name	servicegroup
 	*	@desc	Show servicegroup status
+	* 	@param 	str $group
+	* 	@param 	int $hoststatustypes
+	* 	@param 	int $servicestatustypes
+	* 	@param 	str $style
 	*
 	*/
 	public function servicegroup($group=false, $hoststatustypes=nagstat::HOST_UP, $servicestatustypes=false, $style='overview')
 	{
 		$group = trim($group);
+		if ($style == 'overview') {
+			$this->template->content = $this->add_view('status/group_overview');
+		} else {
+			# add other template for details or possible redirect to separate method?
+			die('detail view not implemented');
+		}
 		$hostlist = $this->current->get_servicegroup_hoststatus($group, $this->convert_status_value($hoststatustypes), $this->convert_status_value($servicestatustypes, 'service'));
+		$group_info_res = ORM::factory('servicegroup')->where('servicegroup_name', $group)->find();
+
+		$this->template->js_header = $this->add_view('js_header');
+		$this->template->css_header = $this->add_view('css_header');
+
+		widget::add('status_totals', array('index', $this->current, $group, $hoststatustypes, $servicestatustypes, 'servicegroup'), $this);
+		$this->xtra_css = array_merge($this->xtra_css, array($this->add_path('/css/common.css')));
+		$this->template->content->widgets = $this->widgets;
+		$this->template->js_header->js = $this->xtra_js;
+		$this->template->css_header->css = $this->xtra_css;
+
+		$content = $this->template->content;
+		$t = $this->translate;
+		$content->lable_host = $t->_('Host');
+		$content->lable_status = $t->_('Status');
+		$content->lable_services = $t->_('Services');
+		$content->lable_actions = $t->_('Actions');
+
+		# @@@FIXME: handle macros
+
+		$content->group_alias = $group_info_res->alias;
 		if ($hostlist->count() > 0) {
+			$content->lable_header = $t->_("Service Overview For Service Group");
+			$content->lable_header .= " '".$group."'";
+			$service_states = false;
+			$hostinfo = false;
+			$lable_extinfo_host = $t->_('View Extended Information For This Host');
+			$lable_svc_status = $t->_('View Service Details For This Host');
+			$lable_statusmap = $t->_('Locate Host On Map');
 			foreach ($hostlist as $host) {
-				echo $host->host_name."<br />";
+				$nacoma_link = false;
+				/**
+				 * Modify config/config.php to enable NACOMA
+				 * and set the correct path in config/config.php,
+				 * if installed, to use this
+				 */
+				if (Kohana::config('config.nacoma_path')!==false) {
+					$lable_nacoma = $t->_('Configure this host using NACOMA (Nagios Configuration Manager)');
+					$nacoma_link = '<a href="'.Kohana::config('config.nacoma_path').'edit.php?obj_type=host&amp;host='.$host->host_name.'"
+						title="'.$lable_nacoma.'">'.html::image($this->img_path('images/nacoma.png')).'</a>';
+				}
+
+				/**
+				 * Enable PNP4Nagios integration
+				 * Set correct path in config/config.php
+				 */
+				$pnp_link = false;
+				if (Kohana::config('config.pnp4nagios_path')!==false) {
+					$lable_pnp = $t->_('Show performance graph');
+					$pnp_link = '<a href="'.Kohana::config('config.pnp4nagios_path').'index.php?host='.$host->host_name.'"
+						title="'.$lable_pnp.'">'.html::image($this->img_path('images/graphlight.png')).'</a>';
+				}
+
+				# decide status_link host- and servicestate parameters
+				$hst_status_type = nagstat::HOST_PENDING|nagstat::HOST_UP|nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE;
+				$svc_status_type = false;
+				switch ($host->service_state) {
+					case Current_status_Model::SERVICE_OK:
+						$svc_status_type = nagstat::SERVICE_OK;
+						break;
+					case Current_status_Model::SERVICE_WARNING:
+						$svc_status_type = nagstat::SERVICE_WARNING;
+						break;
+					case Current_status_Model::SERVICE_UNKNOWN:
+						$svc_status_type = nagstat::SERVICE_UNKNOWN;
+						break;
+					case Current_status_Model::SERVICE_CRITICAL:
+						$svc_status_type = nagstat::SERVICE_CRITICAL;
+						break;
+					case Current_status_Model::SERVICE_PENDING:
+						$svc_status_type = nagstat::SERVICE_PENDING;
+						break;
+				}
+				$service_states[$host->host_name][$host->service_state] = array(
+					'class_name' => 'miniStatus' . $this->current->translate_status($host->service_state, 'service'),
+					'status_link' => html::anchor('status/servicegroup/'.$group.'/'.$hst_status_type.'/'.$svc_status_type.'/detail', html::specialchars($host->state_count.' '.$this->current->translate_status($host->service_state, 'service')) ),
+					'extinfo_link' => html::anchor('extinfo/details/host/'.$host->host_name, html::image($this->img_path('images/detail.gif'), array('alt' => $lable_extinfo_host, 'title' => $lable_extinfo_host)) ),
+					'svc_status_link' => html::anchor('status/service/'.$host->host_name, html::image($this->img_path('images/status2.gif'), array('alt' => $lable_svc_status, 'title' => $lable_svc_status)) ),
+					'statusmap_link' => html::anchor('statusmap/host/'.$host->host_name, html::image($this->img_path('images/status3.gif'), array('alt' => $lable_statusmap, 'title' => $lable_statusmap)) ),
+					'nacoma_link' => $nacoma_link,
+					'pnp_link' => $pnp_link
+					);
+
+				$action_link = false;
+				if (!is_null($host->action_url)) {
+					$lable_host_action = $t->_('Perform Extra Host Actions');
+					$action_link = '<a href="'.$host->action_url.'" target="_blank">'.html::image($this->img_path('images/action.gif'), array('alt' => $lable_host_action, 'title' => $lable_host_action)).'</a>';
+				}
+				$notes_link = false;
+				if (!is_null($host->notes_url)) {
+					$lable_host_notes = $t->_('View Extra Host Notes');
+					$notes_link = '<a href="'.$host->notes_url.'" target="_blank">'.html::image($this->img_path('images/notes.gif'), array('alt' => $lable_host_notes, 'title' => $lable_host_notes)).'</a>';
+				}
+
+				$host_icon = false;
+				# logos_path
+				if (!empty($host->icon_image)) {
+					$host_icon = '<img src="'.$this->logos_path.$host->icon_image.'" WIDTH=20 HEIGHT=20 border=0 alt="'.$host->icon_image_alt.'" title="'.$host->icon_image_alt.'" />';
+				}
+				$hostinfo[$host->host_name] = array(
+					'state_str' => $this->current->translate_status($host->current_state, 'host'),
+					'class_name' => 'statusHOST' . $this->current->translate_status($host->current_state, 'host'),
+					'status_link' => html::anchor('status/service/'.$host->host_name.'/'.$hoststatustypes.'/'.(int)$servicestatustypes, html::specialchars($host->host_name), array('title' => $host->address)),
+					'action_link' => $action_link,
+					'notes_link' => $notes_link,
+					'host_icon' => $host_icon
+					);
 			}
+			$content->service_states = $service_states;
+			$content->hostinfo = $hostinfo;
+			$content->hoststatustypes = $hoststatustypes;
+			$content->servicestatustypes = $servicestatustypes;
+		} else {
+			# nothing found
 		}
 	}
 
