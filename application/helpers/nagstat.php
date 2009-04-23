525555987802d8b05a9a4fb0376e2db6f60f6750
Andreas Ericsson <ae@op5.se>
2009-04-23 16:11:26 +0200

    helpers/nagstat.php: Alter host and service state constants
    In core nagios, service-states go as follows:
      SERVICE_OK = 0
      SERVICE_WARNING = 1
      SERVICE_CRITICAL = 2
      SERVICE_UNKNOWN = 3
    
    There is no SERVICE_PENDING. But in the CGI's the ordering
    of the service-states go like this:
      SERVICE_PENDING = 1
      SERVICE_OK = 2
      SERVICE_WARNING = 4
      SERVICE_UNKNOWN = 8
      SERVICE_CRITICAL = 16
    
    In other words, UNKNOWN and CRITICAL are flipped in order,
    and PENDING is inserted at the top of the list. This change
    adds PENDING as a magic number 64, and re-orders the rest of
    the macros so that they follow the ordering as it's given
    in-core in nagios.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/helpers/nagstat.php
+++ application/helpers/nagstat.php
@@ -14,10 +14,13 @@ class nagstat_Core {
 	const DISPLAY_HOSTGROUPS = 1;
 	const DISPLAY_SERVICEGROUPS = 2;
 
-	const HOST_PENDING = 1;
-	const HOST_UP = 2;
-	const HOST_DOWN	= 4;
-	const HOST_UNREACHABLE = 8;
+	# These differ from the ones in nagios' cgi's
+	# See comment for service states below for why
+	const HOST_UP = 1;
+	const HOST_DOWN	= 2;
+	const HOST_UNREACHABLE = 4;
+	const HOST_PENDING = 64;
+
 	const HOST_STATE_ACKNOWLEDGED = 4;
 	const HOST_STATE_UNACKNOWLEDGED = 8;
 	const HOST_SCHEDULED_DOWNTIME = 1;
@@ -39,11 +42,18 @@ class nagstat_Core {
 	const HOST_HARD_STATE = 262144;
 	const HOST_SOFT_STATE = 524288;
 
-	const SERVICE_PENDING = 1;
-	const SERVICE_OK = 2;
-	const SERVICE_WARNING = 4;
+	# These are different from the ones in nagios' cgi's,
+	# because we use bitmasks in our sql queries, and that
+	# doesn't work unless these consts are sequential and
+	# in the same order as the *real* states are.
+	# We introduce the magic number 64 (1 << 6) for PENDING
+	# instead of keeping special numbers at first.
+	const SERVICE_OK = 1;
+	const SERVICE_WARNING = 2;
+	const SERVICE_CRITICAL = 4;
 	const SERVICE_UNKNOWN = 8;
-	const SERVICE_CRITICAL = 16;
+	const SERVICE_PENDING = 64;
+
 	const SERVICE_SCHEDULED_DOWNTIME = 1;
 	const SERVICE_NO_SCHEDULED_DOWNTIME	= 2;
 	const SERVICE_STATE_ACKNOWLEDGED = 4;
