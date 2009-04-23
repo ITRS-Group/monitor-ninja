60dab60ef25fa2900b26719c2b5d0f606b73a7d3
Per Åsberg <per.asberg@op5.se>
2009-04-23 09:07:58 +0200

    controllers/extinfo: Don't assume database result
    Table program_status should always contain data but we should nevertheless
    check that we actually have something before trying to use it.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/extinfo.php
+++ application/controllers/extinfo.php
@@ -193,7 +193,7 @@ class Extinfo_Controller extends Authenticated_Controller {
 
 		# check if nagios is running, will affect wich template to use
 		$status = Program_status_Model::get_all();
-		if (!$status->current()->is_running) {
+		if (empty($status) || !$status->current()->is_running) {
 			$this->template->content->commands = $this->add_view('extinfo/not_running');
 			$this->template->content->commands->info_message = $t->_('It appears as though Nagios is not running, so commands are temporarily unavailable...');
 			$this->template->content->commands->info_message_extra = sprintf($t->_('Click %s to view Nagios process information'), html::anchor('extinfo/show_process_info', html::specialchars($t->_('here'))));
