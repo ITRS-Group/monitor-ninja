91d18e2d33c288488393f5ab07816ad2ea6d4ebf
Per Åsberg <per.asberg@op5.se>
2009-04-24 10:20:16 +0200

    widget/status_totals: Check for empty host_state
    We used to check if hoststatus is set but we also need to check if
    not empty in order to get correct default hoststatus value.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/status_totals/status_totals.php
+++ application/widgets/status_totals/status_totals.php
@@ -56,7 +56,7 @@ class Status_totals_Widget extends widget_Core {
 		$total_hosts = $current_status->total_hosts;
 		$total_problems = $current_status->hosts_down + $current_status->hosts_unreachable;
 		$host = isset($arguments[0]) ? trim($arguments[0]) : 'all';
-		$host_state = isset($arguments[1]) ? $arguments[1] : nagstat::HOST_UP;
+		$host_state = isset($arguments[1]) && !empty($arguments[1]) ? $arguments[1] : nagstat::HOST_UP;
 		$service_state = isset($arguments[2]) ? $arguments[2] : false;
 		$target_method = $service_state === false ? 'host' : 'service';
 
