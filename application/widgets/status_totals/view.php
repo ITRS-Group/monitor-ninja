8909ccbb2a08b41786453d03c5b07849c1adca76
My Appelgren <my@op5.com>
2009-04-27 15:41:12 +0200

    updated layout for status_totalts
    - now with text lables as well
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/status_totals/view.php
+++ application/widgets/status_totals/view.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class="widget movable collapsable removable closeconfirm right" id="widget-host_totals" style="margin-right: 1%">
+<div class="widget movable collapsable removable closeconfirm left" id="widget-host_totals">
 	<div class="widget-header">
 		<strong><?php echo $host_title ?></strong>
 	</div>
@@ -7,21 +7,28 @@
 		<table class="max" style="border-spacing: 1px">
 			<tr>
 			<?php foreach ($host_header as $row) { ?>
-				<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/16x16/shield-'.strtolower($row['status']).'.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status']))) ?></td>
-				<td class="icon"><?php echo html::anchor($row['url'], html::specialchars($row['lable'])) ?></td>
-			<?php	} ?>
-			</tr>
-			<tr>
-				<td class="status icon"><?php echo $total_problems ?></td>
-				<td colspan="3"><?php echo html::anchor('status/host/'.$host.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE), html::specialchars($label_all_host_problems)) ?></td>
-				<td class="status icon"><?php echo $total_hosts ?></td>
-				<td colspan="3"><?php echo html::anchor('status/service/'.$host.'/false/'.$service_state, html::specialchars($label_all_host_types)) ?></td>
+				<td class="status icon">
+					<?php
+					if ($row['lable'] > 0)
+						echo html::image('application/views/themes/default/images/icons/12x12/shield-'.strtolower($row['status']).'.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status'])));
+					else
+						echo html::image('application/views/themes/default/images/icons/12x12/shield-not-'.strtolower($row['status']).'.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status'])));
+					?>
+				</td>
+				<td style="padding-right: 10px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$this->translate->_($row['status']))) ?></td>
+				<?php	} ?>
+				<td class="status icon">
+					<?php echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status']))); ?>
+				</td>
+				<td style="padding-right: 10px"><?php echo html::anchor('status/host/'.$host.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE), html::specialchars($total_problems.' '.$label_all_host_problems)) ?></td>
+				<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-info.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status']))); ?></td>
+				<td style="padding-right: 10px"><?php echo html::anchor('status/host/'.$host, html::specialchars($total_hosts.' Hosts in total')) ?></td>
 			</tr>
 		</table>
 	</div>
 </div>
 
-<div class="widget movable collapsable removable closeconfirm right" id="widget-status_totals">
+<div class="widget movable collapsable removable closeconfirm left" id="widget-status_totals">
 	<div class="widget-header">
 		<strong><?php echo $service_title ?></strong>
 	</div>
@@ -30,15 +37,20 @@
 			<table style="border-spacing: 1px">
 				<tr>
 					<?php foreach ($service_header as $row) { ?>
-						<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/16x16/shield-'.strtolower($row['status']).'.png',$this->translate->_($row['status'])) ?></td>
-						<td class="icon"><?php echo html::anchor($row['url'], html::specialchars($row['lable'])) ?></td>
+							<td class="status icon">
+								<?php
+									if ($row['lable'] > 0)
+										echo html::image('application/views/themes/default/images/icons/12x12/shield-'.strtolower($row['status']).'.png',$this->translate->_($row['status'])) ;
+									else
+										echo html::image('application/views/themes/default/images/icons/12x12/shield-not-'.strtolower($row['status']).'.png',$this->translate->_($row['status'])) ;
+								?>
+							</td>
+							<td style="padding-right: 10px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$this->translate->_($row['status']))) ?></td>
 					<?php } ?>
-				</tr>
-				<tr>
-					<td class="status icon"><?php echo $svc_total_problems ?></td>
-					<td colspan="5"><?php echo html::anchor('status/service/'.$host.'/'.(nagstat::HOST_PENDING|nagstat::HOST_UP|nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'/'.(nagstat::SERVICE_UNKNOWN|nagstat::SERVICE_WARNING|nagstat::SERVICE_CRITICAL), html::specialchars($label_all_service_problems)) ?></td>
-					<td class="status icon"><?php echo $svc_total_services ?></td>
-					<td colspan="3"><?php echo html::anchor('status/service/'.$host.'/', html::specialchars($label_all_service_types)) ?></td>
+					<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status']))); ?></td>
+					<td style="padding-right: 10px"><?php echo html::anchor('status/service/'.$host.'/'.(nagstat::HOST_PENDING|nagstat::HOST_UP|nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'/'.(nagstat::SERVICE_UNKNOWN|nagstat::SERVICE_WARNING|nagstat::SERVICE_CRITICAL), html::specialchars($svc_total_problems.' '.$label_all_service_problems)) ?></td>
+					<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-info.png',array('title' => $this->translate->_($row['status']), 'alt' => $this->translate->_($row['status']))); ?></td>
+					<td style="padding-right: 10px"><?php echo html::anchor('status/service/'.$host.'/', html::specialchars($svc_total_services.' Services in total')) ?></td>
 				</tr>
 			</table>
 		</div>
