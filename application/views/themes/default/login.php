a44560b78943451aec933941d359a80f91a00c18
My Appelgren <my@op5.com>
2009-04-28 16:59:01 +0200

    updated the layout for the login-page
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/login.php
+++ application/views/themes/default/login.php
@@ -1,13 +1,28 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 if (isset($error_msg)) echo $error_msg; ?>
-<br />
-<?php
-	echo form::open('default/do_login');
-	echo form::label('dologin', $form_title).':<br/>';
-	echo $username.": ".form::input('username').'<br/>';
-	echo $password.": &nbsp;".form::password('password').'<br/>';
-	echo csrf::form_field();
-	echo form::submit('login', $login_btn_txt);
-	echo form::close();
-?>
+
+<?php echo form::open('default/do_login'); ?>
+<table id="login-table">
+	<tr>
+		<td colspan="2"><h3><?php echo $this->translate->_('Login') ?></h3></td>
+	</tr>
+	<tr>
+		<td><?php echo $username ?></td>
+		<td><cite><em></em><?php echo form::input('username','','class="text"') ?><em></em></cite></td>
+	</tr>
+	<tr>
+		<td><?php echo $password ?></td>
+		<td><cite><em></em><?php echo form::password('password','','class="text"') ?><em></em></cite></td>
+	</tr>
+	<tr>
+	<td>&nbsp;</td>
+	<td>
+		<?php
+			echo csrf::form_field();
+			echo form::submit('login', $login_btn_txt, 'class="btn"');
+		?>
+	</td>
+	</tr>
+</table>
+<?php echo form::close() ?>
