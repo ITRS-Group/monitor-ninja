9553a7231352b6d38ea32be543666c73a180693d
Per Åsberg <per.asberg@op5.se>
2009-04-28 17:14:01 +0200

    views/status/group_summary: Added host and service summary to template
    This should be pretty much all the data that is needed to show the
    servicegroup view. Some links and the host_totals widget is still missing.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/status/group_summary.php
+++ application/views/themes/default/status/group_summary.php
@@ -133,6 +133,190 @@ if (!empty($group_details)) {
 			<?php } ?>
 			</table>
 		</td>
+		<td>
+			<table border="0">
+	<?php
+		$service_data = false;
+		$service_data = $details->service_data;
+			if ($service_data->services_ok > 0) { ?>
+				<tr>
+					<td class="miniStatusOK">
+						<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?servicestatustypes='.nagstat::SERVICE_OK.'&hoststatustypes='.$hoststatustypes.'&serviceproperties='.$serviceproperties.'&hostproperties='.$hostproperties, $service_data->services_ok.' '.$label_ok) ?>
+					</td>
+				</tr>
+	<?php 	}
+			if ($service_data->services_warning > 0) { ?>
+				<tr>
+					<td class="miniStatusWARNING">
+						<table border="0">
+							<tr>
+								<td class="miniStatusWARNING">
+									<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?servicestatustypes='.nagstat::SERVICE_WARNING.'&hoststatustypes='.$hoststatustypes.'&serviceproperties='.$serviceproperties.'&hostproperties='.$hostproperties, $service_data->services_warning.' '.$label_warning) ?>
+								</td>
+								<td>
+									<table border="0">
+									<?php if ($service_data->services_warning_unacknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceImportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_WARNING.'&hoststatustypes='.(nagstat::HOST_UP|nagstat::HOST_PENDING).'&serviceproperties='.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED), $service_data->services_warning_unacknowledged.' '.$label_unhandled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_warning_host_problem > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_WARNING.'&hoststatustypes='.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE), $service_data->services_warning_host_problem.' '.$label_on_problem_hosts) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_warning_scheduled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_WARNING.'&serviceproperties='.nagstat::SERVICE_SCHEDULED_DOWNTIME, $service_data->services_warning_scheduled.' '.$label_scheduled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_warning_acknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_WARNING.'&serviceproperties='.nagstat::SERVICE_STATE_ACKNOWLEDGED, $service_data->services_warning_acknowledged.' '.$label_acknowledged) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_warning_disabled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_WARNING.'&serviceproperties='.nagstat::SERVICE_CHECKS_DISABLED, $service_data->services_warning_disabled.' '.$label_disabled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+	<?php 	}
+
+			if ($service_data->services_unknown > 0) { ?>
+				<tr>
+					<td class="miniStatusUNKNOWN">
+						<table border="0">
+							<tr>
+								<td class="miniStatusUNKNOWN">
+									<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&hoststatustypes='.$hoststatustypes.'&serviceproperties='.$serviceproperties.'&hostproperties='.$hostproperties, $service_data->services_unknown.' '.$label_unknown) ?>
+								</td>
+								<td>
+									<table border="0">
+									<?php if ($service_data->services_unknown_unacknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceImportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&hoststatustypes='.(nagstat::HOST_UP|nagstat::HOST_PENDING).'&serviceproperties='.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED), $service_data->services_unknown_unacknowledged.' '.$label_unhandled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_unknown_host_problem > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&hoststatustypes='.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE), $service_data->services_unknown_host_problem.' '.$label_on_problem_hosts) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_unknown_scheduled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&serviceproperties='.nagstat::SERVICE_SCHEDULED_DOWNTIME, $service_data->services_unknown_scheduled.' '.$label_scheduled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_unknown_acknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&serviceproperties='.nagstat::SERVICE_STATE_ACKNOWLEDGED, $service_data->services_unknown_acknowledged.' '.$label_acknowledged) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_unknown_disabled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&serviceproperties='.nagstat::SERVICE_CHECKS_DISABLED, $service_data->services_unknown_disabled.' '.$label_disabled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+	<?php 	}
+
+			if ($service_data->services_critical > 0) { ?>
+				<tr>
+					<td class="miniStatusCRITICAL">
+						<table border="0">
+							<tr>
+								<td class="miniStatusCRITICAL">
+									<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?servicestatustypes='.nagstat::SERVICE_CRITICAL.'&hoststatustypes='.$hoststatustypes.'&serviceproperties='.$serviceproperties.'&hostproperties='.$hostproperties, $service_data->services_critical.' '.$label_critical) ?>
+								</td>
+								<td>
+									<table border="0">
+									<?php if ($service_data->services_critical_unacknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceImportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_CRITICAL.'&hoststatustypes='.(nagstat::HOST_UP|nagstat::HOST_PENDING).'&serviceproperties='.(nagstat::SERVICE_NO_SCHEDULED_DOWNTIME|nagstat::SERVICE_STATE_UNACKNOWLEDGED|nagstat::SERVICE_CHECKS_ENABLED), $service_data->services_critical_unacknowledged.' '.$label_unhandled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_critical_host_problem > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_CRITICAL.'&hoststatustypes='.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE), $service_data->services_critical_host_problem.' '.$label_on_problem_hosts) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_critical_scheduled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_CRITICAL.'&serviceproperties='.nagstat::SERVICE_SCHEDULED_DOWNTIME, $service_data->services_critical_scheduled.' '.$label_scheduled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_critical_acknowledged > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_CRITICAL.'&serviceproperties='.nagstat::SERVICE_STATE_ACKNOWLEDGED, $service_data->services_critical_acknowledged.' '.$label_acknowledged) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									<?php if ($service_data->services_critical_disabled > 0) { ?>
+										<tr>
+											<td width=100%% class='serviceUnimportantProblem'>
+												<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_CRITICAL.'&serviceproperties='.nagstat::SERVICE_CHECKS_DISABLED, $service_data->services_critical_disabled.' '.$label_disabled) ?>
+											</td>
+										</tr>
+									<?php } ?>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+	<?php 	}
+			if ($service_data->services_pending > 0) { ?>
+			<tr>
+				<td class="miniStatusPENDING">
+					<A HREF='%s?servicegroup=%s&style=detail&servicestatustypes=%d&hoststatustypes=%d&serviceprops=%lu&hostprops=%lu'>%d PENDING</A>
+					<?php echo html::anchor('status/servicegroup/'.$details->groupname.'?style=detail&servicestatustypes='.nagstat::SERVICE_PENDING.'&hoststatustypes='.$hoststatustypes.'&serviceproperties='.$serviceproperties.'&hostproperties='.$hostproperties, $service_data->services_pending.' '.$label_pending) ?>
+
+				</td>
+			</tr>
+		<?php 	} ?>
+			</table>
+			<?php if (($service_data->services_ok + $service_data->services_warning + $service_data->services_unknown + $service_data->services_critical + $service_data->services_pending) == 0) { ?>
+				<?php echo $label_no_servicedata ?>
+			<?php } ?>
+		</td>
 	</tr>
 	<?php } ?>
 </table>
