ac57c181f799baaffa2e6f2f9f5f68a44f00d6a1
My Appelgren <my@op5.com>
2009-04-28 16:59:52 +0200

    removed all content from this template.
    it should be removed really, unless someone want to have a plain
    text front page that could be displayed even if not logged in.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/ninja_start.php
+++ application/views/themes/default/ninja_start.php
@@ -1,11 +1,1 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class="box">
-	<?php echo $info ?>
-</div>
-
-
-<ul>
-	<?php foreach ($links as $title => $url): ?>
-		<li><?php echo html::anchor($url, html::specialchars($title)) ?></li>
-	<?php endforeach ?>
-</ul>
