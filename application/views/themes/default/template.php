73b1250fc6c8e536b2912f931cba7e97dafe6be2
My Appelgren <my@op5.com>
2009-04-30 09:20:52 +0200

    added widget-script to main template
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/template.php
+++ application/views/themes/default/template.php
@@ -17,6 +17,8 @@
 		<?php echo html::script('application/media/js/jquery.min.js') ?>
 		<?php echo html::script('application/media/js/jquery.form.js') ?>
 		<?php echo html::script('application/media/js/jquery.tablesorter.min.js') ?>
+		<?php echo html::script('application/media/js/jquery.easywidgets.min.js') ?>
+		<?php echo html::script('application/media/js/jquery.ui.min.js') ?>
 		<?php echo html::script('application/media/js/btn.js') ?>
 		<script type="text/javascript">
 			//<!--
@@ -32,6 +34,29 @@
 						}
 					});
 				});
+
+				$(function(){
+	$.fn.EasyWidgets({
+		i18n : {
+			editText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Settings" style="margin: -12px 30px 0px auto; display: block" />',
+			closeText : '<img src="/ninja/application/views/themes/default/images/icons/box-close.png" alt="Close widget"   style="margin: -12px 0px 0px auto; display: block" />',
+			collapseText : '<img src="/ninja/application/views/themes/default/images/icons/box-maximize.png" alt="Collapse"  style="margin: -12px 15px 0px auto; display: block" />',
+			cancelEditText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Cancel" style="margin: -12px 30px 0px auto; display: block" />',
+			extendText : '<img src="/ninja/application/views/themes/default/images/icons/box-mimimize.png" alt="Extend" style="margin: -12px 15px 0px auto; display: block" />'
+		},
+		effects : {
+			effectDuration : 150,
+			widgetShow : 'slide',
+			widgetHide : 'slide',
+			widgetClose : 'slide',
+			widgetExtend : 'slide',
+			widgetCollapse : 'slide',
+			widgetOpenEdit : 'slide',
+			widgetCloseEdit : 'slide',
+			widgetCancelEdit : 'slide'
+		}
+	});
+});
 			//-->
 		</script>
 		<?php echo html::script('application/media/js/ajax_test.js') ?>
