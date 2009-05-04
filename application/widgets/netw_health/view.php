07b91b9e871c4fdd154f5ac9364c3b5cb8d34525
My Appelgren <my@op5.com>
2009-05-04 16:38:07 +0200

    corrected the display of the network healt diagram
    the bars where not bottom aligned as they should. this wasn't
    visible until everything wan't ok.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -6,17 +6,17 @@
 		<table summary="Network healt" class="healt">
 				<tr>
 					<td style="text-align: center">
-						<div style="color: #ffffff; font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px"><?php echo $host_value ?> %</div>
+						<div style="color: #ffffff; font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px;"><?php echo $host_value ?> %</div>
 						<div style="color: #ffffff; font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px"><?php echo $host_label ?></div>
 						<div class="border">
-							<?php echo html::image($host_image, array('style' => 'height:'.$host_value.'px; width: 100%', 'alt' => $host_label)) ?>
+							<?php echo html::image($host_image, array('style' => 'height:'.$host_value.'px; width: 100%; padding-top: '.round(100-$host_value).'px', 'alt' => $host_label)) ?>
 						</div>
 					</td>
 					<td style="text-align: center">
 						<div style="color: #ffffff; font-size: 22px; position: absolute;padding-top: 62px; padding-left: 10px"><?php echo $service_value ?> %</div>
 						<div style="color: #ffffff; font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px;"><?php echo $service_label ?></div>
 						<div class="border">
-							<?php echo html::image($service_image, array('style' => 'height:'.$service_value.'px; width: 100%', 'alt' => $service_label)) ?>
+							<?php echo html::image($service_image, array('style' => 'height:'.$service_value.'px; width: 100%; padding-top: '.round(100-$service_value).'px', 'alt' => $service_label)) ?>
 						</div>
 					</td>
 				</tr>
