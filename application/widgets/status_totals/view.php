381dc5ddbdc7e05965307f150740490715554459
My Appelgren <my@op5.com>
2009-05-05 16:18:18 +0200

    fixed the highlight of the total problems icon for status totals widget
    if there are any host or service problems the host provblems icon should
    be highlighted.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/status_totals/view.php
+++ application/widgets/status_totals/view.php
@@ -15,14 +15,19 @@
 						echo html::image('application/views/themes/default/images/icons/12x12/shield-not-'.strtolower($row['status']).'.png',array('title' => $row['status'], 'alt' => $row['status']));
 					?>
 				</td>
-				<td style="padding-right: 10px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$row['status'])) ?></td>
+				<td style="padding-right: 7px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$row['status'])) ?></td>
 				<?php	} ?>
 				<td class="status icon">
-					<?php echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $row['status'], 'alt' => $row['status'])); ?>
+					<?php
+						if ($total_problems > 0)
+							echo html::image('application/views/themes/default/images/icons/12x12/shield-warning.png',array('title' => $row['status'], 'alt' => $row['status']));
+						else
+							echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $row['status'], 'alt' => $row['status']));
+					?>
 				</td>
-				<td style="padding-right: 10px"><?php echo html::anchor('status/host/'.$host.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'?group_type='.$grouptype, html::specialchars($total_problems.' '.$label_all_host_problems)) ?></td>
+				<td style="padding-right: 7px"><?php echo html::anchor('status/host/'.$host.'/'.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'?group_type='.$grouptype, html::specialchars($total_problems.' '.$label_all_host_problems)) ?></td>
 				<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-info.png',array('title' => $row['status'], 'alt' => $row['status'])); ?></td>
-				<td style="padding-right: 10px"><?php echo html::anchor('status/'.$target_method.'/'.$host.'?group_type='.$grouptype, html::specialchars($total_hosts.' Hosts in total')) ?></td>
+				<td style="padding-right: 7px"><?php echo html::anchor('status/'.$target_method.'/'.$host.'?group_type='.$grouptype, html::specialchars($total_hosts.' Hosts in total')) ?></td>
 			</tr>
 		</table>
 	</div>
@@ -45,12 +50,19 @@
 										echo html::image('application/views/themes/default/images/icons/12x12/shield-not-'.strtolower($row['status']).'.png',$row['status']) ;
 								?>
 							</td>
-							<td style="padding-right: 10px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$row['status'])) ?></td>
+							<td style="padding-right: 7px"><?php echo html::anchor($row['url'], html::specialchars($row['lable'].' '.$row['status'])) ?></td>
 					<?php } ?>
-					<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $row['status'], 'alt' => $row['status'])); ?></td>
-					<td style="padding-right: 10px"><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.(nagstat::HOST_PENDING|nagstat::HOST_UP|nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'&servicestatustypes='.(nagstat::SERVICE_UNKNOWN|nagstat::SERVICE_WARNING|nagstat::SERVICE_CRITICAL).'&group_type='.$grouptype, html::specialchars($svc_total_problems.' '.$label_all_service_problems)) ?></td>
+					<td class="status icon">
+						<?php
+							if ($svc_total_problems > 0)
+								echo html::image('application/views/themes/default/images/icons/12x12/shield-warning.png',array('title' => $row['status'], 'alt' => $row['status']));
+							else
+								echo html::image('application/views/themes/default/images/icons/12x12/shield-not-warning.png',array('title' => $row['status'], 'alt' => $row['status']));
+						?>
+					</td>
+					<td style="padding-right: 7px"><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.(nagstat::HOST_PENDING|nagstat::HOST_UP|nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'&servicestatustypes='.(nagstat::SERVICE_UNKNOWN|nagstat::SERVICE_WARNING|nagstat::SERVICE_CRITICAL).'&group_type='.$grouptype, html::specialchars($svc_total_problems.' '.$label_all_service_problems)) ?></td>
 					<td class="status icon"><?php echo html::image('application/views/themes/default/images/icons/12x12/shield-info.png',array('title' => $row['status'], 'alt' => $row['status'])); ?></td>
-					<td style="padding-right: 10px"><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.$host_state.'&group_type='.$grouptype, html::specialchars($svc_total_services.' Services in total')) ?></td>
+					<td style="padding-right: 7px"><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.$host_state.'&group_type='.$grouptype, html::specialchars($svc_total_services.' Services in total')) ?></td>
 				</tr>
 			</table>
 		</div>
