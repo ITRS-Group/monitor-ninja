ca22ca0bb4501bda66fb460587a221621b9b87fd
My Appelgren <my@op5.com>
2009-05-08 10:33:54 +0200

    just cleaned up the code slightly for no running & unauthrorized extinfo
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/extinfo/not_running.php
+++ application/views/themes/default/extinfo/not_running.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<div align=center class='infoMessage'>
+<div>
 	<?php echo $info_message ?><br />
 	<?php echo isset($info_message_extra) ? $info_message_extra : '' ?>
 </div>
