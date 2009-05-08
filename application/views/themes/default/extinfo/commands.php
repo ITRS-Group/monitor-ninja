d8426bd3cc4bcfba352c4833736632e53ba67508
My Appelgren <my@op5.com>
2009-05-08 10:37:07 +0200

    changed the image paths so they point local & not to monitor
    the images are probably broken now due to other reasons, but
    i cannot reach the extinfo page atm so i'll fix that when
    i can.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/extinfo/commands.php
+++ application/views/themes/default/extinfo/commands.php
@@ -8,45 +8,45 @@
 		?>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/status3.gif" alt="<?php echo $lable_host_map ?>" title="<?php echo $lable_host_map ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/status3.png', array('alt' => $lable_host_map, 'title' => $lable_host_map)); ?>
 			</td>
 			<td><a href="statusmap/host/<?php echo $host ?>"><?php echo $lable_host_map ?></a></td>
 		</tr>
 		<?php } ?>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_active_checks ?>" title="<?php echo $lable_active_checks ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_active_checks, 'title' => $lable_active_checks)); ?>
 			</td>
 			<td><?php echo $link_active_checks ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/delay.gif" alt="<?php echo $lable_reschedule_check ?>" title="<?php echo $lable_reschedule_check ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/delay.png', array('alt' => $lable_reschedule_check, 'title' => $lable_reschedule_check)); ?>
 			</td>
 			<td><?php echo $link_reschedule_check ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/passiveonly.gif" alt="<?php echo $lable_submit_passive_checks ?>" title="<?php echo $lable_submit_passive_checks ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/passiveonly.png', array('alt' => $lable_submit_passive_checks, 'title' => $lable_submit_passive_checks)); ?>
 			</td>
 			<td><?php echo $link_submit_passive_check ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_stop_start_passive_checks ?>" title="<?php echo $lable_stop_start_passive_checks ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_stop_start_passive_checks, 'title' => $lable_stop_start_passive_checks)); ?>
 			</td>
 			<td><?php echo $link_stop_start_passive_check ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_obsessing ?>" title="<?php echo $lable_obsessing ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_obsessing, 'title' => $lable_obsessing)); ?>
 			</td>
 			<td><?php echo $link_obsessing ?></td>
 		</tr>
 		<?php if ($show_ackinfo) { ?>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/ack.gif' border=0 alt='<?php echo $lable_acknowledge_problem ?>" title="<?php echo $lable_acknowledge_problem ?>">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/ack.png', array('alt' => $lable_acknowledge_problem, 'title' => $lable_acknowledge_problem)); ?>
 			</td>
 			<td><?php echo $link_acknowledge_problem ?></td>
 		</tr>
@@ -66,6 +66,7 @@
 		<?php if ($show_delay) {	?>
 		<tr>
 			<td class="status icon">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/status3.png', array('alt' => $lable_host_map, 'title' => $lable_host_map)); ?>
 				<img src="/monitor/images/delay.gif" border=0 alt="<?php echo $lable_delay_notification ?>" title="<?php echo $lable_delay_notification ?>">
 			</td>
 			<td><?php echo $link_delay_notifications ?></td>
@@ -73,6 +74,7 @@
 		<?php } ?>
 		<tr>
 			<td class="status icon">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/status3.png', array('alt' => $lable_host_map, 'title' => $lable_host_map)); ?>
 				<img src="/monitor/images/downtime.gif" alt="<?php echo $lable_schedule_dt ?>" title="<?php echo $lable_schedule_dt ?>" />
 			</td>
 			<td><?php echo $link_schedule_dt ?></td>
@@ -80,44 +82,44 @@
 		<?php if ($type == 'host') {?>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_disable_service_notifications_on_host ?>" title="<?php echo $lable_disable_service_notifications_on_host ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_disable_service_notifications_on_host, 'title' => $lable_disable_service_notifications_on_host)); ?>
 			</td>
 			<td><?php echo $link_disable_service_notifications_on_host ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/enabled.gif" alt="<?php echo $lable_enable_service_notifications_on_host ?>" title="<?php echo $lable_enable_service_notifications_on_host ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/enabled.png', array('alt' => $lable_enable_service_notifications_on_host, 'title' => $lable_enable_service_notifications_on_host)); ?>
 			</td>
 			<td><?php echo $link_enable_service_notifications_on_host ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/delay.gif" alt="<?php echo $lable_check_all_services ?>" title="<?php echo $lable_check_all_services ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/delay.png', array('alt' => $lable_check_all_services, 'title' => $lable_check_all_services)); ?>
 			</td>
 			<td><?php echo $link_check_all_services ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_disable_servicechecks ?>" title="<?php echo $lable_disable_servicechecks ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_disable_servicechecks, 'title' => $lable_disable_servicechecks)); ?>
 			</td>
 			<td><?php echo $link_disable_servicechecks ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/enabled.gif" alt="<?php echo $lable_enable_servicechecks ?>" title="<?php echo $lable_enable_servicechecks ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/enabled.png', array('alt' => $lable_enable_servicechecks, 'title' => $lable_enable_servicechecks)); ?>
 			</td>
 			<td><?php echo $link_enable_servicechecks ?></td>
 		</tr>
 		<?php } ?>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_enable_disable_event_handler ?>" title="<?php echo $lable_enable_disable_event_handler ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_enable_disable_event_handler, 'title' => $lable_enable_disable_event_handler)); ?>
 			</td>
 			<td><?php echo $link_enable_disable_event_handler ?></td>
 		</tr>
 		<tr>
 			<td class="status icon">
-				<img src="/monitor/images/disabled.gif" alt="<?php echo $lable_enable_disable_flapdetection ?>" title="<?php echo $lable_enable_disable_flapdetection ?>" />
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $lable_enable_disable_flapdetection, 'title' => $lable_enable_disable_flapdetection)); ?>
 			</td>
 			<td><?php echo $link_enable_disable_flapdetection ?></td>
 		</tr>
--- application/views/themes/default/extinfo/nagios_commands.php
+++ application/views/themes/default/extinfo/nagios_commands.php
@@ -1,77 +1,79 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class='commandTitle'><?php echo $title ?></div>
 
-<table>
-	<tr>
-		<td>
-			<img src="/monitor/images/stop.gif" alt="<?php echo $label_shutdown_nagios ?>" title="<?php echo $label_shutdown_nagios ?>" />
-		</td>
-		<td><?php echo $link_shutdown_nagios ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/restart.gif" alt="<?php echo $label_restart_nagios ?>" title="<?php echo $label_restart_nagios ?>" />
-		</td>
-		<td><?php echo $link_shutdown_nagios ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_notifications ?>" title="<?php echo $label_notifications ?>" />
-		</td>
-		<td><?php echo $link_notifications ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_execute_service_checks ?>" title="<?php echo $label_execute_service_checks ?>" />
-		</td>
-		<td><?php echo $link_execute_service_checks ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_passive_service_checks ?>" title="<?php echo $label_passive_service_checks ?>" />
-		</td>
-		<td><?php echo $link_passive_service_checks ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_execute_host_checks ?>" title="<?php echo $label_execute_host_checks ?>" />
-		</td>
-		<td><?php echo $link_execute_host_checks ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_accept_passive_host_checks ?>" title="<?php echo $label_accept_passive_host_checks ?>" />
-		</td>
-		<td><?php echo $link_accept_passive_host_checks ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_enable_event_handlers ?>" title="<?php echo $label_enable_event_handlers ?>" />
-		</td>
-		<td><?php echo $link_enable_event_handlers ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/enabled.gif" alt="<?php echo $label_obsess_over_services ?>" title="<?php echo $label_obsess_over_services ?>" />
-		</td>
-		<td><?php echo $link_obsess_over_services ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/enabled.gif" alt="<?php echo $label_obsess_over_hosts ?>" title="<?php echo $label_obsess_over_hosts ?>" />
-		</td>
-		<td><?php echo $link_obsess_over_hosts ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_flap_detection_enabled ?>" title="<?php echo $label_flap_detection_enabled ?>" />
-		</td>
-		<td><?php echo $link_flap_detection_enabled ?></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="/monitor/images/disabled.gif" alt="<?php echo $label_process_performance_data ?>" title="<?php echo $label_process_performance_data ?>" />
-		</td>
-		<td><?php echo $link_process_performance_data ?></td>
-	</tr>
-</table>
+<div class="widget left" id="nagios_commands">
+	<div class='widget-header'><?php echo $title ?></div>
+	<table>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/stop.png', array('alt' => $label_shutdown_nagios, 'title' => $label_shutdown_nagios)); ?>
+			</td>
+			<td><?php echo $link_shutdown_nagios ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/restart.png', array('alt' => $label_restart_nagios, 'title' => $label_restart_nagios)); ?>
+			</td>
+			<td><?php echo $link_shutdown_nagios ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_notifications, 'title' => $label_notifications)); ?>
+			</td>
+			<td><?php echo $link_notifications ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_execute_service_checks, 'title' => $label_execute_service_checks)); ?>
+			</td>
+			<td><?php echo $link_execute_service_checks ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_passive_service_checks, 'title' => $label_passive_service_checks)); ?>
+			</td>
+			<td><?php echo $link_passive_service_checks ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_execute_host_checks, 'title' => $label_execute_host_checks)); ?>
+			</td>
+			<td><?php echo $link_execute_host_checks ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_accept_passive_host_checks, 'title' => $label_accept_passive_host_checks)); ?>
+			</td>
+			<td><?php echo $link_accept_passive_host_checks ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_enable_event_handlers, 'title' => $label_enable_event_handlers)); ?>
+			</td>
+			<td><?php echo $link_enable_event_handlers ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/enabled.png', array('alt' => $label_obsess_over_services, 'title' => $label_obsess_over_services)); ?>
+			</td>
+			<td><?php echo $link_obsess_over_services ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/enabled.png', array('alt' => $label_obsess_over_hosts, 'title' => $label_obsess_over_hosts)); ?>
+			</td>
+			<td><?php echo $link_obsess_over_hosts ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabledpng', array('alt' => $label_flap_detection_enabled, 'title' => $label_flap_detection_enabled)); ?>
+			</td>
+			<td><?php echo $link_flap_detection_enabled ?></td>
+		</tr>
+		<tr>
+			<td class="dark">
+				<?php echo images::html('application/themes/defautl/images/icons/16x16/disabled.png', array('alt' => $label_process_performance_data, 'title' => $label_process_performance_data)); ?>
+			</td>
+			<td><?php echo $link_process_performance_data ?></td>
+		</tr>
+	</table>
+</div>
