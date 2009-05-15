23951ebe826b7d8fbf43f0407a9ff433fefb116c
My Appelgren <my@op5.com>
2009-05-15 21:41:02 +0200

    fixed the issue with collapsing the widgets on the tac page
    seems like the script got confused when the images were displayed
    as block items. works fine now.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/js/tac_widgets.js
+++ application/views/themes/default/js/tac_widgets.js
@@ -4,11 +4,11 @@ $(function(){
 			useCookies : true
 		},
 		i18n : {
-			editText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Settings" style="margin: -12px 30px 0px auto; display: block" />',
-			closeText : '<img src="/ninja/application/views/themes/default/images/icons/box-close.png" alt="Close widget"   style="margin: -12px 0px 0px auto; display: block" />',
-			collapseText : '<img src="/ninja/application/views/themes/default/images/icons/box-maximize.png" alt="Collapse"  style="margin: -12px 15px 0px auto; display: block" />',
-			cancelEditText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Cancel" style="margin: -12px 30px 0px auto; display: block" />',
-			extendText : '<img src="/ninja/application/views/themes/default/images/icons/box-mimimize.png" alt="Extend" style="margin: -12px 15px 0px auto; display: block" />'
+			editText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Settings" />',
+			closeText : '<img src="/ninja/application/views/themes/default/images/icons/box-close.png" alt="Close widget" style="float: right; margin-top: -14px" />',
+			collapseText : '<img src="/ninja/application/views/themes/default/images/icons/box-maximize.png" alt="Collapse" style="float: right; margin-top: -14px; margin-right: 16px" />',
+			cancelEditText : '<img src="/ninja/application/views/themes/default/images/icons/box-config.png" alt="Cancel" />',
+			extendText : '<img src="/ninja/application/views/themes/default/images/icons/box-mimimize.png" alt="Extend" style="float: right; margin-top: -14px; margin-right: 16px" />'
 		},
 		effects : {
 			effectDuration : 150,
