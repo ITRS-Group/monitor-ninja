01240502b655d3fed74255fddb37597a6910fa0d
My Appelgren <my@op5.com>
2009-05-16 12:20:34 +0200

    updated the login page with the new ninja logo and facicon
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/login.php
+++ application/views/themes/default/login.php
@@ -7,6 +7,7 @@
 		<title><?php if (isset($title)) echo html::specialchars($title) ?></title>
 		<?php echo html::stylesheet('application/views/themes/default/css/common.css') ?>
 		<?php echo html::stylesheet('application/views/themes/default/css/css-buttons.css') ?>
+		<?php echo html::link('application/views/themes/default/images/favicon_ninja.ico','icon','image/icon') ?>
 		<script type="text/javascript">
 			//<!--
 				var _site_domain = '<?php echo Kohana::config('config.site_domain') ?>';
@@ -24,12 +25,11 @@
 		
 		
 			<div id="login-table">
+			<?php echo html::image('/application/views/themes/default/images/ninja_login.png', array('alt' => 'Ninja', 'title' => 'Ninja', 'style' => 'margin-left: 7px')) ?>
 			<?php if (isset($error_msg)) echo $error_msg; ?>
 			<?php echo form::open('default/do_login'); ?>
 			<table>
-				<tr>
-					<td colspan="2"><h3><?php echo $this->translate->_('Login to NINJA') ?></h3><hr /></td>
-				</tr>
+				<tr><td colspan="2"><hr /></td></tr>
 				<tr>
 					<td><?php echo $username ?></td>
 					<td><cite><em></em><?php echo form::input('username','','class="text"') ?><em></em></cite></td>
@@ -38,15 +38,15 @@
 					<td><?php echo $password ?></td>
 					<td><cite><em></em><?php echo form::password('password','','class="text"') ?><em></em></cite></td>
 				</tr>
-				<tr>
 				<tr><td colspan="2"><hr /></td></tr>
-				<td>&nbsp;</td>
-				<td>
-					<?php
-						echo csrf::form_field();
-						echo form::submit('login', $login_btn_txt, 'class="bn"');
-					?>
-				</td>
+				<tr>
+					<td>&nbsp;</td>
+					<td>
+						<?php
+							echo csrf::form_field();
+							echo form::submit('login', $login_btn_txt);
+						?>
+					</td>
 				</tr>
 			</table>
 		<?php echo form::close() ?>
