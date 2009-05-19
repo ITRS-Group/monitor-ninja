c23cbb7ef8262ab271a6b4128e08c0a70c4d8a31
Andreas Ericsson <ae@op5.se>
2009-05-19 17:08:49 +0200

    models/command: Start fleshing out the command model slightly
    So far it's still only stubs with no real content, but we're
    at least paving the way for obtaining the information we need
    and got a clear picture for how we'll be returning it so the
    template view can benefit from it.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/models/command.php
+++ application/models/command.php
@@ -3,8 +3,40 @@
 /**
  * Sends external commands to Nagios command FIFO
  */
-class Command_Model extends ORM {
-	protected $table_names_plural = false;
-	protected $has_and_belongs_to_many = array('service');
-	protected $primary_key = 'id';
+class Command_Model extends Model
+{
+	private $auth = false;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->auth = new Nagios_auth_Model();
+	}
+
+	/**
+	 * Obtain command information
+	 * Complete with information and data needed to request input
+	 * regarding a particular command
+	 * @param $cmd The name (or 'id') of the command
+	 * @return Indexed array
+	 */
+	public function get_command_info($cmd)
+	{
+		$info = nagioscmd::cmd_info($cmd);
+		# we need the template to get the information we need
+		if (empty($info) || !isset($info['template'])) {
+			return false;
+		}
+
+		$raw_params = array_slice(explode(';', $info['template']), 1);
+		$params = array();
+		foreach ($raw_params as $param_name) {
+			# reset between each loop
+			$ary = array();
+
+			# populate $params[$param_name] here
+			$params[$param_name] = array();
+		}
+		$info['params'] = &$params;
+	}
 }
