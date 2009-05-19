d7f12ba5db4392c2d509bdd0dcc2e31f2a4c2cd4
Per Åsberg <per.asberg@op5.se>
2009-05-19 16:52:18 +0200

    Added global search scripts
    Some resources necessary for the "Show host" search box.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/views/themes/default/js/global_search.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+	console.log('ready');
+   $('#query').autocomplete({
+    serviceUrl:_site_domain + _index_page + '/ajax/global_search/',
+    minChars:2,
+    //delimiter: /(,|;)\s*/, // regex or character
+    maxHeight:400,
+    width:140,
+    deferRequestBy: 0, //miliseconds
+    //params: { country:'Yes' } //aditional parameters
+    // callback function:
+    onSelect: function(value, data){ do_redirect(value, data); },
+  });
+
+});
+
+// function from http://forums.devshed.com/t39065/s84ded709f924610aa44fff827511aba3.html
+// author appears to be Robert Pollard
+
+function sprintf()
+{
+   if (!arguments || arguments.length < 1 || !RegExp)
+   {
+      return;
+   }
+   var str = arguments[0];
+   var re = /([^%]*)%('.|0|\x20)?(-)?(\d+)?(\.\d+)?(%|b|c|d|u|f|o|s|x|X)(.*)/;
+   var a = b = [], numSubstitutions = 0, numMatches = 0;
+   while (a = re.exec(str))
+   {
+      var leftpart = a[1], pPad = a[2], pJustify = a[3], pMinLength = a[4];
+      var pPrecision = a[5], pType = a[6], rightPart = a[7];
+
+      numMatches++;
+      if (pType == '%')
+      {
+         subst = '%';
+      }
+      else
+      {
+         numSubstitutions++;
+         if (numSubstitutions >= arguments.length)
+         {
+            alert('Error! Not enough function arguments (' + (arguments.length - 1)
+               + ', excluding the string)\n'
+               + 'for the number of substitution parameters in string ('
+               + numSubstitutions + ' so far).');
+         }
+         var param = arguments[numSubstitutions];
+         var pad = '';
+                if (pPad && pPad.substr(0,1) == "'") pad = leftpart.substr(1,1);
+           else if (pPad) pad = pPad;
+         var justifyRight = true;
+                if (pJustify && pJustify === "-") justifyRight = false;
+         var minLength = -1;
+                if (pMinLength) minLength = parseInt(pMinLength);
+         var precision = -1;
+                if (pPrecision && pType == 'f')
+                   precision = parseInt(pPrecision.substring(1));
+         var subst = param;
+         switch (pType)
+         {
+         case 'b':
+            subst = parseInt(param).toString(2);
+            break;
+         case 'c':
+            subst = String.fromCharCode(parseInt(param));
+            break;
+         case 'd':
+            subst = parseInt(param) ? parseInt(param) : 0;
+            break;
+         case 'u':
+            subst = Math.abs(param);
+            break;
+         case 'f':
+            subst = (precision > -1)
+             ? Math.round(parseFloat(param) * Math.pow(10, precision))
+              / Math.pow(10, precision)
+             : parseFloat(param);
+            break;
+         case 'o':
+            subst = parseInt(param).toString(8);
+            break;
+         case 's':
+            subst = param;
+            break;
+         case 'x':
+            subst = ('' + parseInt(param).toString(16)).toLowerCase();
+            break;
+         case 'X':
+            subst = ('' + parseInt(param).toString(16)).toUpperCase();
+            break;
+         }
+         var padLeft = minLength - subst.toString().length;
+         if (padLeft > 0)
+         {
+            var arrTmp = new Array(padLeft+1);
+            var padding = arrTmp.join(pad?pad:" ");
+         }
+         else
+         {
+            var padding = "";
+         }
+      }
+      str = leftpart + padding + subst + rightPart;
+   }
+   return str;
+}
+
+
+
+function do_redirect(value, data)
+{
+	//var parts = data.split(',');
+
+	//var str = sprintf('/%s/%s/', data[0], data[1]);
+	var match = value.split(';');
+	if (match.length) {
+		value = match[1];
+	}
+
+	var str = sprintf(data[0], data[1], value);
+	//console.dir(data);
+	//alert(str);
+	self.location.href = _site_domain + _index_page + str;
+	//self.location.href = _site_domain + _index_page + '/status/service/' + value;
+	//self.location.href = _site_domain + _index_page + '/extinfo/details/service/' + data + '/?service=' + value;
+}
