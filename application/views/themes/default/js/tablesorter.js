1b96876302b5b484db15f9bd7a0c34cad8f8df46
Per Åsberg <per.asberg@op5.se>
2009-05-25 10:29:31 +0200

    js/tablesorter: Remove default sort order
    The tablesorter plugin was configured to always re-sort the display
    whenever it was used. This slows down the performance of the page rendering
    so let's use the initial sort from the db query.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/js/tablesorter.js
+++ application/views/themes/default/js/tablesorter.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
 	$("#host_table").tablesorter({
-		sortList: [[1,0]],
+		//sortList: [[1,0]],
 		headers: {
 			6: { sorter: false }
 		},
 	}),
 	$("#service_table").tablesorter({
-		sortList: [[1,0]],
+		//sortList: [[1,0]],
 		headers: {
 			7: { sorter: false }
 		},
 	}),
 	// group overview tables
 	$(".group_overview_table").tablesorter({
-		sortList: [[1,0]],
+		//sortList: [[1,0]],
 		headers: {
 			2: { sorter: false },
 			3: { sorter: false }
 		},
-	}),	
+	}),
 	$(".group_grid_table").tablesorter({
-		sortList: [[0,0]],
+		//sortList: [[0,0]],
 		headers: {
 			1: { sorter: false },
 			2: { sorter: false }
 		},
 	}),
 	$("#group_summary_table").tablesorter({
-		sortList: [[0,0]],
+		//sortList: [[0,0]],
 		headers: {
 			1: { sorter: false },
 			2: { sorter: false }
 		},
 	}),
 	$(".comments_table").tablesorter({
-		sortList: [[0,0]],
+		//sortList: [[0,0]],
 		headers: {
 			7: { sorter: false }
 		},
