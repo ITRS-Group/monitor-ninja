d1eea9faad6a3c60bbad6adb8c28082a2a546b00
Per Åsberg <per.asberg@op5.se>
2009-05-25 14:19:22 +0200

    controllers/configuration: Enable direct links configure objects
    Modified the configuration controller to make it possible to pass in
    parameters to the configure() method.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/configuration.php
+++ application/controllers/configuration.php
@@ -11,15 +11,35 @@ class Configuration_Controller extends Authenticated_Controller {
 
 	public $model = false;
 
-	public function __construct()
+	/**
+	 * Enable links from Ninja to Nacoma
+	 *
+	 * If 'service' is passed as GET, it will be used
+	 * Checks are also made that NACOMA is configured in config file
+	 *
+	 * @param string $type
+	 * @param string $name
+	 */
+	public function configure($type=false, $name=false)
 	{
-		parent::__construct();
-		$this->model = new Current_status_Model();
-	}
+		$type = urldecode($this->input->get('type', $type));
+		$name = urldecode($this->input->get('name', $name));
+		$service = urldecode($this->input->get('service', false));
+		if (Kohana::config('config.nacoma_path')===false) {
+			return false;
+		}
+		$type = trim($type);
+		$name = trim($name);
 
-	public function configure()
-	{
-		$this->template->content = '<iframe src="/monitor/op5/webconfig/configure.php" style="width: 100%; height: 768px" frameborder="0"></iframe>';
+		$target_link = 'configure.php';
+		if (!empty($type) && !empty($name)) {
+			if (!empty($service)) {
+				$target_link = 'edit.php?obj_type='.$type.'&host='.$name.'&service='.$service;
+			} else {
+				$target_link = 'edit.php?obj_type='.$type.'&'.$type.'='.$name;
+			}
+		}
+		$this->template->content = '<iframe src="'.Kohana::config('config.nacoma_path').'/'.$target_link.'" style="width: 100%; height: 768px" frameborder="0"></iframe>';
 		$this->template->title = $this->translate->_('Configuration » Configure');
 	}
 }
