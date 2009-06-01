8124d900805402818591008539e0c710b9f5e51d
My Appelgren <my@op5.com>
2009-06-01 13:58:13 +0200

    fixed the layout for the network outages page
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/outages/network_outages.php
+++ application/views/themes/default/outages/network_outages.php
@@ -5,38 +5,52 @@
 		<!--<caption><?php echo $title ?></caption>-->
 		<thead>
 			<tr>
-				<th><?php //echo $label_state ?>&nbsp;</th>
-				<th><?php echo $label_severity ?></th>
-				<th><?php echo $label_host ?></th>
-				<th><?php echo $label_notes ?></th>
-				<th><?php echo $label_duration ?></th>
-				<th><?php echo $label_hosts_affected ?></th>
-				<th><?php echo $label_services_affected ?></th>
-				<th class="no-sort"><?php echo $label_actions ?></th>
+
+				<th class="header"><?php //echo $label_state ?>&nbsp;</th>
+				<th class="header"><?php echo $label_host ?></th>
+				<th class="header"><?php echo $label_severity ?></th>
+				<!--<th class="header"><?php //echo $label_notes ?></th>-->
+				<th class="header"><?php echo $label_duration ?></th>
+				<th class="header"><?php echo $label_hosts_affected ?></th>
+				<th class="header"><?php echo $label_services_affected ?></th>
+				<th  class="header" colspan="6"><?php echo $label_actions ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php
+			$i = 0;
 			if (!empty($outage_data)) {
 				foreach ($outage_data as $host => $details) {
+					$i++;
 			?>
-			<tr>
-				<td><?php echo Current_status_Model::status_text($details['current_state']) ?></td>
-				<td><?php echo $details['severity'] ?></td>
-				<td><a href='extinfo.cgi?type=1&amp;host=switch1'><?php echo $host ?></a></td>				
-				<td>
-					<?php echo html::anchor('extinfo/details/host/'.$host, "<img src='/monitor/images/comment.gif' border='0' alt=
-					'This host has ".$details['comments']." comments associated with it' title='This host has ".$details['comments']." comments associated with it' />") ?>
+			<tr class="<?php echo ($i%2 == 0) ? 'odd' : 'even' ?>">
+
+				<td class="icon">
+					<?php //echo html::image('/application/views/themes/default/icons/16x16/shield-'.strtolower(Current_status_Model::status_text($row->current_state, Router::$method)).'.png',array('alt' => Current_status_Model::status_text($row->current_state, Router::$method), 'title' => $this->translate->_('Host status').': '.Current_status_Model::status_text($row->current_state, Router::$method))) ?>
+					<?php echo html::image('/application/views/themes/default/icons/16x16/shield-'.strtolower(Current_status_Model::status_text($details['current_state'])).'.png',array('alt' => 'hej', 'title' => 'hej')) ?>
 				</td>
+				<td><a href='extinfo.cgi?type=1&amp;host=switch1'><?php echo $host ?></a></td>
+				<td><?php echo $details['severity'] ?></td>
 				<td><?php echo $details['duration'] ?></td>
 				<td><?php echo $details['affected_hosts'] ?></td>
 				<td><?php echo $details['affected_services'] ?></td>
-				<td>
-					<?php echo html::anchor('status/host/'.$host, "<img src='/monitor/images/status2.gif' border='0' alt='View status detail for this host' title='View status detail for this host' />") ?>
-					<?php echo html::anchor('statusmap/'.$host, "<img src='/monitor/images/status3.gif' border='0' alt='View status map for this host and its children' title='View status map for this host and its children' />") ?>
-					<?php echo html::anchor('trends/host/'.$host, "<img src='/monitor/images/trends.gif' border='0' alt='View trends for this host'title='View trends for this host' />") ?>
-					<?php echo html::anchor('history/host/'.$host, "<img src='/monitor/images/history.gif'border='0' alt='View alert history for this host' title='View alert history for this host' />") ?>
-					<?php echo html::anchor('notifications/host/'.$host, "<img src='/monitor/images/notify.gif' border='0' alt='View notifications for this host' title='View notifications for this host' />") ?>
+				<td class="icon">
+					<?php echo html::anchor('extinfo/details/host/'.$host, html::image('/application/views/themes/default/icons/16x16/add-comment.png',array('alt' => $this->translate->_('View comments for this host'), 'title' => $this->translate->_('View comments for this host')))) ?>
+				</td>
+				<td class="icon">
+					<?php echo html::anchor('status/host/'.$host, html::image('/application/views/themes/default/icons/16x16/service-details.gif',array('alt' => $this->translate->_('View status detail for this host'), 'title' => $this->translate->_('View status detail for this host')))) ?>
+				</td>
+				<td class="icon">
+					<?php echo html::anchor('statusmap/'.$host, html::image('/application/views/themes/default/icons/16x16/locate-host-on-map.png',array('alt' => $this->translate->_('View status map for this host and its children'), 'title' => $this->translate->_('View status map for this host and its children')))) ?>
+				</td>
+				<td class="icon">
+					<?php echo html::anchor('trends/host/'.$host, html::image('/application/views/themes/default/icons/16x16/trends.gif',array('alt' => $this->translate->_('View trends for this host'), 'title' => $this->translate->_('View trends for this host')))) ?>
+				</td>
+				<td class="icon">
+					<?php echo html::anchor('history/host/'.$host, html::image('/application/views/themes/default/icons/16x16/history.gif',array('alt' => $this->translate->_('View alert history for this host'), 'title' => $this->translate->_('View alert history for this host')))) ?>
+				</td>
+				<td class="icon">
+					<?php echo html::anchor('notifications/host/'.$host, html::image('/application/views/themes/default/icons/16x16/notify.png',array('alt' => $this->translate->_('View notifications for this host'), 'title' => $this->translate->_('View notifications for this host')))) ?>
 				</td>
 			</tr>
 			<?php }	}	?>
