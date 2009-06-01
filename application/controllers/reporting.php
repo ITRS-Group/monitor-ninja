17f5d155bd9a224758316389ab7b8e2ccfaa8165
Per Åsberg <per.asberg@op5.se>
2009-06-01 16:19:19 +0200

    controllers/reporting: Transfer requests to avail_result
    When we know what to show a report on we can use our reporting controller
    to route the request straight into avail_result instead of forcing the
    user to select what to show the report on.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/reporting.php
+++ application/controllers/reporting.php
@@ -17,9 +17,29 @@ class Reporting_Controller extends Authenticated_Controller {
 		$this->model = new Current_status_Model();
 	}
 
-	public function availability()
+	public function availability($type='host', $name=false, $service=false)
 	{
-		$this->template->content = '<iframe src="/monitor/op5/reports/gui/avail_setup.php" style="width: 100%; height: 600px" frameborder="0" id="iframe"></iframe>';
+		$type = urldecode($this->input->get('type', $type));
+		$name = urldecode($this->input->get('name', $name));
+		$service = urldecode($this->input->get('service', $service));
+
+		$target_link = 'avail_setup.php';
+		if (!empty($type)) {
+			$target_link = 'avail_result.php?';
+			switch ($type) {
+				case 'host': case 'service':
+					$target_link .= $type.'='.$type;
+					if (!empty($service)) {
+						$target_link .= '&service='.$service;
+					}
+					break;
+				case 'hostgroup': case 'servicegroup':
+					$target_link .= $type.'[]='.$name.'&report_type='.$type.'s';
+
+			}
+			$target_link .= '&show_log_entries';
+		}
+		$this->template->content = '<iframe src="/monitor/op5/reports/gui/'.$target_link.'" style="width: 100%; height: 600px" frameborder="0" id="iframe"></iframe>';
 		$this->template->title = $this->translate->_('Reporting » Availability');
 		$this->template->js_header = $this->add_view('js_header');
 		$this->xtra_js = array($this->add_path('/js/iframe-adjust.js'));
