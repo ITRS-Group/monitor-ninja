aaeaf9c7870d445986373cfaf9373a46d8aaf22c
My Appelgren <my@op5.com>
2009-06-02 08:35:56 +0200

    made the text colour change depending on percentage up, for netw health
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -5,15 +5,15 @@
 		<table summary="Network healt" class="healt">
 				<tr>
 					<td style="text-align: center">
-						<div style=" font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px;"><?php echo $host_value ?> %</div>
-						<div style=" font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px"><?php echo $host_label ?></div>
+						<div style="<?php echo ($host_value > 33) ? 'color: #ffffff;' : ''?>font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px;"><?php echo $host_value ?> %</div>
+						<div style="<?php echo ($host_value > 12) ? 'color: #ffffff;' : ''?>font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px"><?php echo $host_label ?></div>
 						<div class="border">
 							<?php echo html::image($host_image, array('style' => 'height:'.$host_value.'px; width: 100%; padding-top: '.round(100-$host_value).'px', 'alt' => $host_label)) ?>
 						</div>
 					</td>
 					<td style="text-align: center">
-						<div style=" font-size: 22px; position: absolute;padding-top: 62px; padding-left: 10px"><?php echo $service_value ?> %</div>
-						<div style=" font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px;"><?php echo $service_label ?></div>
+						<div style="<?php echo ($service_value > 33) ? 'color: #ffffff;' : ''?>font-size: 22px; position: absolute;padding-top: 62px; padding-left: 10px"><?php echo $service_value ?> %</div>
+						<div style="<?php echo ($service_value > 12) ? 'color: #ffffff;' : ''?>font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px;"><?php echo $service_label ?></div>
 						<div class="border">
 							<?php echo html::image($service_image, array('style' => 'height:'.$service_value.'px; width: 100%; padding-top: '.round(100-$service_value).'px', 'alt' => $service_label)) ?>
 						</div>
