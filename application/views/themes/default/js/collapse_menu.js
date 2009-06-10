4518dc9d37950386bcb01e0e2db31f96ec11f3c2
Per Åsberg <per.asberg@op5.se>
2009-06-10 16:27:43 +0200

    collapse_menu.js: Use JQuery instead of legacy javascript code
    By using JQuery we get javascript that has a greater success potential
    when it comes to browser compatibility etc.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/js/collapse_menu.js
+++ application/views/themes/default/js/collapse_menu.js
@@ -1,45 +1,22 @@
+var default_menu_state = 'show';
 function collapse_menu(action) {
-	if (action == '') {
-		kakor = document.cookie;
-		kakor_delar = kakor.split('; ');
-		for (j = 0; j < kakor_delar.length; j++) {
-			tva_delar = kakor_delar[j].split('=');
-			if (tva_delar[0] == 'ninja_menu') {
-				action = tva_delar[1];
-			}
-		}
-	}
 	if (action == 'hide') {
-		document.getElementById('menu').style.width = '35px';
-		document.getElementById('close-menu').style.display = 'none';
-		document.getElementById('show-menu').style.display = 'block';
-		var menu = document.getElementById('menu');
-		for (var i = 1; i < menu.getElementsByTagName('a').length; i = i+2) {
-			menu.getElementsByTagName('a')[i].setAttribute('style','display: none');
-		}
-		for (var j = 0; j < menu.getElementsByTagName('cite').length; j++) {
-			menu.getElementsByTagName('cite')[j].setAttribute('style','display: none');
-		}
-		document.getElementById('content').style.marginLeft = '35px';
-		var date = new Date();
-		date.setTime(date.getTime()+(365*24*60*60*1000));
-		document.cookie = 'ninja_menu=hide;path=/;expires='+date.toGMTString();
+		$('#menu').css('width', '35px');
+		$('#close-menu').hide();
+		$('#show-menu').show();
+		$('.ninja_menu_links').hide();
+		$('li.header cite').hide();
+		$('#content').css('margin-left', '35px');
+		save_ninja_menu_state('hide');
 	}
 	if (action == 'show') {
-		document.getElementById('menu').style.width = '166px';
-		document.getElementById('close-menu').style.display = 'block';
-		document.getElementById('show-menu').style.display = 'none';
-		var menu = document.getElementById('menu');
-		for (var i = 1; i < menu.getElementsByTagName('a').length; i = i+2) {
-			menu.getElementsByTagName('a')[i].setAttribute('style','display: inline');
-		}
-		for (var j = 0; j < menu.getElementsByTagName('cite').length; j++) {
-			menu.getElementsByTagName('cite')[j].setAttribute('style','display: inline');
-		}
-		document.getElementById('content').style.marginLeft = '166px';
-		var date = new Date();
-		date.setTime(date.getTime()+(365*24*60*60*1000));
-		document.cookie = 'ninja_menu=show;path=/;expires='+date.toGMTString();
+		$('#menu').css('width', '166px');
+		$('#close-menu').show();
+		$('#show-menu').hide();
+		$('.ninja_menu_links').show();
+		$('li.header cite').show();
+		$('#content').css('margin-left', '166px');
+		save_ninja_menu_state('show');
 	}
 
 }
