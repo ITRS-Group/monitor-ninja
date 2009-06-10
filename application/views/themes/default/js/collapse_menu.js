cc07f646fb9e1c68120aa9a480b5e42289575934
Per Åsberg <per.asberg@op5.se>
2009-06-10 16:29:06 +0200

    collapse_menu.js: Save menu state to db
    This patch removes the cookie functionality for the menu and instead
    uses ajax calls to save and fetch menu state to/from database.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/js/collapse_menu.js
+++ application/views/themes/default/js/collapse_menu.js
@@ -33,6 +33,38 @@ function settings(action) {
 	}
 }
 
+function get_ninja_menu_state()
+{
+	var url = _site_domain + _index_page + "/ajax/get_setting/";
+	var page_name = '/';
+	var data = {page: escape(page_name), type: 'ninja_menu_state'};
+
+	$.ajax({
+		url: url,
+		dataType:'json',
+		type: 'POST',
+		data: data,
+		success: function(data) {
+			if (data.ninja_menu_state != false) {
+				collapse_menu(data.ninja_menu_state);
+				ret_val = data.ninja_menu_state;
+			}
+			return ret_val;
+		},
+		error: function(obj, msg){alert(msg)}
+	});
+}
+
+function save_ninja_menu_state(state)
+{
+	var url = _site_domain + _index_page + "/ajax/save_page_setting/";
+	var page_name = '/';
+	var data = {page: escape(page_name), type: 'ninja_menu_state', setting: state};
+	$.post(url, data);
+}
+
 window.onload = function() {
-	collapse_menu('');
+	var state = get_ninja_menu_state();
+	action = state != false ? state : default_menu_state;
+	collapse_menu(action);
 }
