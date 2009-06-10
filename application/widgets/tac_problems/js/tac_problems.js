1c542fc2ac277d125f77e4d0e15c694679be38b3
Per Åsberg <per.asberg@op5.se>
2009-06-10 13:26:34 +0200

    widgets/tac_problems.js: Updated javascript to be able to save settings
    Added some javascript logic to get the slider and to save settings
    to the database. It uses SetInterval() + clearInterval() to control
    the update interval and setTimeout() + clearTimeout()  to control
    the settings update to database.
    To prevent namespace collisons, all variables and methods are named
    tac_problems_<something>. This is the desired behaviour for all widgets.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/tac_problems/js/tac_problems.js
+++ application/widgets/tac_problems/js/tac_problems.js
@@ -1,9 +1,56 @@
+var tac_problems_interval = 0;
+var tac_problems_data = false;
+var tac_problems_save_interval = 0;
+var tac_problems_current_interval = $('input[name=tac_problems_refresh]').val();
 $(document).ready(function() {
-	var interval_sec = 60; // interval in seconds
-	var interval = (interval_sec * 1000);
-	setInterval("tac_problems_update()", interval);
+	set_tac_problems_interval(true);
+
+	$("#tac_problems_slider").slider({
+		value: $('input[name=tac_problems_refresh]').val(),
+		min: 0,
+		max: 500,
+		step: 10,
+		slide: function(event, ui) {
+			$("#tac_problems_refresh").val(ui.value);
+			tac_problems_current_interval = ui.value;
+			//set_tac_problems_interval(ui.value);
+			tac_problems_control_save();
+		}
+	});
+	$("#tac_problems_refresh").val($("#tac_problems_slider").slider("value"));
+
 });
 
+function set_tac_problems_interval(is_init)
+{
+	if (tac_problems_interval) {
+		clearInterval(tac_problems_interval);
+	}
+	if (tac_problems_current_interval>0) {
+		var interval = (tac_problems_current_interval * 1000);
+		tac_problems_interval = setInterval("tac_problems_update()", interval);
+	}
+
+	if (!is_init) {
+		// update widget settings
+		tac_problems_data = {page: $('input[name=tac_problems_page]').val(), tac_problems_refresh: tac_problems_current_interval, widget: 'tac_problems'};
+		save_tac_problems_setting(tac_problems_data);
+	}
+}
+
+/*
+Sätt en timeout på 5 sekunder för att spara t databasen
+kanske oxå för att verkställa själva checken
+
+*/
+
+function tac_problems_control_save()
+{
+	if (tac_problems_save_interval) {
+		clearTimeout(tac_problems_save_interval);
+	}
+	tac_problems_save_interval = setTimeout("set_tac_problems_interval()", 5000);
+}
 
 function tac_problems_update()
 {
@@ -12,8 +59,14 @@ function tac_problems_update()
 		url: _site_domain + _index_page + "/ajax/widget/tac_problems/index/",
 		dataType:'json',
 		success: function(data) {
-			$("#" + widget_id).html(data);
+			$("#" + widget_id + ' #widget-content').html(data);
 		},
 		error: function(obj, msg){alert(msg)}
 	});
+}
+
+function save_tac_problems_setting(data)
+{
+	var url = _site_domain + _index_page + "/ajax/save_widget_setting/";
+	$.post(url, data);
 }
