b156888cd9f52ea3277636515f1e404787d0fff2
Per Åsberg <per.asberg@op5.se>
2009-06-11 15:46:45 +0200

    helpers/config: Introducing the config helper class
    This small helper class makes it transparent whether the configuration
    item is stored in the database (for all users or for current) or in a
    config file. It first checks for user setting or global setting in the
    database and then the config file if nothing is found.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/helpers/config.php
@@ -0,0 +1,35 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Help class for handling config items transparently,
+ * i.e independent of storage location (file or database)
+ *
+ * @package NINJA
+ * @author op5 AB
+ * @license GPL
+ */
+class config_Core
+{
+	/**
+	*	Fetch config item from db or config file
+	*	If $page is set it will fetch for a page-specific
+	* 	setting for current user
+	*/
+	public static function get($config_str=false, $page='')
+	{
+		$config_str = trim($config_str);
+		if (empty($config_str) || !is_string($config_str)) {
+			return false;
+		}
+		# first check for database value
+		$cfg = Ninja_setting_Model::fetch_page_setting($config_str, $page);
+		if ($cfg!==false) {
+			$setting =  $cfg->setting;
+		}
+
+		if (!isset($setting)) {
+			$setting = Kohana::config($config_str);
+		}
+		# secondly, if nothing was found - try the config file
+		return $setting;
+	}
+}
