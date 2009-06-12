028b7ef633de16c1db155a475fc8ad3b5921e199
My Appelgren <my@op5.com>
2009-06-12 10:00:15 +0200

    edited styles for the edit-box for the tac problems widget
    still not that pretty, but it looks a whole lot better at least.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/tac_problems/view.php
+++ application/widgets/tac_problems/view.php
@@ -2,10 +2,10 @@
 <?php if (!$ajax_call) { ?>
 <div class="widget editable movable collapsable removable closeconfirm w66 left" id="widget-tac_problems">
 	<div class="widget-header"><span class="tac_problems_editable" id="tac_problems_title"><?php echo $title ?></span></div>
-	<div class="widget-editbox">
+	<div class="widget-editbox" style="background-color: #ffffff; padding: 15px; float: right; margin-top: -1px; border: 1px solid #e9e9e0; right: 0px; width: 200px">
 		<?php echo form::open('ajax/save_widget_setting', array('id' => 'tac_problems_form', 'onsubmit' => 'return false;')); ?>
 		<label for="tac_problems_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
-		<input style="border:0" type="text" name="tac_problems_refresh" id="tac_problems_refresh" value="<?php echo $tac_problems_refresh ?>" />
+		<input style="border:0px solid red; display: inline; padding: 0px; margin-bottom: 7px" size="3" type="text" name="tac_problems_refresh" id="tac_problems_refresh" value="<?php echo $tac_problems_refresh ?>" />
 		<?php echo form::hidden('tac_problems_page', urlencode(Router::$controller.'/'.Router::$method)) ?>
 		<div id="tac_problems_slider"></div>
 		<?php echo form::close() ?>
