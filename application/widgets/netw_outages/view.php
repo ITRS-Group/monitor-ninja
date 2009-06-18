28fa301fe78326b74356e409426c5addb721b7eb
Per Åsberg <per.asberg@op5.se>
2009-06-18 09:54:33 +0200

    widgets/netw_outages/view: Add editing and ajax update to widget
    Added the necessary code to enable editing to the widget view.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/netw_outages/view.php
+++ application/widgets/netw_outages/view.php
@@ -1,8 +1,15 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class="widget movable collapsable removable closeconfirm left w98" id="widget-netw_outages">
-	<div class="widget-header">
-		<strong><?php echo $title ?></strong>
+<?php if (!$ajax_call) { ?>
+<div class="widget editable movable collapsable removable closeconfirm left w98" id="widget-<?php echo $widget_id ?>">
+	<div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
+	<div class="widget-editbox" style="background-color: #ffffff; padding: 15px; float: right; margin-top: -1px; border: 1px solid #e9e9e0; right: 0px; width: 200px">
+		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
+		<label for="<?php echo $widget_id ?>_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
+		<input style="border:0px solid red; display: inline; padding: 0px; margin-bottom: 7px" size="3" type="text" name="<?php echo $widget_id ?>_refresh" id="<?php echo $widget_id ?>_refresh" value="<?php echo $refresh_rate ?>" />
+		<div id="<?php echo $widget_id ?>_slider" style="z-index:1000"></div>
+		<?php echo form::close() ?>
 	</div>
+<?php } ?>
 	<div class="widget-content">
 	<?php
 		if (!$user_has_access) {
@@ -17,6 +24,7 @@
 		<?php
 		} // end if user_has_access
 ?>
+<?php if (!$ajax_call) { ?>
 	</div>
 </div>
-
+<?php } ?>
