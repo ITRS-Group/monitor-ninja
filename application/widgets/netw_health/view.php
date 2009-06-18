f4fbf329b24daa153f1e13957963a4d53bca1abf
Per Åsberg <per.asberg@op5.se>
2009-06-18 09:55:57 +0200

    widgets/netw_health/view: Added ajax update and editing for netw_health
    Added the necessary code to make netw_health widget editable and
    refreshable by AJAX.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -1,7 +1,16 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<div class="widget movable collapsable removable closeconfirm w32 left " id="widget-netw_health">
-	<div class="widget-header"><span><?php echo $title ?></span></div>
+<?php if (!$ajax_call) { ?>
+<div class="widget editable movable collapsable removable closeconfirm w32 left " id="widget-<?php echo $widget_id ?>">
+	<div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
+	<div class="widget-editbox" style="background-color: #ffffff; padding: 15px; float: right; margin-top: -1px; border: 1px solid #e9e9e0; right: 0px; width: 200px">
+		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
+		<label for="<?php echo $widget_id ?>_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
+		<input style="border:0px solid red; display: inline; padding: 0px; margin-bottom: 7px" size="3" type="text" name="<?php echo $widget_id ?>_refresh" id="<?php echo $widget_id ?>_refresh" value="<?php echo $refresh_rate ?>" />
+		<div id="<?php echo $widget_id ?>_slider" style="z-index:1000"></div>
+		<?php echo form::close() ?>
+	</div>
 	<div class="widget-content">
+	<?php } ?>
 		<table summary="Network healt" class="healt">
 				<tr>
 					<td style="text-align: center">
@@ -20,6 +29,8 @@
 					</td>
 				</tr>
 			</table>
+<?php if (!$ajax_call) { ?>
 	</div>
 </div>
 <div style="clear:both"></div>
+<?php } ?>
