b60c77824148ca79562b6c650e1e3e5551c43b01
My Appelgren <my@op5.com>
2009-06-22 13:29:35 +0200

    altered the css_header.php to handle gziped css
    gziped css files don't have a regular css ending, therefore i added
    an if-statement prevent the helper html::stylesheet to add an extra
    .css to the end of the filename.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/css_header.php
+++ application/views/themes/default/css_header.php
@@ -1,7 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-if (!empty($css))
-	foreach ($css as $css_file)
-		echo html::stylesheet($css_file);
+if (!empty($css)) {
+	foreach ($css as $css_file) {
+		if (stripos($css_file,'.php') == true)
+			echo '<link type="text/css" rel="stylesheet" href="'.url::site($css_file).'" />'."\n";
+		else
+			echo html::stylesheet($css_file);
+	}
+}
 
 ?>
