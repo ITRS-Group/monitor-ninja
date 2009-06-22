a8706061da07b48b4b5dc63dacbad09bc743032c
My Appelgren <my@op5.com>
2009-06-22 13:00:49 +0200

    moved the notes column next to the host column
    also removed the dotted bottom border on the icons.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/outages/network_outages.php
+++ application/views/themes/default/outages/network_outages.php
@@ -2,16 +2,17 @@
 
 <div class="widget left w98" id="network_outages">
 	<table id="network_outages_table">
-		<!--<caption><?php echo $title ?></caption>-->
+		<caption><?php echo $title ?></caption>
 		<thead>
 			<tr>
 				<th class="header">&nbsp;</th>
 				<th class="header"><?php echo $label_host ?></th>
+				<th class="header" style="width: 57px"><?php echo $label_notes ?></th>
 				<th class="header"><?php echo $label_severity ?></th>
 				<th class="header"><?php echo $label_duration ?></th>
 				<th class="header"><?php echo $label_hosts_affected ?></th>
 				<th class="header"><?php echo $label_services_affected ?></th>
-				<th class="header" colspan="6"><?php echo $label_actions ?></th>
+				<th class="header" colspan="5"><?php echo $label_actions ?></th>
 			</tr>
 		</thead>
 		<tbody>
@@ -25,30 +26,28 @@
 				<td class="icon">
 					<?php echo html::image('/application/views/themes/default/icons/16x16/shield-'.strtolower(Current_status_Model::status_text($details['current_state'])).'.png',array('alt' => 'hej', 'title' => 'hej')) ?>
 				</td>
-				<td>
-					<?php echo html::anchor('extinfo/details/host/'.$host, $host) ?>
+				<td><?php echo html::anchor('extinfo/details/host/'.$host, $host) ?></td>
+				<td class="icon">
+					<?php echo html::anchor('extinfo/details/host/'.$host, html::image('/application/views/themes/default/icons/16x16/add-comment.png',array('alt' => $this->translate->_('View comments for this host'), 'title' => $this->translate->_('View comments for this host'))),array('style' => 'border: 0px')); ?>
 				</td>
 				<td><?php echo $details['severity'] ?></td>
 				<td><?php echo $details['duration'] ?></td>
 				<td><?php echo $details['affected_hosts'] ?></td>
 				<td><?php echo $details['affected_services'] ?></td>
 				<td class="icon">
-					<?php echo html::anchor('extinfo/details/host/'.$host, html::image('/application/views/themes/default/icons/16x16/add-comment.png',array('alt' => $this->translate->_('View comments for this host'), 'title' => $this->translate->_('View comments for this host')))) ?>
+					<?php echo html::anchor('status/host/'.$host, html::image('/application/views/themes/default/icons/16x16/service-details.gif',array('alt' => $this->translate->_('View status detail for this host'), 'title' => $this->translate->_('View status detail for this host'))),array('style' => 'border: 0px')) ?>
 				</td>
 				<td class="icon">
-					<?php echo html::anchor('status/host/'.$host, html::image('/application/views/themes/default/icons/16x16/service-details.gif',array('alt' => $this->translate->_('View status detail for this host'), 'title' => $this->translate->_('View status detail for this host')))) ?>
+					<?php echo html::anchor('statusmap/'.$host, html::image('/application/views/themes/default/icons/16x16/locate-host-on-map.png',array('alt' => $this->translate->_('View status map for this host and its children'), 'title' => $this->translate->_('View status map for this host and its children'))),array('style' => 'border: 0px')) ?>
 				</td>
 				<td class="icon">
-					<?php echo html::anchor('statusmap/'.$host, html::image('/application/views/themes/default/icons/16x16/locate-host-on-map.png',array('alt' => $this->translate->_('View status map for this host and its children'), 'title' => $this->translate->_('View status map for this host and its children')))) ?>
+					<?php echo html::anchor('trends/host/'.$host, html::image('/application/views/themes/default/icons/16x16/trends.gif',array('alt' => $this->translate->_('View trends for this host'), 'title' => $this->translate->_('View trends for this host'))),array('style' => 'border: 0px')) ?>
 				</td>
 				<td class="icon">
-					<?php echo html::anchor('trends/host/'.$host, html::image('/application/views/themes/default/icons/16x16/trends.gif',array('alt' => $this->translate->_('View trends for this host'), 'title' => $this->translate->_('View trends for this host')))) ?>
+					<?php echo html::anchor('history/host/'.$host, html::image('/application/views/themes/default/icons/16x16/history.gif',array('alt' => $this->translate->_('View alert history for this host'), 'title' => $this->translate->_('View alert history for this host'))),array('style' => 'border: 0px')) ?>
 				</td>
 				<td class="icon">
-					<?php echo html::anchor('history/host/'.$host, html::image('/application/views/themes/default/icons/16x16/history.gif',array('alt' => $this->translate->_('View alert history for this host'), 'title' => $this->translate->_('View alert history for this host')))) ?>
-				</td>
-				<td class="icon">
-					<?php echo html::anchor('notifications/host/'.$host, html::image('/application/views/themes/default/icons/16x16/notify.png',array('alt' => $this->translate->_('View notifications for this host'), 'title' => $this->translate->_('View notifications for this host')))) ?>
+					<?php echo html::anchor('notifications/host/'.$host, html::image('/application/views/themes/default/icons/16x16/notify.png',array('alt' => $this->translate->_('View notifications for this host'), 'title' => $this->translate->_('View notifications for this host'))),array('style' => 'border: 0px')) ?>
 				</td>
 			</tr>
 			<?php }	}	?>
