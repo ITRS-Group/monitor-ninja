0cd951584727f8247914a4b711e9e3283fc5b4c0
My Appelgren <my@op5.com>
2009-06-23 10:44:11 +0200

    strip the index.php from the css-path in css_header.php
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/css_header.php
+++ application/views/themes/default/css_header.php
@@ -3,7 +3,7 @@
 if (!empty($css)) {
 	foreach ($css as $css_file) {
 		if (stripos($css_file,'.php') == true)
-			echo '<link type="text/css" rel="stylesheet" href="'.url::site($css_file).'" />'."\n";
+			echo '<link type="text/css" rel="stylesheet" href="'.str_replace('index.php/','',url::site($css_file)).'" />'."\n";
 		else
 			echo html::stylesheet($css_file);
 	}
