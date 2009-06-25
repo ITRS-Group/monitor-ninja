dc7be25e639b2fbebce4b520075f6a9aa82859f5
My Appelgren <my@op5.com>
2009-06-25 15:10:04 +0200

    replaced some inline styling from widgets with classes
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_outages/view.php
+++ application/widgets/netw_outages/view.php
@@ -2,7 +2,7 @@
 <?php if (!$ajax_call) { ?>
 <div class="widget editable movable collapsable removable closeconfirm left w32" id="widget-<?php echo $widget_id ?>">
 	<div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
-	<div class="widget-editbox" style="background-color: #ffffff; padding: 15px; float: right; margin-top: -1px; border: 1px solid #e9e9e0; right: 0px; width: 200px">
+	<div class="widget-editbox">
 		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
 		<label for="<?php echo $widget_id ?>_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
 		<input style="border:0px solid red; display: inline; padding: 0px; margin-bottom: 7px" size="3" type="text" name="<?php echo $widget_id ?>_refresh" id="<?php echo $widget_id ?>_refresh" value="<?php echo $refresh_rate ?>" />
@@ -14,7 +14,7 @@
 		if (!$user_has_access) {
 			echo $no_access_msg;
 	 	} else { ?>
-		<table style="border-spacing: 1px; background-color: #e9e9e0; margin-top: -1px">
+		<table class="w-table">
 			<tr>
 				<td class="dark"><?php echo html::image('/application/views/themes/default/icons/16x16/shield-critical.png', array('alt' => $label)) ?></td>
 				<td><?php echo html::anchor('outages/index/', html::specialchars($total_blocking_outages.' '.$label)); ?></td>
--- application/widgets/tac_scheduled/view.php
+++ application/widgets/tac_scheduled/view.php
@@ -2,7 +2,7 @@
 <?php if (!$ajax_call) { ?>
 <div class="widget editable movable collapsable removable closeconfirm w32 left" id="widget-<?php echo $widget_id ?>">
 <div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
-	<div class="widget-editbox" style="background-color: #ffffff; padding: 15px; float: right; margin-top: -1px; border: 1px solid #e9e9e0; right: 0px; width: 200px">
+	<div class="widget-editbox">
 		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
 		<label for="<?php echo $widget_id ?>_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
 		<input style="border:0px solid red; display: inline; padding: 0px; margin-bottom: 7px" size="3" type="text" name="<?php echo $widget_id ?>_refresh" id="<?php echo $widget_id ?>_refresh" value="<?php echo $refresh_rate ?>" />
@@ -11,7 +11,7 @@
 	</div>
 	<div class="widget-content">
 <?php } ?>
-		<table style="border-spacing: 1px; background-color: #e9e9e0; margin-top: -1px">
+		<table class="w-table">
 			<?php for ($i = 0; $i < count($problem); $i++) { ?>
 				<tr>
 					<td class="dark"><?php echo html::image('/application/views/themes/default/icons/16x16/scheduled.png', array('alt' => $problem[$i]['status'])) ?></td>
