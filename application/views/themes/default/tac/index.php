080163e80020c083b8c42134ac9a8950ff1dcd6d
My Appelgren <my@op5.com>
2009-06-26 10:54:04 +0200

    added more widget places for the tactical overview
    to make the widgets behave more logically
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/tac/index.php
+++ application/views/themes/default/tac/index.php
@@ -10,3 +10,6 @@
 	}
 ?>
 </div>
+<div class="widget-place" id="widget-placeholder1"></div>
+<div class="widget-place" id="widget-placeholder2"></div>
+<div class="widget-place w98" id="widget-placeholder3"></div>
