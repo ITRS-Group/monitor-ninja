4180ac9b21906b66960338d06ad9af4730b5584d
Andreas Ericsson <ae@op5.se>
2009-06-29 16:59:14 +0200

    base template: Comment out the use of zlib
    Using it hides the pretty important errors one can get from Kohana
    while debugging things, so it's better to not have it enabled until
    we cut the first proper release.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/views/themes/default/template.php
+++ application/views/themes/default/template.php
@@ -1,5 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<?php if(extension_loaded('zlib')){ob_start('ob_gzhandler');} header('Content-type: text/html; charset=utf-8');?>
+<?php
+#if (extension_loaded('zlib')) {
+#	ob_start('ob_gzhandler');
+#}
+header('Content-type: text/html; charset=utf-8');
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -165,4 +170,8 @@
 		</div>
 	</body>
 </html>
-<?php if(extension_loaded('zlib')){ob_end_flush();}?>
+<?php 
+#if (extension_loaded('zlib')) {
+#	ob_end_flush();
+#}
+?>
