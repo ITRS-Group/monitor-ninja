32626a71bb9669253b48a4736a81147d2c78b6f2
Per Åsberg <per.asberg@op5.se>
2009-06-30 13:22:15 +0200

    controllers/authenticated: Add CLI access possibilities
    Sometimes it's nifty to be able to access Ninja from commandline,
    unit testing is one case where it will be very useful but there are
    other cases as well.
    
    This access is controlled from config.php as described in previous commit.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/authenticated.php
+++ application/controllers/authenticated.php
@@ -21,12 +21,32 @@ class Authenticated_Controller extends Ninja_Controller {
 		parent::__construct();
 		# make sure user is authenticated
 		$authentic = new Auth;
-		if (!$authentic->logged_in()) {
-			# store requested uri in session for later redirect
-			$this->session->set('requested_uri', url::current());
-			url::redirect(Kohana::config('routes.log_in_form'));
+
+		# Check if user is accessing through PHP CLI
+		if (PHP_SAPI === "cli") {
+			$cli_access = Kohana::config('config.cli_access');
+			if ($cli_access !== false) {
+				if ($cli_access === true) {
+					# username should be passed as argv[1]
+					if (!empty($_SERVER['argc']) && isset($_SERVER['argv'][2])) {
+						Auth::instance()->force_login($_SERVER['argv'][2]);
+					}
+
+				} else {
+					Auth::instance()->force_login($cli_access);
+				}
+			} else {
+				echo "CLI access denied or not configured\n";
+				exit(1);
+			}
 		} else {
-			$this->user = Auth::instance()->get_user();
+			if (!$authentic->logged_in()) {
+				# store requested uri in session for later redirect
+				$this->session->set('requested_uri', url::current());
+				url::redirect(Kohana::config('routes.log_in_form'));
+			} else {
+				$this->user = Auth::instance()->get_user();
+			}
 		}
 	}
 
