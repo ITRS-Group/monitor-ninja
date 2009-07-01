7bbb814f3144231931906db9bb33d07f31f8f031
Per Åsberg <per.asberg@op5.se>
2009-07-01 16:22:37 +0200

    install_scripts/auth_import: Import cgi.cfg > database
    This script now uses PHP cli to access Ninja and import the current
    authorization data from cgi.cfg. This script could very well be run
    incrementally since it will only update the authorization data for each
    user.
    
    Fixes issue #2456
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- install_scripts/auth_import.php
+++ install_scripts/auth_import.php
@@ -4,8 +4,45 @@
  * Import existing authorization settings from cgi.cfg to database
  * for all users.
  */
+$err_str = "\nAn error occurred and this script will terminate. Too bad...\n";
+$retval = false;
+$cli_access = false;
+exec('/usr/bin/php ../index.php default/get_cli_status ', $cli_access, $retval);
 
-system('/usr/bin/php ../index.php cli/insert_user_data monitor ', $result);
-#print_r($result);
+if (!empty($retval)) {
+	echo $err_str;
+	exit(1);
+}
+
+if (empty($cli_access)) {
+	echo "\nCLI access seems to be disabled so it's impossible to import\n";
+	echo "authorization settings from cgi.cfg to Ninja.\n";
+	echo "Try specifying a valid (existing) user under 'cli_access' in config/config.php \n";
+	echo "or try setting the value to true and try running this \nscript ('" . getcwd() . '/' .
+		basename($_SERVER['PHP_SELF']) . "') again.\n";
+	exit(1);
+}
+
+$user = false;
+if ($cli_access[0] == 1) {
+	# cli_access is set to true which means we should try to find a valid user
+	exec('/usr/bin/php ../index.php default/get_a_user ', $user, $retval);
+	if (!empty($retval)) {
+		echo $err_str;
+		exit(1);
+	}
+	$user = $user[0];
+} else {
+	$user = $cli_access[0];
+}
+
+exec('/usr/bin/php ../index.php cli/insert_user_data '.$user.' ', $result, $retval);
+
+if (!empty($retval)) {
+	echo $err_str;
+	exit(1);
+}
+
+exit(0);
 
 ?>
