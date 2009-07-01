81a1098988850e652a1a7fe7969d2a94292f396c
My Appelgren <my@op5.com>
2009-07-01 10:21:41 +0200

    styled the filter table for the status pages
    might change the look for it. don't know it this is how i want
    it to look in the end.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/status/filters.php
+++ application/views/themes/default/status/filters.php
@@ -1,21 +1,21 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <table id="filter_table">
 	<tr>
-		<td colspan="2"><?php echo $header_title ?></td>
+		<th style="border: 1px solid #d0d0d0" colspan="2"><?php echo $header_title ?></th>
 	</tr>
-	<tr>
+	<tr class="even">
 		<td><?php echo $lable_host_status_types ?></td>
 		<td><?php echo $host_status_type_val ?></td>
 	</tr>
-	<tr>
+	<tr class="odd">
 		<td><?php echo $lable_host_properties ?></td>
 		<td><?php echo $hostprop_val ?></td>
 	</tr>
-	<tr>
+	<tr class="even">
 		<td><?php echo $lable_service_status_types ?></td>
 		<td><?php echo $service_status_type_val ?></td>
 	</tr>
-	<tr>
+	<tr class="odd">
 		<td><?php echo $lable_service_properties ?></td>
 		<td><?php echo $serviceprop_val ?></td>
 	</tr>
