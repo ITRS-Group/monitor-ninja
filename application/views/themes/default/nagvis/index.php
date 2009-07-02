440f7da2b544e1c08db74a90462fc6e88de96b43
Andriy Skuratov <askuratov@op5.com>
2009-07-02 15:47:35 +0300

    Added edit link to ninja's list of maps

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -1,17 +1,29 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div style="margin-left: 20px;">
+	<iframe name="nagvis" src="<?php echo Kohana::config('config.nagvis_path'); ?>" width="900" height="700" frameborder="no">
+		Could not load NagVis!
+	</iframe>
+	<!--
+	<?php
+		echo "NAGVIS";
+		var_dump($_SESSION);
+	?>
+	-->
 	<div style="float: right; width: 200px;">
 		<ul>
 			<li><a href="<?php echo Kohana::config('config.nagvis_path'); ?>nagvis/index.php" target="nagvis">Index</a></li>
 			<?php
 			foreach ($maps as $map)
-				echo '<li><a href="'.Kohana::config('config.nagvis_path').'nagvis/index.php?map='.$map.'" target="nagvis">'.$map.'</a></li>';
+			{
+				echo '<li>';
+				echo '<a href="'.Kohana::config('config.nagvis_path').'nagvis/index.php?map='.$map.'" target="nagvis">'.$map.'</a>';
+				echo '&nbsp;';
+				echo '(<a href="'.Kohana::config('config.nagvis_path').'nagvis/wui/index.php?map='.$map.'" target="nagvis">edit</a>)';
+				echo '</li>';
+			}
 			?>
 			<li><a href="<?php echo Kohana::config('config.nagvis_path'); ?>nagvis/index.php?automap=1" target="nagvis">Automap</a></li>
 		</ul>
 	</div>
-	<iframe name="nagvis" src="<?php echo Kohana::config('config.nagvis_path'); ?>" width="900" height="700" frameborder="no">
-		Could not load NagVis!
-	</iframe>
 </div>
