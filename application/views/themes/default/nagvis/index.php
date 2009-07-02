4a0b611a315e2cce4dd4f995935cb85e5ffbb36e
Andriy Skuratov <askuratov@op5.com>
2009-07-02 17:53:27 +0300

    Layout reaaranged

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -1,16 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div style="margin-left: 20px;">
-	<iframe name="nagvis" src="<?php echo Kohana::config('config.nagvis_path'); ?>" width="900" height="700" frameborder="no">
+	<iframe name="nagvis" src="<?php echo Kohana::config('config.nagvis_path'); ?>" width="100%" height="700" frameborder="no">
 		Could not load NagVis!
 	</iframe>
-	<!--
-	<?php
-		echo "NAGVIS";
-		var_dump($_SESSION);
-	?>
-	-->
-	<div style="float: right; width: 200px;">
+	<div>
 		<ul>
 			<li><a href="<?php echo Kohana::config('config.nagvis_path'); ?>nagvis/index.php" target="nagvis">Index</a></li>
 			<?php
