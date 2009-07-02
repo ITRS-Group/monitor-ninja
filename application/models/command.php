c7cd90397363b662ec3dcb37766585fe51452dd7
Andreas Ericsson <ae@op5.se>
2009-07-02 10:53:38 +0200

    models/command: Add a "dryrun" mode for testing purposes
    When in dryrun mode, we never access $this->db. This is more or
    less a pretty quick hack to avoid having to create a database
    driver mockup in order to test the command model.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/models/command.php
+++ application/models/command.php
@@ -6,6 +6,7 @@
 class Command_Model extends Model
 {
 	private $auth = false;
+	protected $dryrun = false;
 
 	public function __construct()
 	{
@@ -40,6 +41,9 @@ class Command_Model extends Model
 			return $ret_ary;
 		}
 
+		if ($this->dryrun)
+			return array(1 => 'random object');
+
 		$obj_type = substr($param_name, 0, -5);
 		$query = "SELECT $param_name FROM $obj_type";
 		return $this->db->query($query);
@@ -47,6 +51,9 @@ class Command_Model extends Model
 
 	protected function get_comment_ids($command_name = 'DEL_HOST_COMMENT')
 	{
+		if ($this->dryrun)
+			return array(1);
+
 		$type = 'host';
 		if ($command_name != 'DEL_HOST_COMMENT') {
 			$type = 'service';
@@ -88,8 +95,10 @@ class Command_Model extends Model
 	 * @param $defaults Default values for command parameters
 	 * @return Indexed array
 	 */
-	public function get_command_info($cmd, $defaults = false)
+	public function get_command_info($cmd, $defaults = false, $dryrun = false)
 	{
+		$this->dryrun = $dryrun;
+
 		$info = nagioscmd::cmd_info($cmd);
 		# we need the template to get the information we need
 		if (empty($info) || !isset($info['template'])) {
