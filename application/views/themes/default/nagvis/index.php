7d0a3275eed7fa8acc2aa5f81d4a5f98b4d8e851
Andriy Skuratov <askuratov@op5.com>
2009-07-03 14:06:22 +0300

    Fixed the nagvis links to avoid old monitor basic auth

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -1,23 +1,23 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div style="margin-left: 20px;">
-	<iframe name="nagvis" src="<?php echo Kohana::config('config.nagvis_path'); ?>" width="100%" height="700" frameborder="no">
-		Could not load NagVis!
+	<iframe name="nagvis" src="/nagvis" width="100%" height="700" frameborder="no">
+		Error : Can not load NagVis.
 	</iframe>
 	<div>
 		<ul>
-			<li><a href="<?php echo Kohana::config('config.nagvis_path'); ?>nagvis/index.php" target="nagvis">Index</a></li>
+			<li><a href="/nagvis/nagvis/index.php" target="nagvis">Index</a></li>
 			<?php
 			foreach ($maps as $map)
 			{
 				echo '<li>';
-				echo '<a href="'.Kohana::config('config.nagvis_path').'nagvis/index.php?map='.$map.'" target="nagvis">'.$map.'</a>';
+				echo '<a href="/nagvis/nagvis/index.php?map='.$map.'" target="nagvis">'.$map.'</a>';
 				echo '&nbsp;';
-				echo '(<a href="'.Kohana::config('config.nagvis_path').'wui/index.php?map='.$map.'" target="nagvis">edit</a>)';
+				echo '(<a href="/nagvis/wui/index.php?map='.$map.'" target="nagvis">edit</a>)';
 				echo '</li>';
 			}
 			?>
-			<li><a href="<?php echo Kohana::config('config.nagvis_path'); ?>nagvis/index.php?automap=1" target="nagvis">Automap</a></li>
+			<li><a href="/nagvis/nagvis/index.php?automap=1" target="nagvis">Automap</a></li>
 		</ul>
 	</div>
 </div>
