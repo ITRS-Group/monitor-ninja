cba46475f1a070c9f48169b7e07854c2e7cb8e93
Roman Kyrylych <rkyrylych@op5.com>
2009-07-07 19:28:45 +0300

    NagVis page: removed NagVis frame from index page
    Signed-off-by: Roman Kyrylych <rkyrylych@op5.com>

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -1,12 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<div style="margin-left: 20px;">
-	<iframe name="nagvis" src="/nagvis" width="100%" height="700" frameborder="no">
-		Error : Can not load NagVis.
-	</iframe>
+<div class="left">
 	<div>
 		<ul>
-			<li><a href="/nagvis/nagvis/index.php" target="nagvis">Index</a></li>
 			<?php
 			foreach ($maps as $map)
 			{
