3b4110ae5693c01148fa1a0761ae2ea1b5e8e119
Roman Kyrylych <rkyrylych@op5.com>
2009-07-08 12:23:57 +0300

    NagVis page: added forgoten files
    Signed-off-by: Roman Kyrylych <rkyrylych@op5.com>

---
+++ application/views/themes/default/nagvis/edit.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<div style="margin-left: 20px;">
+	<iframe name="nagvis" src="/nagvis/wui/?map=<?php echo $map; ?>" width="100%" height="700" frameborder="no">
+		Error : Can not load NagVis.
+	</iframe>
+</div>
