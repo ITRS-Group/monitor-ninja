742bc1123cb54dac09205177451db108363bca5e
Roman Kyrylych <rkyrylych@op5.com>
2009-07-09 17:33:33 +0300

    NagVis: set iframe height to 100%
    Signed-off-by: Roman Kyrylych <rkyrylych@op5.com>

--- application/views/themes/default/nagvis/automap.php
+++ application/views/themes/default/nagvis/automap.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div style="margin-left: 20px;">
-	<iframe name="nagvis" src="/nagvis/nagvis/index.php?automap=1" width="100%" height="700" frameborder="no">
+	<iframe name="nagvis" src="/nagvis/nagvis/index.php?automap=1" width="100%" height="100%" frameborder="no">
 		Error : Can not load NagVis.
 	</iframe>
 </div>
