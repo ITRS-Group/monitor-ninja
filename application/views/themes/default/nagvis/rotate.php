b72383649a3b8142f18a7e2db2f378a196576635
Roman Kyrylych <rkyrylych@op5.com>
2009-07-10 11:50:38 +0300

    NagVis: make iframes fixed height again due to Firefox (bug #2581
    Signed-off-by: Roman Kyrylych <rkyrylych@op5.com>

--- application/views/themes/default/nagvis/rotate.php
+++ application/views/themes/default/nagvis/rotate.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div style="margin-left: 20px;">
-	<iframe name="nagvis" src="/nagvis/nagvis/?rotation=<?php echo $pool; ?>&map=<?php echo $first_map; ?>" width="100%" height="100%" frameborder="no">
+	<iframe name="nagvis" src="/nagvis/nagvis/?rotation=<?php echo $pool; ?>&map=<?php echo $first_map; ?>" width="100%" height="660" frameborder="no">
 		Error : Can not load NagVis.
 	</iframe>
 </div>
