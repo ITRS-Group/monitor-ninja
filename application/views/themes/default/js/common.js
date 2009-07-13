0803a940f46bf5b51f2cc7d29ed2da18ef6b62b0
Per Åsberg <per.asberg@op5.se>
2009-07-13 11:14:53 +0200

    Merge refresh helper with common.js
    It is usually better to move code to included javascript files instead of
    printing it directly in the page. Since the refresh functionality is rather
    common to say the least, the code is moved to common.js
    
    Some code was left in the helper since we need to print some values from
    PHP.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/js/common.js
+++ application/views/themes/default/js/common.js
@@ -1,3 +1,9 @@
+var sURL = unescape(window.location.pathname + location.search);
+var _interval = 0;
+var _save_page_interval = 0;
+var current_interval = 0;
+var edit_visible = 0;
+
 $(document).ready(function() {
 	/**
 	*	Show the checkbox to show/hide "page header" if
@@ -34,4 +40,80 @@ $(document).ready(function() {
 		}
 		sURL = new_url.toString();
 	});
+
+	// refresh helper code
+	var old_refresh = 0;
+	$("#ninja_refresh_control").bind('change', function() {
+		if ($("#ninja_refresh_control").attr('checked')) {
+			// save previous refresh rate
+			// to be able to restore it later
+			old_refresh = current_interval;
+			$('#ninja_refresh_lable').css('font-weight', 'bold');
+			ninja_refresh(0);
+		} else {
+			// restore previous refresh rate
+			ninja_refresh(old_refresh);
+			$('#ninja_refresh_lable').css('font-weight', '');
+		}
+	});
+	if ($('#ninja_refresh_edit').text()!='') {
+		create_slider('ninja_page_refresh');
+	}
+	$('#ninja_refresh_edit').bind('click', function() {
+		if (!edit_visible) {
+			$('#ninja_page_refresh_slider').show();
+			edit_visible = 1;
+		} else {
+			$('#ninja_page_refresh_slider').hide();
+			edit_visible = 0;
+		}
+	});
+	// -- end refresh helper code
 });
+
+function create_slider(the_id)
+{
+	$("#" + the_id + "_slider").slider({
+		value: current_interval,
+		min: 0,
+		max: 500,
+		step: 10,
+		slide: function(event, ui) {
+			$("#" + the_id + "_value").val(ui.value);
+			current_interval = ui.value;
+			control_save_refreshInterval();
+			ninja_refresh(ui.value);
+		}
+	});
+	// set slider position according to current_interval
+	$("#" + the_id + "_slider").slider("value", current_interval);
+	$('input[name=' + the_id + '_value]').val(current_interval);
+
+}
+
+function control_save_refreshInterval() {
+	if (_save_page_interval) {
+		clearTimeout(_save_page_interval);
+	}
+	_save_page_interval = setTimeout("save_refreshInterval()", 5000);
+}
+
+function save_refreshInterval()
+{
+	var url = _site_domain + _index_page + "/ajax/save_page_setting/";
+	var data = {page: '*', setting: current_interval, type: _refresh_key};
+	$.post(url, data);
+	$.jGrowl(sprintf(_page_refresh_msg, current_interval), { header: _success_header });
+}
+
+function ninja_refresh(val)
+{
+	if (_interval) {
+		clearInterval(_interval);
+	}
+	var refresh_val = (val == null) ? _refresh : val;
+	current_interval = refresh_val;
+	if (val>0) {
+		_interval = setInterval( "refresh()", refresh_val*1000 );
+	}
+}
