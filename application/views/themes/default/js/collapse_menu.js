b4985abd945b461d49088317166063482adb95a5
My Appelgren <my@op5.com>
2009-07-22 09:29:25 +0200

    altered the collapse_menu function
    increades the expanded width from 166px to 176px
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/js/collapse_menu.js
+++ application/views/themes/default/js/collapse_menu.js
@@ -13,15 +13,15 @@ function collapse_menu(action) {
 		save_ninja_menu_state('hide');
 	}
 	if (action == 'show') {
-		$('#menu').css('width', '166px');
-		$('#menu-scroll').css('width', '166px');
-		$('#menu-slider').css('left', '166px');
+		$('#menu').css('width', '176px');
+		$('#menu-scroll').css('width', '176px');
+		$('#menu-slider').css('left', '176px');
 		$('#close-menu').show();
 		$('#show-menu').hide();
 		$('.ninja_menu_links').show();
 		$('li.header cite').show();
 		$('li.header em').hide();
-		$('#content').css('margin-left', '166px');
+		$('#content').css('margin-left', '176px');
 		save_ninja_menu_state('show');
 	}
 }
