a264975871d8db6e9a10a5c442b9f97f8a007e04
Per Åsberg <per.asberg@op5.se>
2009-07-23 11:15:05 +0200

    unit_test: Validate the existence of all menu icons
    The menu is made up of two sets of icons to represent the default and
    selected state. Some of these icons are missing and this test can make
    it easier to detect which ones.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- modules/unit_test/tests/user_Test.php
+++ modules/unit_test/tests/user_Test.php
@@ -73,4 +73,43 @@ class User_Test extends Unit_Test_Case {
 		$this->assert_true(count($result), 'No data exists for '.$table);
 	}
 
+	public function menu_icons_exists_test()
+	{
+		$menu_path = APPPATH.'views/themes/default/icons/menu/*.png';
+		$menu_path_dark = APPPATH.'views/themes/default/icons/menu-dark/*.png';
+		$menu = glob($menu_path);
+		$menu_dark = glob($menu_path_dark);
+		$missing = false;
+		if ((sizeof($menu) != sizeof($menu_dark))) {
+			$menu_icons = false;
+			foreach ($menu as $icon_path) {
+				$parts = explode('/', $icon_path);
+				$icon_name = false;
+				if (is_array($parts)) {
+					$icon_name = array_pop($parts);
+					$menu_icons[] = $icon_name;
+				}
+			}
+
+			$menu_dark_icons = false;
+			foreach ($menu_dark as $icon_path) {
+				$parts = explode('/', $icon_path);
+				$icon_name = false;
+				if (is_array($parts)) {
+					$icon_name = array_pop($parts);
+					$menu_dark_icons[] = $icon_name;
+				}
+			}
+
+			if (is_array($menu_dark_icons)) {
+				foreach ($menu_icons as $icon) {
+					if (!in_array($icon, $menu_dark_icons)) {
+						$missing[] = $icon;
+					}
+				}
+			}
+		}
+		$this->assert_true((sizeof($menu) == sizeof($menu_dark)), 'Missing: '.implode(', ', $missing));
+	}
+
 }
