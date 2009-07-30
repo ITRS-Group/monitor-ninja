5061984db36f445c6ef2593f176a4e0e52cbd063
Roman Kyrylych <rkyrylych@op5.com>
2009-07-30 16:04:17 +0300

    NagVis: Show map thumbnails on index page
    Signed-off-by: Roman Kyrylych <rkyrylych@op5.com>

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -2,26 +2,52 @@
 
 <iframe id="nagvis" name="nagvis" src="/nagvis/nagvis/" style="display: none;"></iframe>
 
+<style type="text/css">
+	ul.thumbnails li {
+		display: inline;
+		float: left;
+		margin: 5px;
+		width: 200px;
+	}
+
+	ul.thumbnails li a.view {
+		display: block;
+		width: 200px;
+	}
+
+	ul.thumbnails li a.view span {
+		display: block;
+		width: 200px;
+		text-align: center;
+		overflow: hidden;
+	}
+
+	ul.thumbnails li a.edit {
+		position: relative;
+		left: 177px;
+		top: -15px;
+	}
+</style>
+
 <div class="left">
 	<div>
 		<strong>Maps</strong>
-		<ul>
+		<ul class="thumbnails">
 			<?php
 			foreach ($maps as $map)
 			{
 				echo '<li>';
-				echo '<a href="/ninja/index.php/nagvis/view/'.$map.'" onmouseover="show_thumbnail($(this));" onmouseout="hide_thumbnail();">'.$map.'</a>';
-				echo '&nbsp;';
-				echo '(<a href="/ninja/index.php/nagvis/edit/'.$map.'">edit</a>)';
+				echo '<a class="view" href="/ninja/index.php/nagvis/view/'.$map.'"><img src="/nagvis/var/'.$map.'-thumb.png" alt="" /><span>'.$map.'</span></a>';
+				echo '<a class="edit" href="/ninja/index.php/nagvis/edit/'.$map.'">[edit]</a>';
 				echo '</li>';
 			}
 			?>
-			<li><a href="/ninja/index.php/nagvis/automap" onmouseover="show_thumbnail($(this));" onmouseout="hide_thumbnail();">Automap</a></li>
+			<li><a class="view" href="/ninja/index.php/nagvis/automap"><img src="/nagvis/var/__automap-thumb.png" alt="" /><span>Automap</span></a></li>
 		</ul>
 	</div>
 </div>
 
-<div class="left">
+<div class="left" style="clear: left;">
 	<strong>Rotation pools</strong>
 	<div>
 		<ul>
