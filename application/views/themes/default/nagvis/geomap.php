472a2aefe1ff7d34ae7fd5e4e0ca31bb1a1d6ab8
Roman Kyrylych <rkyrylych@n-ix.com.ua>
2009-08-03 19:15:59 +0300

    NagVis: Added the forgotten file
    Signed-off-by: Roman Kyrylych <rkyrylych@n-ix.com.ua>

---
+++ application/views/themes/default/nagvis/geomap.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<style type="text/css">
+	html,
+	body,
+	#content,
+	#content div,
+	iframe {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+	}
+	iframe {
+		display: block;
+		width: 100%;
+	}
+</style>
+
+<div style="margin-left: 20px;">
+	<iframe name="nagvis" src="/nagvis/netmap/shell.html" width="100%" height="100%">
+		Error : Can not load NagVis.
+	</iframe>
+</div>
