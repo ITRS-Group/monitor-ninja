0a450a9dbe7dc7abf0f4b446af987dfb68346a3b
Roman Kyrylych <rkyrylych@n-ix.com.ua>
2009-08-04 14:30:59 +0300

    NagVis: Moved Geomap out of iframe
    Signed-off-by: Roman Kyrylych <rkyrylych@n-ix.com.ua>

--- application/views/themes/default/nagvis/geomap.php
+++ application/views/themes/default/nagvis/geomap.php
@@ -1,23 +1,35 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <style type="text/css">
+	/* this is the iframe 100% height workaround used to fix object height in Firefox */
 	html,
 	body,
 	#content,
 	#content div,
-	iframe {
+	object {
 		margin: 0;
 		padding: 0;
 		height: 100%;
 	}
-	iframe {
+	object {
 		display: block;
 		width: 100%;
 	}
 </style>
 
 <div style="margin-left: 20px;">
-	<iframe name="nagvis" src="/nagvis/netmap/shell.html" width="100%" height="100%">
-		Error : Can not load NagVis.
-	</iframe>
+	<object	type="application/x-shockwave-flash" data="/nagvis/netmap/shell.swf" width="100%" height="100%">
+		<param name="base" value="/nagvis/netmap" />
+		<param name="movie" value="/nagvis/netmap/shell.swf" />
+		<!--
+		this works in most normal browsers and even MSIE,
+		but Firefox (tested in 3.5) wants <embed> - what a shame!
+		-->
+		<embed src="/nagvis/netmap/shell.swf"
+			base="/nagvis/netmap"
+			width="100%" height="100%"
+			type="application/x-shockwave-flash">
+			<p>Adobe Flash Player is not installed</p>
+		</embed>
+	</object>
 </div>
