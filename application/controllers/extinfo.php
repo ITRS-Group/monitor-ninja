c7b49726924411a3554ff38c9dc547dabf8fd535
Per Åsberg <per.asberg@op5.se>
2009-09-01 12:53:24 +0200

    controllers/extinfo: Added "Host Name" label
    Somehow the host_name label wasn't set in controller which this patch
    fixes.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/extinfo.php
+++ application/controllers/extinfo.php
@@ -945,6 +945,7 @@ class Extinfo_Controller extends Authenticated_Controller {
 		$comments->host = $host;
 		$comments->label_title = $type == 'host' ? $t->_('Host Comments') : $t->_('Service Comments');
 		$comments->service = $service;
+		$comments->label_host_name = $t->_('Host Name');
 		$comments->label_entry_time = $t->_('Entry Time');
 		$comments->label_author = $t->_('Author');
 		$comments->label_comment = $t->_('Comment');
