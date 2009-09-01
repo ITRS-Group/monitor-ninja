6a98f8a2d116ff0f957218fc7191676ce8068721
My Appelgren <my@op5.com>
2009-09-01 09:41:04 +0200

    accidentily added an 's' on the page when saving.. this broke the page
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/template.php
+++ application/views/themes/default/template.php
@@ -121,7 +121,7 @@ if (isset($this->template->js_header))
 							<em>'.substr(html::specialchars($header),0,1).'</em>
 						</li>'."\n";
 						foreach ($link as $title => $url):
-							if($url[0] == str_replace('/ninja/index.php/','',$_SERVER['PHP_SELF']).(!empty($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : ''))s
+							if($url[0] == str_replace('/ninja/index.php/','',$_SERVER['PHP_SELF']).(!empty($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : ''))
 								echo '<li class="'.html::specialchars($header).'">'.html::anchor($url[0], html::image($this->add_path('icons/menu-dark/'.$url[1].'.png'),array('title' => html::specialchars($title), 'alt' => html::specialchars($title)))).' '.html::anchor($url[0], html::specialchars($title),array('style' => 'font-weight: bold', 'class' => 'ninja_menu_links')).'</li>'."\n";
 							elseif($url[0] == '')
 								echo '<li class="hr '.html::specialchars($header).'">&nbsp;</li>'."\n";
