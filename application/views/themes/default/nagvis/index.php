c17fea46f4968c5d9c4cd9f8c5e6ddbc297a40d2
My Appelgren <my@op5.com>
2009-09-16 10:33:47 +0200

    fixed the layout for the maps and rotation pools on the nagvis frontpage
    added icons, wiget-like boxes around the each map etc.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/nagvis/index.php
+++ application/views/themes/default/nagvis/index.php
@@ -3,51 +3,71 @@
 <iframe id="nagvis" name="nagvis" src="/nagvis/nagvis/" style="display: none;"></iframe>
 
 <style type="text/css">
+
+	strong {
+		margin: 2px 0px 10px 0px;
+		display: block;
+	}
+	ul li.pool {
+		border: 1px solid #cdcdcd;
+	}
+	ul li.pool a {
+		display: block;
+		width: 206px;
+		background: #cdcdcd url('/ninja/application/views/themes/default/css/default/images/bg.png') repeat-x;
+		padding: 3px 0px 3px 3px;
+		font-weight: bold;
+	}
 	ul.thumbnails li {
 		display: inline;
 		float: left;
-		margin: 5px;
-		width: 200px;
+		margin-right: 5px;
+		margin-bottom: 5px;
+		width: 206px;
+		border: 1px solid #cdcdcd;
 	}
-
 	ul.thumbnails li a.view {
 		display: block;
-		width: 200px;
+		width: 206px;
 	}
-
 	ul.thumbnails li a.view span,
 	ul.thumbnails li a.create {
 		display: block;
-		width: 200px;
-		text-align: center;
+		font-weight: bold;
+		width: 203px;
 		overflow: hidden;
+		background: #cdcdcd url('/ninja/application/views/themes/default/css/default/images/bg.png') repeat-x;
+		border-bottom: 1px solid #cdcdcd;
+		margin:0px;
+		padding: 3px 0px 3px 3px;
 	}
-
+	ul.thumbnails li a.view img,
+	ul.thumbnails li a.view2 img,
+	ul.thumbnails li a.create img {
+		padding: 3px;
+		display: block;
+	}
+	ul.thumbnails li a.delete,
 	ul.thumbnails li a.edit {
 		position: relative;
-		left: 165px;
-		top: -15px;
+		float: right;
+		left: -16px;
+		margin-top: 3px;
+		margin-bottom: -15px;
+		z-index: 100;
 	}
-
 	ul.thumbnails li a.delete {
-		position: relative;
-		left: 167px;
-		top: -15px;
+		left: 9px;
 	}
-
-	ul.thumbnails li.create {
-		padding-top: 59px;
-		text-align: center;
-	}
-
 	ul.thumbnails li.create form {
-		margin: 35px 0 4px;
+		margin: 10px 5px;
 	}
-
 	ul.thumbnails li a.create {
 		display: block;
-		width: 200px;
+		width: 203px;
+		border-bottom: 1px solid #cdcdcd;
 	}
+	input { width: 180px }
 </style>
 
 <script type="text/javascript">
@@ -69,22 +89,24 @@
 		<strong>Maps</strong> <a class="view" href="/ninja/index.php/nagvis/configure">[configure]</a>
 		<ul class="thumbnails">
 			<?php
-			foreach ($maps as $map)
-			{
+			foreach ($maps as $map){
 				echo '<li>';
-				echo '<a class="view" href="/ninja/index.php/nagvis/view/'.$map.'"><img src="/nagvis/var/'.$map.'-thumb.png" alt="" /><span>'.$map.'</span></a>';
-				echo '<a class="edit" href="/ninja/index.php/nagvis/edit/'.$map.'">[edit]</a>';
-				echo '<a class="delete" href="/ninja/index.php/nagvis/delete/'.$map.'" onclick="return confirm(\'Are you sure you want to delete map '.$map.'?\')">[x]</a>';
+				echo '<a class="edit" href="/ninja/index.php/nagvis/edit/'.$map.'" style="border: 0px">'.html::image($this->add_path('icons/12x12/box-config.png'),'Edit').'</a>';
+				echo '<a class="delete" href="/ninja/index.php/nagvis/delete/'.$map.'" onclick="return confirm(\'Are you sure you want to delete map '.$map.'?\')" style="border: 0px">'.html::image($this->add_path('icons/12x12/box-close.png'),'Delete').'</a>';
+				echo '<a class="view" href="/ninja/index.php/nagvis/view/'.$map.'" style="border: 0px"><span>'.$map.'</span><img src="/nagvis/var/'.$map.'-thumb.png" alt="" /></a>';
 				echo '</li>';
 			}
 			?>
-			<li><a class="view" href="/ninja/index.php/nagvis/automap"><img src="/nagvis/var/__automap-thumb.png" alt="" /><span>Automap</span></a></li>
-			<li><a class="view" href="/ninja/index.php/nagvis/geomap"><img src="/ninja/geomap-thumb.png" alt="" /><span>Geomap</span></a></li>
-			<li class="create"><?php echo html::image($this->add_path('icons/add.png')); ?>
+			<li><a class="view" href="/ninja/index.php/nagvis/automap" style="border: 0px"><span>Automap</span><img src="/nagvis/var/__automap-thumb.png" alt="" /></a></li>
+			<li><a class="view" href="/ninja/index.php/nagvis/geomap" style="border: 0px"><span>Geomap</span><img src="/ninja/geomap-thumb.png" alt="" /></a></li>
+			<li class="create">
+				<a class="create" href="#" onclick="$('#createmap').submit(); return false" style="border: 0px">Create map</a>
+				<?php //echo html::image($this->add_path('icons/add.png')); ?>
 				<form id="createmap" action="/ninja/index.php/nagvis/create" method="post">
 					<input type="text" id="mapname" name="name" maxlength="25" />
 				</form>
 				<a class="create" href="#" onclick="createmap(); return false">Create</a></li>
+			</li>
 		</ul>
 	</div>
 </div>
@@ -96,8 +118,8 @@
 			<?php
 			foreach ($pools as $pool => $first_map)
 			{
-				echo '<li>';
-				echo '<a href="/ninja/index.php/nagvis/rotate/'.$pool.'/'.$first_map.'">'.$pool.'</a>';
+				echo '<li class="pool">';
+				echo '<a href="/ninja/index.php/nagvis/rotate/'.$pool.'/'.$first_map.'" style="border: 0px">'.$pool.'</a>';
 				echo '</li>';
 			}
 			?>
