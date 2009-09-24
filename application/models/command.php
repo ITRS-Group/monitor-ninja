126a0e15775bed39676231bbb84d51dec6b5d5c6
Per Åsberg <per.asberg@op5.se>
2009-09-24 12:17:40 +0200

    models/command: Convert end_time to readable date string
    The end_time parameter was never converted to a human readable date
    string and this patch amends just that.
    
    Fixes issue #2783
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/models/command.php
+++ application/models/command.php
@@ -223,7 +223,7 @@ class Command_Model extends Model
 			 case 'start_time':
 				$ary = array('type' => 'time', 'default' => date(nagstat::date_format(), time()));
 				if ($param_name === 'end_time')
-					$ary['default'] = time() + 7200;
+					$ary['default'] = date(nagstat::date_format(), time() + 7200);
 				break;
 			}
 
