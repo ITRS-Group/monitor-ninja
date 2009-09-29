bce4dc19010fb10c30b58dccd84238317778cabe
Per Åsberg <per.asberg@op5.se>
2009-09-29 11:19:26 +0200

    Display better error messages to user
    Modified the error part and added translation to the status message
    displayed to a user after submitting a command.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/command/commit.php
+++ application/views/themes/default/command/commit.php
@@ -1,7 +1,12 @@
 <?php
+echo "<br />";
 if ($result === true) {
-	echo '<div class="widget w32 left">Your command was successfully submitted to Nagios.<br /><br />'.
+	echo '<div class="widget w32 left">'.$this->translate->_('Your command was successfully submitted to Nagios.').'<br /><br />'.
 			 '<input type="button" value="Done" onclick="location.href=\''.url::site('/extinfo/details/'.$this->session->get('back_extinfo')).'\'" /></div>'."\n";
 } else {
-	echo '<div class="widget w32 left">There was an error submitting your command to Nagios.</div>'."\n";
+	echo '<div class="widget w32 left">'.$this->translate->_('There was an error submitting your command to Nagios.');
+	if (!empty($error)) {
+		echo '<br /><br />'.$this->translate->_('ERROR').': '.$error;
+	}
+	echo "</div>\n";
 }
