0e09282ccce1309cf51504ed0ae9f58976c6e11d
My Appelgren <my@op5.com>
2009-09-29 15:51:29 +0200

    changed the left margin on the view & edit page for nagvis
    from 20px to 1px.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/nagvis/edit.php
+++ application/views/themes/default/nagvis/edit.php
@@ -16,7 +16,7 @@
 	}
 </style>
 
-<div style="margin-left: 20px;">
+<div style="margin-left: 1px;">
 	<iframe name="nagvis" src="/nagvis/wui/?map=<?php echo $map; ?>" width="100%" height="100%">
 		Error : Can not load NagVis.
 	</iframe>
--- application/views/themes/default/nagvis/view.php
+++ application/views/themes/default/nagvis/view.php
@@ -16,7 +16,7 @@
 	}
 </style>
 
-<div style="margin-left: 20px;">
+<div style="margin-left: 1px; ">
 	<iframe name="nagvis" src="/nagvis/nagvis/?map=<?php echo $map; ?>" width="100%" height="100%">
 		Error : Can not load NagVis.
 	</iframe>
