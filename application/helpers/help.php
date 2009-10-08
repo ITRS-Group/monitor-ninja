e825a4978a105b750cbe31c3444b8e6a99d1a26a
Per Asberg <perasb@op5.se>
2009-10-08 16:26:27 +0200

    helpers/help.php: Added help text helper
    This new helper takes care of rendering a help text icon and the
    required code to make inline help work in Ninja.
    
    Help texts are stored in each controller to provide translation
    and to keep them logically connected to where they are needed.
    The individual help text is fetched when a user clicks on the icon
    by an ajax call and is displayed using the JQuery qtip plugin.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

---
+++ application/helpers/help.php
@@ -0,0 +1,45 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Help class to provide links to render help texts
+ *
+ * @package NINJA
+ * @author op5 AB
+ * @license GPL
+ */
+class help_Core
+{
+	/**
+	*	Render the help text calls to fetch
+	*	translated help texts vis ajax calls.
+	*
+	* 	Example usage:
+	* 		Entering <?php help::render('edit'); ?> somewhere on a page (view) will
+	* 		create a clickable icon that fetches the help text from the current
+	* 		controller.
+	*
+	* 		By adding a second parameter to the call:
+	* 		<?php help::render('edit', 'extinfo'); ?> the information is instead
+	* 		fetched from the extinfo controller.
+	*/
+	public function render($key=false, $controller=false)
+	{
+		if (empty($key)) {
+			return false;
+		}
+
+		# fetch current theme path from registry
+		# to make help icon to always be in current theme
+		$theme_path = zend::instance('Registry')->get('theme_path');
+		$img_rel_path = 'icons/12x12/shield-info.png';
+		$img_path = url::base(false).'application/views/'.$theme_path.$img_rel_path;
+
+		$translate = zend::instance('Registry')->get('Zend_Translate');
+		$controller = !empty($controller) ? $controller : Router::$controller;
+
+		# build the element ID with random nr | controller | help key
+		$id = 'help_'.rand(0, 10000).'|'.$controller.'|'.$key;
+
+		echo '<a class="helptext_target" id="'.$id.'" href="#">'.
+		'<img src="'.$img_path.'" title="'.$translate->_('Click for help').'" alt="'.$translate->_('Click for help').'" /></a>';
+	}
+}
