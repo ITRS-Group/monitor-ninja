8d6c2516a5cbce525bdd9919d5d08e536bda5786
Per Åsberg <per.asberg@op5.se>
2009-10-15 16:34:16 +0200

    controllers: Added op5 copyright notice to all controllers
    We are urged to include a copyright statement in all files before
    officially releasing this project.
    This is the first step towards this for Ninja.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/cli.php
+++ application/controllers/cli.php
@@ -1,5 +1,19 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-
+/**
+ * 	CLI controller for command line access to Ninja
+ *
+ * @package    NINJA
+ * @author     op5 AB
+ * @license    GPL
+ * @copyright 2009 op5 AB
+ *  op5, and the op5 logo are trademarks, servicemarks, registered servicemarks
+ *  or registered trademarks of op5 AB.
+ *  All other trademarks, servicemarks, registered trademarks, and registered
+ *  servicemarks mentioned herein may be the property of their respective owner(s).
+ *  The information contained herein is provided AS IS with NO WARRANTY OF ANY
+ *  KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY, AND FITNESS FOR A
+ *  PARTICULAR PURPOSE.
+*/
 class Cli_Controller extends Authenticated_Controller {
 
 	public function __construct()
--- application/controllers/reporting.php
+++ application/controllers/reporting.php
@@ -6,6 +6,14 @@
  * @package    NINJA
  * @author     op5 AB
  * @license    GPL
+ * @copyright 2009 op5 AB
+ *  op5, and the op5 logo are trademarks, servicemarks, registered servicemarks
+ *  or registered trademarks of op5 AB.
+ *  All other trademarks, servicemarks, registered trademarks, and registered
+ *  servicemarks mentioned herein may be the property of their respective owner(s).
+ *  The information contained herein is provided AS IS with NO WARRANTY OF ANY
+ *  KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY, AND FITNESS FOR A
+ *  PARTICULAR PURPOSE.
  */
 class Reporting_Controller extends Authenticated_Controller {
 
