30b06bf18117a9e25542486c6c9a7f894bd719d8
My Appelgren <my@op5.com>
2009-10-22 12:50:21 +0200

    added normal white-space in the filter table.
    so long lines won't force the table to be wider than necessary
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/status/filters.php
+++ application/views/themes/default/status/filters.php
@@ -5,18 +5,18 @@
 	</tr>
 	<tr class="even">
 		<td><?php echo $lable_host_status_types ?></td>
-		<td><?php echo $host_status_type_val ?></td>
+		<td style="white-space:normal"><?php echo $host_status_type_val ?></td>
 	</tr>
 	<tr class="odd">
 		<td><?php echo $lable_host_properties ?></td>
-		<td><?php echo $hostprop_val ?></td>
+		<td style="white-space:normal"><?php echo $hostprop_val ?></td>
 	</tr>
 	<tr class="even">
 		<td><?php echo $lable_service_status_types ?></td>
-		<td><?php echo $service_status_type_val ?></td>
+		<td style="white-space:normal"><?php echo $service_status_type_val ?></td>
 	</tr>
 	<tr class="odd">
 		<td><?php echo $lable_service_properties ?></td>
-		<td><?php echo $serviceprop_val ?></td>
+		<td style="white-space:normal"><?php echo $serviceprop_val ?></td>
 	</tr>
 </table>
