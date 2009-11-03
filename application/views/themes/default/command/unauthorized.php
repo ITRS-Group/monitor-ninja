3edf09c7b901300fb0c17d404a0342765c30e275
Per Åsberg <per.asberg@op5.se>
2009-11-03 16:32:11 +0100

    views/command/unauthorized: use correct css classes and js methods
    The unauthorized template was using wrong css classes which made it
    render under the top bar, making the text impossible to read.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/views/themes/default/command/unauthorized.php
+++ application/views/themes/default/command/unauthorized.php
@@ -1,13 +1,14 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<br />
 
 <p>
-	<div class='errorMessage'>
+	<div class='error_message'>
 		<?php echo $error_message ?>
 	</div>
 </p>
 <p>
-	<div class='errorDescription'>
+	<div class='error_description'>
 		<?php echo $error_description ?><br /><br />
-		<a href='javascript:window.history.go(-2)'><?php echo $return_link_lable ?></a>
+		<a href='javascript:window.history.back()'><?php echo $return_link_lable ?></a>
 	</div>
 </p>
