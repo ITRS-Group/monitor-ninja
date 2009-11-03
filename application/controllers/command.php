265ac907cdc2e4ccba3a22b9e203ac075bb7bc39
Per Åsberg <per.asberg@op5.se>
2009-11-03 16:28:08 +0100

    controllers/command: Removed unused method
    The method use_authentication_off() in command controller wasn't used
    and is therefore removed.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/command.php
+++ application/controllers/command.php
@@ -280,14 +280,4 @@ class Command_Controller extends Authenticated_Controller
 		$this->template->content->error_description = $this->translate->_('Read the section of the documentation that deals with authentication and authorization in the CGIs for more information.');
 		$this->template->content->return_link_lable = $this->translate->_('Return from whence you came');
 	}
-
-	/**
-	 * Show info to user when use_authentication is disabled in cgi.cfg.
-	 */
-	public function use_authentication_off()
-	{
-		$this->template->content = $this->add_view('command/use_authentication_off');
-		$this->template->content->error_msg = $this->translate->_('Error: Authentication is not enabled!');
-		$this->template->content->error_description = $this->translate->_("As a safety precaution, commands aren't allowed when authentication is turned off.");
-	}
 }
