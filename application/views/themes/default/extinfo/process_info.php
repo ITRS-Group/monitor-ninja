4f142cfc687de9ea7f1a34a96e2cb500abad4a52
My Appelgren <my@op5.com>
2009-11-10 10:21:53 +0100

    changed the look of the extinfo-tables.
    tables with only two columns doesn't need the odd/even-row look.
    they look better with a dark column and an light, just as on the
    tac page.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/extinfo/process_info.php
+++ application/views/themes/default/extinfo/process_info.php
@@ -1,85 +1,85 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div class="widget left w66" id="extinfo_current" style="width: 350px">
-<table>
-<caption><?php echo $this->translate->_('Process Information'); ?></caption>
-	<tr class="odd">
-		<td class="bt" style="width: 220px"><?php echo $lable_program_version ?>:</td>
-		<td class="bt"><?php echo $program_version ?></td>
+<table class="ext">
+	<tr><th class="headerNone" colspan="2" style="border-left:0px"><?php echo $this->translate->_('Process Information'); ?></th></tr>
+	<tr>
+		<td class="dark"><?php echo $lable_program_version ?></td>
+		<td><?php echo $program_version ?></td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_program_start_time ?>:</td>
+	<tr>
+		<td class="dark"><?php echo $lable_program_start_time ?></td>
 		<td><?php echo $program_start ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_total_run_time ?>:</td>
+	<tr>
+		<td class="dark"><?php echo $lable_total_run_time ?></td>
 		<td><?php echo $run_time ?></td>
-	</tr><?php  //$last_command_check = 0; ?>
-	<tr class="even">
-		<td><?php echo $lable_last_external_cmd_check ?>:</td>
+	</tr>
+	<tr>
+		<td class="dark"><?php echo $lable_last_external_cmd_check ?></td>
 		<td><?php echo $last_command_check != 0 ? date('Y-m-d H:m:i',$last_command_check) : $this->translate->_('N/A') ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_last_logfile_rotation ?>:</td>
+	<tr>
+		<td class="dark"><?php echo $lable_last_logfile_rotation ?></td>
 		<td><?php echo $last_log_rotation ?></td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_pid ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_pid ?></td>
 		<td><?php echo $nagios_pid ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_notifications_enabled ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_notifications_enabled ?></td>
 		<td>
 			<?php echo html::image($this->add_path('icons/12x12/'.strtolower($notifications_str).'.png'),$notifications_str);?>
 			<?php echo ucfirst(strtolower($notifications_str)) ?>
 		</td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_service_checks ?> </td>
+	<tr>
+		<td class="dark"><?php echo $lable_service_checks ?> </td>
 		<td>
 			<?php echo html::image($this->add_path('icons/12x12/'.strtolower($servicechecks_str).'.png'),$servicechecks_str);?>
 			<?php echo ucfirst(strtolower($servicechecks_str)) ?>
 		</td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_service_checks_passive ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_service_checks_passive ?></td>
 		<td>
 			<?php echo html::image($this->add_path('icons/12x12/'.strtolower($passive_servicechecks_str).'.png'),$passive_servicechecks_str);?>
 			<?php echo ucfirst(strtolower($passive_servicechecks_str)) ?>
 		</td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_host_checks ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_host_checks ?></td>
 		<td>
 			<?php echo html::image($this->add_path('icons/12x12/'.strtolower($hostchecks_str).'.png'),$hostchecks_str);?>
 			<?php echo ucfirst(strtolower($hostchecks_str)) ?>
 		</td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_host_checks_passive ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_host_checks_passive ?></td>
 		<td>
 			<?php echo html::image($this->add_path('icons/12x12/'.strtolower($passive_hostchecks_str).'.png'),$passive_hostchecks_str);?>
 			<?php echo ucfirst(strtolower($passive_hostchecks_str)) ?>
 		</td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_event_handlers ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_event_handlers ?></td>
 		<td><?php echo $eventhandler_str ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_obsess_services ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_obsess_services ?></td>
 		<td><?php echo $obsess_services_str ?></td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_obsess_hosts ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_obsess_hosts ?></td>
 		<td><?php echo $obsess_hosts_str ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_flap_enabled ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_flap_enabled ?></td>
 		<td><?php echo $flap_detection_str ?></td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_performance_data ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_performance_data ?></td>
 		<td><?php echo $performance_data_str ?></td>
 	</tr>
 </table>
