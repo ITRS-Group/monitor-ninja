fdb92a618dc1706a5e66204178eba607415b407c
My Appelgren <my@op5.com>
2009-11-10 10:26:16 +0100

    changed the look of the filters table, displayd on the host/service page
    two-column tables have no need for the odd/even-row look - it's better
    with a dark and light column.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/status/filters.php
+++ application/views/themes/default/status/filters.php
@@ -1,22 +1,22 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<table id="filter_table" style="width: auto">
+<table id="filter_table" style="width: auto" class="ext">
 	<tr>
-		<th style="border: 1px solid #d0d0d0" colspan="2"><?php echo $header_title ?></th>
+		<th colspan="2"><?php echo $header_title ?></th>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_host_status_types ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_host_status_types ?></td>
 		<td style="white-space:normal"><?php echo $host_status_type_val ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_host_properties ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_host_properties ?></td>
 		<td style="white-space:normal"><?php echo $hostprop_val ?></td>
 	</tr>
-	<tr class="even">
-		<td><?php echo $lable_service_status_types ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_service_status_types ?></td>
 		<td style="white-space:normal"><?php echo $service_status_type_val ?></td>
 	</tr>
-	<tr class="odd">
-		<td><?php echo $lable_service_properties ?></td>
+	<tr>
+		<td class="dark"><?php echo $lable_service_properties ?></td>
 		<td style="white-space:normal"><?php echo $serviceprop_val ?></td>
 	</tr>
 </table>
