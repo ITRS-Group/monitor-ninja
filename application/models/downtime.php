1d2eead73fd77bd9a1b267550316acc4371bb1e7
Per Åsberg <per.asberg@op5.se>
2009-11-18 14:04:26 +0100

    models/downtime: Fetch downtime information based on user access
    This patch makes ninja fetch all the correct downtime ids that a user
    has access to.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/models/downtime.php
+++ application/models/downtime.php
@@ -13,19 +13,43 @@ class Downtime_Model extends Model
 	{
 		$db = new Database();
 		$auth = new Nagios_auth_Model();
-
 		$host_query = $auth->authorized_host_query();
 		if ($host_query === true) {
 			# don't use auth_host fields etc
-			$auth_host_alias = 'h';
-			$auth_from = '';
-			$auth_where = '';
+			$sql = "SELECT d.* FROM scheduled_downtime AS d";
 		} else {
+			# hosts
 			$auth_host_alias = $host_query['host_field'];
 			$auth_from = ' ,'.$host_query['from'];
 			$auth_where = ' WHERE '.sprintf($host_query['where'], "d.host_name");
+			$sql = "SELECT d.* FROM scheduled_downtime AS d ".$auth_from.$auth_where;
+
+			$query_contact = "SELECT d.* FROM scheduled_downtime AS d, host, ".
+			"contact, host_contact ".
+			"WHERE host.id = host_contact.host ".
+			"AND host_contact.contact=contact.id ".
+			"AND contact.contact_name=".$db->escape(Auth::instance()->get_user()->username).
+			" AND d.host_name=host.host_name";
+
+			# services
+			$query_svc =
+				'SELECT d.* FROM scheduled_downtime AS d, host, service, contact, contact_contactgroup, service_contactgroup ' .
+				'WHERE service.id = service_contactgroup.service ' .
+				'AND service_contactgroup.contactgroup = contact_contactgroup.contactgroup ' .
+				'AND contact_contactgroup.contact = ' . (int)$auth->id." AND host.host_name=service.host_name ".
+				"AND d.host_name=service.host_name AND d.service_description=service.service_description";
+
+			# contact <-> service_contact relation
+			$query_svc_contact = "SELECT d.* FROM scheduled_downtime AS d, host h, service s, contact c, service_contact sc ".
+				"WHERE s.id=sc.service AND c.id=sc.contact ".
+				"AND sc.contact=c.id ".
+				"AND c.contact_name=".$db->escape(Auth::instance()->get_user()->username).
+				" AND h.host_name=s.host_name AND d.host_name=s.host_name AND d.service_description=s.service_description";
+
+			$sql = '(' . $sql . ') UNION (' . $query_contact . ') UNION (' . $query_svc . ') UNION (' . $query_svc_contact . ')';
 		}
-		$sql = "SELECT d.* FROM scheduled_downtime AS d ".$auth_from.$auth_where;
+		$sql .= " ORDER BY downtime_id";
+
 		$result = $db->query($sql);
 		return $result->count() ? $result: false;
 	}
