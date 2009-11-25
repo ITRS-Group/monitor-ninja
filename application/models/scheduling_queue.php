cdba24ce980f2e033964256f30a4eea398c7eec2
My Appelgren <my@op5.com>
2009-11-25 14:58:13 +0100

    added a model for scheduling_queue
    Signed-off-by: My Appelgren <my@op5.com>

---
+++ application/models/scheduling_queue.php
@@ -0,0 +1,64 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Handle comments for hosts and services
+ */
+class Scheduling_queue_Model extends Model {
+
+	public $sort_field ='next_check';
+	public $sort_order='ASC';
+	public $num_per_page = false;
+	public $offset = false;
+	public $count = false;
+
+	/**
+	*	Fetch scheduled events
+	*
+	*/
+	public function show_scheduling_queue($num_per_page=false, $offset=false, $count=false)
+	{
+
+		$db = new Database();
+		$auth = new Nagios_auth_Model();
+
+		$host_query = $auth->authorized_host_query();
+		if ($host_query === true) {
+
+			$num_per_page = (int)$num_per_page;
+
+			# only use LIMIT when NOT counting
+			if ($offset !== false)
+				$offset_limit = $count!==false ? "" : " LIMIT " . $offset.", ".$num_per_page;
+			else
+				$offset_limit = '';
+				//$offset_limit = $count!==false ? "" : " LIMIT ".$num_per_page;
+
+			//echo 'offset_limit: '.$offset_limit;
+
+			$sql = "(SELECT host_name, service_description, next_check, last_check, check_type, active_checks_enabled
+							FROM service
+							WHERE should_be_scheduled=1
+							) UNION (
+							SELECT host_name, concat('') as service_description, next_check, last_check, check_type, active_checks_enabled
+							FROM host
+							WHERE should_be_scheduled=1
+							) ORDER BY ".$this->sort_field." ".$this->sort_order." ".$offset_limit;
+
+			$result = $db->query($sql);
+
+			if ($count !== false) {
+				return $result ? count($result) : 0;
+			}
+			return $result->count() ? $result->result(): false;
+		}
+	}
+
+	/**
+	*	Wrapper method to fetch no of hosts in the scheduling queue
+	*/
+	public function count_queue()
+	{
+		return self::show_scheduling_queue(false, false, true);
+	}
+
+}
