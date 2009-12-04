111ad86cd963e4f5b1a446fd9ffac85881056b8b
My Appelgren <my@op5.com>
2009-12-04 13:36:09 +0100

    changed the way the widgets are distributed...
    before the information from the database is fetched. instead of
    printing all in one collumn they're distrubited over the three
    columns. looks a little better - even though the best thing would
    be if they could be printed in the right order from the start.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/tac/index.php
+++ application/views/themes/default/tac/index.php
@@ -4,12 +4,16 @@
 <div class="widget-place" id="widget-placeholder">
 <?php
 	if (!empty($widgets)) {
+		$a = 0;
 		foreach ($widgets as $widget) {
 			echo $widget;
+			$a++;
+			if ($a == round(count($widgets)/3))
+				echo '</div><div class="widget-place" id="widget-placeholder1">';
+			elseif ($a == round(count($widgets)/3*2))
+				echo '</div><div class="widget-place" id="widget-placeholder2">';
 		}
 	}
 ?>
 </div>
-<div class="widget-place" id="widget-placeholder1"></div>
-<div class="widget-place" id="widget-placeholder2"></div>
 <div class="widget-place w98" id="widget-placeholder3"></div>
