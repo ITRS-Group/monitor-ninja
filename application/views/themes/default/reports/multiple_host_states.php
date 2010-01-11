94d84d961900c4db74bb6ecb5215757763090115
My Appelgren <my@op5.com>
2010-01-11 09:31:42 +0100

    ninjafied the multiple host states
    there should be some more info added - reporing time period and
    some header, but to add this some things need to be adjusted in
    the controller, so i'll add this later. also - i want to move
    the pie-chars to the right of the tables.. but that also requires
    some controller edits
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/reports/multiple_host_states.php
+++ application/views/themes/default/reports/multiple_host_states.php
@@ -1,71 +1,61 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-foreach ($multiple_states as $data) { ?>
-<div class="host_breakdown wide">
-	<h1 onclick="show_hide('host<?php echo str_replace(' ','_',$data['groupname']);?>',this)"><?php echo (!empty($data['groupname']) ? $data['groupname'] : 'Hosts state breakdown'); ?></h1>
-	<div class="icon-help" onclick="general_help('multiple_host_states')"></div>
-	<fieldset id="host<?php echo str_replace(' ','_',$data['groupname']);?>">
-			<table summary="Host state breakdown" style="width: 100%">
-			<colgroup>
-				<col style="width: 80%" />
-				<col style="width: auto" />
-				<col class="col_space" />
-				<col style="width: auto" />
-				<col class="col_space" />
-				<col style="width: auto" />
-				<col class="col_space" />
-				<col style="width: auto" />
-			</colgroup>
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<div class="host_breakdown wide" style="margin-top: 15px;">
+<!--<h1 style="margin-left:0px"><?php echo (!empty($data['groupname']) ? 'Hostgroup breakdown' : 'Hosts state breakdown'); ?></h1>
+<p style="margin-top: -12px; margin-bottom: 15px">Reporting period: last year (2009-01-01 00:00 to 2010-01-01 00:00)</p>-->
+<?php foreach ($multiple_states as $data) { ?>
+		<table summary="Host state breakdown" id="multiple_hosts" style="margin-top: 7px">
 			<tr>
-				<th class="null">Time:</th>
-				<th class="up">Up</th>
-				<th class="null"></th>
-				<th class="unreachable">Unreachable</th>
-				<th class="null"></th>
-				<th class="down">Down</th>
-				<th class="null"></th>
-				<th class="undetermined">Undetermined</th>
+				<th class="headerNone" style="width: 90%; text-align: left"><?php echo (!empty($data['groupname']) ? str_replace('Hostgroup:','',$data['groupname']) : 'Selected hosts'); ?></th>
+				<th class="headerNone">Up</th>
+				<th class="headerNone">Unreachable</th>
+				<th class="headerNone">Down</th>
+				<th class="headerNone">Undetermined</th>
 			</tr>
 			<?php for ($i=0;$i<$data['nr_of_items'];$i++) { ?>
-			<tr>
+			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
 			<?php if (!$use_alias) { ?>
-				<td class="label"><?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?></td>
+				<td><?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?></td>
 				<?php } else { ?>
-				<td class="label"><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?>)</td>
+				<td><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?>)</td>
 				<?php } ?>
-				<td style="white-space:nowrap"><?php echo $this->_format_report_value($data['up'][$i]) ?> %</td>
-				<td class="null"></td>
-				<td><?php echo $this->_format_report_value($data['unreachable'][$i]) ?> %</td>
-				<td class="null"></td>
-				<td style="white-space:nowrap"><?php echo $this->_format_report_value($data['down'][$i]) ?> %</td>
-				<td class="null"></td>
-				<td class="border-right"><?php echo $this->_format_report_value($data['undetermined'][$i]) ?> %</td>
+				<td class="data">
+				<?php echo $this->_format_report_value($data['up'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($this->_format_report_value($data['up'][$i]) > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $this->_format_report_value($data['unreachable'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($this->_format_report_value($data['unreachable'][$i]) > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $this->_format_report_value($data['down'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($this->_format_report_value($data['down'][$i]) > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $this->_format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($this->_format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
 			</tr>
 			<?php	} if ($use_average==0) { ?>
-			<tr>
-				<td class="label">Average</td>
-				<td style="white-space:nowrap"><?php echo $data['average_up'] ?> %</td>
-				<td class="null"></td>
-				<td><?php echo $data['average_unreachable'] ?> %</td>
-				<td class="null"></td>
-				<td style="white-space:nowrap"><?php echo $data['average_down'] ?> %</td>
-				<td class="null"></td>
-				<td class="border-right"><?php echo $data['average_undetermined'] ?> %</td>
+			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'; $i++?>">
+				<td>Average</td>
+				<td class="data"><?php echo $data['average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_up'] > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_down'] > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
 			</tr>
 			<?php } ?>
-			<tr class="group-average">
-				<td class="label"><?php echo ($use_average==0) ? 'Group availability (SLA)' :'Average'; ?></td>
-				<td class="up" style="white-space:nowrap"><?php echo $data['group_average_up'] ?> %</td>
-				<td class="null"></td>
-				<td class="unreachable"><?php echo $data['group_average_unreachable'] ?> %</td>
-				<td class="null"></td>
-				<td class="down" style="white-space:nowrap"><?php echo $data['group_average_down'] ?> %</td>
-				<td class="null"></td>
-				<td class="undetermined border-right"><?php echo $data['group_average_undetermined'] ?> %</td>
+			<tr class="group-average <?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
+				<td><?php echo ($use_average==0) ? 'Group availability (SLA)' :'Average'; ?></td>
+				<td class="data"><?php echo $data['group_average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_up'] > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['group_average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['group_average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_down'] > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
+				<td class="data"><?php echo $data['group_average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => 'Up', 'title' => 'Up','style' => 'margin-bottom: -1px')) ?></td>
 			</tr>
 			<tr id="pdf-hide">
-				<td colspan="8" style="background: #ffffff;border-bottom: 0px;padding: 7px 0px 0px 0px"><?php echo $this->_build_testcase_form($data[';testcase;']); ?></td>
+				<td colspan="5" class="testcase-button"><?php echo $this->_build_testcase_form($data[';testcase;']); ?></td>
 			</tr>
 		</table>
-	</fieldset>
-</div>
 <?php } ?>
+</div>
