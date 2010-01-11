4f644ea5b71b3e1fb70ef6d2d655de8760771a94
My Appelgren <my@op5.com>
2010-01-11 09:36:44 +0100

    ninjafied the piecharts
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/reports/pie_chart.php
+++ application/views/themes/default/reports/pie_chart.php
@@ -5,21 +5,16 @@ if (isset($data_str)) {
 	if (is_array($data_str)) {
 		for ($i = 0; $i < count($data_str); $i++) { ?>
 		<div class="pie-chart">
-			<h1 onclick="show_hide('pie<?php echo $i; ?>',this)">
-				<?php echo $label_status ?> <?php echo ($data_str[$i]['host'] != '') ? ': '.$data_str[$i]['host'] : ''; ?>
-			</h1>
-			<div class="icon-help" onclick="general_help('pie_chart')"></div>
-			<img src="/ninja/index.php/reports/piechart/<?php echo $data_str[$i]['img'] ?>" alt="Uptime" id="pie" class="chart-border" />
-
+			<table>
+				<tr><th class="headerNone" style="text-align: left"><?php //echo help::render('pie_chart') ?> <?php echo $label_status ?> <?php echo ($data_str[$i]['host'] != '') ? ': '.$data_str[$i]['host'] : ''; ?></th></tr>
+				<tr class="even"><td><img src="/ninja/index.php/reports/piechart/<?php echo $data_str[$i]['img'] ?>" alt="Uptime" id="pie" /></td>
+			</table>
 		</div>
 		<?php
 		}
 	} else if(!empty($data_str)) { ?>
 	<div class="pie-chart">
-		<h1 onclick="show_hide('pie',this)"><?php echo $label_status ?></h1>
-		<div class="icon-help" onclick="general_help('pie_chart')"></div>
-		<!--<img src="chart.php?type=pie&amp;data=<?php echo $data_str ?>" alt="Uptime" id="pie" class="chart-border" />-->
+		<?php echo help::render('pie_chart') ?><strong><?php echo $label_status ?></strong><br />
 		<img src="/ninja/index.php/reports/piechart/<?php echo $data_str ?>" alt="Uptime" id="pie" class="chart-border" />
-	</div><?php
-	}
-} ?>
+	</div>
+<?php } } ?>
