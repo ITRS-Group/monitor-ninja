0a6360295b2a816903f37cb661a71a9f79ccdfb4
My Appelgren <my@op5.com>
2010-01-12 12:14:23 +0100

    ninjafied the log table
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/reports/log.php
+++ application/views/themes/default/reports/log.php
@@ -10,46 +10,44 @@ if (!empty($log)) {
 	$service_state_txt 	= array(-1 => 'PENDING', 0 => 'OK', 1 => 'WARNING', 2 => 'CRITICAL', 3 => 'UNKNOWN');
 ?>
 	<div id="log_entries">
-		<h1 id="log-h1" onclick="show_hide('log',this)"><?php echo ucfirst($type) ?> <?php echo $label_entries ?> <?php echo $source; ?></h1>
-		<div class="icon-help" onclick="general_help('log_entries')"></div>
-		<fieldset id="log">
-			<form onsubmit="return false;" action=""><div><input type="text" size="60" name="filterbox" id="filterbox" value="Enter text to filter" /></div></form>
+
+		<!--<div class="icon-help" onclick="general_help('log_entries')"></div>-->
+			<!--<form onsubmit="return false;" action=""><div><input type="text" size="60" name="filterbox" id="filterbox" value="Enter text to filter" /></div></form>-->
 			<table id="log-table">
-				<colgroup>
-					<col class="col_label" />
-					<col class="col_date" />
-					<col class="col_date" />
-					<col class="col_duration" />
-					<col class="col_information" />
-				</colgroup>
+				<caption><?php echo ucfirst($type) ?> <?php echo $label_entries ?> <?php echo $source; ?></caption>
 				<thead>
 				<tr>
-					<th>&nbsp;</th>
-					<th>Start time</th>
-					<th>End time</th>
-					<th>Duration</th>
-					<th>Information</th>
+					<th class="headerNone left"><?php echo $this->translate->_('Status');?></th>
+					<th class="headerNone left" style="width: 110px"><?php echo $this->translate->_('Start time');?></th>
+					<th class="headerNone left" style="width: 110px"><?php echo $this->translate->_('End time');?></th>
+					<th class="headerNone left" style="width: 100px"><?php echo $this->translate->_('Duration');?></th>
+					<th class="headerNone left"><?php echo $this->translate->_('Log message');?></th>
 				</tr>
 				</thead>
 				<tbody>
 				<?php
+				$i = 0;
 				foreach ($log as $key => $value) {
+					$i++;
 					if (isset($value['state'])) {
 					$event_end_time = $value['the_time'] + $value['duration'];
 				?>
-				<tr>
-					<td class="label <? echo strtolower(${$type.'_state_txt'}[$value['state']]); ?>-left">
-						<?php echo ucfirst(strtolower(${$type.'_state_txt'}[$value['state']])); ?>
+				<tr class="<?php echo ($i%2 == 0) ? 'odd' : 'even' ?>">
+					<td>
+						<?php
+							echo html::image($this->add_path('icons/12x12/shield-'.strtolower(${$type.'_state_txt'}[$value['state']]).'.png'),
+									 array('alt' => strtolower(${$type.'_state_txt'}[$value['state']]),'title' => strtolower(${$type.'_state_txt'}[$value['state']]),'style' => 'margin-bottom: -1px'));
+							echo '&nbsp;'.ucfirst(strtolower(${$type.'_state_txt'}[$value['state']]));
+						?>
 					</td>
 					<td><?php echo date('Y-m-d, H:i:s', $value['the_time']); ?></td>
 					<td><?php echo date('Y-m-d, H:i:s', $event_end_time); ?></td>
 					<td><?php echo time::to_string($value['duration']); ?></td>
-					<td class="border-right"><?php echo $value['output']; ?></td>
+					<td style="white-space: normal"><?php echo $value['output']; ?></td>
 				</tr>
 				<?php } } ?>
 				</tbody>
 			</table>
-		</fieldset>
 	</div>
 	<script type="text/javascript">
 		document.getElementById('log').style.display='none';
