d025519623fdd4cb317b5952076575551e355b26
Per Åsberg <per.asberg@op5.se>
2010-01-22 09:54:03 +0100

    Added 2 new templates for reports
    Those templates are for displaying all the scheduled reports and
    for adding new schedules.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

---
+++ application/views/themes/default/reports/new_schedule.php
@@ -0,0 +1,62 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+				<div id="new_schedule_area">
+				<?php	echo form::open('reports/schedule', array('id' => 'new_schedule_report_form', 'onsubmit' => 'return ajax_submit(this)'));
+					?>
+					<h1><?php echo $label_new_schedule ?></h1>
+					<table id="new_schedule_report_table" class="white-table">
+						<tr class="none">
+							<td><?php echo $label_report_type ?></td>
+						</tr>
+						<tr class="none">
+							<td><?php echo form::dropdown(array('name' => 'rep_type'), $defined_report_types); ?></td>
+						</tr>
+
+						<!--	saved_report_id as drop-down depending on type		-->
+						<tr class="none">
+							<td><?php echo $label_select_report ?></td>
+						</tr>
+						<tr class="none">
+							<td>
+								<select name="saved_report_id" id="saved_report_id">
+									<option value=""> - <?php echo $label_select_saved_report ?> - </option>
+								<?php	foreach ($saved_reports as $report) { ?>
+											<option value="<?php echo $report->id ?>"><?php echo $report->report_name ?></option>
+								<?php 	} ?>
+								</select>
+							</td>
+						</tr>
+
+
+						<?php if (!empty($available_schedule_periods)) { ?>
+						<tr class="none">
+							<td><?php echo $label_interval ?></td>
+						</tr>
+						<tr class="none">
+							<td>
+								<select name="period" id="period">
+								<?php	foreach ($available_schedule_periods as $id => $period) { ?>
+									<option value="<?php echo $id ?>"><?php echo $period ?></option>
+								<?php	} ?>
+								</select>
+							</td>
+						</tr>
+						<?php } ?>
+						<tr class="none">
+							<td><?php echo $label_recipients ?><br /><input type="text" class="schedule" name="recipients" id="recipients" value="" /></td>
+						</tr>
+						<tr class="none">
+							<td><?php echo $label_filename ?><br /><input type="text" class="schedule" name="filename" id="filename" value="" /></td>
+						</tr>
+						<tr class="none">
+							<td><?php echo $label_description ?><br /><textarea cols="31" rows="3" id="description" name="description"></textarea></td>
+						</tr>
+						<tr class="none">
+							<td id="scheduled_btn_ctrl">
+								<input type="submit" class="button save" name="sched_subm" id="sched_subm" value="<?php echo $label_save ?>" />
+								<input type="reset" class="button clear" name="reset_frm" id="reset_frm" value="<?php echo $label_clear ?>" />
+							</td>
+						</tr>
+					</table>
+					</form>
+				</div>
---
+++ application/views/themes/default/reports/schedules.php
@@ -0,0 +1,76 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+			<div id="schedules_area">
+				<?php echo isset($new_schedule) ? $new_schedule : '' ?>
+
+				<div id="scheduled_avail_reports" style='width: 100%'>
+					<span><strong><?php echo $avail_header ?>:</strong></span>
+					<table id="avail_scheduled_reports_table" style='width: 100%; margin-top: 10px' class="white-table">
+					<?php if (!empty($avail_schedules) && count($avail_schedules)) { ?>
+						<thead>
+							<tr class="setup">
+								<th style='width: 9%'><?php echo $label_sch_interval ?></th>
+								<th style='width: 9%'><?php echo $label_sch_name ?></th>
+								<th style='width: 20%'><?php echo $label_sch_recipients ?></th>
+								<th style='width: 20%'><?php echo $label_sch_filename ?></th>
+								<th style='width: 50%'><?php echo $label_sch_description ?></th>
+								<th style='width: 1%'></th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php	$recipients = false;
+								foreach ($avail_schedules as $schedule) {
+									$schedule = (object)$schedule;
+									$recipients = str_replace(' ', '', $schedule->recipients);
+									$recipients = str_replace(',', ', ', $recipients);	?>
+								<tr id="report-<?php echo $schedule->id ?>">
+								<td class="period_select" title="<?php echo $label_dblclick ?>" id="period_id-<?php echo $schedule->id ?>"><?php echo $schedule->periodname ?></td>
+								<td class="report_name" id="<?php echo $schedule->report_type_id ?>.report_id-<?php echo $schedule->id ?>"><?php echo $schedule->reportname ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="recipients-<?php echo $schedule->id ?>"><?php echo $recipients ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="filename-<?php echo $schedule->id ?>"><?php echo $schedule->filename ?></td>
+								<td class="iseditable_txtarea" title="<?php echo $label_dblclick ?>" id="description-<?php echo $schedule->id ?>"><?php echo utf8_decode($schedule->description) ?></td>
+								<td class="delete_schedule" id="alldel_<?php echo $schedule->id ?>" style='text-align: right'><?php echo html::image($this->add_path('icons/12x12/cross.gif'), array('class' => 'deleteimg')) ?></td>
+							</tr>
+							<?php } } else {?>
+								<?php echo $label_no_schedules ?>
+							<?php } ?>
+						</tbody>
+					</table>
+				</div>
+
+				<br />
+
+				<div id="scheduled_sla_reports" style='width: 100%'>
+					<span><strong><?php echo $sla_header ?>:</strong></span>
+					<table id="sla_scheduled_reports_table" style='width: 100%; margin-top: 10px' class="white-table">
+					<?php if (!empty($sla_schedules) && count($sla_schedules)) { ?>
+						<thead>
+							<tr class="setup">
+								<th style='width: 9%'><?php echo $label_sch_interval ?></th>
+								<th style='width: 9%'><?php echo $label_sch_name ?></th>
+								<th style='width: 20%'><?php echo $label_sch_recipients ?></th>
+								<th style='width: 20%'><?php echo $label_sch_filename ?></th>
+								<th style='width: 50%'><?php echo $label_sch_description ?></th>
+								<th style='width: 1%'></th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php	$recipients = false;
+								foreach ($sla_schedules as $schedule) {
+									$schedule = (object)$schedule;
+									$recipients = str_replace(' ', '', $schedule->recipients);
+									$recipients = str_replace(',', ', ', $recipients);	?>
+								<tr id="report-<?php echo $schedule->id ?>">
+								<td class="period_select" title="<?php echo $label_dblclick ?>" id="period_id-<?php echo $schedule->id ?>"><?php echo $schedule->periodname ?></td>
+								<td class="report_name" id="<?php echo $schedule->report_type_id ?>.report_id-<?php echo $schedule->id ?>"><?php echo $schedule->reportname ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="recipients-<?php echo $schedule->id ?>"><?php echo $recipients ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="filename-<?php echo $schedule->id ?>"><?php echo $schedule->filename ?></td>
+								<td class="iseditable_txtarea" title="<?php echo $label_dblclick ?>" id="description-<?php echo $schedule->id ?>"><?php echo utf8_decode($schedule->description) ?></td>
+								<td class="delete_schedule" id="alldel_<?php echo $schedule->id ?>" style='text-align: right'><?php echo html::image($this->add_path('icons/12x12/cross.gif')) ?></td>
+							</tr>
+							<?php } } else {?>
+								<?php echo $label_no_schedules ?>
+							<?php } ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
