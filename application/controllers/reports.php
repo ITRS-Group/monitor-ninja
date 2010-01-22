932c035e484f49a73d5b51e23c1cf4f8ba37fdd3
Per Åsberg <per.asberg@op5.se>
2010-01-22 09:43:49 +0100

    Added JQuery UI stylesheet and images
    These resources are required to be able to use tabs and some other
    UI items from Jquery UI. There are probably some things that could be
    stripped from the css (and probably some images) but this will have to
    suffice for now.
    
    Signed-off-by: Per Åsberg <per.asberg@op5.se>

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -307,6 +307,7 @@ class Reports_Controller extends Authenticated_Controller
 		$this->template->css_header = $this->add_view('css_header');
 		$this->xtra_css[] = $this->add_path('reports/css/datePicker');
 		$this->xtra_css[] = $this->add_path('reports/css/reports');
+		$this->xtra_css[] = $this->add_path('css/default/jquery-ui-custom.css');
 		$this->template->css_header->css = $this->xtra_css;
 
 		$t = $this->translate;
