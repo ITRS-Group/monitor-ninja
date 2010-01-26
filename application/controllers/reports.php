4663f2483f4a95b55d0aec41393d04fab2270d44
Per Asberg <perasb@op5.se>
2010-01-26 17:18:40 +0100

    controllers/reports: Move _get_date_format() to helper
    We want this code in more than just the reports controller and moving
    it to a helper makes this easy.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -2691,31 +2691,7 @@ class Reports_Controller extends Authenticated_Controller
 	*/
 	public function _get_date_format($get_php=false)
 	{
-		$date_format = false;
-		$nagios_config = System_Model::parse_config_file('nagios.cfg');
-		$nagios_format_name = $nagios_config['date_format'];
-		switch (strtolower($nagios_format_name)) {
-			case 'us': # MM-DD-YYYY
-				$date_format = 'mm/dd/yyyy';
-				break;
-			case 'euro': # DD-MM-YYYY
-				$date_format = 'dd-mm-YYYY';
-				break;
-			case 'iso8601': # YYYY-MM-DD
-				$date_format = 'yyyy-mm-dd';
-				break;
-			case 'strict-iso8601': # YYYY-MM-DD
-				$date_format = 'yyyy-mm-dd';
-				break;
-		}
-
-		# convert to PHP equivalent
-		if ($get_php === true) {
-			$date_format = str_replace('yyyy', 'Y', $date_format);
-			$date_format = str_replace('mm', 'm', $date_format);
-			$date_format = str_replace('dd', 'd', $date_format);
-		}
-		return $date_format;
+		return cal::get_calendar_format($get_php);
 	}
 
 	/**
