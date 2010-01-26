fb0df925a09e7d472361f698551960983b565343
My Appelgren <my@op5.com>
2010-01-26 14:50:35 +0100

    updated the looks for alter history on ninja
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/showlog/showlog.php
+++ application/views/themes/default/showlog/showlog.php
@@ -1,98 +1,110 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
-
-echo html::script('application/media/js/jscalendar/calendar.js');
-echo html::script('application/media/js/jscalendar/lang/calendar-en.js');
-echo html::script('application/media/js/jscalendar/calendar-setup.js');
-echo html::script('application/media/js/calendar-extras.js');
-echo html::stylesheet('application/media/js/jscalendar/skins/aqua/theme.css');
-
-$x = $this->translate;
-echo "<br />\n";
-
-echo "\$options = " . Kohana::debug($options);
-
-echo form::open();
-echo "<table><tr><th width=10></th><th width=200></th></tr>\n";
-echo '<tr><td>' . $x->_('State type options') . "</td></tr>\n";
-
-$set = isset($options['state_types']['soft']);
-echo '<tr><td>&nbsp; &nbsp;' . form::checkbox('state_types[soft]', $set) . "</td>\n";
-echo "<td>" . $x->_('Soft states') . "</td></tr>\n";
-
-$set = isset($options['state_types']['hard']);
-echo '<tr><td>&nbsp; &nbsp' . form::checkbox('state_types[hard]', $set) . "</td>\n";
-echo '<td>' . $x->_('Hard states') . "</td></tr>\n";
-echo '<tr><td>' .  $x->_('Host state options') . "</td></tr>\n";
-foreach ($host_state_options as $k => $v) {
-	$set = isset($options['host_state_options'][$v]);
-	$name = 'host_state_options[' . $v . ']';
-	echo '<tr><td>&nbsp; &nbsp;' . form::checkbox($name, $set) . "</td>\n";
-	echo "<td>$k</td></tr>\n";
-}
-echo '<tr><td>' . $x->_('Service state options') . "</td></tr>\n";
-foreach ($service_state_options as $k => $v) {
-	$set = isset($options['service_state_options'][$v]);
-	$name = 'service_state_options[' . $v . ']';
-	echo '<tr><td>&nbsp; &nbsp;' . form::checkbox($name, $set) . "</td>\n";
-	echo "<td>$k</td></tr>\n";
-}
-
-echo '<tr><td>' . form::checkbox('hide_flapping') . "</td>\n";
-echo '<td>' . $x->_('Hide flapping alerts') . "</td></tr>\n";
-
-echo '<tr><td>' . form::checkbox('hide_downtime') . "</td>\n";
-echo '<td>' . $x->_('Hide downtime alerts') . "</td></tr>\n";
-
-echo '<tr><td>' . form::checkbox('hide_process') . "</td>\n";
-echo '<td>' . $x->_('Hide process messages') . "</td></tr>\n";
-
-echo '<tr><td>' . form::checkbox('parse_forward') . "</td>\n";
-echo '<td>' . $x->_('Older entries first') . "</td></tr>\n";
-
-echo '<tr><td>' . $x->_('First time') . '</td>';
-echo '<td>' . form::input('first') . "</td></tr>\n";
+	echo html::script('application/media/js/jscalendar/calendar.js');
+	echo html::script('application/media/js/jscalendar/lang/calendar-en.js');
+	echo html::script('application/media/js/jscalendar/calendar-setup.js');
+	echo html::script('application/media/js/calendar-extras.js');
+	echo html::stylesheet('application/media/js/jscalendar/skins/aqua/theme.css');
+	$x = $this->translate;
+	//echo "\$options = ".Kohana::debug($options);
+	echo form::open();
 ?>
-<script type="text/javascript">
-Calendar.setup({
-	inputField      : 'first',           // id of the input field
-	ifFormat        : '%Y-%m-%d %H:%M',  // format of the input field
-	daFormat        : '%Y-%m-%d %H:%M',  // format of the displayed field
-	button          : 'f_trigger_start', // trigger for the calendar (button ID)
-	align           : 'Bl',              // alignment (defaults to "Bl")
-	timeFormat      : '24',
-	showsTime       : true,
-	displayArea     : 'start_time_tmp',
-	firstDay        : 1,
-	onClose         : store_start_date,
-	singleClick     : true
-});
-</script>
+<div class="widget left w98">
+	<table class="showlog">
+		<tr>
+			<td>
+	<h2><?php echo $x->_('State type options'); ?></h2>
+	<?php echo form::checkbox('state_types[soft]', isset($options['state_types']['soft'])).' '.$x->_('Soft states'); ?><br />
+	<?php echo form::checkbox('state_types[hard]', isset($options['state_types']['hard'])).' '.$x->_('Hard states'); ?>
+	</td>
+	<td>
+	<h2><?php echo $x->_('Host state options'); ?></h2>
+		<?php
+			$i = 0;
+			foreach ($host_state_options as $k => $v) {
+				$i++;
+				$set = isset($options['host_state_options'][$v]);
+				$name = 'host_state_options[' . $v . ']';
+				//echo ($i%2 == 0) ? '': '<tr>';
+				echo form::checkbox($name, $set).' '.$k.'<br />';
+				//echo ($i%2 == 0) ? '</tr>': ''."\n";
+			}
+		?>
+		</td><td>
+		<h2><?php echo $x->_('Service state options'); ?></h2>
+		<?php
+			$i = 0;
+			foreach ($service_state_options as $k => $v) {
+				$set = isset($options['service_state_options'][$v]);
+				$i++;
+				$name = 'service_state_options[' . $v . ']';
+				//echo ($i%2 == 0) ? '': '<tr>';
+				echo form::checkbox($name, $set).' '.$k.'<br />';
+				//echo ($i%2 == 0) ? '</tr>': ''."\n";
+			}
+		?>
+		</td><td>
+		<h2><?php echo $x->_('General options'); ?></h2>
+		<?php echo form::checkbox('hide_flapping').' '.$x->_('Hide flapping alerts'); ?><br />
+		<?php echo form::checkbox('hide_downtime').' '.$x->_('Hide downtime alerts'); ?><br />
+		<?php echo form::checkbox('hide_process').' '.$x->_('Hide process messages'); ?><br />
+		<?php echo form::checkbox('parse_forward').' '.$x->_('Older entries first'); ?>
+		</td>
+		</tr>
+		<tr>
+		<td colspan="2">
+			<h2><?php echo $x->_('First time').'</h2>'.form::input('first'); ?>
+			<?php echo html::image($this->add_path('icons/16x16/calendar.png'),'First'); ?>
+				<script type="text/javascript">
+				Calendar.setup({
+					inputField      : 'first',           // id of the input field
+					ifFormat        : '%Y-%m-%d %H:%M',  // format of the input field
+					daFormat        : '%Y-%m-%d %H:%M',  // format of the displayed field
+					button          : 'f_trigger_start', // trigger for the calendar (button ID)
+					align           : 'Bl',              // alignment (defaults to "Bl")
+					timeFormat      : '24',
+					showsTime       : true,
+					displayArea     : 'start_time_tmp',
+					firstDay        : 1,
+					onClose         : store_start_date,
+					singleClick     : true
+				});
+				</script>
+			</td>
+			<td colspan="2">
+				<h2><?php echo $x->_('Last time').'</h2>'.form::input('last'); ?>
+				<?php echo html::image($this->add_path('icons/16x16/calendar.png'),'First'); ?>
+				<script type="text/javascript">
+				Calendar.setup({
+					inputField      : 'last',            // id of the input field
+					ifFormat        : '%Y-%m-%d %H:%M',  // format of the input field
+					daFormat        : '%Y-%m-%d %H:%M',  // format of the displayed field
+					button          : 'f_trigger_end',   // trigger for the calendar (button ID)
+					align           : 'Bl',              // alignment (defaults to "Bl")
+					timeFormat      : '24',
+					showsTime       : true,
+					displayArea     : 'end_time_tmp',
+					firstDay        : 1,
+					onClose         : check_start_date,
+					singleClick     : true
+				});
+				</script>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="4">
+			<?php
+				echo form::hidden('have_options', 1);
+				echo form::submit('Update', 'Update');
+			?>
+			</td>
+		</tr>
+		</table>
+	<?php echo form::close(); ?>
 
 <?php
-echo '<tr><td>' . $x->_('Last time') . '</td>';
-echo '<td>' . form::input('last') . "</td></tr>\n";
-?>
-<script type="text/javascript">
-Calendar.setup({
-	inputField      : 'last',            // id of the input field
-	ifFormat        : '%Y-%m-%d %H:%M',  // format of the input field
-	daFormat        : '%Y-%m-%d %H:%M',  // format of the displayed field
-	button          : 'f_trigger_end',   // trigger for the calendar (button ID)
-	align           : 'Bl',              // alignment (defaults to "Bl")
-	timeFormat      : '24',
-	showsTime       : true,
-	displayArea     : 'end_time_tmp',
-	firstDay        : 1,
-	onClose         : check_start_date,
-	singleClick     : true
-});
-</script>
+	# this hidden thing marks "no options chosen" as a chosen set of options
+	# so we avoid overriding "no options" with the default ones
 
-<?php
-echo "</table>\n";
-# this hidden thing marks "no options chosen" as a chosen set of options
-# so we avoid overriding "no options" with the default ones
-echo form::hidden('have_options', 1);
-echo form::submit('Update', 'Update');
-echo form::close();
-$this->_show_log_entries();
+	$this->_show_log_entries();
+?>
+</div>
