2c7cd49de05af636f1c159a3f151b5313dbf5f30
My Appelgren <my@op5.com>
2010-02-03 09:20:09 +0100

    created the notifications controller for ninja
    there is still fixed limit set to the controller since the
    database is overfull atm. this must be removed before ninja
    is released. there are probably a few more things that need
    to be adjusted later on as well, but this is how it looks
    today.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/controllers/notifications.php
+++ application/controllers/notifications.php
@@ -3,9 +3,9 @@
  * Notifications controller
  * Requires authentication
  *
- * @package    NINJA
- * @author     op5 AB
- * @license    GPL
+ * @package	NINJA
+ * @author	op5 AB
+ * @license	GPL
  * @copyright 2009 op5 AB
  *  op5, and the op5 logo are trademarks, servicemarks, registered servicemarks
  *  or registered trademarks of op5 AB.
@@ -16,13 +16,48 @@
  *  PARTICULAR PURPOSE.
  */
 class Notifications_Controller extends Authenticated_Controller {
-	public function index()
+	public $current = false;
+	public $logos_path = '';
+
+	public function __construct()
 	{
-		url::redirect('underconstruction/');
+		parent::__construct();
+
+		$this->logos_path = Kohana::config('config.logos_path');
 	}
 
-	public function __call($method, $arguments)
+	/**
+	 * Default controller method
+	 * Redirects to show_process_info() which
+	 * is the equivalent of calling extinfo.cgi?type=0
+	 */
+	public function index($sort_field='host_name', $sort_order='ASC', $notification_option = '')
 	{
-		url::redirect('underconstruction/');
+
+		//$items_per_page = urldecode($this->input->get('items_per_page', Kohana::config('pagination.default.items_per_page'))); # @@@FIXME: should be configurable from GUI
+		$items_per_page = urldecode($this->input->get('items_per_page', 20)); # @@@FIXME: should be configurable from GUI
+		$items_per_page = 100;
+		$note_model = new Notifications_Model($items_per_page, true, true);
+		$note_model->sort_order = urldecode($this->input->get('sort_order', $sort_order));
+		$note_model->sort_field = urldecode($this->input->get('sort_field', $sort_field));
+
+		$note_model->where = urldecode($this->input->get('notification_option', $notification_option));
+
+		$result = $note_model->show_notifications();
+
+		$pagination = new Pagination(
+			array(
+				'total_items'=> $note_model->count_notifications(),
+				'items_per_page' => $items_per_page
+			)
+		);
+
+		$note_model->offset = $pagination->sql_offset;
+
+		$this->template->title = $this->translate->_('Reporting').' » '.$this->translate->_('Contact Notifications');
+		$this->template->content = $this->add_view('notifications/index');
+		$this->template->content->data = $result;
+		$this->template->content->notification_option = $notification_option;
+		$this->template->content->pagination = isset($pagination) ? $pagination : false;
 	}
 }
