1a90d4f976adbdc23dd5e90c2efebee41c16af1e
My Appelgren <my@op5.com>
2010-02-03 09:32:27 +0100

    translated all text in the notifications template
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/notifications/index.php
+++ application/views/themes/default/notifications/index.php
@@ -1,28 +1,29 @@
+<?php $t = $this->translate; ?>
 <div class="widget w98 left">
 	<?php //echo (isset($pagination)) ? $pagination : ''; ?>
 	<form method="get" action="">
-		<strong><?php echo $this->translate->_('Detail level for all contacts');?></strong><br />
+		<strong><?php echo $t->_('Detail level for all contacts');?></strong><br />
 		<select onchange="submit()" name="notification_option" style="margin-top: 3px; margin-left: 0px">
 			<?php //if($query_type != nagstat::FIND_SERVICE){ ?>
-				<option value="<?php echo nagstat::NOTIFICATION_SERVICE_ALL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_ALL ? ' selected' : ''?>><?php echo $this->translate->_('All service notifications');?></option>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_ALL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_ALL ? ' selected' : ''?>><?php echo $this->translate->_('All host notifications');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_SERVICE_ALL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_ALL ? ' selected' : ''?>><?php echo $t->_('All service notifications');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_ALL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_ALL ? ' selected' : ''?>><?php echo $t->_('All host notifications');?></option>
 			<?php //} ?>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_ACK;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_ACK ? ' selected' : ''?>><?php echo $this->translate->_('Service acknowledgements');?></option>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_WARNING;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_WARNING ? ' selected' : ''?>><?php echo $this->translate->_('Service warning');?></option>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_UNKNOWN;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_UNKNOWN ? ' selected' : ''?>><?php echo $this->translate->_('Service unknown');?></option>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_CRITICAL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_CRITICAL ? ' selected' : ''?>><?php echo $this->translate->_('Service critical');?></option>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_RECOVERY;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_RECOVERY ? ' selected' : ''?>><?php echo $this->translate->_('Service recovery');?></option>
-			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_FLAP;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_FLAP ? ' selected' : ''?>><?php echo $this->translate->_('Service flapping');?></option>-->
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_ACK;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_ACK ? ' selected' : ''?>><?php echo $t->_('Service acknowledgements');?></option>
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_WARNING;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_WARNING ? ' selected' : ''?>><?php echo $t->_('Service warning');?></option>
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_UNKNOWN;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_UNKNOWN ? ' selected' : ''?>><?php echo $t->_('Service unknown');?></option>
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_CRITICAL;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_CRITICAL ? ' selected' : ''?>><?php echo $t->_('Service critical');?></option>
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_RECOVERY;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_RECOVERY ? ' selected' : ''?>><?php echo $t->_('Service recovery');?></option>
+			<option value="<?php echo nagstat::NOTIFICATION_SERVICE_FLAP;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_SERVICE_FLAP ? ' selected' : ''?>><?php echo $t->_('Service flapping');?></option>-->
 			<?php //if($query_type != nagstat::FIND_SERVICE){ ?>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_ACK;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_ACK ? ' selected' : ''?>><?php echo $this->translate->_('Host acknowledgements');?></option>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_DOWN;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_DOWN ? ' selected' : ''?>><?php echo $this->translate->_('Host down');?></option>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_UNREACHABLE;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_UNREACHABLE ? ' selected' : ''?>><?php echo $this->translate->_('Host unreachable');?></option>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_RECOVERY;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_RECOVERY ? ' selected' : ''?>><?php echo $this->translate->_('Host recovery');?></option>
-				<option value="<?php echo nagstat::NOTIFICATION_HOST_FLAP;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_FLAP ? ' selected' : ''?>><?php echo $this->translate->_('Host flapping');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_ACK;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_ACK ? ' selected' : ''?>><?php echo $t->_('Host acknowledgements');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_DOWN;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_DOWN ? ' selected' : ''?>><?php echo $t->_('Host down');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_UNREACHABLE;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_UNREACHABLE ? ' selected' : ''?>><?php echo $t->_('Host unreachable');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_RECOVERY;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_RECOVERY ? ' selected' : ''?>><?php echo $t->_('Host recovery');?></option>
+				<option value="<?php echo nagstat::NOTIFICATION_HOST_FLAP;?>"<?php echo  $notification_option == nagstat::NOTIFICATION_HOST_FLAP ? ' selected' : ''?>><?php echo $t->_('Host flapping');?></option>
 			<?php //} ?>
 		</select> &nbsp;
-		<input type="checkbox" name="sort_order" value="desc" /> <?php echo $this->translate->_('Older Entries First');?> &nbsp;
-		<!--<input type="submit" value="<?php echo $this->translate->_('Update');?>" />-->
+		<input type="checkbox" name="sort_order" value="desc" /> <?php echo $t->_('Older Entries First');?> &nbsp;
+		<!--<input type="submit" value="<?php echo $t->_('Update');?>" />-->
 		<br /><br />
 	</form>
 
@@ -34,12 +35,12 @@
 		</caption>
 		<tr>
 			<th class="headerNone">&nbsp;</th>
-			<th class="headerNone"><?php echo $this->translate->_('Host'); ?></th>
-			<th class="headerNone"><?php echo $this->translate->_('Service'); ?></th>
-			<th class="headerNone"><?php echo $this->translate->_('Time'); ?></th>
-			<th class="headerNone"><?php echo $this->translate->_('Contact'); ?></th>
-			<th class="headerNone"><?php echo $this->translate->_('Command'); ?></th>
-			<th class="headerNone"><?php echo $this->translate->_('Information'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Host'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Service'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Time'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Contact'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Command'); ?></th>
+			<th class="headerNone"><?php echo $t->_('Information'); ?></th>
 		</tr>
 		<?php
 			$i = 0;
@@ -52,26 +53,26 @@
 				<?php
 				// services
 				if($row->notification_type == nagstat::NOTIFICATION_SERVICE_CRITICAL)
-					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Critical'), 'title' => $t->_('Critical')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_SERVICE_RECOVERY)
-					echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => $t->_('Recovery'), 'title' => $t->_('Recovery')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_SERVICE_ACK)
-					echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_SERVICE_FLAP)
-					echo html::image($this->add_path('icons/16x16/flapping.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/flapping.png'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_SERVICE_UNKNOWN)
-					echo html::image($this->add_path('icons/16x16/shield-uknown.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-uknown.png'), array('alt' => $t->_('Unknown'), 'title' => $t->_('Unknown')));
 				// hosts
 				elseif($row->notification_type == nagstat::NOTIFICATION_HOST_DOWN)
-					echo html::image($this->add_path('icons/16x16/shield-down.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-down.png'), array('alt' => $t->_('Down'), 'title' => $t->_('Down')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_HOST_UNREACHABLE)
-					echo html::image($this->add_path('icons/16x16/shield-unreachable.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-unreachable.png'), array('alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_HOST_RECOVERY)
-					echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => $t->_('Recovery'), 'title' => $t->_('Recovery')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_HOST_ACK)
-					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
 				elseif($row->notification_type == nagstat::NOTIFICATION_HOST_FLAP)
-					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => 'Critical', 'title' => 'Critical'));
+					echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
 				?>
 			</td>
 			<td><?php echo $row->host_name; ?></td>
@@ -83,7 +84,7 @@
 		</tr>
 		<?php } } else { ?>
 		<tr class="even">
-			<td colspan="7"><?php echo $this->translate->_('No notifications'); ?></td>
+			<td colspan="7"><?php echo $t->_('No notifications'); ?></td>
 		</tr>
 		<?php } ?>
 	</table>
