8b286e33637799a0fbed6a9f6d8cd80374297d5e
Per Asberg <perasb@op5.se>
2010-02-04 17:23:05 +0100

    config/reports: Added config file for reports
    We need to be able to change the sender address in an easy way
    and for now this is the easiest.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

---
+++ application/config/reports.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * @package reports
+ *
+ * Sender of reports
+ */
+$config['from'] = 'Ninja';
+
+$mail_sender_address = '';
+
+if (!empty($mail_sender_address)) {
+	$from_email = $mail_sender_address;
+} else {
+	$hostname = exec('hostname --long');
+	if (empty($hostname) && $hostname != '(none)') {
+		// unable to get a valid hostname
+		$from_email = $config['from'] . '@localhost';
+	} else {
+		$from_email = $config['from'] . '@'.$hostname;
+	}
+}
+
+$config['from_email'] = $from_email;
+
