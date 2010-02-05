62f432e9ce7a987420fdb7b64fb46296d1da598c
My Appelgren <my@op5.com>
2010-02-05 14:56:16 +0100

    ninjafied the toplist template for alert summary
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/summary/toplist.php
+++ application/views/themes/default/summary/toplist.php
@@ -1,25 +1,34 @@
-<?php defined('SYSPATH') OR die("No direct access allowed");
+<?php defined('SYSPATH') OR die("No direct access allowed"); ?>
 
-echo "<table><tr><th>$label_rank</th><th>$label_producer_type</th>" .
-	"<th>$label_host</th><th>$label_service</th>" .
-	"<th>$label_total_alerts</th></tr>\n";
+<div class="widget left w98">
+	<table>
+		<tr>
+			<th class="headerNone left"><?php echo $label_rank; ?></th>
+			<th class="headerNone left"><?php echo $label_producer_type; ?></th>
+			<th class="headerNone left"><?php echo $label_host; ?></th>
+			<th class="headerNone left"><?php echo $label_service; ?></th>
+			<th class="headerNone left"><?php echo $label_total_alerts; ?></th>
+		</tr>
+		<?php
+		$i=0;
+		foreach ($result as $rank => $ary) {
+			$i++;
+			echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').'">';
+			if (empty($ary['service_description'])) {
+				$producer = $label_host;
+				$ary['service_description'] = 'N/A';
+			} else {
+				$producer = $label_service;
+			}
+		?>
+			<td><?php echo $rank; ?></td>
+			<td><?php echo $producer; ?></td>
+			<td><?php echo $ary['host_name']; ?></td>
+			<td><?php echo $ary['service_description']; ?></td>
+			<td><?php echo $ary['total_alerts']; ?></td>
+		</tr>
+		<?php } ?>
+	</table>
+</div>
 
-foreach ($result as $rank => $ary) {
-	echo "<tr>\n";
-	if (empty($ary['service_description'])) {
-		$producer = $label_host;
-		$ary['service_description'] = 'N/A';
-	} else {
-		$producer = $label_service;
-	}
-
-	echo "<td>$rank</td>\n";
-	echo "<td>$producer</td>\n";
-	echo "<td>$ary[host_name]</td>\n";
-	echo "<td>$ary[service_description]</td>\n";
-	echo "<td>$ary[total_alerts]</td>\n";
-	echo "</tr>\n\n";
-}
-echo "</table>\n";
-
-printf("Report completed in %.3f seconds<br />\n", $completion_time);
+<?php // printf("Report completed in %.3f seconds<br />\n", $completion_time); ?>
