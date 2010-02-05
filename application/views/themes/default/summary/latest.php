a6790532e2122b502ee4e945dfff9caa8eeebaea
My Appelgren <my@op5.com>
2010-02-05 14:51:15 +0100

    ninjafied the latest-template for alert summary
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/summary/latest.php
+++ application/views/themes/default/summary/latest.php
@@ -1,31 +1,42 @@
-<?php defined('SYSPATH') OR die("No direct access allowed");
+<?php defined('SYSPATH') OR die("No direct access allowed"); ?>
 
-echo "<table><tr><th>$label_time</th><th>$label_alert_type</th>" .
-	"<th>$label_host</th><th>$label_service</th>" .
-	"<th>$label_state</th><th>$label_state_type</th>" .
-	"<th>$label_information</th></tr>\n";
-
-foreach ($result as $ary) {
-	echo "<tr>\n";
-	if (empty($ary['service_description'])) {
-		$alert_type = $label_host_alert;
-		$ary['service_description'] = 'N/A';
-		$state = $host_state_names[$ary['state']];
-	} else {
-		$alert_type = $label_service_alert;
-		$state = $service_state_names[$ary['state']];
-	}
-	$softhard = $ary['hard'] == 1 ? $label_hard : $label_soft;
-
-	echo "<td>" . date("Y-m-d H:i:s", $ary['timestamp']) . "</td>\n";
-	echo "<td>$alert_type</td>\n";
-	echo "<td>$ary[host_name]</td>\n";
-	echo "<td>$ary[service_description]</td>\n";
-	echo "<td>$state</td>\n";
-	echo "<td>$softhard</td>\n";
-	echo "<td>$ary[output]</td>\n";
-	echo "</tr>\n\n";
-}
-echo "</table>\n";
-
-printf("Report completed in %.3f seconds<br />\n", $completion_time);
+<div class="widget w98 left">
+	<table>
+		<tr>
+			<th class="headerNone left"><?php //echo $label_state; ?></th>
+			<th class="headerNone left"><?php echo $label_host; ?></th>
+			<th class="headerNone left"><?php echo $label_service; ?></th>
+			<th class="headerNone left"><?php echo $label_time; ?></th>
+			<th class="headerNone left"><?php echo $label_alert_type; ?></th>
+			<th class="headerNone left"><?php echo $label_state_type; ?></th>
+			<th class="headerNone left"><?php echo $label_information; ?></th>
+		</tr>
+		<?php
+			$i = 0;
+			foreach ($result as $ary) {
+				$i++;
+				echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').'">';
+				if (empty($ary['service_description'])) {
+					$alert_type = $label_host_alert;
+					$ary['service_description'] = 'N/A';
+					$state = $host_state_names[$ary['state']];
+				} else {
+					$alert_type = $label_service_alert;
+					$state = $service_state_names[$ary['state']];
+				}
+				$softhard = $ary['hard'] == 1 ? $label_hard : $label_soft;
+		?>
+			<td class="icon status">
+				<?php echo html::image($this->add_path('icons/16x16/shield-'.strtolower($state).'.png'), array('title' => $state, 'alt' => $state)); ?>
+			</td>
+			<td><?php echo $ary['host_name']; ?></td>
+			<td><?php echo $ary['service_description']; ?></td>
+			<td><?php echo date("Y-m-d H:i:s", $ary['timestamp']); ?></td>
+			<td><?php echo $alert_type; ?></td>
+			<td><?php echo $softhard; ?></td>
+			<td><?php echo $ary['output']; ?></td>
+		</tr>
+		<?php } ?>
+	</table>
+</div>
+<?php // printf("Report completed in %.3f seconds<br />\n", $completion_time); ?>
