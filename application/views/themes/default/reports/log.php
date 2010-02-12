3b1408eec00500d5fc5e82b315bd834dc9ec9dd6
My Appelgren <my@op5.com>
2010-02-12 15:31:32 +0100

    adjusted the report pages so that they look ok as pdf
    they're far from pretty, but threy're better than they
    were before at least. might be able to make em prettier
    if html-head and all that were added. but this have
    to do for now
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/reports/log.php
+++ application/views/themes/default/reports/log.php
@@ -10,43 +10,42 @@ if (!empty($log)) {
 	$service_state_txt 	= array(-1 => 'PENDING', 0 => 'OK', 1 => 'WARNING', 2 => 'CRITICAL', 3 => 'UNKNOWN');
 ?>
 	<div id="log_entries">
-
-		<!--<div class="icon-help" onclick="general_help('log_entries')"></div>-->
-			<!--<form onsubmit="return false;" action=""><div><input type="text" size="60" name="filterbox" id="filterbox" value="Enter text to filter" /></div></form>-->
-			<table id="log-table" border="1">
-				<caption><?php echo ucfirst($type) ?> <?php echo $label_entries ?> <?php echo $source; ?><br /></caption>
-				<thead>
-				<tr>
-					<th class="headerNone left"><?php echo $this->translate->_('Status');?></th>
-					<th class="headerNone left"<?php if (!$create_pdf) { ?> style="width: 110px"<?php } ?>><?php echo $this->translate->_('Start time');?></th>
-					<th class="headerNone left"<?php if (!$create_pdf) { ?> style="width: 110px"<?php } ?>><?php echo $this->translate->_('End time');?></th>
-					<th class="headerNone left"<?php if (!$create_pdf) { ?> style="width: 110px"<?php } ?>><?php echo $this->translate->_('Duration');?></th>
-					<th class="headerNone left"><?php echo $this->translate->_('Log message');?></th>
-				</tr>
-				</thead>
-				<tbody>
-				<?php
-				$i = 0;
-				foreach ($log as $key => $value) {
-					$i++;
-					if (isset($value['state'])) {
-					$event_end_time = $value['the_time'] + $value['duration'];
-				?>
-				<tr class="<?php echo ($i%2 == 0) ? 'odd' : 'even' ?>">
-					<td>
-						<?php
-							echo html::image($this->add_path('icons/12x12/shield-'.strtolower(${$type.'_state_txt'}[$value['state']]).'.png'),
-									 array('alt' => strtolower(${$type.'_state_txt'}[$value['state']]),'title' => strtolower(${$type.'_state_txt'}[$value['state']]),'style' => 'margin-bottom: -1px'));
-							echo '&nbsp;'.ucfirst(strtolower(${$type.'_state_txt'}[$value['state']]));
-						?>
-					</td>
-					<td><?php echo date('Y-m-d, H:i:s', $value['the_time']); ?></td>
-					<td><?php echo date('Y-m-d, H:i:s', $event_end_time); ?></td>
-					<td><?php echo time::to_string($value['duration']); ?></td>
-					<td style="white-space: normal"><?php echo $value['output']; ?></td>
-				</tr>
-				<?php } } ?>
-				</tbody>
-			</table>
+		<!--<form onsubmit="return false;" action=""><div><input type="text" size="60" name="filterbox" id="filterbox" value="Enter text to filter" /></div></form>-->
+		<table id="log-table" <?php echo ($create_pdf) ? 'style="border: 1px solid #cdcdcd" cellpadding="5"' : '';?>>
+			<caption><?php echo ucfirst($type) ?> <?php echo $label_entries ?> <?php echo $source; ?><br /></caption>
+			<thead>
+			<tr>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left"';?>><?php echo $this->translate->_('Status');?></th>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left" style="width: 110px"';?>><?php echo $this->translate->_('Start time');?></th>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left" style="width: 110px"';?>><?php echo $this->translate->_('End time');?></th>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left" style="width: 110px"';?>><?php echo $this->translate->_('Duration');?></th>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left"';?>><?php echo $this->translate->_('Log message');?></th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php //
+			$i = 0;
+			foreach ($log as $key => $value) {
+				$i++;
+				if (isset($value['state'])) {
+				$event_end_time = $value['the_time'] + $value['duration'];
+			?>
+			<?php $bg_color = ($i%2 == 0) ? '#fafafa' : '#f2f2f2'; ?>
+			<tr class="<?php echo ($i%2 == 0) ? 'odd' : 'even' ?>">
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>>
+					<?php
+						echo html::image($this->add_path('icons/12x12/shield-'.strtolower(${$type.'_state_txt'}[$value['state']]).'.png'),
+								 array('alt' => strtolower(${$type.'_state_txt'}[$value['state']]),'title' => strtolower(${$type.'_state_txt'}[$value['state']]),'style' => 'margin-bottom: -1px'));
+						echo '&nbsp;'.ucfirst(strtolower(${$type.'_state_txt'}[$value['state']]));
+					?>
+				</td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo date('Y-m-d, H:i:s', $value['the_time']); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo date('Y-m-d, H:i:s', $event_end_time); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo time::to_string($value['duration']); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : 'style="white-space: normal"'; ?>><?php echo $value['output']; ?></td>
+			</tr>
+			<?php } } ?>
+			</tbody>
+		</table>
 	</div>
 <?php } ?>
--- application/views/themes/default/reports/multiple_host_states.php
+++ application/views/themes/default/reports/multiple_host_states.php
@@ -2,58 +2,56 @@
 <?php $t = $this->translate; ?>
 <div class="host_breakdown wide" style="margin-top: 15px;">
 <?php foreach ($multiple_states as $data) { ?>
-		<table summary="<?php echo $t->_('Host state breakdown') ?>" id="multiple_hosts" border="1">
+		<table summary="<?php echo $t->_('Host state breakdown') ?>" id="multiple_hosts"<?php echo ($create_pdf) ? 'style="border: 1px solid #cdcdcd" cellpadding="5"' : '';?>>
 			<tr>
-				<th class="headerNone left" style="<?php echo (!$create_pdf) ? 'width: 90%;' : ''; ?>"><?php echo (!empty($data['groupname']) ? str_replace('Hostgroup:','',$data['groupname']) : 'Selected hosts'); ?></th>
-				<th class="headerNone"><?php echo $t->_('Up') ?></th>
-				<th class="headerNone"><?php echo $t->_('Unreachable') ?></th>
-				<th class="headerNone"><?php echo $t->_('Down') ?></th>
-				<th class="headerNone"><?php echo $t->_('Undetermined') ?></th>
+				<th <?php echo ($create_pdf) ? 'style="background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone left" style="width: 90%"';?>><?php echo (!empty($data['groupname']) ? str_replace('Hostgroup:','',$data['groupname']) : 'Selected hosts'); ?></th>
+				<th <?php echo ($create_pdf) ? 'style="text-align: right; background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone"';?>><?php echo $t->_('Up') ?></th>
+				<th <?php echo ($create_pdf) ? 'style="text-align: right; background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone"';?>><?php echo $t->_('Unreachable') ?></th>
+				<th <?php echo ($create_pdf) ? 'style="text-align: right; background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone"';?>><?php echo $t->_('Down') ?></th>
+				<th <?php echo ($create_pdf) ? 'style="text-align: right; background-color: #e2e2e2; font-size: 0.9em"' : 'class="headerNone"';?>><?php echo $t->_('Undetermined') ?></th>
 			</tr>
 			<?php for ($i=0;$i<$data['nr_of_items'];$i++): ?>
+			<?php $bg_color = ($i%2 == 0) ? '#fafafa' : '#f2f2f2'; ?>
 			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
 			<?php if (!$use_alias) { ?>
-				<td><?php echo $create_pdf != false ? wordwrap($data['HOST_NAME'][$i],30,'<br />',true) : '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo $create_pdf != false ? wordwrap($data['HOST_NAME'][$i],30,'<br />',true) : '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?></td>
 				<?php } else { ?>
-				<td><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo $create_pdf != false ? wordwrap($data['HOST_NAME'][$i],30,'<br />',true) :'<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?>)</td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo $create_pdf != false ? wordwrap($data['HOST_NAME'][$i],30,'<br />',true) :'<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . wordwrap($data['HOST_NAME'][$i],30,'<br />',true) . '</a>' ?>)</td>
 				<?php } ?>
-				<td class="data">
-					<?php echo reports::format_report_value($data['up'][$i]) ?> %
-					<?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['up'][$i]) > 0 ? '' : 'not-').'up.png'),
-											array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 12px')); ?>
-				</td>
-				<td class="data">
-					<?php echo reports::format_report_value($data['unreachable'][$i]) ?> %
-					<?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['unreachable'][$i]) > 0 ? '' : 'not-').'unreachable.png'),
-										array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo reports::format_report_value($data['down'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['down'][$i]) > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo reports::format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo reports::format_report_value($data['up'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['up'][$i]) > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 11px')); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo reports::format_report_value($data['unreachable'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['unreachable'][$i]) > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo reports::format_report_value($data['down'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['down'][$i]) > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo reports::format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 11px')) ?></td>
 			</tr>
 			<?php endfor; if ($use_average==0): ?>
+			<?php $bg_color = ($i%2 == 0) ? '#fafafa' : '#f2f2f2'; ?>
 			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'; $i++?>">
-				<td><?php echo $t->_('Average'); ?></td>
-				<td class="data"><?php echo $data['average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_up'] > 0 ? '' : 'not-').'up.png'),
-							array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
-							array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_down'] > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo $t->_('Average'); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_up'] > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_down'] > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 11px')) ?></td>
 			</tr>
 			<?php endif; ?>
+			<?php $bg_color = ($i%2 == 0) ? '#fafafa' : '#f2f2f2'; ?>
 			<tr class="group-average <?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
-				<td><?php echo ($use_average==0) ? $t->_('Group availability (SLA)') : $t->_('Average'); ?></td>
-				<td class="data"><?php echo $data['group_average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_up'] > 0 ? '' : 'not-').'up.png'),
-							array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['group_average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
-							array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['group_average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_down'] > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo $data['group_average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; background-color: '.$bg_color.'"' : ''; ?>><?php echo ($use_average==0) ? $t->_('Group availability (SLA)') : $t->_('Average'); ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['group_average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_up'] > 0 ? '' : 'not-').'up.png'),
+							array( 'alt' => $t->_('Up'), 'title' => $t->_('Up'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['group_average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+							array( 'alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['group_average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_down'] > 0 ? '' : 'not-').'down.png'),
+							array( 'alt' => $t->_('Down'), 'title' => $t->_('Down'), 'style' => 'height: 12px; width: 11px')) ?></td>
+				<td <?php echo ($create_pdf) ? 'style="font-size: 0.9em; text-align: right; background-color: '.$bg_color.'"' : 'class="data"'; ?>><?php echo $data['group_average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+							array( 'alt' => $t->_('Undetermined'), 'title' => $t->_('Undetermined'), 'style' => 'height: 12px; width: 11px')) ?></td>
 			</tr>
 			<?php if (!$create_pdf) { ?>
 			<tr id="pdf-hide">
