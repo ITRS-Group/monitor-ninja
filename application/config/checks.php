a02dc3fddded41a2141bafb3ff34ecd718ad2787
Per Asberg <perasb@op5.se>
2010-02-15 12:09:45 +0100

    config/checks.php: Clarified the use of the show_passive_as_active flag
    Added a note about that users have to logout and login again for changes
    to this variable will have any effect (since it is saved as session variable).
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/config/checks.php
+++ application/config/checks.php
@@ -7,6 +7,9 @@
 * By settings this flag to true, all passive checks will be treated
 * as active in tactical overview.
 *
+* Please note that this value is cached as a session variable so
+* you need to logout and login again for changes to take effect
+*
 * Default value: false
 */
 $config['show_passive_as_active'] = false;
