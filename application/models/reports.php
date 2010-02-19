fa8cbad7572392939755e2e7b0d5e9d2594c7cd8
Andreas Ericsson <ae@op5.se>
2010-02-19 13:30:09 +0100

    models/reports: Fix typo in database error reporting
    s/db/this->db/
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/models/reports.php
+++ application/models/reports.php
@@ -2996,7 +2996,7 @@ class Reports_Model extends Model
 
 		$dbr = $this->db->query($query);
 		if (!is_object($dbr)) {
-			echo Kohana::debug($db->errorinfo(), $query);
+			echo Kohana::debug($this->db->errorinfo(), $query);
 			die;
 		}
 
