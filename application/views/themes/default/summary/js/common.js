1730ee9027ed32a4dde04d3dab26991721fdc9c7
Per Asberg <perasb@op5.se>
2010-02-19 10:04:26 +0100

    summary/common.js: Validate entered time values
    Since it's possibly to override the timepicker plugin and enter time values
    manually, we need to validate that we don't pass bogus values to reports
    model.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/summary/js/common.js
+++ application/views/themes/default/summary/js/common.js
@@ -412,13 +412,15 @@ function check_form_values()
 	var rpt_type = $("#report_type").val();
 	if ($("#report_period").val() == 'custom') {
 		// date validation
+		var cur_startdate = Date.fromString($("#cal_start").attr('value'));
+		var cur_enddate = Date.fromString($("#cal_end").attr('value'));
 		var now = new Date();
-		if (!startDate || !endDate) {
-			if (!startDate) {
+		if (!startDate || !endDate || !cur_startdate || !cur_enddate) {
+			if (!startDate || !cur_startdate) {
 				errors++;
 				err_str += "<li>" + _reports_invalid_startdate + ".</li>";
 			}
-			if (!endDate) {
+			if (!endDate || !cur_enddate) {
 				errors++;
 				err_str += "<li>" + _reports_invalid_enddate + ".</li>";
 			}
@@ -431,11 +433,19 @@ function check_form_values()
 				}
 			}
 		}
-	}
 
-	if ($("#" + field_obj.map[rpt_type]).is('select') && $("#" + field_obj.map[rpt_type] + ' option').length == 0) {
-		errors++;
-		err_str += "<li>" + _reports_err_str_noobjects + ".</li>";
+		// time validation: _time_error and _time_error_start
+		if (_time_error || _time_error_start) {
+			errors++;
+			err_str += "<li>" + _reports_invalid_timevalue + ".</li>";
+		}
+
+		// date and time seems OK so let's add time to date field
+		var curval_starttime = $("#time_start").attr('value');
+		$("#start_time").attr('value', startDate + ' ' + curval_starttime);
+
+		var curval_endtime = $("#time_end").attr('value');
+		$("#end_time").attr('value', endDate + ' ' + curval_endtime);
 	}
 
 	/**
