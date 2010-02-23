5a27c30acd6bad32bab954032f81e712ae5a10fe
My Appelgren <my@op5.com>
2010-02-23 15:03:07 +0100

    ninjafied the alert summary totals for services
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/summary/alert_totals_service.php
+++ application/views/themes/default/summary/alert_totals_service.php
@@ -1,12 +1,17 @@
-<?php defined('SYSPATH') OR die("No direct access allowed");
+<?php defined('SYSPATH') OR die("No direct access allowed");?>
 
-echo "<br />" . $label_overall_totals . "<br />\n";
-foreach ($result as $service_name => $ary) {
-	$foo = explode(';');
-	$host_name = $foo[0];
-	$service = $foo[1];
-	echo $label_service . "'" . $service . "' on " .
-		$label_host . "'" . $host_name . "'<br />\n";
-	$this->_print_alert_totals_table($label_service_alerts, $ary['service'], $service_state_names, $ary['service_totals']);
-}
-printf("Report completed in %.3f seconds<br />\n", $completion_time);
+<div style="margin: 0px 1%; border: 1px solid white">
+	<h2 style="margin-top: 11px; margin-bottom: 0px"><?php echo $label_overall_totals ?></h2>
+
+		<?php
+		foreach ($result as $service_name => $ary) {
+			$foo = explode(';');
+			$host_name = $foo[0];
+			$service = $foo[1];
+			//echo $label_service . "'" . $service . "' on " .$label_host . "'" . $host_name . "'<br />\n";
+			$name = $service .' on '.$label_host.': '.$host_name;
+			$this->_print_alert_totals_table($label_service_alerts, $ary['service'], $service_state_names, $ary['service_totals'], $name.);
+		}
+		//printf("Report completed in %.3f seconds<br />\n", $completion_time);
+		?>
+</div>
