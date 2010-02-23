5a2752aa92ac12b6e95ec501ba6c0a7bc34f8777
My Appelgren <my@op5.com>
2010-02-23 16:13:34 +0100

    updated notifications template to display correct states
    and reason types. notification_command is still missing and the sorting
    selectbox is probably not quite right..
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/views/themes/default/notifications/index.php
+++ application/views/themes/default/notifications/index.php
@@ -32,7 +32,7 @@
 			<span style="float: left; display: block; margin-top: 2px;"><?php echo (isset($label_title)) ? $label_title : $t->_('Notifications'); ?></span>
 		</caption>
 		<tr>
-			<th class="headerNone">&nbsp;</th>
+			<th class="headerNone"><?php echo $t->_('&nbsp;'); ?></th>
 			<th class="headerNone"><?php echo $t->_('Host'); ?></th>
 			<th class="headerNone"><?php echo $t->_('Service'); ?></th>
 			<th class="headerNone"><?php echo $t->_('Time'); ?></th>
@@ -49,52 +49,43 @@
 		<tr class="<?php echo $i%2 == 0 ? 'odd' : 'even'; ?>">
 			<td class="icon status">
 				<?php
-				//echo $row->state;
 				if($row->notification_type == nagstat::SERVICE_NOTIFICATION) {
-					if($row->state == nagstat::SERVICE_CRITICAL)
-						echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Critical'), 'title' => $t->_('Critical')));
-					elseif($row->state == nagstat::SERVICE_RECOVERY)
+					// state
+					if($row->state == nagstat::NOTIFICATION_SERVICE_RECOVERY)
 						echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => $t->_('Recovery'), 'title' => $t->_('Recovery')));
-					elseif($row->state == nagstat::SERVICE_WARNING)
+					elseif($row->state == nagstat::NOTIFICATION_SERVICE_CRITICAL)
+						echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Critical'), 'title' => $t->_('Critical')));
+					elseif($row->state == nagstat::NOTIFICATION_SERVICE_WARNING)
 						echo html::image($this->add_path('icons/16x16/shield-warning.png'), array('alt' => $t->_('Warning'), 'title' => $t->_('Warning')));
-					elseif($row->state == nagstat::NOTIFICATION_SERVICE_ACK)
-						echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
-					elseif($row->state == nagstat::NOTIFICATION_SERVICE_FLAP)
-						echo html::image($this->add_path('icons/16x16/flapping.png'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
 					elseif($row->state == nagstat::NOTIFICATION_SERVICE_UNKNOWN)
 						echo html::image($this->add_path('icons/16x16/shield-uknown.png'), array('alt' => $t->_('Unknown'), 'title' => $t->_('Unknown')));
+					// reason type
+					if($row->reason_type == nagstat::NOTIFICATION_SERVICE_ACK)
+						echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
+					elseif($row->reason_type == nagstat::NOTIFICATION_SERVICE_FLAP)
+						echo html::image($this->add_path('icons/16x16/flapping.gif'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
 				}
 				elseif($row->notification_type == nagstat::HOST_NOTIFICATION) {
-					if($row->state == nagstat::HOST_DOWN)
+					// state
+					if($row->state == nagstat::NOTIFICATION_HOST_DOWN)
 						echo html::image($this->add_path('icons/16x16/shield-down.png'), array('alt' => $t->_('Down'), 'title' => $t->_('Down')));
-					elseif($row->state == nagstat::HOST_UNREACHABLE)
+					elseif($row->state == nagstat::NOTIFICATION_HOST_UNREACHABLE)
 						echo html::image($this->add_path('icons/16x16/shield-unreachable.png'), array('alt' => $t->_('Unreachable'), 'title' => $t->_('Unreachable')));
 					elseif($row->state == nagstat::NOTIFICATION_HOST_RECOVERY)
 						echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => $t->_('Recovery'), 'title' => $t->_('Recovery')));
-					elseif($row->state == nagstat::NOTIFICATION_HOST_ACK)
-						echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
-					elseif($row->state == nagstat::NOTIFICATION_HOST_FLAP)
-						echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
+					// reason type
+					if($row->reason_type == nagstat::NOTIFICATION_HOST_ACK)
+						echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
+					elseif($row->reason_type == nagstat::NOTIFICATION_HOST_FLAP)
+						echo html::image($this->add_path('icons/16x16/flapping.gif'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));
 				}
-				/*if($row->state == 1)
-						echo html::image($this->add_path('icons/16x16/shield-warning.png'), array('alt' => $t->_('Warning'), 'title' => $t->_('Warning')));
-				elseif($row->state == 2)
-						echo html::image($this->add_path('icons/16x16/shield-critical.png'), array('alt' => $t->_('Critical'), 'title' => $t->_('Critical')));
-				elseif($row->state == 3)
-					echo html::image($this->add_path('icons/16x16/shield-unknown.png'), array('alt' => $t->_('Unknown'), 'title' => $t->_('Unknown')));
-				elseif($row->state == 4)
-					echo html::image($this->add_path('icons/16x16/shield-ok.png'), array('alt' => $t->_('Recovery'), 'title' => $t->_('Recovery')));
-				elseif($row->state == 8) // ?
-					echo html::image($this->add_path('icons/16x16/acknowledged.png'), array('alt' => $t->_('Acknowledged'), 'title' => $t->_('Acknowledged')));
-				elseif($row->state == 16) //?
-					echo html::image($this->add_path('icons/16x16/flapping.png'), array('alt' => $t->_('Flapping'), 'title' => $t->_('Flapping')));*/
 				?>
 			</td>
 			<td><?php echo html::anchor('extinfo/details/host/'.$row->host_name, $row->host_name); ?></td>
 			<td><?php echo html::anchor('extinfo/details/service/'.$row->host_name.'/?service='.$row->service_description, $row->service_description); ?></td>
 			<td><?php echo date('Y-m-d H:i:s',$row->start_time); ?></td>
-			<td><?php echo html::anchor('config/contacts/'.$row->contact_name, !empty($row->contact_name) ? $row->contact_name: ''); ?></td>
-			<td><?php echo html::anchor('config/commands/'.$row->reason_type, $row->reason_type); ?></td>
+			<td><?php echo html::anchor('config?type=contacts#config'.$row->contact_name, !empty($row->contact_name) ? $row->contact_name: ''); ?></td>
+			<td><?php //echo html::anchor('config?type=commands#'.$row->notification_command, $row->notification_command); ?>&nbsp;</td>
 			<td><?php echo $row->output; ?></td>
 		</tr>
 		<?php } } else { ?>
