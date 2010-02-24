1bb5c1dcf05c4438740d8a9764157c61aacc08da
My Appelgren <my@op5.com>
2010-02-24 14:50:34 +0100

    added histogram to the ninja menu
    Signed-off-by: My Appelgren <my@op5.com>

--- application/controllers/ninja.php
+++ application/controllers/ninja.php
@@ -100,6 +100,7 @@ class Ninja_Controller extends Template_Controller {
 			$this->translate->_('Reporting') => array(
 				$this->translate->_('Trends') 								=> array('trends', 'trends'),
 				$this->translate->_('Schedule reports') 			=> array('reports?show_schedules', 'schedulereports'),
+				$this->translate->_('Histogram') 							=> array('histogram', 'histogram'),
 				$this->translate->_('Alert history') 					=> array('showlog/showlog', 'alerthistory'),
 				$this->translate->_('Alert summary') 					=> array('summary', 'alertsummary'),
 				$this->translate->_('Notifications') 					=> array('notifications', 'notifications'),
