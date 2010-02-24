ba07377bf0b3d0f4ee914ecd54a6f1a64d286457
Per Asberg <perasb@op5.se>
2010-02-24 18:35:22 +0100

    models/downtime: Modified get_downtime_data() to handle filters and sorting
    Added sorting and filtering functionality to downtime model to make it
    possible to filter out host- or service downtime data.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/models/downtime.php
+++ application/models/downtime.php
@@ -9,27 +9,28 @@ class Downtime_Model extends Model
 	/**
 	*	Fetch current downtime information
 	*/
-	public function get_downtime_data()
+	public function get_downtime_data($order_by='downtime_id', $filter=3)
 	{
 		$db = new Database();
 		$auth = new Nagios_auth_Model();
 		$host_query = $auth->authorized_host_query();
 		if ($host_query === true) {
 			# don't use auth_host fields etc
-			$sql = "SELECT d.* FROM scheduled_downtime AS d";
+			$sql = "SELECT d.* FROM scheduled_downtime AS d WHERE d.downtime_type & " . $filter;
 		} else {
 			# hosts
 			$auth_host_alias = $host_query['host_field'];
 			$auth_from = ' ,'.$host_query['from'];
 			$auth_where = ' WHERE '.sprintf($host_query['where'], "d.host_name");
-			$sql = "SELECT d.* FROM scheduled_downtime AS d ".$auth_from.$auth_where;
+			$sql = "SELECT d.* FROM scheduled_downtime AS d ".$auth_from.$auth_where." AND d.downtime_type & " . $filter;
 
 			$query_contact = "SELECT d.* FROM scheduled_downtime AS d, host, ".
 			"contact, host_contact ".
 			"WHERE host.id = host_contact.host ".
 			"AND host_contact.contact=contact.id ".
 			"AND contact.contact_name=".$db->escape(Auth::instance()->get_user()->username).
-			" AND d.host_name=host.host_name";
+			" AND d.host_name=host.host_name ".
+			"AND d.downtime_type & " . $filter;
 
 			# services
 			$query_svc =
@@ -37,18 +38,18 @@ class Downtime_Model extends Model
 				'WHERE service.id = service_contactgroup.service ' .
 				'AND service_contactgroup.contactgroup = contact_contactgroup.contactgroup ' .
 				'AND contact_contactgroup.contact = ' . (int)$auth->id." AND host.host_name=service.host_name ".
-				"AND d.host_name=service.host_name AND d.service_description=service.service_description";
+				"AND d.host_name=service.host_name AND d.service_description=service.service_description AND d.downtime_type & " . $filter;
 
 			# contact <-> service_contact relation
 			$query_svc_contact = "SELECT d.* FROM scheduled_downtime AS d, host h, service s, contact c, service_contact sc ".
 				"WHERE s.id=sc.service AND c.id=sc.contact ".
 				"AND sc.contact=c.id ".
 				"AND c.contact_name=".$db->escape(Auth::instance()->get_user()->username).
-				" AND h.host_name=s.host_name AND d.host_name=s.host_name AND d.service_description=s.service_description";
+				" AND h.host_name=s.host_name AND d.host_name=s.host_name AND d.service_description=s.service_description AND d.downtime_type &" . $filter;
 
 			$sql = '(' . $sql . ') UNION (' . $query_contact . ') UNION (' . $query_svc . ') UNION (' . $query_svc_contact . ')';
 		}
-		$sql .= " ORDER BY downtime_id";
+		$sql .= " ORDER BY ".$order_by;
 
 		$result = $db->query($sql);
 		return $result->count() ? $result: false;
