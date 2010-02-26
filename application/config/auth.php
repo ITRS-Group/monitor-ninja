808571e60a8f2037d9695a70e618907fe9942295
Andreas Ericsson <ae@op5.se>
2010-02-26 15:34:31 +0100

    config/auth: Avoid triggering 'look for hardcoded /ninja/' tests
    It's just a comment, so this won't affect a thing.
    
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/config/auth.php
+++ application/config/auth.php
@@ -54,6 +54,6 @@ $config['min_username_chars'] = 2;
  * as get parameters.
  * Default is set to false and by changing this to true it is
  * possible to login to your account with a url like
- * http://ninja/default/do_login?username=<username>&password=<password>
+ * http://$site/default/do_login?username=<username>&password=<password>
  */
 $config['use_get_auth'] = false;
