7ba15daa28b1ae8dc0b53c6961740ec6113ab1c4
Andreas Ericsson <ae@op5.se>
2010-03-01 11:50:36 +0100

    alert_totals_service template: Fix a syntax error
    Signed-off-by: Andreas Ericsson <ae@op5.se>

--- application/views/themes/default/summary/alert_totals_service.php
+++ application/views/themes/default/summary/alert_totals_service.php
@@ -10,7 +10,7 @@
 			$service = $foo[1];
 			//echo $label_service . "'" . $service . "' on " .$label_host . "'" . $host_name . "'<br />\n";
 			$name = $service .' on '.$label_host.': '.$host_name;
-			$this->_print_alert_totals_table($label_service_alerts, $ary['service'], $service_state_names, $ary['service_totals'], $name.);
+			$this->_print_alert_totals_table($label_service_alerts, $ary['service'], $service_state_names, $ary['service_totals'], $name);
 		}
 		//printf("Report completed in %.3f seconds<br />\n", $completion_time);
 		?>
