28bdf758a5dff39ad808d72c6943fc996a91bbb7
My Appelgren <my@op5.com>
2010-03-04 09:52:19 +0100

    tac: if user has no rights, the outages widget looked weird
    there was only a textstring prompted, made it look like the
    other widgets, ie a grey icon and a N/A text..
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_outages/view.php
+++ application/widgets/netw_outages/view.php
@@ -13,9 +13,14 @@
 	</div>
 	<div class="widget-content">
 <?php }
-		if (!$user_has_access) {
-			echo $no_access_msg;
-	 	} else { ?>
+		if (!$user_has_access) { ?>
+		<table class="w-table">
+			<tr>
+				<td class="dark"><?php echo html::image($this->add_path('icons/16x16/shield-not-critical.png'), array('alt' => $label)) ?></td>
+				<td><?php echo $no_access_msg; ?></td>
+			</tr>
+		</table>
+	 	<?php	 } else { ?>
 		<table class="w-table">
 			<?php if ($total_blocking_outages > 0) { ?>
 			<tr>
