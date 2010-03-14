8d0e264b2adcaecd0584a9b967f9132c7f4cf7c6
Per Asberg <perasb@op5.se>
2010-03-14 17:13:34 +0100

    reports/common.js: Initialize datepicker properly with dates
    We need to make sure that datepicker is properly initialized when we already
    have start- and end_dates.
    
    This fixes issue #3127
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/reports/js/common.js
+++ application/views/themes/default/reports/js/common.js
@@ -73,6 +73,15 @@ function fancybox_datepicker()
 	var datepicker_enddate = new Date().addDays(1).asString();
 	$('.date-pick').datePicker({clickInput:true, startDate:_start_date, endDate:datepicker_enddate});
 
+	if ($('#fancy_content #cal_start').attr('value')) {
+		var ds = Date.fromString($('#fancy_content #cal_start').attr('value'));
+		$('#fancy_content #cal_end').dpSetStartDate(ds.addDays(1).asString());
+	}
+	if ($('#fancy_content #cal_end').attr('value')) {
+		var ds = Date.fromString($('#fancy_content #cal_end').attr('value'));
+		$('#fancy_content #cal_start').dpSetEndDate(ds.addDays(1).asString());
+	}
+
 	$('.datepick-start').bind(
 		'dpClosed',
 		function(e, selectedDates)
@@ -81,12 +90,13 @@ function fancybox_datepicker()
 			if (d) {
 				d = new Date(d);
 				startDate = d.asString();
-				$('#start_time').attr('value', d.asString());
+				$('#fancy_content #start_time').attr('value', d.asString());
 				$("input[name=start_time]").attr('value', d.asString());
-				$('#cal_end').dpSetStartDate(d.addDays(1).asString());
+				$('#fancy_content #cal_end').dpSetStartDate(d.addDays(1).asString());
 			}
 		}
 	);
+
 	$('.datepick-end').bind(
 		'dpClosed',
 		function(e, selectedDates)
@@ -94,12 +104,10 @@ function fancybox_datepicker()
 			var d = selectedDates[0];
 			if (d) {
 				d = new Date(d);
-				$('#cal_start').dpSetEndDate(d.addDays(-1).asString());
-				//console.log(d.addDays(1).asString());
-				$('#end_time').attr('value', d.asString());
-				$("input[name=end_time]").attr('value', d.addDays(1).asString());
 				endDate = d.asString();
-				//console.log( Math.round(d.getTime()/1000) ); // working valid timestamp
+				$('#fancy_content #end_time').attr('value', d.asString());
+				$("input[name=end_time]").attr('value', d.addDays(1).asString());
+				$('#fancy_content #cal_start').dpSetEndDate(d.addDays(-1).asString());
 			}
 		}
 	);
