b6c2f502c9261022d69de5c5b61671fb776a1610
Roman Kyrylych <rkyrylych@n-ix.com.ua>
2010-03-15 13:35:48 +0200

    Fix invalid HTML that made autobackup after verification impossible in IE
    Signed-off-by: Roman Kyrylych <rkyrylych@n-ix.com.ua>

--- application/views/themes/default/backup/verify.php
+++ application/views/themes/default/backup/verify.php
@@ -1,3 +1,3 @@
-<span class="<?php echo $status ? 'ok' : 'error'; ?>"><?php echo $message;
-if (!$status) { ?></span> &nbsp;&nbsp;&nbsp; <a href="#" onclick="backup(); return false;">Backup anyway</a><?php } ?>
+<span class="<?php echo $status ? 'ok' : 'error'; ?>"><?php echo $message; ?></span>
+<?php if (!$status): ?> &nbsp;&nbsp;&nbsp; <a href="#" onclick="backup(); return false;">Backup anyway</a><?php endif; ?>
 <span><a id="backup" href="<?php echo url::base() . 'index.php/backup/backup/'; ?>" class="hidden">Backup now</a></span>
