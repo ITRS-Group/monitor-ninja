0b7cf90a24786d71daf6d9dc7dc1c82481795244
My Appelgren <my@op5.com>
2010-03-19 10:28:57 +0100

    removed histogram from the main menu in ninja
    Signed-off-by: My Appelgren <my@op5.com>

--- application/controllers/ninja.php
+++ application/controllers/ninja.php
@@ -99,7 +99,7 @@ class Ninja_Controller extends Template_Controller {
 			),
 			$this->translate->_('Reporting') => array(
 				$this->translate->_('Trends') 								=> array('trends', 'trends'),
-				$this->translate->_('Histogram') 							=> array('histogram', 'histogram'),
+				//$this->translate->_('Histogram') 							=> array('histogram', 'histogram'),
 				$this->translate->_('Alert history') 					=> array('showlog/alert_history', 'alerthistory'),
 				$this->translate->_('Alert summary') 					=> array('summary', 'alertsummary'),
 				$this->translate->_('Notifications') 					=> array('notifications', 'notifications'),
