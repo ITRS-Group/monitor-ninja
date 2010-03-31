c8175540310eb4936ade33eaed7827deae03d07f
Per Asberg <perasb@op5.se>
2010-03-31 10:51:43 +0200

    models/program_status: Added method last_alive()
    Simple method for fetching last_alive from program status table.
    This will be used to detect if merlin is still alive populating the
    database so we know that users can trust the GUI.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/models/program_status.php
+++ application/models/program_status.php
@@ -16,4 +16,15 @@ class Program_status_Model extends Model
 		return (!$res || count($res) == 0) ? false : $res;
 	}
 
+	/**
+	*	Check last_alive from program_status
+	*	to detect when data was updated
+	*/
+	public function last_alive()
+	{
+		$db = new Database();
+		$sql = "SELECT last_alive FROM program_status";
+		$res = $db->query($sql);
+		return (!$res || count($res) == 0) ? false : $res->current()->last_alive;
+	}
 }
