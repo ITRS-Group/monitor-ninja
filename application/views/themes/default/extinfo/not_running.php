de8b71fd6fd042d728a8ab3ca2c2fb2e63b3e2b2
Per Asberg <perasb@op5.se>
2010-03-31 10:57:03 +0200

    views/extinfo/not_runnning: Add some space around output
    The output was too close to surrounding page elements.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/extinfo/not_running.php
+++ application/views/themes/default/extinfo/not_running.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<div>
-	<?php echo $info_message ?><br />
+<div style="padding:13px">
+	&nbsp;<?php echo $info_message ?><br />
 	<?php echo isset($info_message_extra) ? $info_message_extra : '' ?>
 </div>
