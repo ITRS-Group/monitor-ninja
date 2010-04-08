83ea036cc5084f603d465354773a8e7ca81c3175
Per Asberg <perasb@op5.se>
2010-04-08 11:47:43 +0200

    Upgrade monitor_reports tables properly
    Since we had some clashes between old reports GUI db upgrade scripts
    and the new versions in ninja reports, the upgrade script needs to
    increment the db_versions to v6 for both sla and abail to add the
    necessary fields.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- op5-upgradescripts/avail_v5_to_v6.sql
+++ op5-upgradescripts/avail_v5_to_v6.sql
@@ -3,3 +3,4 @@
 ALTER TABLE avail_config COLLATE latin1_general_cs;
 ALTER TABLE avail_config_objects COLLATE latin1_general_cs;
 ALTER TABLE avail_config ADD cluster_mode INT NOT NULL DEFAULT 0;
+UPDATE avail_db_version SET version = 6;
--- op5-upgradescripts/sla_v5_to_v6.sql
+++ op5-upgradescripts/sla_v5_to_v6.sql
@@ -3,3 +3,4 @@
 ALTER TABLE sla_config COLLATE latin1_general_cs;
 ALTER TABLE sla_config_objects COLLATE latin1_general_cs;
 ALTER TABLE sla_config ADD cluster_mode INT NOT NULL DEFAULT 0;
+UPDATE sla_db_version SET version = 6;
