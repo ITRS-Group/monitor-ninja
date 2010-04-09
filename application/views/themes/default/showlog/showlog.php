cb0c7ce79157a7c494770471b225935266e3bb79
Per Asberg <perasb@op5.se>
2010-04-09 10:44:17 +0200

    views/showlog: Pass form to correct controller method
    Since this template is used both for alert_history and event log and these
    pages uses different controller methods to render it's data, we have to
    change the action target for the form depending on what we are showing.
    
    Partial fix for issue #3373
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/showlog/showlog.php
+++ application/views/themes/default/showlog/showlog.php
@@ -2,7 +2,7 @@
 	$date_format = cal::get_calendar_format(true);
 	$x = $this->translate; ?>
 <div id="response"></div>
-<?php echo form::open('showlog/showlog', array('id' => 'summary_form')); ?>
+<?php echo form::open('showlog/'.Router::$method, array('id' => 'summary_form')); ?>
 <div class="widget left w98">
 	<table class="showlog">
 		<tr>
