bf363af0709589d90a3740ec8e030af9c33f7b1f
My Appelgren <my@op5.com>
2010-06-01 10:07:40 +0200

    configuration.php - added variable nacoma
    in the main template there is a check if this is set to true,
    if so - assing class ie7conf - to adjust the margins.
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/controllers/configuration.php
+++ application/controllers/configuration.php
@@ -67,6 +67,7 @@ class Configuration_Controller extends Authenticated_Controller {
 		$this->template->disable_refresh = true;
 		$this->template->content = '<iframe src="'.Kohana::config('config.nacoma_path').'/'.$target_link.'" style="width: 100%; height: 768px" frameborder="0" id="iframe"></iframe>';
 		$this->template->title = $this->translate->_('Configuration » Configure');
+		$this->template->nacoma = true;
 		$this->template->js_header = $this->add_view('js_header');
 		$this->xtra_js = array($this->add_path('/js/iframe-adjust.js'));
 		$this->template->js_header->js = $this->xtra_js;
