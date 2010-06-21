0cae9ea7b093f1b66d0942e53f4248a434576eef
Per Asberg <perasb@op5.se>
2010-06-21 11:27:39 +0200

    helpers/sql.php: Added sql help class
    We need an easy way of transforming some of our
    sql calls to be more ANSI SQL and this class will
    make these things easier.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

---
+++ application/helpers/sql.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+* SQL Help class
+*
+* @package NINJA
+* @author op5 AB
+* @license GPL
+*/
+class sql_Core
+{
+	/**
+	*	Parse the limit string and split into
+	*	more sql standard LIMIT <val> OFFSET <val>
+	*/
+	public function limit_parse($str)
+	{
+		$str = trim($str);
+		if (empty($str))
+			return false;
+		$limit_str = false;
+		if (strstr($str, ',')) {
+			$limit_parts = explode(',', $str);
+			if (!empty($limit_parts) && count($limit_parts)==2) {
+				$limit = $limit_parts[1];
+				$offset = $limit_parts[0];
+				$limit_str = " LIMIT ".$limit." OFFSET ".$offset;
+			}
+		} else
+			$limit_str = !empty($limit) ? ' LIMIT '.$limit : '';
+
+		return $limit_str;
+	}
+}
