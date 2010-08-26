ac5fb074955ebf313989b5e5492e7844273b46c3
Per Asberg <perasb@op5.se>
2010-08-26 13:44:28 +0200

    views/reports/schedules: List summary schedules
    We make the summary schedules to be listed at the
    same place as all the other schedules for 
    consistency.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/reports/schedules.php
+++ application/views/themes/default/reports/schedules.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 			<div id="schedules_area">
 				<?php echo isset($new_schedule) ? $new_schedule : '' ?>
-				<br /><br />
+				<br /><br /><a name="avail_schedules"></a>
 				<div id="scheduled_avail_reports">
 					<table id="avail_scheduled_reports_table">
 					<caption><?php echo $avail_header ?></caption>
@@ -41,7 +41,7 @@
 					</table>
 				</div>
 
-				<br /><br />
+				<br /><br /><a name="sla_schedules"></a>
 				<div id="scheduled_sla_reports" style="width: 100%">
 					<table id="sla_scheduled_reports_table">
 					<caption><?php echo $sla_header ?></caption>
@@ -79,6 +79,47 @@
 						</tbody>
 						<?php }?>
 							<tr id="sla_no_result" class="no-result"<?php if (!empty($sla_schedules) && count($sla_schedules)) { ?> style="display:none"<?php } ?>><td colspan="7"><?php echo $label_no_schedules ?></td></tr>
+					</table>
+				</div>
+
+				<br /><br /><a name="summary_schedules"></a>
+				<div id="scheduled_summary_reports" style="width: 100%">
+					<table id="summary_scheduled_reports_table">
+					<caption><?php echo $summary_header ?></caption>
+						<thead id="summary_headers"<?php if (!count($summary_schedules)) { ?> style="display:none"<?php } ?>>
+							<tr class="setup">
+								<th class="headerNone left"><?php echo $label_sch_interval ?></th>
+								<th class="headerNone left"><?php echo $label_sch_name ?></th>
+								<th class="headerNone left"><?php echo $label_sch_recipients ?></th>
+								<th class="headerNone left"><?php echo $label_sch_filename ?></th>
+								<th class="headerNone left"><?php echo $label_sch_description ?></th>
+								<th class="headerNone left"style='width: 60px'><?php echo $this->translate->_('Actions'); ?></th>
+							</tr>
+						</thead>
+						<?php if (!empty($summary_schedules) && count($summary_schedules)) { ?>
+						<tbody>
+							<?php	$recipients = false;
+								$j=0;
+								foreach ($summary_schedules as $schedule) {
+									$j++;
+									$schedule = (object)$schedule;
+									$recipients = str_replace(' ', '', $schedule->recipients);
+									$recipients = str_replace(',', ', ', $recipients);	?>
+								<tr id="report-<?php echo $schedule->id ?>" class="<?php echo ($j%2 == 0 ? 'odd' : 'even')?>">
+								<td class="period_select" title="<?php echo $label_dblclick ?>" id="period_id-<?php echo $schedule->id ?>"><?php echo $schedule->periodname ?></td>
+								<td class="report_name" id="<?php echo $schedule->report_type_id ?>.report_id-<?php echo $schedule->id ?>"><?php echo $schedule->reportname ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="recipients-<?php echo $schedule->id ?>"><?php echo $recipients ?></td>
+								<td class="iseditable" title="<?php echo $label_dblclick ?>" id="filename-<?php echo $schedule->id ?>"><?php echo $schedule->filename ?></td>
+								<td class="iseditable_txtarea" title="<?php echo $label_dblclick ?>" id="description-<?php echo $schedule->id ?>"><?php echo utf8_decode($schedule->description) ?></td>
+								<td>
+									<form><input type="button" class="send_report_now" id="send_now_summary_<?php echo $schedule->id ?>" title="<?php echo $this->translate->_('Send this report now') ?>" value="&nbsp;"></form>
+									<div class="delete_schedule summary_del" id="alldel_<?php echo $schedule->id ?>"><?php echo html::image($this->add_path('icons/16x16/delete-schedule.png'),array('alt' => $this->translate->_('Delete scheduled report'), 'title' => $this->translate->_('Delete scheduled report'),'class' => 'deleteimg')) ?></div>
+								</td>
+							</tr>
+							<?php } ?>
+						</tbody>
+						<?php }?>
+							<tr id="summary_no_result" class="no-result"<?php if (!empty($summary_schedules) && count($summary_schedules)) { ?> style="display:none"<?php } ?>><td colspan="7"><?php echo $label_no_schedules ?></td></tr>
 					</table>
 				</div>
 			</div>
