41eaa0ce7c7f86a9c4270ef726089ad81e3265b2
Per Asberg <perasb@op5.se>
2010-08-30 11:35:41 +0200

    Added table definitions for saved summary reports
    SQL table definitions for saved scheduled reports.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

---
+++ op5-upgradescripts/reports_v6.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS summary_config;
+CREATE TABLE IF NOT EXISTS summary_config (
+  id int(11) NOT NULL auto_increment,
+  user varchar(200) NOT NULL,
+  report_name varchar(200) NOT NULL,
+  setting text NOT NULL,
+  PRIMARY KEY (id),
+  KEY user (user)
+);
