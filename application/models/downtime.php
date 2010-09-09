d6e4872ff06d99b9122d5e488c2faaca57a5ce27
Per Asberg <perasb@op5.se>
2010-09-09 16:15:18 +0200

    models/downtime: Added method check_if_scheduled()
    Added a method which enables us to check if an 
    object has been scheduled for downtime with given
    start time and duration to prevent us from adding
    it twice.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/models/downtime.php
+++ application/models/downtime.php
@@ -55,4 +55,75 @@ class Downtime_Model extends Model
 		$result = $db->query($sql);
 		return $result->count() ? $result: false;
 	}
+
+	/**
+	*	Try to figure out if an object already has been scheduled
+	*/
+	public function check_if_scheduled($type=false, $name=false, $start_time=false, $duration=false)
+	{
+		if (empty($type) || empty($start_time) || empty($duration)) {
+			return false;
+		}
+
+		$db = new Database();
+		$sql = false;
+		switch ($type) {
+			case 'hosts':
+				$sql = "SELECT * FROM scheduled_downtime WHERE host_name=".$db->escape($name).
+					" AND start_time=".$start_time." AND downtime_type=2 ".
+					"AND duration=".(int)$duration;
+				break;
+			case 'services':
+				if (strstr($name, ';')) {
+					$parts = explode(';', $name);
+					$host = $parts[0];
+					$service = $parts[1];
+					$sql = "SELECT * FROM scheduled_downtime WHERE host_name=".$db->escape($host)." AND service_description=".$db->escape($service).
+						" AND start_time=".$start_time." AND downtime_type=1 ".
+						"AND duration=".(int)$duration;
+				}
+				break;
+			case 'hostgroups':
+				# find members
+				$members = Host_Model::get_hosts_for_group($name);
+				$found = false;
+				$member_cnt = count($members);
+				if (count($members)) {
+					foreach ($members as $member) {
+						if (self::check_if_scheduled('hosts', $member->host_name, $start_time, $duration)) {
+							$found++;
+						}
+					}
+
+					if ($found == $member_cnt) {
+						return true;
+					}
+				}
+				return false;
+				break;
+
+			case 'servicegroups':
+				$members = Service_Model::get_services_for_group($name, 'service');
+				$member_cnt = count($members);
+				$found = false;
+				if (count($members)) {
+					foreach ($members as $member) {
+						if (self::check_if_scheduled('services', $member->host_name.';'.$member->service_description, $start_time, $duration)) {
+							$found++;
+						}
+					}
+
+					if ($found == $member_cnt) {
+						return true;
+					}
+				}
+				return false;
+		}
+
+		if (!empty($sql)) {
+			$res = $db->query($sql);
+			return count($res) ? true : false;
+		}
+		return false;
+	}
 }
