a29256f521499f9f28f922f6a05f6fbb42738d11
Per Asberg <perasb@op5.se>
2010-09-22 11:06:13 +0200

    widgets/tac_disabled: Fix links and add all states
    This patch fixes the filter links for all state
    types and adds the states that was missing.
    
    This fixes issues #3670 and #3671
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/widgets/tac_disabled/tac_disabled.php
+++ application/widgets/tac_disabled/tac_disabled.php
@@ -50,10 +50,18 @@ class Tac_disabled_Widget extends widget_Core {
 		$problem = array();
 		$i = 0;
 
+		if ($current_status->hosts_up_disabled) {
+			$problem[$i]['type'] = $this->translate->_('Host');
+			$problem[$i]['status'] = $this->translate->_('Up');
+			$problem[$i]['url'] = 'status/host/all/?hoststatustypes='.nagstat::HOST_UP.'&hostprops='.nagstat::HOST_CHECKS_DISABLED;
+			$problem[$i]['title'] = $current_status->hosts_up_disabled.' '.$this->translate->_('Disabled hosts');
+			$i++;
+		}
+
 		if ($current_status->hosts_down_disabled) {
 			$problem[$i]['type'] = $this->translate->_('Host');
 			$problem[$i]['status'] = $this->translate->_('Down');
-			$problem[$i]['url'] = 'status/host/all/'.nagstat::HOST_DOWN.'/'.nagstat::HOST_CHECKS_DISABLED;
+			$problem[$i]['url'] = 'status/host/all/?hoststatustypes='.nagstat::HOST_DOWN.'&hostprops='.nagstat::HOST_CHECKS_DISABLED;
 			$problem[$i]['title'] = $current_status->hosts_down_disabled.' '.$this->translate->_('Disabled hosts');
 			$i++;
 		}
@@ -61,32 +69,56 @@ class Tac_disabled_Widget extends widget_Core {
 		if ($current_status->hosts_unreachable_disabled) {
 			$problem[$i]['type'] = $this->translate->_('Host');
 			$problem[$i]['status'] = $this->translate->_('Unreachable');
-			$problem[$i]['url'] = 'status/host/all/'.nagstat::HOST_UNREACHABLE.'/'.nagstat::HOST_CHECKS_DISABLED;
+			$problem[$i]['url'] = 'status/host/all/?hoststatustypes='.nagstat::HOST_UNREACHABLE.'&hostprops='.nagstat::HOST_CHECKS_DISABLED;
 			$problem[$i]['title'] = $current_status->hosts_unreachable_disabled.' '.$this->translate->_('Disabled hosts');
 			$i++;
 		}
 
-		if ($current_status->services_critical_disabled) {
+		if ($current_status->hosts_pending_disabled) {
+			$problem[$i]['type'] = $this->translate->_('Host');
+			$problem[$i]['status'] = $this->translate->_('Pending');
+			$problem[$i]['url'] = 'status/host/all/?hoststatustypes='.nagstat::HOST_PENDING.'&hostprops='.nagstat::HOST_CHECKS_DISABLED;
+			$problem[$i]['title'] = $current_status->hosts_pending_disabled.' '.$this->translate->_('Disabled hosts');
+			$i++;
+		}
+
+		if ($current_status->services_ok_disabled) {
 			$problem[$i]['type'] = $this->translate->_('Service');
-			$problem[$i]['status'] = $this->translate->_('Critical');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_CHECKS_DISABLED;
-			$problem[$i]['title'] = $current_status->services_critical_disabled.' '.$this->translate->_('Disabled services');
+			$problem[$i]['status'] = $this->translate->_('OK');
+			$problem[$i]['url'] = 'status/service/all/?servicestatustypes='.nagstat::SERVICE_OK.'&serviceprops='.nagstat::SERVICE_CHECKS_DISABLED;
+			$problem[$i]['title'] = $current_status->services_ok_disabled.' '.$this->translate->_('Disabled services');
 			$i++;
 		}
 
 		if ($current_status->services_warning_disabled) {
 			$problem[$i]['type'] = $this->translate->_('Service');
 			$problem[$i]['status'] = $this->translate->_('Warning');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_CHECKS_DISABLED;
+			$problem[$i]['url'] = 'status/service/all/?servicestatustypes='.nagstat::SERVICE_WARNING.'&serviceprops='.nagstat::SERVICE_CHECKS_DISABLED;
 			$problem[$i]['title'] = $current_status->services_warning_disabled.' '.$this->translate->_('Disabled services');
+			$i++;
+		}
+
+		if ($current_status->services_critical_disabled) {
+			$problem[$i]['type'] = $this->translate->_('Service');
+			$problem[$i]['status'] = $this->translate->_('Critical');
+			$problem[$i]['url'] = 'status/service/all/?servicestatustypes='.nagstat::SERVICE_CRITICAL.'&serviceprops='.nagstat::SERVICE_CHECKS_DISABLED;
+			$problem[$i]['title'] = $current_status->services_critical_disabled.' '.$this->translate->_('Disabled services');
 			$i++;
 		}
 
 		if ($current_status->services_unknown_disabled) {
 			$problem[$i]['type'] = $this->translate->_('Service');
 			$problem[$i]['status'] = $this->translate->_('Unknown');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_CHECKS_DISABLED;
+			$problem[$i]['url'] = 'status/service/all/?servicestatustypes='.nagstat::SERVICE_UNKNOWN.'&serviceprops='.nagstat::SERVICE_CHECKS_DISABLED;
 			$problem[$i]['title'] = $current_status->services_unknown_disabled.' '.$this->translate->_('Disabled services');
+			$i++;
+		}
+
+		if ($current_status->services_pending_disabled) {
+			$problem[$i]['type'] = $this->translate->_('Service');
+			$problem[$i]['status'] = $this->translate->_('Pending');
+			$problem[$i]['url'] = 'status/service/all/?servicestatustypes='.nagstat::SERVICE_PENDING.'&serviceprops='.nagstat::SERVICE_CHECKS_DISABLED;
+			$problem[$i]['title'] = $current_status->services_pending_disabled.' '.$this->translate->_('Disabled services');
 			$i++;
 		}
 
