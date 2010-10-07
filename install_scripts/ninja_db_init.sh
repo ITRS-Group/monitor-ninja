53dce57aabb89f04ca9ed7b3f845d80ae776ed20
Per Asberg <perasb@op5.se>
2010-10-07 12:17:33 +0200

    ninja_db_init: Don't try to move data on fresh install
    If we are installing Ninja for the first time, we
    shouldn't assume that monitor_reports exists or 
    we will get some ugly SQL errors during install.
    
    This fixes issue #3730
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- install_scripts/ninja_db_init.sh
+++ install_scripts/ninja_db_init.sh
@@ -51,19 +51,25 @@ then
 	run_sql_file $db_login_opts "$prefix/install_scripts/recurring_downtime.sql"
 
 	# check if we should import data fr monitor_reports
-	is_old_reports=$(mysql $db_login_opts -Be "SELECT version FROM scheduled_reports_db_version" merlin 2>/dev/null)
+	is_new_reports=$(mysql $db_login_opts -Be "SELECT version FROM scheduled_reports_db_version" merlin 2>/dev/null)
 	if [ $? -ne 0 ]
 	then
 		# doesn't exist - make sure we upgrade if necessary
 		echo "Report tables doesn't seem to exist in merlin - installing"
 		sh $prefix/op5-upgradescripts/merlin-reports-db-upgrade.sh /opt/monitor
 
-		# move old data from monitor_reports -> merlin
-		/usr/bin/env php $prefix/op5-upgradescripts/move_reports_tables.php $prefix $db_user $db_pass
+		# move old data from monitor_reports -> merlin if monitor_reports exists
+		is_old_reports=$(mysql $db_login_opts -Be "SELECT version FROM scheduled_reports_db_version" monitor_reports 2>/dev/null)
+		if [ $? -ne 0 ]
+		then
+			echo "monitor_reports db was not found which is OK.";
+		else
+			/usr/bin/env php $prefix/op5-upgradescripts/move_reports_tables.php $prefix $db_user $db_pass
+		fi
 		mysql $db_login_opts -Be "UPDATE ninja_db_version SET version=2" merlin 2>/dev/null
 
 		# let's check this once more to be sure
-		is_old_reports=$(mysql $db_login_opts -Be "SELECT version FROM scheduled_reports_db_version" merlin 2>/dev/null)
+		is_reports=$(mysql $db_login_opts -Be "SELECT version FROM scheduled_reports_db_version" merlin 2>/dev/null)
 		if [ $? -ne 0 ]
 		then
 			echo "Ooops - this is bad. All the info from old monitor_reports should have been transferred"
