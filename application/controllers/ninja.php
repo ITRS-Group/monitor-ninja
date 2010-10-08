41b614c5ec11fd985fd8ff7a60ccdfa784621fd0
Per Asberg <perasb@op5.se>
2010-10-08 13:15:40 +0200

    controllers/ninja: Add My Account instead of change password
    This change make the old menu link for changing
    password to point to the new controller for setting
    some of the settings directly from the GUI.
    A link will be available to change password from
    the settings page.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/controllers/ninja.php
+++ application/controllers/ninja.php
@@ -118,7 +118,7 @@ class Ninja_Controller extends Template_Controller {
 				),
 				$this->translate->_('Configuration') => array(
 					$this->translate->_('View config') 					=> array('/config', 'viewconfig',0),
-					$this->translate->_('Change password') 			=> array('/change_password', 'password',0),
+					$this->translate->_('My Account') 			=> array('/user', 'password',0),
 					$this->translate->_('Backup/Restore')				=> array('/backup', 'backup',0),
 				)
 			);
