a313489fd1c5cc72bf26b3bd76a082d8134be9ec
Per Asberg <perasb@op5.se>
2010-10-22 11:08:55 +0200

    Command: Added some more default values
    Some checkboxes didn't behave as in the old GUI
    and this patch changes this for sticky, persistent
    and notify to make them checked by default.
    
    This fixes issue #3774
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/command/request.php
+++ application/views/themes/default/command/request.php
@@ -63,7 +63,7 @@ foreach ($params as $pname => $ary) {
 			}
 			# fallthrough
 		case 'bool':
-			$default_checked = array('fixed', '_services-too');
+			$default_checked = array('fixed', '_services-too', 'sticky', 'notify', 'persistent');
 			$checked = in_array($pname, $default_checked) ? true : false;
 			echo form::checkbox(array('name' => $form_name, 'id' => 'field_'.$pname), $dflt, $checked, 'class="checkbox"');
 			break;
