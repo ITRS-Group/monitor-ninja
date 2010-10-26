1c6bdabf361cadbda933cbc1047ac5d874e88405
Per Asberg <perasb@op5.se>
2010-10-26 10:03:21 +0200

    widgets/tac-problems/js: Restore class when bgcolor is white
    Restoring proper element class when background is
    set to white. This way we don't have to wait until
    next ajax update before the widget gets the correct
    look again.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/widgets/tac_problems/js/tac_problems.js
+++ application/widgets/tac_problems/js/tac_problems.js
@@ -3,27 +3,45 @@ $(document).ready(function() {
 
 	$('#col_outages').bind('colorpicked', function () {
 		$('#id_outages').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_outages')
+		save_tac_disabled_colors($(this).val(), 'col_outages');
+		if ($(this).val() == '#ffffff') {
+			$('#id_outages').addClass('status-outages').css('background', '');
+		}
 	});
 	$('#col_hosts_down').bind('colorpicked', function () {
 		$('#id_host_down').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_host_down')
+		save_tac_disabled_colors($(this).val(), 'col_host_down');
+		if ($(this).val() == '#ffffff') {
+			$('#id_host_down').addClass('status-down').css('background', '');
+		}
 	});
 	$('#col_services_critical').bind('colorpicked', function () {
 		$('#id_service_critical').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_critical')
+		save_tac_disabled_colors($(this).val(), 'col_service_critical');
+		if ($(this).val() == '#ffffff') {
+			$('#id_service_critical').addClass('status-critical').css('background', '');
+		}
 	});
 	$('#col_hosts_unreachable').bind('colorpicked', function () {
 		$('#id_host_unreachable').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_host_unreachable')
+		save_tac_disabled_colors($(this).val(), 'col_host_unreachable');
+		if ($(this).val() == '#ffffff') {
+			$('#id_host_unreachable').addClass('status-unreachable').css('background', '');
+		}
 	});
 	$('#col_services_warning').bind('colorpicked', function () {
 		$('#id_service_warning').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_warning')
+		save_tac_disabled_colors($(this).val(), 'col_service_warning');
+		if ($(this).val() == '#ffffff') {
+			$('#id_service_warning').addClass('status-warning').css('background', '');
+		}
 	});
 	$('#col_services_unknown').bind('colorpicked', function () {
 		$('#id_service_unknown').css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_unknown')
+		save_tac_disabled_colors($(this).val(), 'col_service_unknown');
+		if ($(this).val() == '#ffffff') {
+			$('#id_service_unknown').addClass('status-unknown').css('background', '');
+		}
 	});
 });
 
