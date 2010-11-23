1b0b38310372b5fcb641eccf402dc73acc6bdbd4
Per Asberg <perasb@op5.se>
2010-11-23 10:03:31 +0100

    js_header.js: Add support for external resources
    Since Kohana tries to be smart (?) and are adding
    .js to every file that doesn't end in this, we
    here add support for external files which simply
    bypasses this check and echoes out the correct
    script tag.
    
    This doesn't solve a possible problem if anyone
    would want to use parameters to local javascript
    files but we will handle this when/if it occurs.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/views/themes/default/js_header.php
+++ application/views/themes/default/js_header.php
@@ -1,7 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 if (!empty($js))
-	foreach ($js as $js_file)
-		echo html::script($js_file);
+	foreach ($js as $js_file) {
+		if (!strstr($js_file, 'http')) {
+			echo html::script($js_file);
+		} else {
+			echo '<script type="text/javascript" src="'.$js_file.'"></script>';
+		}
+	}
 
 ?>
