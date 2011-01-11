d4c7dc6f55228861474465a19fb2211741cde1c3
My Appelgren <my@op5.com>
2011-01-11 11:18:20 +0100

    widget netw_health view - fixed positioning for background image
    in ie7 and opera the margins were a litte off due to differnt way
    of calcuation round. fixed by rounding the numbers at all places
    and not only in the margin
    
    Signed-off-by: My Appelgren <my@op5.com>

--- application/widgets/netw_health/view.php
+++ application/widgets/netw_health/view.php
@@ -41,14 +41,14 @@
 						<div style="<?php echo ($host_value > 33) ? 'color: #ffffff;' : ''?>font-size: 22px; position: absolute; padding-top: 62px; padding-left: 10px;"><?php echo $host_value ?> %</div>
 						<div style="<?php echo ($host_value > 12) ? 'color: #ffffff;' : ''?>font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px"><?php echo $host_label ?></div>
 						<div class="border">
-							<?php echo html::image($host_image, array('style' => 'height:'.$host_value.'px; width: 100%; padding-top: '.round(100-$host_value).'px', 'alt' => $host_label)) ?>
+							<?php echo html::image($host_image, array('style' => 'height:'.round($host_value).'px; width: 100%; padding-top: '.(100-round($host_value)).'px', 'alt' => $host_label)) ?>
 						</div>
 					</td>
 					<td style="text-align: center">
 						<div style="<?php echo ($service_value > 33) ? 'color: #ffffff;' : ''?>font-size: 22px; position: absolute;padding-top: 62px; padding-left: 10px"><?php echo $service_value ?> %</div>
 						<div style="<?php echo ($service_value > 12) ? 'color: #ffffff;' : ''?>font-size: 10px; position: absolute; padding-top: 84px; padding-left: 10px;"><?php echo $service_label ?></div>
 						<div class="border">
-							<?php echo html::image($service_image, array('style' => 'height:'.$service_value.'px; width: 100%; padding-top: '.round(100-$service_value).'px', 'alt' => $service_label)) ?>
+							<?php echo html::image($service_image, array('style' => 'height:'.round($service_value).'px; width: 100%; padding-top: '.(100-round($service_value)).'px', 'alt' => $service_label)) ?>
 						</div>
 					</td>
 				</tr>
