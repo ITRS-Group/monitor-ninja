54b75c31f4931eeb1e79120c11467d611ef0669b
Per Asberg <perasb@op5.se>
2011-01-17 10:50:57 +0100

    widgets/status_totals: Fixed alt and title strings
    Some of the image alts and title strings used
    'Pending' instead of All Problems and All types.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/widgets/status_totals/view.php
+++ application/widgets/status_totals/view.php
@@ -32,14 +32,14 @@
 			<?php echo ($i%2 == 1) ? '</tr>' : '' ?>
 			<?php	 $i++; } ?>
 			<tr>
-				<td class="status icon" style="padding: 4px 7px"><?php echo html::image($this->add_path('icons/12x12/shield-info.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px')); ?></td>
+				<td class="status icon" style="padding: 4px 7px"><?php echo html::image($this->add_path('icons/12x12/shield-info.png'),array('title' => $label_all_types, 'alt' => $label_all_types, 'style' => 'margin-bottom: -2px')); ?></td>
 				<td><?php echo html::anchor('status/'.$target_method.'/'.$host.'?'.$grouptype_arg, html::specialchars($total_hosts.' Hosts'), array('class' => 'status-total')) ?></td>
 				<td class="status icon" style="padding: 4px 7px">
 					<?php
 						if ($total_problems > 0)
-							echo html::image($this->add_path('icons/12x12/shield-warning.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px'));
+							echo html::image($this->add_path('icons/12x12/shield-warning.png'),array('title' => $label_all_problems, 'alt' => $label_all_problems, 'style' => 'margin-bottom: -2px'));
 						else
-							echo html::image($this->add_path('icons/12x12/shield-not-warning.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px'));
+							echo html::image($this->add_path('icons/12x12/shield-not-warning.png'),array('title' => $label_all_problems, 'alt' => $label_all_problems, 'style' => 'margin-bottom: -2px'));
 					?>
 				</td>
 				<td><?php echo html::anchor('status/host/'.$host.'/?hoststatustypes='.(nagstat::HOST_DOWN|nagstat::HOST_UNREACHABLE).'&'.$grouptype_arg, html::specialchars($total_problems.' Problems'), array('class' => 'status-warning')) ?></td>
@@ -60,16 +60,16 @@
 				<td style="width: 85px"><?php echo ($row['status_id'] == $servicestatus ? '<strong>' : '').html::anchor($row['url'], html::specialchars($row['lable'].' '.$row['status']), array('class' => 'status-'.strtolower($row['status']))).($row['status_id'] == $servicestatus ? '</strong>' : '') ?></td>
 			<?php echo ($i%2 == 1) ? '</tr>' : ''; ?>
 			<?php  $i++; } ?>
-				<td class="status icon" style="padding: 4px 7px"><?php echo html::image($this->add_path('icons/12x12/shield-info.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px')); ?></td>
+				<td class="status icon" style="padding: 4px 7px"><?php echo html::image($this->add_path('icons/12x12/shield-info.png'),array('title' => $label_all_types, 'alt' => $label_all_types, 'style' => 'margin-bottom: -2px')); ?></td>
 				<td><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.$host_state.'&'.$grouptype_arg, html::specialchars($svc_total_services.' Services'), array('class' => 'status-warning')) ?></td>
 			</tr>
 			<tr>
 				<td class="status icon" style="padding: 4px 7px">
 					<?php
 						if ($svc_total_problems > 0)
-							echo html::image($this->add_path('icons/12x12/shield-warning.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px'));
+							echo html::image($this->add_path('icons/12x12/shield-warning.png'),array('title' => $label_all_problems, 'alt' => $label_all_problems, 'style' => 'margin-bottom: -2px'));
 						else
-							echo html::image($this->add_path('icons/12x12/shield-not-warning.png'),array('title' => $row['status'], 'alt' => $row['status'], 'style' => 'margin-bottom: -2px'));
+							echo html::image($this->add_path('icons/12x12/shield-not-warning.png'),array('title' => $label_all_problems, 'alt' => $label_all_problems, 'style' => 'margin-bottom: -2px'));
 					?>
 				</td>
 				<td><?php echo html::anchor('status/service/'.$host.'/?hoststatustypes='.$host_state.'&servicestatustypes='.(nagstat::SERVICE_UNKNOWN|nagstat::SERVICE_WARNING|nagstat::SERVICE_CRITICAL).'&'.$grouptype_arg, html::specialchars($svc_total_problems.' Problems'), array('class' => 'status-warning')) ?></td>
