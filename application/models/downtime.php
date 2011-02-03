8ed359d673280795f57b496fdc881d3762cfe88a
Per Asberg <perasb@op5.se>
2011-02-03 13:58:14 +0100

    models/downtime: Make query more robust
    This patch makes the downtime query more robust
    as it always joins the tables with contact_access
    instead of the old way of joining with contact,
    contact group and what not.
    
    It also works on oracle - hooray ;) 
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/models/downtime.php
+++ application/models/downtime.php
@@ -13,7 +13,6 @@ class Downtime_Model extends Model
 	{
 		$db = new Database();
 		$filter = empty($filter) ? 3 : $filter;
-		# don't use auth_host fields etc
 		$bitary = db::bitmask_to_array($filter);
 		$bits = '';
 		foreach ($bitary as $bit => $is_set) {
@@ -23,44 +22,42 @@ class Downtime_Model extends Model
 		}
 		$bits = substr($bits, 1);
 		$auth = new Nagios_auth_Model();
-		$host_query = $auth->authorized_host_query();
-		if ($host_query === true) {
+		if ($auth->view_hosts_root) {
 			$sql = "SELECT d.* FROM scheduled_downtime d WHERE d.downtime_type IN ($bits)";
 		} else {
 			# hosts
-			$auth_host_alias = $host_query['host_field'];
-			$auth_from = ' ,'.$host_query['from'];
-			$auth_where = ' WHERE '.sprintf($host_query['where'], "d.host_name");
-			$sql = "SELECT d.* FROM scheduled_downtime d ".$auth_from.$auth_where." AND d.downtime_type IN ($bits)";
-
-			$query_contact = "SELECT d.* FROM scheduled_downtime d, host, ".
-			"contact, host_contact ".
-			"WHERE host.id = host_contact.host ".
-			"AND host_contact.contact=contact.id ".
-			"AND contact.contact_name=".$db->escape(Auth::instance()->get_user()->username).
-			" AND d.host_name=host.host_name ".
-			"AND d.downtime_type IN ($bits)";
+			$sql = "SELECT d.* FROM scheduled_downtime d, host ".
+				"INNER JOIN contact_access ON contact_access.host=host.id ".
+				"WHERE contact_access.service IS NULL ".
+				"AND host.host_name=d.host_name AND d.service_description IS NULL ".
+				"AND contact_access.contact=".$auth->id.
+				" AND d.downtime_type IN (" . $bits . ")";
 
 			# services
-			$query_svc =
-				'SELECT d.* FROM scheduled_downtime d, host, service, contact, contact_contactgroup, service_contactgroup ' .
-				'WHERE service.id = service_contactgroup.service ' .
-				'AND service_contactgroup.contactgroup = contact_contactgroup.contactgroup ' .
-				'AND contact_contactgroup.contact = ' . (int)$auth->id." AND host.host_name=service.host_name ".
-				"AND d.host_name=service.host_name AND d.service_description=service.service_description AND d.downtime_type IN ($bits)";
+			$query_svc = "SELECT d.* ".
+				"FROM scheduled_downtime d ".
+				"INNER JOIN service ON service.host_name=d.host_name ".
+				"INNER JOIN contact_access ON contact_access.service=service.id ".
+				"WHERE ".
+				"contact_access.service IS NOT NULL ".
+				"AND d.service_description=service.service_description ".
+				"AND d.host_name=service.host_name ".
+				"AND contact_access.contact=".$auth->id.
+				" AND d.downtime_type IN (".$bits.")";
 
-			# contact <-> service_contact relation
-			$query_svc_contact = "SELECT d.* FROM scheduled_downtime d, host h, service s, contact c, service_contact sc ".
-				"WHERE s.id=sc.service AND c.id=sc.contact ".
-				"AND sc.contact=c.id ".
-				"AND c.contact_name=".$db->escape(Auth::instance()->get_user()->username).
-				" AND h.host_name=s.host_name AND d.host_name=s.host_name AND d.service_description=s.service_description AND d.downtime_type IN ($filter)";
-
-			$sql = '(' . $sql . ') UNION (' . $query_contact . ') UNION (' . $query_svc . ') UNION (' . $query_svc_contact . ')';
+			switch ($bits) {
+				case 2:
+					$query = $sql." ORDER BY d.host_name";
+					break;
+				case 1:
+					$query = $query_svc." ORDER BY d.host_name";
+					break;
+				default:
+					$query = '(' . $sql . ') UNION ALL (' . $query_svc . ')';
+			}
 		}
-		$sql .= " ORDER BY ".$order_by;
 
-		$result = $db->query($sql);
+		$result = $db->query($query);
 		return $result->count() ? $result: false;
 	}
 
