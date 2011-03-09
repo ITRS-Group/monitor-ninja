65ba2c4ee5b969d91659b37361b8bf33079c31a0
Robin Sonefors <robin.sonefors@op5.com>
2011-03-09 09:23:43 +0100

    program status: Check if notifications are enabled on local node
    In e83650f, I fixed the logic for checking if the local merlin node is
    running, but the same thing was also a problem for checking if
    notifications and active service checks are enabled.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/models/program_status.php
+++ application/models/program_status.php
@@ -5,6 +5,8 @@
  */
 class Program_status_Model extends Model
 {
+	const local_node_name = 'Local Nagios/Merlin instance';
+
 	/**
 	 * Fetch all info from program_status table
 	 */
@@ -31,7 +33,7 @@ class Program_status_Model extends Model
 	public function last_alive()
 	{
 		$db = new Database();
-		$sql = "SELECT last_alive FROM program_status WHERE instance_name = 'Local Nagios/Merlin instance'";
+		$sql = "SELECT last_alive FROM program_status WHERE instance_name = '".self::local_node_name."'";
 		$res = $db->query($sql);
 		$cur = ($res && count($res)) ? $res->current() : false;
 		return $cur ? $cur->last_alive : false;
@@ -44,7 +46,7 @@ class Program_status_Model extends Model
 	public function notifications_checks()
 	{
 		$db = new Database();
-		$sql = "SELECT notifications_enabled, active_service_checks_enabled FROM program_status";
+		$sql = "SELECT notifications_enabled, active_service_checks_enabled FROM program_status WHERE instance_name = '".self::local_node_name."'";
 		$res = $db->query($sql);
 		return (!$res || count($res) == 0) ? false : $res;
 	}
