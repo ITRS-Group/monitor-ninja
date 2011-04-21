9343bfc3e65777846c74dbfc83ef856d30f95c06
Robin Sonefors <robin.sonefors@op5.com>
2011-04-21 13:29:28 +0200

    models/downtime: Rewrite fetch(_all)?_comments to use contact_access
    The old code was not only amazingly slow, but also incomprehensible.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/models/downtime.php
+++ application/models/downtime.php
@@ -26,21 +26,21 @@ class Downtime_Model extends Model
 			$query = "SELECT d.* FROM scheduled_downtime d WHERE d.downtime_type IN ($bits)";
 		} else {
 			# hosts
-			$sql = "SELECT d.* FROM scheduled_downtime d, host ".
+			$sql = "SELECT d.* FROM scheduled_downtime d ".
+				"INNER JOIN host ON host.host_name=d.host_name ".
 				"INNER JOIN contact_access ON contact_access.host=host.id ".
 				"WHERE contact_access.service IS NULL ".
-				"AND host.host_name=d.host_name AND d.service_description IS NULL ".
+				"AND d.service_description IS NULL ".
 				"AND contact_access.contact=".$auth->id.
 				" AND d.downtime_type IN (" . $bits . ")";
 
 			# services
 			$query_svc = "SELECT d.* ".
 				"FROM scheduled_downtime d ".
+				"INNER JOIN host ON host.host_name=d.host_name ".
 				"INNER JOIN service ON service.host_name=d.host_name ".
 				"INNER JOIN contact_access ON contact_access.service=service.id ".
-				"WHERE ".
-				"contact_access.service IS NOT NULL ".
-				"AND d.service_description=service.service_description ".
+				"WHERE d.service_description=service.service_description ".
 				"AND d.host_name=service.host_name ".
 				"AND contact_access.contact=".$auth->id.
 				" AND d.downtime_type IN (".$bits.")";
@@ -170,52 +170,32 @@ class Downtime_Model extends Model
 		$db = new Database();
 		$auth = new Nagios_auth_Model();
 
-		# service comments or not?
-		$svc_selection = empty($service) ? " AND (d.service_description='' OR d.service_description is null) "
-			: " AND d.service_description=".$db->escape($service);
+		$from = 'FROM scheduled_downtime d';
+		$where = 'WHERE d.host_name = '.$db->escape($host);
+		if ($service) {
+			$where .= ' AND d.service_description = '.$db->escape($service);
+			if (!$auth->view_services_root) {
+				$from .= ' INNER JOIN service s '.
+				         'ON d.host_name = s.host_name '.
+				         'AND d.service_description = s.service_description '.
+				         'INNER JOIN host h ON d.host_name = h.host_name '.
+				         'INNER JOIN contact_access ca '.
+				         'ON s.id = ca.service OR h.id = ca.host';
+				$where .= " AND ca.contact=$auth->id";
+			}
+		} else {
+			$where .= ' AND d.service_description IS NULL';
+			if (!$auth->view_hosts_root) {
+				$from .= ' INNER JOIN host h ON d.host_name = h.host_name '.
+				         'INNER JOIN contact_access ca ON h.id = ca.host';
+				$where .= " AND ca.host IS NULL AND ca.contact=$auth->id";
+			}
+		}
 
 		# only use LIMIT when NOT counting
 		$offset_limit = $count!==false || empty($num_per_page) ? "" : " LIMIT " . $num_per_page." OFFSET ".$offset;
 
-		$host_query = $auth->authorized_host_query();
-		if ($host_query === true) {
-			# don't use auth_host fields etc
-			$auth_host_alias = 'h';
-			$auth_from = ', host '.$auth_host_alias;
-			$auth_where = ' AND ' . $auth_host_alias . ".host_name = d.host_name";
-		} else {
-			$auth_host_alias = $host_query['host_field'];
-			$auth_from = isset($host_query['from']) && !empty($host_query['from']) ? ' ,'.$host_query['from'] : '';
-			$auth_where = ' AND '.sprintf($host_query['where'], "d.host_name");
-		}
-
-		$num_per_page = (int)$num_per_page;
-
-		if (!$auth->view_hosts_root) {
-			# this part is not necessary when authorized_for_all_hosts
-			$service_query = $auth->authorized_service_query();
-
-			$auth_where = !empty($host_query['where']) ? ' AND '.sprintf($host_query['where'], "d.host_name") : '';
-			$sql = "SELECT d.* FROM scheduled_downtime d ".$auth_from." WHERE d.host_name=".$db->escape($host).
-				$svc_selection.$auth_where;
-
-			if ($service_query !== true) {
-				$from = !empty($service_query['from']) ? ','.$service_query['from'] : '';
-				# via service_contactgroup
-
-				# @@@FIXME: handle direct relation contact -> {host,service}_contact
-				$sql2 = "SELECT d.* FROM scheduled_downtime d ".$from." WHERE d.host_name=".$db->escape($host).
-					$svc_selection.' AND '.$service_query['where'];
-				$sql = '(' . $sql . ') UNION ALL (' . $sql2 . ')';
-			} else {
-				$sql = "SELECT d.* FROM scheduled_downtime d WHERE d.host_name=".$db->escape($host).$svc_selection;
-			}
-		} else {
-			$sql = "SELECT d.* FROM scheduled_downtime d ".$auth_from." WHERE d.host_name=".$db->escape($host).
-				$svc_selection.$auth_where;
-		}
-
-		$sql .= " ".$offset_limit;
+		$sql = "SELECT d.* $from $where $offset_limit";
 
 		$result = $db->query($sql);
 		if ($count !== false) {
@@ -235,73 +215,31 @@ class Downtime_Model extends Model
 		$db = new Database();
 		$auth = new Nagios_auth_Model();
 
-		$host_query = $auth->authorized_host_query();
+		$sql = 'SELECT d.* FROM scheduled_downtime d';
 
-		# service comments or not?
-		$svc_selection = empty($service) ? " AND (d.service_description='' OR d.service_description is null) "
-			: " AND d.service_description!='' ";
+		if ($service) {
+			if (!$auth->view_services_root)
+				$sql .= ' INNER JOIN service s '.
+				        'ON d.service_description=s.service_description '.
+				        'AND d.host_name = s.host_name '.
+				        'INNER JOIN host h '.
+				        'ON h.host_name = d.host_name '.
+				        'INNER JOIN contact_access ca '.
+				        'ON s.id = ca.service OR h.id = ca.host';
+			$sql .= ' WHERE d.service_description IS NOT NULL';
+			if (!$auth->view_services_root)
+				$sql .= " AND ca.contact = {$auth->id}";
+		} else {
+			if (!$auth->view_hosts_root)
+				$sql .= ' INNER JOIN host h ON d.host_name = h.host_name '.
+				        'INNER JOIN contact_access ca ON h.id = ca.host';
+			$sql .= ' WHERE d.service_description IS NULL';
+			if (!$auth->view_hosts_root)
+				$sql .= " AND ca.contact = {$auth->id} AND ca.service IS NULL";
+		}
 
 		# only use LIMIT when NOT counting
 		$offset_limit = $count!==false ? "" : " LIMIT " . $num_per_page." OFFSET ".$offset;
-
-		if ($host_query === true) {
-			# don't use auth_host fields etc since
-			# user is authenticated_for_all_hosts
-			$auth_host_alias = 'h';
-			$auth_from = ', host '.$auth_host_alias;
-			$auth_where = ' AND '.$auth_host_alias . ".host_name = d.host_name";
-			$sql = "SELECT d.* FROM scheduled_downtime d ".$auth_from." WHERE".
-				" d.host_name!='' ".$svc_selection.$auth_where;
-		} else {
-			# we only make this check if user isn't authorized_for_all_hosts as above
-			$service_query = $auth->authorized_service_query();
-
-			$auth_host_alias = $host_query['host_field'];
-			$auth_from = ' ,'.$host_query['from'];
-			$auth_where = !empty($host_query['where']) ? ' AND '.sprintf($host_query['where'], "d.host_name") : '';
-
-			if (!$service) { # host comments
-				# comments via host_contactgroup
-				$sql = "SELECT d.* FROM scheduled_downtime d ".$auth_from." WHERE".
-					" d.host_name!='' ".$svc_selection.$auth_where;
-
-				# comments via host_contact
-				$from = "FROM scheduled_downtime d, host auth_host, contact auth_contact, host_contact auth_host_contact";
-				# via host_contact
-				$sql2 = "SELECT d.* ".$from." WHERE".
-					" d.host_name!='' ".$svc_selection." AND auth_contact.contact_name=".
-					$db->escape(Auth::instance()->get_user()->username).
-					" AND auth_host_contact.contact=auth_contact.id ".
-					"AND auth_host.id=auth_host_contact.host ".
-					"AND auth_host.host_name=d.host_name";
-				$sql = '(' . $sql . ') UNION ALL (' . $sql2 . ') ';
-
-			} else { # service comments
-				if ($service_query !== true) {
-
-					# comments via service_contactgroup
-					$from = ','.$service_query['from'];
-					$sql = "SELECT d.* FROM scheduled_downtime d".$from." WHERE ".
-						"(d.service_description!='' AND d.service_description is NOT null) AND ".
-						$service_query['where']." AND d.service_description=".$service_query['service_field'].".service_description ".
-						"AND d.host_name=".$service_query['service_field'].".host_name";
-
-					# comments via service_contact
-					$from = "FROM scheduled_downtime d, host auth_host, contact auth_contact, service_contact auth_servicecontact, service auth_service ";
-					$sql2 = "SELECT d.* ".$from." WHERE ".
-						"(d.service_description!='' AND d.service_description is NOT null) ".
-						"AND auth_service.id=auth_servicecontact.service ".
-						"AND auth_servicecontact.contact=auth_contact.id ".
-						"AND auth_contact.contact_name=".$db->escape(Auth::instance()->get_user()->username).
-						" AND auth_service.host_name=auth_host.host_name ".
-						"AND d.service_description=auth_service.service_description ".
-						"AND d.host_name=auth_service.host_name";
-					$sql = '(' . $sql . ') UNION ALL (' . $sql2 . ') ';
-				} else {
-					$sql = "SELECT * FROM scheduled_downtime WHERE (service_description!='' OR service_description is NOT null) ";
-				}
-			}
-		}
 
 		$sql .= $offset_limit;
 		#echo $sql."<br />";
