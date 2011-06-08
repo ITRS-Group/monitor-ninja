3f4f11e34061284dc1f66f6739606b156cafd52d
Peter Ostlin <peter.ostlin@op5.com>
2011-06-08 22:57:34 +0200

    Since it is not critical if notifications_checks() fail we silently catch db exception
    The Ninja controller will let the user know if we fail to find out if notifications
    or service checks are disabled. For this reason we silently catch db error instead
    of thowing the exception as a stack trace.
    
    Signed-off-by: Peter Ostlin <peter.ostlin@op5.com>
    (cherry picked from commit b7eeff841c3b4bf4d20dca56afdc8bf466b30724)

--- application/models/program_status.php
+++ application/models/program_status.php
@@ -56,7 +56,11 @@ class Program_status_Model extends Model
 	{
 		$db = new Database();
 		$sql = "SELECT notifications_enabled, active_service_checks_enabled FROM program_status WHERE instance_id = 0";
-		$res = $db->query($sql);
+		try {           
+			$res = $db->query($sql);
+		} catch (Kohana_Database_Exception $e) {
+			/* FIXME: This should be logged to file perhaps? */
+		}
 		return (!$res || count($res) == 0) ? false : $res;
 	}
 }
