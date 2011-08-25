c2ce50aeb8502f19de0452b4e0a2718824d972dc
Per Asberg <perasb@op5.se>
2011-08-25 15:14:38 +0200

    Make menu config extendable
    Added section to menu config file to make it 
    possible to drop a custom config file in the
    config/custom dir.
    
    Signed-off-by: Per Asberg <perasb@op5.se>

--- application/config/menu.php
+++ application/config/menu.php
@@ -1,3 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 $config['items'] = false;
+
+# check for custom config files that
+# won't be overwritten on upgrade
+if (file_exists(realpath(dirname(__FILE__)).'/custom/'.basename(__FILE__))) {
+	include(realpath(dirname(__FILE__)).'/custom/'.basename(__FILE__));
+}
