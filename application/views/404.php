0aa2ef506879daa9cc9fb13bd5c34537776dc366
Per Asberg <perasb@op5.se>
2011-09-08 09:17:44 +0200

    Added 403 + 404 error code handling
    We used to handle the case when a user entered a
    bad method (page) for an existing controller
    but not when trying to access a non existing
    controller. This patch fixes this by adding
    new 403/404 messages. Could probably be enhanced
    quite a bit but they will do for now.
    
    Partial fix for issue #3970
    
    Signed-off-by: Per Asberg <perasb@op5.se>

---
+++ application/views/404.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<div align="center" style="padding-top:10px;">
+	<?php echo html::image('application/views/themes/default/icons/icon.png',''); ?><br /><br />
+
+	<?php echo $this->translate->_('404 Not Found'); ?><br /><br />
+
+	<?php echo $this->translate->_('Ooops, the page you requested could not be found.'); ?><br />
+	<?php echo $this->translate->_('Please contact your administrator.'); ?>
+</div>
