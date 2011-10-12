7f41386e55a20130ba32933a265b08c4240beda8
Carl Helmertz <chelmertz@op5.com>
2011-10-12 13:46:09 +0200

    Support Kohana's setting config.site_domain when printing base url
    This fixes the bug that was introduced with the base_url helper.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/helpers/base_url.php
+++ application/helpers/base_url.php
@@ -6,6 +6,7 @@ class base_url_Core {
 	 * @return string similar to https://192.168.1.211/ninja/index.php/
 	 */
 	public static function get() {
-		return 'http'.(!empty($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'].(80 != $_SERVER['SERVER_PORT']         ? ':'.$_SERVER['SERVER_PORT'] : '').'/ninja/index.php/';
+		$site_domain = Kohana::config('config.site_domain', true);
+		return 'http'.(!empty($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'].(80 != $_SERVER['SERVER_PORT']         ? ':'.$_SERVER['SERVER_PORT'] : '').$site_domain.'index.php/';
 	}
 }
