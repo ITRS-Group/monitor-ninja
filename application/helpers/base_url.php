50a594d6640bf8fd9bb9c36cf32245063a674a66
Carl Helmertz <chelmertz@op5.com>
2011-10-28 12:28:27 +0200

    Added check and fallbacks for base_url helper in a CLI enviroment
    Noticed by running scheduled reports
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/helpers/base_url.php
+++ application/helpers/base_url.php
@@ -3,10 +3,28 @@
 class base_url_Core {
 
 	/**
+	 * This relies on the setting op5reports.site_address or a webserver
+	 * CLI will fail to resolve an URI without having the mentioned config key
+	 *
 	 * @return string similar to https://192.168.1.211/ninja/index.php/
 	 */
 	public static function get() {
+		$check_port = 80;
+		$protocol = 'http';
+		if(isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS'])) {
+			$protocol .= 's';
+			$check_port = 443;
+		}
+		$host = Kohana::config('op5reports.site_address', false, false);
+		if(!$host && isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST'])) {
+			$host = $_SERVER['HTTP_HOST'];
+		}
+		$port = '';
+		if(isset($_SERVER['SERVER_PORT']) && $check_port != $_SERVER['SERVER_PORT']) {
+			$port = ':'.$_SERVER['SERVER_PORT'];
+		}
 		$site_domain = Kohana::config('config.site_domain', true);
-		return 'http'.(!empty($_SERVER['HTTPS']) ? 's' : '').'://'.$_SERVER['HTTP_HOST'].(80 != $_SERVER['SERVER_PORT']         ? ':'.$_SERVER['SERVER_PORT'] : '').$site_domain.'index.php/';
+		$uri = $protocol.'://'.$host.$port.$site_domain.'index.php/';
+		return $uri;
 	}
 }
