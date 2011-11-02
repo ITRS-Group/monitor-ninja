cf22072a1209612d4e84d3f4491c14a41040ac2e
Valery Stukanov <vstukanov@n-ix.com.ua>
2011-11-02 12:13:16 +0300

    Add min height for iframe and add scrollbar if it needed
    Signed-off-by: Valery Stukanov <vstukanov@n-ix.com.ua>

--- application/views/themes/default/js/iframe-adjust.js
+++ application/views/themes/default/js/iframe-adjust.js
@@ -4,17 +4,21 @@ $(document).ready(function() {
 });
 
 function adjust_height() {
-	$('#iframe').css('height', parseInt(document.documentElement.clientHeight-68)+'px');
-	$('#nagvis').css('height', parseInt(document.documentElement.clientHeight-68)+'px');
-	$('#hypermap').css('height', parseInt(document.documentElement.clientHeight-68)+'px');
-	$('body').css('overflow-y', 'hidden');
-}
+	var new_height = parseInt(document.documentElement.clientHeight) - 68;
+	if(new_height > 500) {
+		$('#iframe').css('height', new_height+'px');
+		$('#nagvis').css('height', new_height+'px');
+		$('body').css('overflow-y', 'hidden');
+	} else {
+		$('body').css('overflow-y', 'scroll');
+	}
+};
 
 window.onload = function(){
- adjust_height();
- collapse_menu('');
-}
+	adjust_height();
+	collapse_menu('');
+};
 
 window.onresize = function (){
- adjust_height();
-}
+	adjust_height();
+};
