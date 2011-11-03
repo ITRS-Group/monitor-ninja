33365e5005d805a412a4c9e92952a48e8f7969f5
Carl Helmertz <chelmertz@op5.com>
2011-11-03 10:02:07 +0100

    pnp: Focus iframe on load
    This fixes bug 4815.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/views/themes/default/js/pnp.js
+++ application/views/themes/default/js/pnp.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	$('#iframe').load(function() {
+		$(this).focus();
 		$(this.contentDocument).find('.ui-widget-header tr').each(function () {
 			$(this).append('<td align="right"><a href="#" class="default" title="Make default graph" alt="Make default graph"><img src="' + _site_domain + '/application/views/themes/default/icons/16x16/shield-ok.png"/></a></td>');
 			$(this).find('.default').click(function() {
