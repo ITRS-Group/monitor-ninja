3a50a09c982d509becb6ce373c0099c67e1cd3a7
Robin Sonefors <robin.sonefors@op5.com>
2011-11-09 14:01:28 +0100

    test: Fix copy-paste-error in last
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/tests/saved_searches.php
+++ application/views/tests/saved_searches.php
@@ -4,12 +4,12 @@ $tap = unittest::instance();
 
 class tests
 {
-↦   public static function run($tap)
-↦   {
-↦   ↦   $tap->ok("Dummy file ".__FILE__." likes everybody, you too! Test passed, sir.");
+	public static function run($tap)
+	{
+		$tap->ok("Dummy file ".__FILE__." likes everybody, you too! Test passed, sir.");
 
-↦   ↦   return $tap->done();
-↦   }
+		return $tap->done();
+	}
 }
 
 $benchmark = Benchmark::get('system_benchmark_total_execution');
