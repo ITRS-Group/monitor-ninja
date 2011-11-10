60ff92e714428b380c0d38620b89c86ac42af918
Carl Helmertz <chelmertz@op5.com>
2011-11-10 10:22:16 +0100

    breadcrumbs: Rename "PNP" to "Graphs"
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/controllers/pnp.php
+++ application/controllers/pnp.php
@@ -32,7 +32,7 @@ class Pnp_Controller extends Authenticated_Controller {
 
 		$target_link = pnp::url($host, $srv);
 		$this->template->content = '<iframe src="'.$target_link.'" style="width: 100%; height: 600px" frameborder="0" id="iframe"></iframe>';
-		$this->template->title = $this->translate->_('Reporting » PNP');
+		$this->template->title = $this->translate->_('Reporting » Graphs');
 		$this->template->js_header = $this->add_view('js_header');
 		$this->template->disable_refresh = true;
 		$this->xtra_js = array($this->add_path('/js/iframe-adjust.js'), $this->add_path('/js/pnp.js'));
