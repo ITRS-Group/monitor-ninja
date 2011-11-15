de7fe5b69850978b48f79be0e15e36ab298ded55
Carl Helmertz <chelmertz@op5.com>
2011-11-15 11:31:21 +0100

    Use exceptions for the sake of being able to pass an error message
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/helpers/persist_pdf.php
+++ application/helpers/persist_pdf.php
@@ -1,4 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
+
 /**
  * PDF help class
  */
@@ -7,22 +8,23 @@ class persist_pdf_Core
 	/**
 	 * @param string $original_file
 	 * @param string $save_here
-	 * @return boolean
+	 * @throws Exception with reason of failure
 	 */
 	public function save($original_file, $save_here) {
+		$original_file = (string) $original_file;
+		$save_here = (string) $save_here;
 		// @todo bug 612
 		if(!is_readable($original_file)) {
-			return false;
+			throw new Exception("Can't read original file '$original_file'");
 		}
 		$local_persistent_filepath = rtrim(preg_replace('/\.pdf$/', null, $save_here), '/').'/';
 		if(!is_writable($local_persistent_filepath)) {
-			return false;
+			throw new Exception("Can't write output file '$save_here'");
 		}
-		$local_persistent_filepath .= date('Y-m-d').'-'.str_replace(K_PATH_CACHE.'/', null, $filename);
-		$could_copy = copy($filename, $local_persistent_filepath);
-		if(!$could_copy) {
-			return false;
+		$local_persistent_filepath .= date('Y-m-d').'-'.str_replace(K_PATH_CACHE.'/', null, $original_file);
+		$able_to_copy = copy($original_file, $local_persistent_filepath);
+		if(!$able_to_copy) {
+			throw new Exception("Failed to copy '$original_file' to '$local_persistent_filepath'");
 		}
-		return true;
 	}
 }
