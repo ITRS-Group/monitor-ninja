582a886eb14d8bdaf0382157408af62fc8baa4bf
Robin Sonefors <robin.sonefors@op5.com>
2011-11-16 15:31:22 +0100

    showlog: Fix alert log filtering
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/models/alertlog.php
+++ application/models/alertlog.php
@@ -56,7 +56,7 @@ class Alertlog_Model extends Model
 			$sql_where[] = implode(' OR ', $hg_cond);
 		}
 		if (isset($options['servicegroups']) && !empty($options['servicegroups'])) {
-			$sql_join['service'] = 'service.service_description = report_data.service_description AND service.host_name = report_data.host_name';
+			$sql_join['service'] = '(service.service_description = report_data.service_description OR (report_data.service_description=\'\' AND report_data.event_type > 900)) AND service.host_name = report_data.host_name';
 			$sql_join['service_servicegroup'] = 'service.id = service_servicegroup.service';
 			$sql_join['servicegroup'] = 'servicegroup.id = service_servicegroup.servicegroup';
 			$hg_cond = array();
@@ -69,22 +69,22 @@ class Alertlog_Model extends Model
 			if (isset($options['state_type']['soft']) && (int)$options['state_type']['soft'] && isset($options['state_type']['hard']) && (int)$options['state_type']['hard'])
 				;
 			else if (isset($options['state_type']['soft']) && (int)$options['state_type']['soft'])
-				$sql_where[] = 'hard = 0 AND event_type >= 700 AND event_type < 900';
+				$sql_where[] = 'hard = 0 OR event_type < 700 OR event_type > 900';
 			else
-				$sql_where[] = 'hard = 1 AND event_type >= 700 AND event_type < 900';
+				$sql_where[] = 'hard = 1 OR event_type < 700 OR event_type > 900';
 		}
 		if (isset($options['host_state_options'])) {
 			$cond = array();
 			foreach ($options['host_state_options'] as $state => $ison) {
 				if ((int)$ison)
-					$cond[] = 'state = '. $this->host_ccode_to_ncode[$state];
+					$cond[] = 'event_type state = '. $this->host_ccode_to_ncode[$state];
 			}
 			if (count($cond) == 3)
 				;
 			else if (!empty($cond))
 				$sql_where[] = implode(' OR ', $cond);
 			else
-				$sql_where[] = "report_data.service_description != ''";
+				$sql_where[] = "report_data.service_description != '' OR event_type < 700 OR event_type > 900";
 		}
 		if (isset($options['service_state_options'])) {
 			$cond = array();
@@ -92,12 +92,12 @@ class Alertlog_Model extends Model
 				if ((int)$ison)
 					$cond[] = 'state = '. $this->service_ccode_to_ncode[$state];
 			}
-			if (count($cond) == 3)
+			if (count($cond) == 4)
 				;
 			else if (!empty($cond))
 				$sql_where[] = implode(' OR ', $cond);
 			else
-				$sql_where[] = "report_data.service_description = ''";
+				$sql_where[] = "report_data.service_description = '' OR event_type < 700 OR event_type > 900";
 		}
 
 		if (isset($options['hide_downtime']) && $options['hide_downtime'])
