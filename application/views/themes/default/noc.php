55afbc3fe332055158c1b1be789cce7e022eaf88
Robin Sonefors <robin.sonefors@op5.com>
2011-11-22 11:06:47 +0100

    templates: Use HTML5 doctype, not XHTML1 strict
    Calling what we've got XHTML is just terribly, terribly broken: it
    really must be parsed through a HTML parser, not an XML one, so use a
    easy-to-remember doctype that means that.
    
    Not that we haven't always used a HTML parser, but now the doctype
    doesn't lie, so validators can actually tell what it is we want them to
    validate, so I can use them to find errors. Convenient.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/noc.php
+++ application/views/themes/default/noc.php
@@ -21,9 +21,9 @@ $ninja_menu_state = 'hide';
 if (isset($this->template->js_header))
 	$this->template->js_header->js = $this->xtra_js;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html lang="en">
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=8" /> <!-- Please remove me -->
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
