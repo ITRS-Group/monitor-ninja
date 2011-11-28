788d8a57328affa72e1b8a2241af3b6f06b4e930
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 17:20:15 +0100

    widgets/geomap: Port to new API
    As we don't really maintain this widget anymore, I haven't felt like
    porting the widget to be duplicatable, but it now works again.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/widgets/geomap/geomap.php
+++ application/widgets/geomap/geomap.php
@@ -6,43 +6,16 @@
  * @author op5 AB
  * @license GPL
  */
-class Geomap_Widget extends widget_Core {
-
-	public function __construct()
+class Geomap_Widget extends widget_Base {
+	public function __construct($model)
 	{
-		parent::__construct();
-
-		# needed to figure out path to widget
-		$this->set_widget_name(__CLASS__, basename(__FILE__));
+		parent::__construct($model);
 
 		$_SESSION['nagvis_user'] = user::session('username');
 	}
 
-	public function index($arguments=false, $master=false)
+	public function options()
 	{
-		$this->master_obj = $master;
-
-		# fetch widget view path
-		$view_path = $this->view_path('view');
-
-		# assign variables to widget
-		$widget_id = $this->widgetname;
-		$refresh_rate = 0;
-		$title = $this->translate->_('Geomap');
-		if (isset($arguments['widget_title'])) {
-			$title = $arguments['widget_title'];
-		}
-
-		$choosen_map = 'geomap';
-		if (isset($arguments['map'])) {
-			$choosen_map = $arguments['map'];
-		}
-
-		$height = 400;
-		if (isset($arguments['height']) && (((int) $arguments['height']) > 0)) {
-			$height = $arguments['height'];
-		}
-
 		$mapmodel = new Nagvis_Maps_Model;
 		$map_array = $mapmodel->get_list();
 		foreach ($map_array as $map) {
@@ -51,22 +24,16 @@ class Geomap_Widget extends widget_Core {
 		$all_maps['geomap'] = 'geomap';
 		$all_maps['automap'] = 'automap';
 
-		# let view template know if wrapping div should be hidden or not
-		$ajax_call = request::is_ajax() ? true : false;
+		$options = parent::options();
+		$options[] = new option($this->model->name, 'geomap_map', 'Map', 'dropdown', array('options' => $all_maps), 'geomap');
+		$options[] = new option($this->model->name, 'geomap_height', 'Height (px)', 'input', array('size'=>3, 'type'=>'text'), 400);
+		return $options;
+	}
 
-		# set required extra resources
-		$this->js = array('/js/geomap');
-
-		# fetch widget content
-		require_once($view_path);
-
-		if(request::is_ajax()) {
-			# output widget content
-			echo json::encode($this->output());
-		} else {
-			# call parent helper to assign all
-			# variables to master controller
-			return $this->fetch();
-		}
+	public function index()
+	{
+		$arguments = $this->get_arguments();
+		$view_path = $this->view_path('view');
+		require($view_path);
 	}
 }
--- application/widgets/geomap/view.php
+++ application/widgets/geomap/view.php
@@ -1,71 +1,49 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<?php if (!$ajax_call) { ?>
-<div class="widget editable movable collapsable removable closeconfirm" id="widget-<?php echo $widget_id ?>">
-	<div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
-	<div class="widget-editbox">
-		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
-		<fieldset>
-		<div id="<?php echo $widget_id ?>_slider"></div>
-		<label for="<?php echo $widget_id ?>_map"><?php echo $this->translate->_('Map') ?>:</label>
-		<?php echo form::dropdown(array('name' => $widget_id . '_map'), $all_maps, $choosen_map); ?>
-		<br />
-		<label for="<?php echo $widget_id ?>_height"><?php echo $this->translate->_('Height (px)') ?>:</label>
-		<input size="3" type="text" name="<?php echo $widget_id ?>_height" id="<?php echo $widget_id; ?>_height" value="<?php echo floatval($height) ?>" />
-		</fieldset>
-		<?php echo form::close() ?>
-	</div>
-	<div class="widget-content">
-	<?php } ?>
-	<?php if ($choosen_map == 'geomap') { ?>
-	<object	type="application/x-shockwave-flash" data="/nagvis/netmap/shell.swf"
-			id="nagvis" width="100%" <?php if ($height > 0) echo 'style="height: '.$height.'px;"'?>>
-		<param name="base" value="/nagvis/netmap" />
-		<param name="movie" value="/nagvis/netmap/shell.swf" />
-		<param name="bgcolor" value="#ffffff" />
-		<param name="wmode" value="opaque" />
-		<param name="allowScriptAccess" value="sameDomain" />
-	</object>
+<?php if ($arguments['geomap_map'] == 'geomap') { ?>
+<object	type="application/x-shockwave-flash" data="/nagvis/netmap/shell.swf"
+		id="nagvis" width="100%" <?php if ($arguments['geomap_height'] > 0) echo 'style="height: '.$arguments['geomap_height'].'px;"'?>>
+	<param name="base" value="/nagvis/netmap" />
+	<param name="movie" value="/nagvis/netmap/shell.swf" />
+	<param name="bgcolor" value="#ffffff" />
+	<param name="wmode" value="opaque" />
+	<param name="allowScriptAccess" value="sameDomain" />
+</object>
 
-	<script type="text/javascript">
-	if (!(document.attachEvent))
-		window.document["nagvis"].addEventListener("DOMMouseScroll", handleWheel, false);
-	else
-		window.document["nagvis"].onmousewheel = handleWheel;
+<script type="text/javascript">
+if (!(document.attachEvent))
+	window.document["nagvis"].addEventListener("DOMMouseScroll", handleWheel, false);
+else
+	window.document["nagvis"].onmousewheel = handleWheel;
 
-	function handleWheel(event)
+function handleWheel(event)
+{
+	var app = window.document["nagvis"];
+	if (app)
 	{
-		var app = window.document["nagvis"];
-		if (app)
-		{
-			if (!event)
-				event = window.event;
+		if (!event)
+			event = window.event;
 
-			var o = {x: event.clientX - parseInt($("#nagvis").offset().left),
-				y: event.clientY - parseInt($("#nagvis").offset().top),
-				delta: (event.detail ? event.detail : -event.wheelDelta/40),
-				ctrlKey: event.ctrlKey, altKey: event.altKey,
-				shiftKey: event.shiftKey
-			};
+		var o = {x: event.clientX - parseInt($("#nagvis").offset().left),
+			y: event.clientY - parseInt($("#nagvis").offset().top),
+			delta: (event.detail ? event.detail : -event.wheelDelta/40),
+			ctrlKey: event.ctrlKey, altKey: event.altKey,
+			shiftKey: event.shiftKey
+		};
 
-			app.handleWheel(o);
-			if (event.preventDefault)
-				event.preventDefault();
-			else
-				event.returnValue = false;
-		}
+		app.handleWheel(o);
+		if (event.preventDefault)
+			event.preventDefault();
+		else
+			event.returnValue = false;
 	}
-	</script>
-	<?php } else if ($choosen_map == 'automap') { ?>
-	<iframe name="nagvis" id="nagvis" src="/nagvis/nagvis/index.php?automap=1" width="100%" <?php if ($height > 0) echo 'style="height: '.$height.'px;"'?>>
-		Error : Can not load NagVis.
-	</iframe>
-	<?php } else { ?>
-	<iframe name="nagvis" id="nagvis" src="/nagvis/nagvis/?map=<?php echo $choosen_map; ?>" width="100%" <?php if ($height > 0) echo 'style="height: '.$height.'px;"'?>>
-		Error : Can not load NagVis.
-	</iframe>
-	<?php } ?>
-<?php if (!$ajax_call) { ?>
-	</div>
-</div>
-<div style="clear:both"></div>
+}
+</script>
+<?php } else if ($arguments['geomap_map'] == 'automap') { ?>
+<iframe name="nagvis" id="nagvis" src="/nagvis/nagvis/index.php?automap=1" width="100%" <?php if ($arguments['geomap_height'] > 0) echo 'style="height: '.$arguments['geomap_height'].'px;"'?>>
+	Error : Can not load NagVis.
+</iframe>
+<?php } else { ?>
+<iframe name="nagvis" id="nagvis" src="/nagvis/nagvis/?map=<?php echo $arguments['geomap_map']; ?>" width="100%" <?php if ($arguments['geomap_height'] > 0) echo 'style="height: '.$arguments['geomap_height'].'px;"'?>>
+	Error : Can not load NagVis.
+</iframe>
 <?php } ?>
