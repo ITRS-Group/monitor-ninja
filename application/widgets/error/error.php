6790049e72cb47632f959464fca6492c847761a6
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 16:50:19 +0100

    widgets: Add new error widget
    There's currently this horrible string in the widget helper that
    is being shipped off to the user when there's an error. Let's create a
    real widget instead!
    
    As it's not installed into the database, we don't have to worry about it
    showing up at inconvenient times.
    
    This is a newstyle widget. The newstyle API doesn't exist yet, but I
    predict it will soon magically spring into existence.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ application/widgets/error/error.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Widget for rendering load errors
+ */
+class Error_Widget extends widget_Base {
+	private $message = false;
+	public function __construct($widget_obj, $exception) {
+		$this->model = $widget_obj;
+		if ($this->model == false) {
+			$this->model = new stdclass();
+			$this->model->name = 'unknown';
+			$this->model->instance_id = 1;
+			$this->model->friendly_name = 'Unknown widget';
+		}
+		if (empty($exception))
+			$exception = new Exception("Unknown error");
+		$this->exception = $exception;
+
+		$this->auto_render = FALSE;
+		$this->theme_path = zend::instance('Registry')->get('theme_path');
+
+		# fetch our translation instance
+		$this->translate = zend::instance('Registry')->get('Zend_Translate');
+	}
+	public function index() {
+		require(dirname(__FILE__).'/view.php');
+	}
+}
---
+++ application/widgets/error/view.php
@@ -0,0 +1,2 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+Couldn't load widget <?php echo $this->model->name.': ['.$this->exception->getFile().':'.$this->exception->getLine().'] '.$this->exception->getMessage() ?>
