ed0c52f987d92d02ecd05d3abc0cf79cc2587a18
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 17:24:33 +0100

    widgets/tac_disabled: Port to new widget API
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/widgets/tac_disabled/tac_disabled.php
+++ application/widgets/tac_disabled/tac_disabled.php
@@ -6,47 +6,14 @@
  * @author     op5 AB
  * @license    GPL
  */
-class Tac_disabled_Widget extends widget_Core {
-	public function __construct()
+class Tac_disabled_Widget extends widget_Base {
+	protected $duplicatable = true;
+	public function index()
 	{
-		parent::__construct();
-
-		# needed to figure out path to widget
-		$this->set_widget_name(__CLASS__, basename(__FILE__));
-	}
-
-	public function index($arguments=false, $master=false)
-	{
-		# required to enable us to assign the correct
-		# variables to the calling controller
-		$this->master_obj = $master;
-
 		# fetch widget view path
 		$view_path = $this->view_path('view');
 
-		if (is_object($arguments[0])) {
-			$current_status = $arguments[0];
-			array_shift($arguments);
-		} else {
-			$current_status = new Current_status_Model();
-		}
-		if (!$current_status->data_present()) {
-			$current_status->analyze_status_data();
-		}
-
-		$widget_id = $this->widgetname;
-		$refresh_rate = 60;
-		if (isset($arguments['refresh_interval'])) {
-			$refresh_rate = $arguments['refresh_interval'];
-		}
-
-		$title = $this->translate->_('Disabled checks');
-		if (isset($arguments['widget_title'])) {
-			$title = $arguments['widget_title'];
-		}
-
-		# let view template know if wrapping div should be hidden or not
-		$ajax_call = request::is_ajax() ? true : false;
+		$current_status = $this->get_current_status();
 
 		# HOSTS DOWN / problems
 		$problem = array();
@@ -124,17 +91,6 @@ class Tac_disabled_Widget extends widget_Core {
 			$i++;
 		}
 
-		# fetch widget content
-		require_once($view_path);
-
-		if(request::is_ajax()) {
-			# output widget content
-			echo json::encode( $this->output());
-		} else {
-			$this->js = array('/js/tac_disabled');
-			# call parent helper to assign all
-			# variables to master controller
-			return $this->fetch();
-		}
+		require($view_path);
 	}
 }
