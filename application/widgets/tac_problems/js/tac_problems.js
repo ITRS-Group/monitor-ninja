4aa9410696c3e4402d39a5dfee0b93fbb1f7c11e
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 17:25:50 +0100

    widgets/tac_problems: Port to new widget API, and fix z-index for color picker
    It seems it got a too low value after the recent upgrade.
    
    This also removes a lot of redundant js code.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/widgets/tac_problems/js/tac_problems.js
+++ application/widgets/tac_problems/js/tac_problems.js
@@ -1,58 +1,19 @@
-function init_tac_problems(instance) {
-	$('#col_outages' + instance).live('colorpicked', function () {
-		$('#id_outages' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_outages', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_outages' + instance).addClass('status-outages').css('background', '');
-		}
-	});
+widget.register_widget_load('tac_problems', function() {
+	var tac_problems = this;
+	var problem_field_register = function(name) {
+		$('#' + tac_problems.widget_id + ' #col_' + name + '-' + tac_problems.instance_id).live('colorpicked', function () {
+			$('#' + tac_problems.widget_id + ' #id_' + name + tac_problems.instance_id).css('background', $(this).val());
+			tac_problems.save_custom_val($(this).val(), 'col_' + name);
+			if ($(this).val() == '#ffffff') {
+				$('#' + tac_problems.widget_id + ' #id_' + name + tac_problems.instance_id).css('background', '');
+			}
+		});
+	};
 
-	$('#col_hosts_down' + instance).live('colorpicked', function () {
-		$('#id_host_down' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_host_down', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_host_down' + instance).addClass('status-down').css('background', '');
-		}
-	});
-
-	$('#col_services_critical' + instance).live('colorpicked', function () {
-		$('#id_service_critical' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_critical', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_service_critical' + instance).addClass('status-critical').css('background', '');
-		}
-	});
-	$('#col_hosts_unreachable' + instance).live('colorpicked', function () {
-		$('#id_host_unreachable' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_host_unreachable', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_host_unreachable' + instance).addClass('status-unreachable').css('background', '');
-		}
-	});
-	$('#col_services_warning' + instance).live('colorpicked', function () {
-		$('#id_service_warning' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_warning', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_service_warning' + instance).addClass('status-warning').css('background', '');
-		}
-	});
-	$('#col_services_unknown' + instance).live('colorpicked', function () {
-		$('#id_service_unknown' + instance).css('background', $(this).val());
-		save_tac_disabled_colors($(this).val(), 'col_service_unknown', instance);
-		if ($(this).val() == '#ffffff') {
-			$('#id_service_unknown' + instance).addClass('status-unknown').css('background', '');
-		}
-	});
-}
-
-/**
-*	Save new color selection for this widget instance
-*/
-function save_tac_disabled_colors(newval, fieldname, instance)
-{
-	var ajax_url = _site_domain + _index_page + '/ajax/';
-	var url = ajax_url + "save_dynamic_widget_setting/";
-	var data = {page: _current_uri, fieldvalue: newval, fieldname:fieldname+instance, widget: instance};
-	$.post(url, data);
-	$.jGrowl(sprintf(_widget_settings_msg, self.name), { header: _success_header });
-}
+	problem_field_register('outages');
+	problem_field_register('host_down');
+	problem_field_register('services_critical');
+	problem_field_register('host_unreachable');
+	problem_field_register('services_warning');
+	problem_field_register('services_unknown');
+});
--- application/widgets/tac_problems/view.php
+++ application/widgets/tac_problems/view.php
@@ -1,65 +1,21 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<?php if (!$ajax_call) { ?>
-<div class="widget editable movable collapsable removable closeconfirm" id="widget-<?php echo $widget_id ?>">
-	<div class="widget-header"><span class="<?php echo $widget_id ?>_editable" id="<?php echo $widget_id ?>_title"><?php echo $title ?></span></div>
-	<div class="widget-editbox">
-		<?php echo form::open('ajax/save_widget_setting', array('id' => $widget_id.'_form', 'onsubmit' => 'return false;')); ?>
-		<fieldset>
-		<label for="<?php echo $widget_id ?>_refresh"><?php echo $this->translate->_('Refresh (sec)') ?>:</label>
-		<input size="3" type="text" name="<?php echo $widget_id ?>_refresh" id="<?php echo $widget_id ?>_refresh" value="<?php echo $refresh_rate ?>" />
-		<div id="<?php echo $widget_id ?>_slider"></div>
-		</fieldset><br />
-		<table cellpadding="0" cellspacing="0">
-			<tr>
-				<td colspan="2"><?php echo $this->translate->_('Color settings') ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $this->translate->_('Network outages') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_outages<?php echo $widget_id ?>" id="col_outages<?php echo $widget_id ?>" value="<?php echo $col_outages ?>" style="height:10px;width:10px;"></td>
-				<td style="width:10px"></td>
-				<td><?php echo $this->translate->_('Host Down') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_hosts_down<?php echo $widget_id ?>" id="col_hosts_down<?php echo $widget_id ?>" value="<?php echo $col_host_down ?>" style="height:10px;width:10px;"></td>
-			</tr>
-			<tr>
-				<td><?php echo $this->translate->_('Host Unreachable') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_hosts_unreachable<?php echo $widget_id ?>" id="col_hosts_unreachable<?php echo $widget_id ?>" value="<?php echo $col_host_unreachable ?>" style="height:10px;width:10px;"></td>
-				<td style="width:10px"></td>
-				<td><?php echo $this->translate->_('Service Critical') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_services_critical<?php echo $widget_id ?>" id="col_services_critical<?php echo $widget_id ?>" value="<?php echo $col_service_critical ?>" style="height:10px;width:10px;"></td>
-			</tr>
-			<tr>
-				<td><?php echo $this->translate->_('Service Warning') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_services_warning<?php echo $widget_id ?>" id="col_services_warning<?php echo $widget_id ?>" value="<?php echo $col_service_warning ?>" style="height:10px;width:10px;"></td>
-				<td style="width:10px"></td>
-				<td><?php echo $this->translate->_('Service Unknown') ?></td>
-				<td><input type="color" data-text="hidden" data-hex="true" name="col_services_unknown<?php echo $widget_id ?>" id="col_services_unknown<?php echo $widget_id ?>" value="<?php echo $col_service_unknown ?>" style="height:10px;width:10px;"></td>
-			</tr>
-		</table>
-		<?php echo form::close() ?>
-	</div>
-	<div class="widget-content">
-<?php } ?>
-		<table class="w-table">
-			<?php for ($i = 0; $i < count($problem); $i++) { ?>
-				<tr>
-					<td class="dark"><?php echo html::image($this->add_path('icons/24x24/shield-'.strtolower($problem[$i]['status']).'.png'), array('alt' => $problem[$i]['status'])) ?></td>
-					<td style="white-space: normal;<?php if ($problem[$i]['bgcolor']!='#ffffff') {?>background:<?php echo $problem[$i]['bgcolor']?><?php } ?>" <?php if ($problem[$i]['bgcolor']=='#ffffff') {?> class="status-<?php echo strtolower($problem[$i]['status']); ?>"<?php } ?> id="<?php echo $problem[$i]['html_id']?>">
-						<strong><?php echo strtoupper($problem[$i]['type']).' '.strtoupper($problem[$i]['status']) ?></strong><br />
-						<?php
-							echo html::anchor($problem[$i]['url'],$problem[$i]['title']);
-							if ($problem[$i]['no'] > 0)
-								echo ' / '.html::anchor($problem[$i]['onhost'],$problem[$i]['title2']);
-						?>
-					</td>
-				</tr>
-			<?php } if (count($problem) == 0) { ?>
-				<tr>
-					<td class="dark"><?php echo html::image($this->add_path('icons/24x24/shield-not-down.png'), array('alt' => $this->translate->_('N/A'))) ?></td>
-					<td><?php echo $this->translate->_('N/A')?></td>
-				</tr>
-			<?php } ?>
-		</table>
-<?php if (!$ajax_call) { ?>
-	</div>
-</div>
-<?php } ?>
+<table class="w-table">
+	<?php for ($i = 0; $i < count($problem); $i++) { ?>
+		<tr>
+			<td class="dark"><?php echo html::image($this->add_path('icons/24x24/shield-'.strtolower($problem[$i]['status']).'.png'), array('alt' => $problem[$i]['status'])) ?></td>
+			<td style="white-space: normal;<?php if ($problem[$i]['bgcolor']!='#ffffff') {?>background:<?php echo $problem[$i]['bgcolor']?><?php } ?>" <?php if ($problem[$i]['bgcolor']=='#ffffff') {?> class="status-<?php echo strtolower($problem[$i]['status']); ?>"<?php } ?> id="<?php echo $problem[$i]['html_id']?>">
+				<strong><?php echo strtoupper($problem[$i]['type']).' '.strtoupper($problem[$i]['status']) ?></strong><br />
+				<?php
+					echo html::anchor($problem[$i]['url'],$problem[$i]['title']);
+					if ($problem[$i]['no'] > 0)
+						echo ' / '.html::anchor($problem[$i]['onhost'],$problem[$i]['title2']);
+				?>
+			</td>
+		</tr>
+	<?php } if (count($problem) == 0) { ?>
+		<tr>
+			<td class="dark"><?php echo html::image($this->add_path('icons/24x24/shield-not-down.png'), array('alt' => $this->translate->_('N/A'))) ?></td>
+			<td><?php echo $this->translate->_('N/A')?></td>
+		</tr>
+	<?php } ?>
+</table>
