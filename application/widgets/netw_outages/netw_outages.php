61d6d1baf83a245a5ffff3faebd4e02f9c094a0f
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 17:22:29 +0100

    widgets/netw_outages: Port to new widget API
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/widgets/netw_outages/netw_outages.php
+++ application/widgets/netw_outages/netw_outages.php
@@ -6,52 +6,18 @@
  * @author     op5 AB
  * @license    GPL
  */
-class Netw_outages_Widget extends widget_Core {
-
-	public function __construct()
+class Netw_outages_Widget extends widget_Base {
+	protected $duplicatable = true;
+	public function index()
 	{
-		parent::__construct();
-
-		# needed to figure out path to widget
-		$this->set_widget_name(__CLASS__, basename(__FILE__));
-	}
-
-	public function index($arguments=false, $master=false)
-	{
-		# required to enable us to assign the correct
-		# variables to the calling controller
-		$this->master_obj = $master;
-
 		# fetch widget view path
 		$view_path = $this->view_path('view');
 
 		$auth = new Nagios_auth_Model();
 
-		$current_status = false;
-		if (is_object($arguments[0])) {
-			$current_status = $arguments[0];
-			array_shift($arguments);
-		}
-
 		# fetch info on outages
 		$outages = new Outages_Model();
-		$outage_data = $outages->fetch_outage_data($current_status);
-
-		# assign variables for our view
-
-		$widget_id = $this->widgetname;
-		$refresh_rate = 60;
-		if (isset($arguments['refresh_interval'])) {
-			$refresh_rate = $arguments['refresh_interval'];
-		}
-
-		$title = $this->translate->_('Network Outages');
-		if (isset($arguments['widget_title'])) {
-			$title = $arguments['widget_title'];
-		}
-
-		# let view template know if wrapping div should be hidden or not
-		$ajax_call = request::is_ajax() ? true : false;
+		$outage_data = $outages->fetch_outage_data($this->current_status);
 
 		$label = $this->translate->_('Blocking Outages');
 		$no_access_msg = $this->translate->_('N/A');
@@ -60,19 +26,6 @@ class Netw_outages_Widget extends widget_Core {
 
 		$user_has_access = $auth->view_hosts_root ? true : false;
 
-		# fetch widget content
-		require_once($view_path);
-
-		if(request::is_ajax()) {
-			# output widget content
-			echo json::encode( $this->output());
-		} else {
-			# set required extra resources
-			$this->js = array('/js/netw_outages');
-
-			# call parent helper to assign all
-			# variables to master controller
-			return $this->fetch();
-		}
+		require($view_path);
 	}
 }
