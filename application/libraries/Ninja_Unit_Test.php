633a092f329f2551f4446a704f1308354ea5f7dc
Robin Sonefors <robin.sonefors@op5.com>
2011-11-28 16:46:15 +0100

    ninja_unit_test: Put this back, make it use TapUnit
    This file is referenced occasionally, but didn't actually exist. When
    I claimed that I resurrected the unit tests, this file would have been
    required for anyone but me to be able to run them.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

---
+++ application/libraries/Ninja_Unit_Test.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'phptap/phptap.php';
+require_once 'phptap/tapunit.php';
+
+class Ninja_Unit_Test {
+	function __construct() {
+		$paths = array(MODPATH . 'unit_test');
+		ob_end_clean();
+		$main_tap = new phptap("Ninja unit test suite");
+
+		foreach ($paths as $path) {
+			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::KEY_AS_PATHNAME)) as $path => $file) {
+				// Skip files without "_Test" suffix
+				if ( ! $file->isFile() OR substr($path, -9) !== '_Test'.EXT)
+					continue;
+
+				// The class name should be the same as the file name
+				$class = substr($path, strrpos($path, '/') + 1, -(strlen(EXT)));
+
+				// Skip hidden files
+				if (substr($class, 0, 1) === '.')
+					continue;
+
+				// Check for duplicate test class name
+				if (class_exists($class, FALSE))
+					$main_tap->fail("Duplicate test class: $class in $path");
+
+				// Include the test class
+				include_once $path;
+				runUnit($class, $main_tap);
+			}
+		}
+
+		$exit_code = $main_tap->done(false);
+		exit((int)($exit_code > 1));
+	}
+}
