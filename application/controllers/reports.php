1ab38d0a13d2f69e764869221ecf8858ebcaff67
Carl Helmertz <chelmertz@op5.com>
2011-12-12 15:56:46 +0100

    reports: Do not close buffer when printing PDF
    There's not an extra buffer open, removing this call to close_buffers()
    also removes a couple of warnings mentioned in #4915
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -889,10 +889,6 @@ class Reports_Controller extends Authenticated_Controller
 			$this->auto_render=false;
 		}
 
-		if ($this->create_pdf) {
-			Kohana::close_buffers(FALSE);
-		}
-
 		$in_host = arr::search($_REQUEST, 'host', false);
 		if ($in_host === false)
 			$in_host = arr::search($_REQUEST, 'host_name', false);
