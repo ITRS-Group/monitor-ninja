910e594e419f8f6643b524206f01c608a7d90e6d
Carl Helmertz <chelmertz@op5.com>
2012-01-19 11:59:51 +0100

    Generate new name for saving local report copy, if already existing
    Using a combination of today's (Swedishly formatted) timestamp and part
    of a md5 hash, the loop shouln'd need to run that many times, even on
    systems generating a lot of reports.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/helpers/persist_pdf.php
+++ application/helpers/persist_pdf.php
@@ -13,7 +13,6 @@ class persist_pdf_Core
 	public function save($original_file, $save_here) {
 		$original_file = (string) $original_file;
 		$save_here = (string) $save_here;
-		// @todo bug 612
 		if(!is_readable($original_file)) {
 			throw new Exception("Can't read original file '$original_file'");
 		}
@@ -21,7 +20,20 @@ class persist_pdf_Core
 		if(!is_writable($local_persistent_filepath)) {
 			throw new Exception("Can't write output file '$save_here'");
 		}
-		$local_persistent_filepath .= date('Y-m-d').'-'.str_replace(K_PATH_CACHE.'/', null, $original_file);
+		$stripped_name = defined('K_PATH_CACHE') ? str_replace(K_PATH_CACHE.'/', null, $original_file) : $original_file;
+		$local_persistent_filepath .= date('Y-m-d').'-'.$stripped_name;
+		$safety_limit = 1000;
+		$current_try = 1;
+		while(file_exists($local_persistent_filepath)) {
+			$local_persistent_filepath = preg_replace(
+				'~(-[a-h0-9]{6})?\.pdf$~',
+				'-'.substr(md5(rand()), 0, 6).'.pdf',
+				$local_persistent_filepath
+			);
+			if(++$current_try > $safety_limit) {
+				throw new Exception("Do not want to overwrite '$local_persistent_filepath', aborting. Please remove the already existing reports.");
+			}
+		}
 		$able_to_copy = copy($original_file, $local_persistent_filepath);
 		if(!$able_to_copy) {
 			throw new Exception("Failed to copy '$original_file' to '$local_persistent_filepath'");
