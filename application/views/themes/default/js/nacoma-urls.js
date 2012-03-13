b908005e1d91089631b6526e65c2da46979e1793
Robin Sonefors <robin.sonefors@op5.com>
2012-03-13 15:14:56 +0100

    nacoma-urls: Fix so that it works with edit_special.php
    edit_special.php introduced paths-that-weren't-quite-paths in nacoma, so
    we need to use the location where we know we installed it instead of
    trying to guess.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/js/nacoma-urls.js
+++ application/views/themes/default/js/nacoma-urls.js
@@ -1,7 +1,9 @@
 var nacoma_base = _site_domain + '/' + _index_page + '/' + _current_uri;
+var nacoma_install_location = '/monitor/op5/nacoma/';
 
 // called by nacoma whenever a nacoma page is loaded
 onnacomaload = function (nacomawin) {
-	var nacomaurl = nacomawin.document.URL.split('/').pop();
-	history.replaceState(undefined, document.title, nacoma_base + '?page=' + encodeURIComponent(nacomaurl));
+	var nacomaurl = nacomawin.document.URL;
+	var interesting_url = nacomaurl.substr(nacomaurl.indexOf(nacoma_install_location) + nacoma_install_location.length).replace(/^\/+/, '');
+	history.replaceState(undefined, document.title, nacoma_base + '?page=' + encodeURIComponent(interesting_url));
 }
