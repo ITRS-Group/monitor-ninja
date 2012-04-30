8d9e9819b4fe224dba02414084b31bd00d0e5cde
Carl Helmertz <chelmertz@op5.com>
2012-04-30 15:56:27 +0200

    schedule_downtime.js: Remove console.log() calls
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/views/themes/default/command/js/schedule_downtime.js
+++ application/views/themes/default/command/js/schedule_downtime.js
@@ -6,8 +6,6 @@ $(function() {
 		var form = $(this);
 		var timestamp = new Date($('#field_start_time').val());
 		var grace_time_in_ms = grace_time_in_s * 1000;
-		console.log(timestamp.getTime());
-		console.log(new Date().getTime() - grace_time_in_ms);
 		if(timestamp.getTime() < (new Date().getTime() - grace_time_in_ms)) {
 			return confirm("Since you submitted a starting time that's more than "+grace_time_in_s+" seconds old, this will be a retroactively scheduled downtime and thus it will show up separately in logs.\n\nIf this is a mistake, choose 'cancel' and correct the value.");
 		}
