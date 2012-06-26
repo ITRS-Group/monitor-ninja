c04b598d9b04dabe64bfaaa5ebd44f9d9641ada4
Robin Sonefors <robin.sonefors@op5.com>
2012-06-26 13:29:45 +0200

    Remove FirePHP
    The FirePHP profiler looks cooler than the vanilla Kohana one, but it
    isn't any more informative, it's Firefox-only (which I'm pretty much the
    only dev that uses), and if you're unlucky (which I am) it breaks and
    you have to delete your Firefox profile and it might start to work
    again. So, now I kill it.
    
    While we're at it, leave the decision whether to use the profiler up to
    the profiler config, stop doing stupid and incorrect things to the weird
    ALLOW_PRODUCTION flag.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -5,7 +5,7 @@
  * Array of section names to display in the Profiler, TRUE to display all of them.
  * Built in sections are benchmarks, database, session, post and cookies, custom sections can be used too.
  */
-$config['show'] = TRUE;
+$config['show'] = false;
 
 # check for custom config files that
 # won't be overwritten on upgrade
