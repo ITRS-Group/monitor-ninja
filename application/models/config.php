ef1b5cc6ce400d32a30b8c4d51105bad13f0fcf3
Robin Sonefors <robin.sonefors@op5.com>
2012-07-02 17:45:24 +0200

    models/config: Implement the count option properly
    When trying to find out the number of pages to use in the pager, instead
    of retrieving all data on all objects in the config and counting them,
    only retrieve a count(). This is faster.
    
    How much faster? On my test config, it brings down the config rendering
    time from the minute range to the sub-second range. So "quite a bit".
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/models/config.php
+++ application/models/config.php
@@ -51,11 +51,11 @@ class Config_Model extends Model {
 				$offset_limit = $count!==false ? "" : " LIMIT " . $num_per_page." OFFSET ".$offset;
 			else
 				$offset_limit = '';
-				//$offset_limit = $count!==false ? "" : " LIMIT ".$num_per_page;
 
 			switch($type) {
 				case 'hosts':
-			        $sql = "SELECT ".
+					if (!$count) {
+						$sql = "SELECT ".
 							 "h.host_name, h.alias, h.address,  h.max_check_attempts, h.check_interval,".
 							 "h.retry_interval, h.check_command, h.check_period, h.notification_interval,".
 							 "h.notes, h.notes_url, h.action_url, h.icon_image, h.icon_image_alt,".
@@ -66,11 +66,14 @@ class Config_Model extends Model {
 							 "h.high_flap_threshold, h.process_perf_data, h.failure_prediction_enabled,".
 							 "h.retain_status_information, h.retain_nonstatus_information ".
 							 "FROM host h ORDER BY h.host_name".$offset_limit;
-			                 /* Failure Prediction Options*/
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM host';
+					}
 				break;
 
 				case 'services':
-					$sql = "SELECT ".
+					if (!$count) {
+						$sql = "SELECT ".
 									"host_name, service_description, max_check_attempts, check_interval,".
 									"retry_interval, check_command, check_period, parallelize_check, is_volatile,".
 									"obsess_over_service, active_checks_enabled, passive_checks_enabled,".
@@ -82,10 +85,14 @@ class Config_Model extends Model {
 									"retain_status_information, retain_nonstatus_information ".
 									"FROM service ORDER BY host_name, service_description".$offset_limit;
 									/* Failure Prediction Options*/
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM service';
+					}
 				break;
 
 				case 'contacts':
-					$sql = "SELECT c.contact_name, c.alias, c.email, c.pager, c.service_notification_options,".
+					if (!$count) {
+						$sql = "SELECT c.contact_name, c.alias, c.email, c.pager, c.service_notification_options,".
 									"c.host_notification_options, c.service_notification_period, c.host_notification_period,".
 									"c.".self::SERVICE_NOTIFICATION_COMMANDS.", c.".self::HOST_NOTIFICATION_COMMANDS.",".
 									"c.retain_status_information, c.retain_nonstatus_information,".
@@ -93,63 +100,102 @@ class Config_Model extends Model {
 									"FROM contact c, timeperiod h_n, timeperiod s_n ".
 									"WHERE h_n.id = c.host_notification_period AND s_n.id = c.service_notification_period ".
 									"ORDER BY c.contact_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM contact';
+					}
 				break;
 
 				case 'commands':
-					$sql = "SELECT command_name, command_line FROM command ORDER BY command_name".$offset_limit;
+					if (!$count) {
+						$sql = "SELECT command_name, command_line FROM command ORDER BY command_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM command';
+					}
 				break;
 
 				case 'timeperiods':
-					$sql = "SELECT timeperiod_name, alias, monday, tuesday, wednesday, thursday, friday,".
+					if (!$count) {
+						$sql = "SELECT timeperiod_name, alias, monday, tuesday, wednesday, thursday, friday,".
 									"saturday, sunday ".
 									"FROM timeperiod ORDER BY timeperiod_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM timeperiod';
+					}
 				break;
 
 				case 'host_groups':
-					$sql = "SELECT hostgroup_name, alias, notes, notes_url, action_url ".
+					if (!$count) {
+						$sql = "SELECT hostgroup_name, alias, notes, notes_url, action_url ".
 									"FROM hostgroup ORDER BY hostgroup_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM hostgroup';
+					}
 				break;
 
 				case 'service_groups':
-					$sql = "SELECT servicegroup_name, alias, notes, notes_url, action_url ".
+					if (!$count) {
+						$sql = "SELECT servicegroup_name, alias, notes, notes_url, action_url ".
 									"FROM servicegroup ORDER BY servicegroup_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM servicegroup';
+					}
 				break;
 
 				case 'host_escalations':
-					$sql = "SELECT h.host_name, he.first_notification, he.last_notification, he.notification_interval,".
+					if (!$count) {
+						$sql = "SELECT h.host_name, he.first_notification, he.last_notification, he.notification_interval,".
 									"he.escalation_period, he.escalation_options, he.id as he_id ".
 									"FROM hostescalation he, host h ".
 									"WHERE h.id = he.host_name ".
 									"ORDER BY h.host_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM hostescalation';
+					}
 				break;
 
 				case 'service_escalations':
-					$sql = "SELECT s.host_name, s.service_description, se.first_notification, se.last_notification, se.notification_interval,".
+					if (!$count) {
+						$sql = "SELECT s.host_name, s.service_description, se.first_notification, se.last_notification, se.notification_interval,".
 									"se.escalation_period, se.escalation_options, se.id as se_id ".
 									"FROM serviceescalation se, service s ".
 									"WHERE s.id = se.service ".
 									"ORDER BY s.service_description".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM serviceescalation';
+					}
 				break;
 
 				case 'contact_groups':
-					$sql = "SELECT contactgroup_name, alias FROM contactgroup ORDER BY contactgroup_name".$offset_limit;
+					if (!$count) {
+						$sql = "SELECT contactgroup_name, alias FROM contactgroup ORDER BY contactgroup_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM contactgroup';
+					}
 				break;
 
 				case 'host_dependencies';
-					$sql = "SELECT dh.host_name as dependent, mh.host_name as master, dependency_period, execution_failure_options,".
+					if (!$count) {
+						$sql = "SELECT dh.host_name as dependent, mh.host_name as master, dependency_period, execution_failure_options,".
 									"notification_failure_options ".
 									"FROM hostdependency hd, host mh, host dh ".
 									"WHERE hd.dependent_host_name = dh.id AND hd.host_name = mh.id ".
 									"ORDER BY dh.host_name".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM hostdependency';
+					}
 				break;
 
 				case 'service_dependencies';
-					$sql = "SELECT ds.host_name as dependent_host, ds.service_description as dependent_service,".
+					if (!$count) {
+						$sql = "SELECT ds.host_name as dependent_host, ds.service_description as dependent_service,".
 									"ms.service_description as master_service, ms.host_name as master_host,".
 									"dependency_period, execution_failure_options, notification_failure_options ".
 									"FROM servicedependency sd, service ds, service ms ".
 									"WHERE ds.id = sd.dependent_service AND ms.id = sd.service ".
 									"ORDER BY ds.service_description".$offset_limit;
+					} else {
+						$sql = 'SELECT COUNT(1) AS count FROM servicedependency';
+					}
 				break;
 			}
 
@@ -234,8 +280,8 @@ class Config_Model extends Model {
 		    }
 			# End host/service special casing #
 
-		    if ($count !== false) {
-				return $result ? count($result) : 0;
+		    if ($count) {
+				return $result[0]->count;
 		    }
 		    return count($result) ? $result : false;
                         //$result->count() ? $result->result(): false;
