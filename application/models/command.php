ITRS-Group/monitor-ninja

commit 82b83dda6639dd913c782c8d5318d8beead1e660

models/command: Use config::get, rather than reimplementing it
Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

application/models/command.php
@@ -23,13 +23,7 @@ class Command_Model extends Model
 		# the underscore is an implementation detail ("don't pass this straight
 		# to nagios") that should not be exposed in config/nagdefault.php
 		$setting = 'nagdefault.'.ltrim($setting, '_');
-		$value = Ninja_setting_Model::fetch_page_setting($setting, '*');
-		if (is_object($value))
-			$value = $value->setting;
-		else
-			# all checkboxes that used to be hardcoded to true is set to true
-			# in the config file. as this defaults to false, we stay consistent.
-			$value = Kohana::config($setting);
+		$value = config::get($setting);
 		return $value;
 	}
 

commit e68c907652a00b7cda98accdaef206bfec789880

models/command: Fix nagdefault handling
This restores it again after commit
82b83dda6639dd913c782c8d5318d8beead1e660 broke it. Fixes bug #5721.

Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

application/models/command.php
@@ -23,7 +23,7 @@ class Command_Model extends Model
 		# the underscore is an implementation detail ("don't pass this straight
 		# to nagios") that should not be exposed in config/nagdefault.php
 		$setting = 'nagdefault.'.ltrim($setting, '_');
-		$value = config::get($setting);
+		$value = config::get($setting, '*', false, true);
 		return $value;
 	}
 