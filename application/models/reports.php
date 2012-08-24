d24483694548bc296bcc4e093a7e603606ec3c38
Robin Sonefors <robin.sonefors@op5.com>
2012-08-24 15:14:00 +0200

    Revert "models/reports: Exceptions should be included if they match"
    Sorry, never meant to push :/
    
    This reverts commit 7cb462701ecf38816e19c0f33d400941b7d64f0b.

--- application/models/reports.php
+++ application/models/reports.php
@@ -993,7 +993,8 @@ class Reports_Model extends Model
 					$check_exception = !($days_since_start % $x['skip_interval']);
 				}
 
-				if (!$check_exception || $exp_start < $day_time || $exp_end > 86400 + $day_time)
+				# We must add 1 day to exp_end, as times during that day must be included
+				if (!$check_exception || $exp_start > $day_time || $exp_end + 86400 < $day_time)
 					continue;
 
 				if(!isset($all_exceptions[$day_year]))
