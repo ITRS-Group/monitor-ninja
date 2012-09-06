6642e40e1493d88b3954188cb5cc93991a34e662
Robin Sonefors <robin.sonefors@op5.com>
2012-09-06 11:03:53 +0200

    Avail_options: Set proper default value for report period
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/libraries/Avail_options.php
+++ application/libraries/Avail_options.php
@@ -1,3 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Avail_options extends Report_options {}
+class Avail_options extends Report_options {
+	public function __construct($options = false) {
+		parent::__construct($options);
+		$this->vtypes['report_period']['default'] = 'last7days';
+	}
+}
