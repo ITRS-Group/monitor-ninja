972c71d6d2ded5561d5be830d940f224fa80b2b0
Carl Helmertz <chelmertz@op5.com>
2012-09-12 14:16:34 +0200

    schedule: Refer to correct controller for helptexts
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/views/themes/default/schedule/new_schedule.php
+++ application/views/themes/default/schedule/new_schedule.php
@@ -6,29 +6,29 @@
 		<table id="new_schedule_report_table">
 			<tr>
 				<td>
-					<label for="type"><?php echo help::render('report-type-save').' '._('Select report type') ?></label><br />
+					<label for="type"><?php echo help::render('report-type-save', 'reports').' '._('Select report type') ?></label><br />
 					<?php echo form::dropdown(array('name' => 'type'), $defined_report_types); ?><br />
 					<?php if (!empty($available_schedule_periods)) { ?>
-						<label for="period"><?php echo help::render('interval').' '._('Report Interval') ?></label><br />
+						<label for="period"><?php echo help::render('interval', 'reports').' '._('Report Interval') ?></label><br />
 						<select name="period" id="period">
 						<?php	foreach ($available_schedule_periods as $id => $period) { ?>
 							<option value="<?php echo $id ?>"><?php echo $period ?></option>
 						<?php	} ?>
 						</select><br />
 					<?php } ?>
-					<label for="saved_report_id"><?php echo help::render('select-report').' '._('Select report') ?></label><br />
+					<label for="saved_report_id"><?php echo help::render('select-report', 'reports').' '._('Select report') ?></label><br />
 					<select name="saved_report_id" id="saved_report_id">
 						<option value=""> - <?php echo _('Select saved report') ?> - </option>
 						<?php foreach ($saved_reports as $report) { ?>
 						<option value="<?php echo $report->id ?>"><?php echo $report->report_name ?></option>
 						<?php } ?>
 					</select><br />
-					<label for="recipients"><?php echo help::render('recipents').' '._('Recipients') ?></label><br /><input type="text" class="schedule" name="recipients" id="recipients" value="" />
+					<label for="recipients"><?php echo help::render('recipents', 'reports').' '._('Recipients') ?></label><br /><input type="text" class="schedule" name="recipients" id="recipients" value="" />
 				</td>
 				<td>
-					<label for="filename"><?php echo help::render('filename').' '._('Filename (defaults to pdf, may end in .csv)') ?></label><br /><input type="text" class="schedule" name="filename" id="filename" value="" /><br />
-					<label for="description"><?php echo help::render('description').' '._('Description') ?></label><br /><textarea cols="31" rows="4" id="description" name="description"></textarea><br />
-					<label for="local_persistent_filepath"><?php echo help::render('local_persistent_filepath').' '._("Save report in this local folder") ?></label><br /><input type="text" class="schedule" name="local_persistent_filepath" id="local_persistent_filepath" value="" />
+					<label for="filename"><?php echo help::render('filename', 'reports').' '._('Filename (defaults to pdf, may end in .csv)') ?></label><br /><input type="text" class="schedule" name="filename" id="filename" value="" /><br />
+					<label for="description"><?php echo help::render('description', 'reports').' '._('Description') ?></label><br /><textarea cols="31" rows="4" id="description" name="description"></textarea><br />
+					<label for="local_persistent_filepath"><?php echo help::render('local_persistent_filepath', 'reports').' '._("Save report in this local folder") ?></label><br /><input type="text" class="schedule" name="local_persistent_filepath" id="local_persistent_filepath" value="" />
 				</td>
 			</tr>
 		</table>
