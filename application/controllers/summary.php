ITRS-Group/monitor-ninja

commit af1f526a5f68d8c03ee158c608e28df600798a24

summary: Remove all "alert totals by" functions
The only one that actually works is "alert totals by whatever it is we
just selected". Apparently, nobody was upset by this blaring bug, so I'm
hereby removing the UI for controlling absolutely nothing.

Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

application/controllers/summary.php
@@ -16,10 +16,6 @@ class Summary_Controller extends Base_reports_Controller
 	const RECENT_ALERTS = 1;
 	const ALERT_TOTALS = 2;
 	const TOP_ALERT_PRODUCERS = 3;
-	const ALERT_TOTALS_HG = 4;
-	const ALERT_TOTALS_HOST = 5;
-	const ALERT_TOTALS_SERVICE = 6;
-	const ALERT_TOTALS_SG = 7;
 
 	public $type = 'summary';
 	public $reports_model = false;
@@ -157,27 +153,6 @@ class Summary_Controller extends Base_reports_Controller
 	}
 
 	/**
-	 * @param int $report_type
-	 * @return string
-	 * @throws InvalidArgumentException
-	 */
-	private function _get_summary_variant_by_report_type($report_type) {
-		$types = array(
-			self::RECENT_ALERTS => _("Most recent hard alerts"),
-			self::ALERT_TOTALS => _("Most recent hard host alerts"),
-			self::TOP_ALERT_PRODUCERS => _("Top hard alert producers"),
-			self::ALERT_TOTALS_HG => _('Overall totals'),
-			self::ALERT_TOTALS_HOST => _('Overall totals'),
-			self::ALERT_TOTALS_SERVICE => _('Overall totals'),
-			self::ALERT_TOTALS_SG => _('Overall totals')
-		);
-		if(!array_key_exists($report_type, $types)) {
-			throw new InvalidArgumentException("Invalid report type");
-		}
-		return $types[$report_type];
-	}
-
-	/**
 	 * Generates an alert summary report
 	 */
 	public function generate($input=false)
@@ -200,14 +175,6 @@ class Summary_Controller extends Base_reports_Controller
 			break;
 
 		 case self::ALERT_TOTALS:
-		 case self::ALERT_TOTALS_HG:
-		 case self::ALERT_TOTALS_SG:
-		 case self::ALERT_TOTALS_HOST:
-			$result = $this->reports_model->alert_totals();
-			break;
-
-		case self::ALERT_TOTALS_SERVICE:
-			$this->options['service_description'] = $this->_populate_services();
 			$result = $this->reports_model->alert_totals();
 			break;
 
@@ -260,10 +227,7 @@ class Summary_Controller extends Base_reports_Controller
 		$views = array(
 			self::TOP_ALERT_PRODUCERS => 'toplist',
 			self::RECENT_ALERTS => 'latest',
-			self::ALERT_TOTALS_HG => 'alert_totals',
-			self::ALERT_TOTALS_HOST => 'alert_totals',
-			self::ALERT_TOTALS_SERVICE => 'alert_totals',
-			self::ALERT_TOTALS_SG => 'alert_totals',
+			self::ALERT_TOTALS => 'alert_totals',
 		);
 
 		$this->template->set_global('type', $this->type);

application/libraries/Summary_options.php
@@ -7,16 +7,12 @@ class Summary_options_Core extends Report_options
 		$this->vtypes['summary_type'] = array('type' => 'enum', 'default' => Summary_Controller::TOP_ALERT_PRODUCERS, 'options' => array(
 			Summary_Controller::RECENT_ALERTS => _('Most Recent Alerts'),
 			Summary_Controller::ALERT_TOTALS => _('Alert Totals'),
-			Summary_Controller::TOP_ALERT_PRODUCERS => _('Top Alert Producers'),
-			Summary_Controller::ALERT_TOTALS_HG => _('Alert Totals By Hostgroup'),
-			Summary_Controller::ALERT_TOTALS_HOST => _('Alert Totals By Host'),
-			Summary_Controller::ALERT_TOTALS_SG => _('Alert Totals By Servicegroup'),
-			Summary_Controller::ALERT_TOTALS_SERVICE => _('Alert Totals By Service')));
-		$this->vtypes['standardreport'] = array('type' => 'enum', 'default' => false, 'options' => array(
+			Summary_Controller::TOP_ALERT_PRODUCERS => _('Top Alert Producers')));
+		$this->vtypes['standardreport'] = array('type' => 'enum', 'default' => 1, 'options' => array(
 			1 => _('Most Recent Hard Alerts'),
 			2 => _('Most Recent Hard Host Alerts'),
 			3 => _('Most Recent Hard Service Alerts'),
-			4 => _('Top Alert Producers'),
+			4 => _('Top Hard Alert Producers'),
 			5 => _('Top Hard Host Alert Producers'),
 			6 => _('Top Hard Service Alert Producers')));
 
@@ -57,34 +53,6 @@ class Summary_options_Core extends Report_options
 				break;
 			}
 
-			break;
-		 case 'summarytype':
-			if ($value === Summary_Controller::ALERT_TOTALS) {
-				if (isset($this->options['servicegroup']))
-					$value = self::ALERT_TOTALS_SG;
-				elseif (isset($this->options['hostgroup']))
-					$value = self::ALERT_TOTALS_HG;
-				elseif (isset($this->options['service_description']))
-					$value = self::ALERT_TOTALS_SERVICE;
-				elseif (isset($this->options['host_name']))
-					$value = self::ALERT_TOTALS_HOST;
-			}
-			break;
-		 case 'servicegroup':
-			if ($this['summarytype'] === Summary_Controller::ALERT_TOTALS)
-				$this->options['summarytype'] = self::ALERT_TOTALS_SG;
-			break;
-		 case 'hostgroup':
-			if ($this['summarytype'] === Summary_Controller::ALERT_TOTALS)
-				$this->options['summarytype'] = self::ALERT_TOTALS_HG;
-			break;
-		 case 'host_name':
-			if ($this['summarytype'] === Summary_Controller::ALERT_TOTALS)
-				$this->options['summarytype'] = self::ALERT_TOTALS_HOST;
-			break;
-		 case 'service_description':
-			if ($this['summarytype'] === Summary_Controller::ALERT_TOTALS)
-				$this->options['summarytype'] = self::ALERT_TOTALS_SERVICE;
 			break;
 		}
 		return parent::update_value($name, $value);

application/models/reports.php
@@ -1726,14 +1726,19 @@ class Reports_Model extends Model
 		$result = false;
 		# groups must be first here, since the other variables
 		# are expanded in the build_alert_summary_query() method
-		if ($this->options['servicegroup']) {
+		switch ($this->options['report_type']) {
+		 case 'servicegroups':
 			$result = $this->alert_totals_by_servicegroup($dbr);
-		} elseif ($this->options['hostgroup']) {
+			break;
+		 case 'hostgroups':
 			$result = $this->alert_totals_by_hostgroup($dbr);
-		} elseif ($this->options['service_description']) {
+			break;
+		 case 'services':
 			$result = $this->alert_totals_by_service($dbr);
-		} elseif ($this->options['host_name']) {
+			break;
+		 case 'hosts':
 			$result = $this->alert_totals_by_host($dbr);
+			break;
 		}
 
 		$this->set_alert_total_totals($result);

commit 8ab41f534ba043ce69f9c62daa0d4bed8c0c978d

CSV: Fix "alert by" output
This was (mostly) broken in commit af1f526.

Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

application/controllers/summary.php
@@ -191,6 +191,8 @@ class Summary_Controller extends Base_reports_Controller
 			$this->template->summary_type = $this->options['summary_type'];
 			$this->template->result = $result;
 			$this->template->date_format = nagstat::date_format();
+			$this->template->host_state_names = $this->host_state_names;
+			$this->template->service_state_names = $this->service_state_names;
 
 			return;
 		}

application/views/themes/default/summary/csv.php
@@ -58,19 +58,19 @@ if(Summary_Controller::RECENT_ALERTS == $options['summary_type']) {
 		'HARD ALERTS',
 		'TOTAL ALERTS'
 	);
-	switch($options['summary_type']) {
-		case Summary_Controller::ALERT_TOTALS_HG:
+	switch($options['report_type']) {
+		case 'hostgroups':
 			$label = _('Hostgroup');
 			array_splice($header, 1, 1, 'HOSTGROUP');
 			break;
-		case Summary_Controller::ALERT_TOTALS_HOST:
+		case 'hosts':
 			$label = _('Host');
 			break;
-		case Summary_Controller::ALERT_TOTALS_SERVICE:
+		case 'services':
 			$label = _('Service');
 			array_splice($header, 2, 0, 'SERVICE');
 			break;
-		case Summary_Controller::ALERT_TOTALS_SG:
+		case 'servicegroups':
 			$label = _('Servicegroup');
 			array_splice($header, 1, 1, 'SERVICEGROUP');
 			break;
@@ -78,14 +78,14 @@ if(Summary_Controller::RECENT_ALERTS == $options['summary_type']) {
 	$csv_content[] = '"'.implode('", "', $header).'"';
 	foreach ($result as $host_name => $ary) {
 		$service_name = null;
-		if($options['summary_type'] == Summary_Controller::ALERT_TOTALS_SERVICE) {
+		if($options['report_type'] == 'services') {
 			list($host_name, $service_name) = explode(';', $host_name);
 		}
 		foreach($ary['host'] as $state => $host) {
 			$row = array(
 				$label,
 				$host_name,
-				$this->host_state_names[$state],
+				$host_state_names[$state],
 				$host[0], # soft
 				$host[1], # hard
 				$host[0] + $host[1] # total
@@ -99,7 +99,7 @@ if(Summary_Controller::RECENT_ALERTS == $options['summary_type']) {
 			$row = array(
 				$label,
 				$host_name,
-				$this->service_state_names[$state],
+				$service_state_names[$state],
 				$service[0], # soft
 				$service[1], # hard
 				$service[0] + $service[1] # total