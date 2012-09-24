5be3c789a0b36d18ba21c7c31588421c239fbb0e
Carl Helmertz <chelmertz@op5.com>
2012-09-24 12:32:24 +0200

    mysql: Fix collation for merlin.ninja_report_comments
    Otherwise it's very hard to join with otherwise collated table rows (by
    default, my collation is latin1_swedish_ci, of some reason).
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- sql/mysql/ninja_db_v5_to_v6.sql
+++ sql/mysql/ninja_db_v5_to_v6.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS ninja_report_comments (
   username varchar(100) NOT NULL,
   user_comment TEXT NOT NULL,
   KEY report_entry (timestamp, host_name, service_description, event_type)
-);
+) COLLATE latin1_general_cs;
 
