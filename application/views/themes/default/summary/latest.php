3dcb89441a72c965c5d82bdd50f26bc6a4ee743f
Robin Sonefors <robin.sonefors@op5.com>
2012-09-24 14:30:55 +0200

    views/summary: Remove extra div wrapper
    The extra div wrapper created double margins, which looked silly.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/summary/latest.php
+++ application/views/themes/default/summary/latest.php
@@ -1,45 +1,43 @@
 <?php defined('SYSPATH') OR die("No direct access allowed"); ?>
-<div class="w98 left">
-	<?php echo isset($pagination)?$pagination:'' ?>
-	<p style="margin-top:50px; margin-bottom: 14px"><?php date::duration($options['start_time'], $options['end_time']); ?></p>
-	<table>
-		<tr>
-			<th class="headerNone left"><?php //echo _('State'); ?></th>
-			<th class="headerNone left"><?php echo _('Time'); ?></th>
-			<th class="headerNone left"><?php echo _('Alert Types'); ?></th>
-			<th class="headerNone left"><?php echo _('Host'); ?></th>
-			<th class="headerNone left"><?php echo _('Service'); ?></th>
-			<th class="headerNone left"><?php echo _('State Types'); ?></th>
-			<th class="headerNone left"><?php echo _('Information'); ?></th>
-		</tr>
-		<?php
-		$i = 0;
-		if (!empty($result)) {
-			foreach ($result as $ary) {
-				$row = alert_history::get_user_friendly_representation($ary);
-				$i++;
-				echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').' eventrow" data-statecode="'.$ary['event_type'].'" data-timestamp="'.$ary['timestamp'].'" data-hostname="'.$ary['host_name'].'" data-servicename="'.$ary['service_description'].'">';
+<?php echo isset($pagination)?$pagination:'' ?>
+<p style="margin-top:50px; margin-bottom: 14px"><?php date::duration($options['start_time'], $options['end_time']); ?></p>
+<table>
+	<tr>
+		<th class="headerNone left"><?php //echo _('State'); ?></th>
+		<th class="headerNone left"><?php echo _('Time'); ?></th>
+		<th class="headerNone left"><?php echo _('Alert Types'); ?></th>
+		<th class="headerNone left"><?php echo _('Host'); ?></th>
+		<th class="headerNone left"><?php echo _('Service'); ?></th>
+		<th class="headerNone left"><?php echo _('State Types'); ?></th>
+		<th class="headerNone left"><?php echo _('Information'); ?></th>
+	</tr>
+	<?php
+	$i = 0;
+	if (!empty($result)) {
+		foreach ($result as $ary) {
+			$row = alert_history::get_user_friendly_representation($ary);
+			$i++;
+			echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').' eventrow" data-statecode="'.$ary['event_type'].'" data-timestamp="'.$ary['timestamp'].'" data-hostname="'.$ary['host_name'].'" data-servicename="'.$ary['service_description'].'">';
+	?>
+		<td class="icon status">
+			<?php echo $row['image'] ?>
+		</td>
+		<td><?php echo date(nagstat::date_format(), $ary['timestamp']); ?></td>
+		<td><?php echo $row['type']; ?></td>
+		<td><?php echo $ary['host_name']?html::anchor(base_url::get().'extinfo/details/?type=host&host='.urlencode($ary['host_name']), $ary['host_name']):'' ?></td>
+		<td><?php echo $ary['service_description']?html::anchor(base_url::get().'extinfo/details/?type=service&host='.urlencode($ary['host_name']).'&service='.$ary['service_description'], $ary['service_description']):'' ?></td>
+		<td><?php echo $row['softorhard']; ?></td>
+		<td><div class="regular-output"><?php echo htmlspecialchars($ary['output']); ?></div>
+		<div class="comments">
+		<?php if (isset($ary['user_comment']))
+			echo '<span class="content">'.$ary['user_comment'].'</span><br /><span class="author">/'.$ary['username'].'</span>';
+		else
+			echo '<img src="'.ninja::add_path('icons/16x16/add-comment.png').'"/>'
 		?>
-			<td class="icon status">
-				<?php echo $row['image'] ?>
-			</td>
-			<td><?php echo date(nagstat::date_format(), $ary['timestamp']); ?></td>
-			<td><?php echo $row['type']; ?></td>
-			<td><?php echo $ary['host_name']?html::anchor(base_url::get().'extinfo/details/?type=host&host='.urlencode($ary['host_name']), $ary['host_name']):'' ?></td>
-			<td><?php echo $ary['service_description']?html::anchor(base_url::get().'extinfo/details/?type=service&host='.urlencode($ary['host_name']).'&service='.$ary['service_description'], $ary['service_description']):'' ?></td>
-			<td><?php echo $row['softorhard']; ?></td>
-			<td><div class="regular-output"><?php echo htmlspecialchars($ary['output']); ?></div>
-			<div class="comments">
-			<?php if (isset($ary['user_comment']))
-				echo '<span class="content">'.$ary['user_comment'].'</span><br /><span class="author">/'.$ary['username'].'</span>';
-			else
-				echo '<img src="'.ninja::add_path('icons/16x16/add-comment.png').'"/>'
-			?>
-			</div>
-			</td>
-		</tr>
-		<?php }
-		} ?>
-	</table>
-	<?php echo isset($pagination)?$pagination:'' ?>
-</div>
+		</div>
+		</td>
+	</tr>
+	<?php }
+	} ?>
+</table>
+<?php echo isset($pagination)?$pagination:'' ?>
--- application/views/themes/default/summary/toplist.php
+++ application/views/themes/default/summary/toplist.php
@@ -1,37 +1,35 @@
 <?php defined('SYSPATH') OR die("No direct access allowed"); ?>
-<div class="left w98">
-	<p style="margin-top:-10px; margin-bottom: 14px"><?php date::duration($options['start_time'], $options['end_time']); ?></p>
-	<table>
-		<tr>
-			<th class="headerNone left"><?php echo _('Rank'); ?></th>
-			<th class="headerNone left"><?php echo _('Producer Type'); ?></th>
-			<th class="headerNone left"><?php echo _('Host'); ?></th>
-			<th class="headerNone left"><?php echo _('Service'); ?></th>
-			<th class="headerNone left"><?php echo _('Total Alerts'); ?></th>
-		</tr>
-		<?php
-		$i=0;
-	if (count($result)>0 && !empty($result)) {
-		foreach ($result as $rank => $ary) {
-			$i++;
-			echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').'">';
-			if (empty($ary['service_description'])) {
-				$producer = _('Host');
-				$ary['service_description'] = 'N/A';
-			} else {
-				$producer = _('Service');
-				$ary['service_description'] = html::anchor(base_url::get().'extinfo/details/?type=service&host='.urlencode($ary['host_name']).'&service='.urlencode($ary['service_description']), $ary['service_description']);
-			}
-		?>
-			<td class="icon"><?php echo $rank; ?></td>
-			<td><?php echo $producer; ?></td>
-			<td><?php echo html::anchor(base_url::get().'extinfo/details/?type=host&host='.urlencode($ary['host_name']), $ary['host_name']) ?></td>
-			<td><?php echo $ary['service_description']; ?></td>
-			<td><?php echo $ary['total_alerts']; ?></td>
-		</tr>
-		<?php }
-	}?>
-	</table>
-</div>
+<p style="margin-top:-10px; margin-bottom: 14px"><?php date::duration($options['start_time'], $options['end_time']); ?></p>
+<table>
+	<tr>
+		<th class="headerNone left"><?php echo _('Rank'); ?></th>
+		<th class="headerNone left"><?php echo _('Producer Type'); ?></th>
+		<th class="headerNone left"><?php echo _('Host'); ?></th>
+		<th class="headerNone left"><?php echo _('Service'); ?></th>
+		<th class="headerNone left"><?php echo _('Total Alerts'); ?></th>
+	</tr>
+	<?php
+	$i=0;
+if (count($result)>0 && !empty($result)) {
+	foreach ($result as $rank => $ary) {
+		$i++;
+		echo '<tr class="'.($i%2 == 0 ? 'odd' : 'even').'">';
+		if (empty($ary['service_description'])) {
+			$producer = _('Host');
+			$ary['service_description'] = 'N/A';
+		} else {
+			$producer = _('Service');
+			$ary['service_description'] = html::anchor(base_url::get().'extinfo/details/?type=service&host='.urlencode($ary['host_name']).'&service='.urlencode($ary['service_description']), $ary['service_description']);
+		}
+	?>
+		<td class="icon"><?php echo $rank; ?></td>
+		<td><?php echo $producer; ?></td>
+		<td><?php echo html::anchor(base_url::get().'extinfo/details/?type=host&host='.urlencode($ary['host_name']), $ary['host_name']) ?></td>
+		<td><?php echo $ary['service_description']; ?></td>
+		<td><?php echo $ary['total_alerts']; ?></td>
+	</tr>
+	<?php }
+}?>
+</table>
 
 <?php // printf("Report completed in %.3f seconds<br />\n", $completion_time); ?>
