31862ddcf846e7ed32e794eac61ebe868712a458
Sven Nierlein <sven@nierlein.de>
2012-09-28 13:37:39 +0200

    migrated outages page to livestatus

--- application/controllers/outages.php
+++ application/controllers/outages.php
@@ -12,6 +12,8 @@
  */
 class Outages_Controller extends Authenticated_Controller
 {
+	const SERVICE_SEVERITY_DIVISOR = 4; /**< Magical constant that tells us how many times less interesting a service is compared to a host */
+
 	/**
 	*	@name	index
 	*	@desc	default method
@@ -27,25 +29,60 @@ class Outages_Controller extends Authenticated_Controller
 	*/
 	public function display_network_outages()
 	{
-		$auth = Nagios_auth_Model::instance();
-		if(!$auth->view_hosts_root) {
-			$this->template->content = $this->add_view('unauthorized');
-			$this->template->content->error_message = _('It appears as though you do not have permission to view information you requested...');
-			$this->template->content->error_description = _('If you believe this is an error, check the HTTP server authentication requirements for accessing this page
-			and check the authorization options in your CGI configuration file.');
-			return;
+		$ls = Livestatus::instance();
+		$outages = $ls->getHosts(array('filter' => array( 	'state'  => 1,
+															'childs' => array( '!=' => '' )
+													)));
+
+
+		if(count($outages) > 0) {
+			$all_hosts = array();
+			$hosts = $ls->getHosts();
+			foreach($hosts as &$h) {
+				$all_hosts[$h['name']] =& $h;
+			}
+
+			foreach($outages as &$host) {
+				# count number of affected hosts / services
+				list($affected_hosts,$affected_services) = _count_affected_hosts_and_services($host['name'], $all_hosts);
+				$host['affected_hosts']    = $affected_hosts;
+				$host['affected_services'] = $affected_services;
+
+				$host['severity'] = round($affected_hosts + $affected_services/self::SERVICE_SEVERITY_DIVISOR);
+			}
 		}
+
+
 		$this->template->content = $this->add_view('outages/network_outages');
 		$this->template->js_header = $this->add_view('js_header');
 		$content = $this->template->content;
-		$outages = new Outages_Model();
-		$outage_data = $outages->fetch_outage_data();
 		$content->title = _('Blocking Outages');
 
-		$content->outage_data = $outage_data;
+		$content->outage_data = $outages;
 		$this->template->title = _('Monitoring » Network outages');
-		# date::timespan(time(), $result->last_update, 'days,hours,minutes,seconds');
+	}
+}
 
+function _count_affected_hosts_and_services($host, $all_hosts) {
+	$affected_hosts    = 0;
+	$affected_services = 0;
+
+	if(!isset($all_hosts[$host]))
+		return(array(0,0));
+
+	if(isset($all_hosts[$host]['childs']) && $all_hosts[$host]['childs'] != '') {
+		foreach($all_hosts[$host]['childs'] as $child) {
+			list($child_affected_hosts,$child_affected_services) = _count_affected_hosts_and_services($child, $all_hosts);
+			$affected_hosts    += $child_affected_hosts;
+			$affected_services += $child_affected_services;
+		}
 	}
 
+	# add number of directly affected hosts
+	$affected_hosts++;
+
+	# add number of directly affected services
+	$affected_services += $all_hosts[$host]['num_services'];
+
+	return(array($affected_hosts, $affected_services));
 }
--- application/views/themes/default/outages/network_outages.php
+++ application/views/themes/default/outages/network_outages.php
@@ -19,16 +19,16 @@
 			<?php
 			$i = 0;
 			if (!empty($outage_data)) {
-				foreach ($outage_data as $host => $details) {
+				foreach ($outage_data as $details) {
 					$i++;
 			?>
 			<tr class="<?php echo ($i%2 == 0) ? 'odd' : 'even' ?>">
-				<td class="icon <?php echo strtolower(Current_status_Model::status_text($details['current_state'])); ?>">
-					<em><?php echo Current_status_Model::status_text($details['current_state']) ?></em>
+				<td class="icon <?php echo strtolower(Current_status_Model::status_text($details['state'])); ?>">
+					<em><?php echo Current_status_Model::status_text($details['state']) ?></em>
 				</td>
-				<td><?php echo html::anchor('extinfo/details/host/'.$host, $host) ?></td>
+				<td><?php echo html::anchor('extinfo/details/host/'.$details['name'], $details['name']) ?></td>
 				<td class="icon">
-					<?php echo $details['comments'] == 0 ? '' : html::anchor('extinfo/details/host/'.$host, html::image($this->add_path('icons/16x16/add-comment.png'),array('alt' => _('View comments for this host'), 'title' => _('View comments for this host'))),array('style' => 'border: 0px')); ?>
+					<?php echo $details['comments'] == 0 ? '' : html::anchor('extinfo/details/host/'.$details['name'], html::image($this->add_path('icons/16x16/add-comment.png'),array('alt' => _('View comments for this host'), 'title' => _('View comments for this host'))),array('style' => 'border: 0px')); ?>
 				</td>
 				<td><?php echo $details['severity'] ?></td>
 				<td><?php echo time::to_string($details['duration']) ?></td>
@@ -36,13 +36,13 @@
 				<td><?php echo $details['affected_services'] ?></td>
 				<td>
 					<?php
-						echo html::anchor('status/service/'.$host, html::image($this->add_path('icons/16x16/service-details.gif'),array('alt' => _('View status detail for this host'), 'title' => _('View status detail for this host'))),array('style' => 'border: 0px')).'&nbsp;';
+						echo html::anchor('status/service/'.$details['name'], html::image($this->add_path('icons/16x16/service-details.gif'),array('alt' => _('View status detail for this host'), 'title' => _('View status detail for this host'))),array('style' => 'border: 0px')).'&nbsp;';
 						if ( Kohana::config('config.nagvis_path') ) {
-							echo html::anchor('statusmap/host/'.$host, html::image($this->add_path('icons/16x16/locate-host-on-map.png'),array('alt' => _('View status map for this host and its children'), 'title' => _('View status map for this host and its children'))),array('style' => 'border: 0px')).'&nbsp;'; 
+							echo html::anchor('statusmap/host/'.$details['name'], html::image($this->add_path('icons/16x16/locate-host-on-map.png'),array('alt' => _('View status map for this host and its children'), 'title' => _('View status map for this host and its children'))),array('style' => 'border: 0px')).'&nbsp;';
 						}
-						echo html::anchor('trends/generate?host_name[]='.$host, html::image($this->add_path('icons/16x16/trends.png'),array('alt' => _('View trends for this host'), 'title' => _('View trends for this host'))),array('style' => 'border: 0px')).'&nbsp;';
-						echo html::anchor('alert_history/generate?host_name[]='.$host, html::image($this->add_path('icons/16x16/alert-history.png'),array('alt' => _('View alert history for this host'), 'title' => _('View alert history for this host'))),array('style' => 'border: 0px')).'&nbsp;';
-						echo html::anchor('notifications/host/'.$host, html::image($this->add_path('icons/16x16/notify.png'),array('alt' => _('View notifications for this host'), 'title' => _('View notifications for this host'))),array('style' => 'border: 0px'))
+						echo html::anchor('trends/generate?host_name[]='.$details['name'], html::image($this->add_path('icons/16x16/trends.png'),array('alt' => _('View trends for this host'), 'title' => _('View trends for this host'))),array('style' => 'border: 0px')).'&nbsp;';
+						echo html::anchor('alert_history/generate?host_name[]='.$details['name'], html::image($this->add_path('icons/16x16/alert-history.png'),array('alt' => _('View alert history for this host'), 'title' => _('View alert history for this host'))),array('style' => 'border: 0px')).'&nbsp;';
+						echo html::anchor('notifications/host/'.$details['name'], html::image($this->add_path('icons/16x16/notify.png'),array('alt' => _('View notifications for this host'), 'title' => _('View notifications for this host'))),array('style' => 'border: 0px'))
 					?>
 				</td>
 			</tr>
