14dd653782a48cfdf090f6442da7c2d34e0a8e4d
Max Sikström <msikstrom@op5.com>
2012-10-01 13:28:34 +0200

    Updated ExpParser to generate livestatus filters
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/exp/libraries/ExpParser.php
+++ modules/exp/libraries/ExpParser.php
@@ -1,32 +1,128 @@
 <?php
 
 abstract class ExpParser_Core {
-	public function __construct( $tokens ) {
-		$this->tokens = $tokens;
-		$this->ptr    = 0;
-	}
+	private $expr;
+	private $ptr;
 	
-	public function parse() {
-		$result = $this->expression();
-		$this->expect( array( 'end' ) );
+	public function parse( $expr ) {
+		$this->expr = $expr;
+		$this->ptr  = 0;
+		
+		$result = $this->run();
+
+		$curptr = $this->ptr; /* Keep $this->ptr for error message */
+		while( ctype_space( substr( $this->expr, $curptr, 1 ) ) ) $this->ptr++;
+		if( $curptr != strlen( $this->expr ) ) {
+			$this->error( 'Expected end' );
+		}
+		
 		return $result;
 	}
 
-	abstract protected function expression();
+	abstract protected function run();
 	
-	protected function accept( $match ) {
-		$sym = $this->tokens[$this->ptr];
-		if( array_slice( $sym, 0, count( $match ) ) == $match ) {
-			$this->ptr++;
-			return $sym;
+	protected function acceptSym( $tokenlist ) {
+		/* Trim left */
+		while( ctype_space( substr( $this->expr, $this->ptr, 1 ) ) ) $this->ptr++;
+		
+		/* Test tokens */
+		foreach( $tokenlist as $token ) {
+			if( substr( $this->expr, $this->ptr, strlen( $token ) ) == $token ) {
+				$this->ptr += strlen( $token );
+				return $token;
+			}
 		}
 		return false;
 	}
 	
-	protected function expect( $match ) {
-		$sym = $this->accept( $match );
+	protected function expectSym( $tokenlist ) {
+		$sym = $this->acceptSym( $tokenlist );
 		if( $sym === false )
-			throw new Exception('Unexpected symbol, expected '.$match[0].' got '.$this->tokens[$this->ptr][0]);
+			$this->error('Unexpected token, expected '.implode(',',$tokenlist));
 		return $sym;
+	}
+	
+	protected function acceptKeyword( $keywordlist = false ) {
+		/* Trim left */
+		while( ctype_space( substr( $this->expr, $this->ptr, 1 ) ) ) $this->ptr++;
+		
+		/* Peek at next keyword */
+		$curptr = $this->ptr;
+		$buffer = '';
+		while( ctype_alpha( $c = substr( $this->expr, $curptr, 1 ) ) || substr( $this->expr, $curptr, 1 ) == '_' ) {
+			$curptr++;
+			$buffer .= $c;
+		}
+		
+		if( $keywordlist === false || in_array( $buffer, $keywordlist ) ) {
+			$this->ptr = $curptr;
+			return $buffer;
+		}
+		return false;
+	}
+	
+	protected function expectKeyword( $keywordlist = false ) {
+		$sym = $this->acceptKeyword( $keywordlist );
+		if( $sym === false )
+			$this->error('Unexpected token, expected '.(($keywordlist===false)?('keyword'):implode(',',$keywordlist)));
+		return $sym;
+	}
+	
+	protected function acceptString( ) {
+		/* Trim left */
+		while( ctype_space( substr( $this->expr, $this->ptr, 1 ) ) ) $this->ptr++;
+		
+		/* Fetch first " */
+		if( substr( $this->expr, $this->ptr, 1 ) != '"' ) {
+			return false;
+		}
+		$this->ptr++;
+		
+		$buffer = '';
+		while( ($c = substr( $this->expr, $this->ptr++, 1 )) != '"' ) {
+			if( $c == '\\' ) {
+				$c = substr( $this->expr, $this->ptr++, 1 );
+			}
+			$buffer .= $c;
+		}
+		return $buffer;
+	}
+	
+	protected function expectString() {
+		$sym = $this->acceptString();
+		if( $sym === false )
+			$this->error('Unexpected token, expected string');
+		return $sym;
+	}
+	
+	/* FIXME: number should handle more than positive integers */
+	protected function acceptNum() {
+		/* Trim left */
+		while( ctype_space( substr( $this->expr, $this->ptr, 1 ) ) ) $this->ptr++;
+		
+		/* Peek at next integer */
+		$curptr = $this->ptr;
+		$buffer = '';
+		while( ctype_digit( $c = substr( $this->expr, $curptr, 1 ) ) ) {
+			$curptr++;
+			$buffer .= $c;
+		}
+		
+		if( strlen( $buffer ) > 0 ) {
+			$this->ptr = $curptr;
+			return intval($buffer);
+		}
+		return false;
+	}
+	
+	protected function expectNum() {
+		$sym = $this->acceptNum();
+		if( $sym === false )
+			$this->error('Unexpected token, expected number');
+		return $sym;
+	}
+	
+	protected function error( $msg ) {
+		throw new Exception($msg . ' at ' . $this->ptr);
 	}
 }
--- modules/exp/libraries/ExpParser_searchfilter.php
+++ modules/exp/libraries/ExpParser_searchfilter.php
@@ -1,61 +1,97 @@
 <?php
 
 class ExpParser_SearchFilter_Core extends ExpParser_Core {
-	protected function expression() {
-		return $this->expa();
+	protected $columns     = false;
+	protected $head_and    = "And:";
+	protected $head_or     = "Or:";
+	protected $head_negate = "Negate:";
+	protected $head_filter = "Filter:";
+	
+	public function setColumns( $columns ) {
+		$this->columns = $columns;
 	}
 
-	protected function expa() {
-		$val = $this->expb();
+	public function setStats() {
+		$this->head_and    = "StatsAnd:";
+		$this->head_or     = "StatsOr:";
+		$this->head_negate = "StatsNegate:";
+		$this->head_filter = "Stats:";
+	}
+	
+/* Expression entry point */
+	protected function run() {
+		return $this->bool_entry();
+	}
+	
+/* And/Or rules */
+	
+	protected function bool_entry() {
+		return $this->bool_or();
+	}
+
+	protected function bool_or() {
+		$res = $this->bool_and();
+		$count = 1;
 		do {
-			$sym = $this->accept( array('op','+') );
-			if( $sym === false )
-				$sym = $this->accept( array('op','-') );
+			$sym = $this->acceptSym( array('or') );
 			if( $sym !== false ) {
-				$r = $this->expb();
-				switch( $sym[1] ) {
-					case '+': $val += $r; break;
-					case '-': $val -= $r; break;
-				}
+				$res .= $this->bool_and();
+				$count++;
 			}
 		} while( $sym !== false );
-		return $val;
+		if( $count > 1 )
+			$res .= $this->head_or . " $count\n";
+		return $res;
 	}
-	protected function expb() {
-		$val = $this->expc();
+	
+	protected function bool_and() {
+		$res = $this->bool_expr();
+		$count = 1;
 		do {
-			$sym = $this->accept( array('op','*') );
-			if( $sym === false )
-				$sym = $this->accept( array('op','/') );
+			$sym = $this->acceptSym( array('and') );
 			if( $sym !== false ) {
-				$r = $this->expc();
-				switch( $sym[1] ) {
-					case '*': $val *= $r; break;
-					case '/': $val /= $r; break;
-				}
+				$res .= $this->bool_expr();
+				$count++;
 			}
 		} while( $sym !== false );
-		return $val;
+		if( $count > 1 )
+			$res .= $this->head_and . " $count\n";
+		return $res;
 	}
-	protected function expc() {
-		$val = $this->exp_fin();
-		do {
-			$sym = $this->accept( array('op','^') );
-			if( $sym !== false ) {
-				$r = $this->exp_fin();
-				$val = pow( $val, $r );
-			}
-		} while( $sym !== false );
-		return $val;
-	}
-	protected function exp_fin() {
-		if( $this->accept( array( 'op', '(' ) ) ) {
-			$res = $this->expa();
-			$this->expect( array( 'op', ')' ) );
-			return $res;
+	
+	protected function bool_expr() {
+		$neg = false;
+		if( $this->acceptKeyword( array( 'not' ) ) ) {
+			$neg = true;
 		}
-		if( $sym = $this->expect( array( 'num' ) ) ) {
-			return $sym[1];
+		
+		if( $this->acceptSym( array( '(' ) ) ) {
+			$res = $this->bool_or();
+			$this->expectSym( array( ')' ) );
+		} else {
+			$res = $this->filter_entry();
 		}
+		if( $neg ) {
+			$res .= $this->head_negate . "\n";
+		}
+		return $res;
+	}
+	
+/* Column filter rules */
+	
+	protected function filter_entry() {
+		$column   = $this->expectKeyword( $this->columns );
+
+		$operator = $this->acceptSym( array('~~','=~','~') );
+		if( $operator !== false ) {
+			$value = $this->expectString();
+		} else {
+			$operator = $this->expectSym( array('>=','<=','>','<','=') );
+			$value = $this->acceptString();
+			if( $value === false ) {
+				$value = $this->expectNum();
+			}
+		}
+		return $this->head_filter." $column $operator $value\n";
 	}
 }
