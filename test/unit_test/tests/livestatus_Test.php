a36ea68e558ee489b242d5f8f578086115219020
Robin Sonefors <robin.sonefors@op5.com>
2012-10-09 13:45:30 +0200

    livestatus_Test: Use correct API when generating filters
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- test/unit_test/tests/livestatus_Test.php
+++ test/unit_test/tests/livestatus_Test.php
@@ -17,25 +17,25 @@ class Livestatus_Test extends TapUnit {
         # basic filter 1
         $filter = array('name' => 'test');
         $expect = 'Filter: name = test';
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "basic filter 1: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # basic filter 2
         $filter = array('name' => array('!=' => 'test'));
         $expect = 'Filter: name != test';
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "basic filter 2: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # basic filter 3
         $filter = "";
         $expect = '';
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "basic filter 3: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # basic filter 4
         $filter = array('', 'num_hosts' => array('>' => 0));
         $expect = 'Filter: num_hosts > 0';
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "basic filter 4: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
     }
@@ -46,13 +46,13 @@ class Livestatus_Test extends TapUnit {
             'name' => array('-and' => array( 'test1' ) )
         );
         $expect = "Filter: name = test1";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "and filter 1: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # and filter 2
         $filter = array( 'host_has_been_checked' => 1, 'host_state' => 0 );
         $expect = "Filter: host_has_been_checked = 1\nFilter: host_state = 0\nAnd: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "and filter 2: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # and filter 3
@@ -63,7 +63,7 @@ class Livestatus_Test extends TapUnit {
                       )
         );
         $expect = "Filter: host_has_been_checked = 1\nFilter: host_state = 0\nAnd: 2\nFilter: has_been_checked = 1\nFilter: state = 3\nAnd: 2\nAnd: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "and filter 3: \nexpect:\n".$expect."\n\ngot:\n".$filter);
     }
 
@@ -73,7 +73,7 @@ class Livestatus_Test extends TapUnit {
             'name' => array('-or' => array( 'test1', 'test2' ) )
         );
         $expect = "Filter: name = test1\nFilter: name = test2\nOr: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "or filter 1: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # or filter 2
@@ -85,7 +85,7 @@ class Livestatus_Test extends TapUnit {
                         )
         );
         $expect = "Filter: name = test1\nFilter: name != test2\nOr: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "or filter 2: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # or filter 3
@@ -94,7 +94,7 @@ class Livestatus_Test extends TapUnit {
                            'name2' => 'test2')
         );
         $expect = "Filter: name1 = test1\nFilter: name2 = test2\nOr: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "or filter 3: \nexpect:\n".$expect."\n\ngot:\n".$filter);
 
         # or filter 4
@@ -103,7 +103,7 @@ class Livestatus_Test extends TapUnit {
                            'name2' => array('!=' => 'test2'))
         );
         $expect = "Filter: name1 = test1\nFilter: name2 != test2\nOr: 2";
-        $filter = chop($this->lsb->getQueryFilter(false, $filter));
+        $filter = chop($this->lsb->getQueryFilter($filter));
         $this->ok($filter === $expect, "or filter 4: \nexpect:\n".$expect."\n\ngot:\n".$filter);
     }
 
