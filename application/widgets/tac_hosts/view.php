6234272487995253494dd92905a7ae6012ddf4de
Tobias Sjöndin <tsjondin@op5.com>
2012-10-17 14:05:59 +0200

    Fixed widget icon tables
    They displayed poorly
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/widgets/tac_hosts/view.php
+++ application/widgets/tac_hosts/view.php
@@ -17,7 +17,7 @@
 			<table>
 					<?php if (count($hosts_down) > 0) { foreach ($hosts_down as $url => $title) { ?>
 					<tr>
-						<td class="dark">
+						<td class="icon dark">
 						<?php
 							$icon = explode(' ',$title);
 							echo html::image($this->add_path('icons/16x16/'.(($icon[1] == 'Unhandled') ? 'shield-critical' : strtolower($icon[1]).($icon[1] == 'Scheduled' ? '-downtime' : '')).'.png'),$icon[1]);
@@ -27,7 +27,7 @@
 					</tr>
 					<?php } } else { ?>
 					<tr>
-						<td class="dark"><?php 
+						<td class="icon dark"><?php 
 							echo '<span class="icon-16 x16-shield-not-critical" title="'._('Down').'"></span>';
 						?></td>
 						<td><?php echo html::anchor($default_links['down'], _('N/A')) ?></td>
@@ -39,7 +39,7 @@
 			<table>
 					<?php if (count($hosts_unreachable) > 0) { foreach ($hosts_unreachable as $url => $title) { ?>
 					<tr>
-						<td class="dark">
+						<td class="icon dark">
 							<?php
 								$icon = explode(' ',$title);
 								echo '<span class="icon-16 x16-'.(($icon[1] == 'Unhandled') ? 'shield-unreachable' : strtolower($icon[1]).($icon[1] == 'Scheduled' ? '-downtime' : '')).'" title="'.$icon[1].'"></span>';
@@ -49,7 +49,7 @@
 					</tr>
 					<?php } } else { ?>
 					<tr>
-						<td class="dark"><?php 
+						<td class="icon dark"><?php 
 							echo '<span class="icon-16 x16-shield-not-unreachable" title="'._('Unreachable').'"></span>'; 
 						?></td>
 						<td><?php echo html::anchor($default_links['unreachable'], _('N/A')) ?></td>
@@ -61,17 +61,17 @@
 			<table>
 					<?php	if ($current_status->hst->up > 0) { ?>
 					<tr>
-						<td class="dark"><?php echo '<span class="icon-16 x16-shield-ok" title="'._('Up').'"></span>';  ?></td>
+						<td class="icon dark"><?php echo '<span class="icon-16 x16-shield-ok" title="'._('Up').'"></span>';  ?></td>
 						<td class="status-up"><?php echo html::anchor('status/host/all/1/', html::specialchars($current_status->hst->up.' '._('Up'))) ?></td>
 					</tr>
 					<?php } if (count($hosts_up_disabled) > 0) { foreach ($hosts_up_disabled as $url => $title) { ?>
 						<tr>
-						<td class="dark"><?php echo '<span class="icon-16 x16-shield-disabled" title="'._('Disabled').'"></span>';  ?></td>
+						<td class="icon dark"><?php echo '<span class="icon-16 x16-shield-disabled" title="'._('Disabled').'"></span>';  ?></td>
 						<td><?php echo html::anchor($url, html::specialchars($title)) ?></td>
 					</tr>
 					<?php } } if (count($hosts_up_disabled) == 0 && $current_status->hst->up == 0) { ?>
 					<tr>
-						<td class="dark"><?php echo '<span class="icon-16 x16-shield-not-up" title="'._('Up').'"></span>';  ?></td>
+						<td class="icon dark"><?php echo '<span class="icon-16 x16-shield-not-up" title="'._('Up').'"></span>';  ?></td>
 						<td><?php echo html::anchor($default_links['up'], _('N/A')) ?></td>
 					</tr>
 					<?php } ?>
@@ -81,12 +81,12 @@
 			<table>
 					<?php if (count($hosts_pending) > 0) { foreach ($hosts_pending as $url => $title) { ?>
 					<tr>
-						<td class="dark"><?php echo '<span class="icon-16 x16-shield-pending" title="'._('Pending').'"></span>';  ?></td>
+						<td class="icon dark"><?php echo '<span class="icon-16 x16-shield-pending" title="'._('Pending').'"></span>';  ?></td>
 						<td><?php echo html::anchor($url, html::specialchars($title)) ?></td>
 					</tr>
 					<?php } } else { ?>
 					<tr>
-						<td class="dark"><?php echo '<span class="icon-16 x16-shield-not-pending" title="'._('Critical').'"></span>';  ?></td>
+						<td class="icon dark"><?php echo '<span class="icon-16 x16-shield-not-pending" title="'._('Critical').'"></span>';  ?></td>
 						<td><?php echo html::anchor($default_links['pending'], _('N/A')) ?></td>
 					</tr>
 					<?php } ?>
