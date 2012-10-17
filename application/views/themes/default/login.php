38e6bf1d4380b12e55fbf17e95d40356e6a5d805
Tobias Sjöndin <tsjondin@op5.com>
2012-10-17 12:07:29 +0200

    Updated to make styles for login page work!
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/views/themes/default/login.php
+++ application/views/themes/default/login.php
@@ -6,8 +6,6 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title><?php echo Kohana::config('config.product_name').' '._('login'); ?></title>
 		<link type="text/css" rel="stylesheet" href="<?php echo url::base().'application/views/themes/default/css/default/common.css' ?>" media="all" />
-		<link type="text/css" rel="stylesheet" href="<?php echo url::base().'application/views/themes/default/css/default/screen.css' ?>" media="screen" />
-		<link type="text/css" rel="stylesheet" href="<?php echo url::base().'application/views/themes/default/css/default/status.css' ?>" media="screen" />
 		<link type="text/css" rel="stylesheet" href="<?php echo url::base().'application/views/themes/default/css/default/print.css' ?>" media="print" />
 		<link type="text/css" rel="stylesheet" href="<?php echo url::base().'application/views/themes/default/css/default/jquery-ui-custom.css' ?>" />
 		<?php echo html::link($this->add_path('icons/16x16/favicon.ico'),'icon','image/icon') ?>
