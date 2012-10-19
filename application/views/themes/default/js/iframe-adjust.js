08ff4634f6fa5e6b6d408e305d2a6d609722192d
Tobias Sjöndin <tsjondin@op5.com>
2012-10-19 11:37:35 +0200

    Should fix iframed views, nagvis, geomap, nacoma, hypermap
    They were offset by padding and now fixed!
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/views/themes/default/js/iframe-adjust.js
+++ application/views/themes/default/js/iframe-adjust.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 function adjust_height() {
-	var new_height = parseInt(document.documentElement.clientHeight) - 68;
+	var new_height = parseInt(document.documentElement.clientHeight) - 49;
 	$('#iframe').css('height', new_height+'px');
 	$('#nagvis').css('height', new_height+'px');
 	$('#hypermap').css('height', new_height+'px');
