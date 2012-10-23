9dca8e7604d4830bb121ce024d5a1eab726377f3
Tobias Sjöndin <tsjondin@op5.com>
2012-10-23 11:53:03 +0200

    Fixed styling on schedule report and removed some commented lines in host and service
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/views/themes/default/schedule/new_schedule.php
+++ application/views/themes/default/schedule/new_schedule.php
@@ -3,10 +3,9 @@
 <div id="new_schedule_area">
 	<form action="schedule/schedule" id="new_schedule_report_form">
 		<h1><?php echo _('New schedule') ?></h1>
-		<table id="new_schedule_report_table">
-			<tr>
-				<td>
-					<label for="type"><?php echo help::render('report-type-save', 'reports').' '._('Select report type') ?></label><br />
+		
+		<div class="left w-50">
+			<label for="type"><?php echo help::render('report-type-save', 'reports').' '._('Select report type') ?></label><br />
 					<?php echo form::dropdown(array('name' => 'type'), $defined_report_types); ?><br />
 					<?php if (!empty($available_schedule_periods)) { ?>
 						<label for="period"><?php echo help::render('interval', 'reports').' '._('Report Interval') ?></label><br />
@@ -24,17 +23,18 @@
 						<?php } ?>
 					</select><br />
 					<label for="recipients"><?php echo help::render('recipents', 'reports').' '._('Recipients') ?></label><br /><input type="text" class="schedule" name="recipients" id="recipients" value="" />
-				</td>
-				<td>
-					<label for="filename"><?php echo help::render('filename', 'reports').' '._('Filename (defaults to pdf, may end in .csv)') ?></label><br /><input type="text" class="schedule" name="filename" id="filename" value="" /><br />
-					<label for="description"><?php echo help::render('description', 'reports').' '._('Description') ?></label><br /><textarea cols="31" rows="4" id="description" name="description"></textarea><br />
-					<label for="local_persistent_filepath"><?php echo help::render('local_persistent_filepath', 'reports').' '._("Save report in this local folder") ?></label><br /><input type="text" class="schedule" name="local_persistent_filepath" id="local_persistent_filepath" value="" />
-				</td>
-			</tr>
-		</table>
-		<p>
+		</div>
+		
+		<div class="left w-50">
+			<label for="filename"><?php echo help::render('filename', 'reports').' '._('Filename (defaults to pdf, may end in .csv)') ?></label><br /><input type="text" class="schedule" name="filename" id="filename" value="" /><br />
+			<label for="description"><?php echo help::render('description', 'reports').' '._('Description') ?></label><br /><textarea cols="31" rows="4" id="description" name="description"></textarea><br />
+			<label for="local_persistent_filepath"><?php echo help::render('local_persistent_filepath', 'reports').' '._("Save report in this local folder") ?></label><br /><input type="text" class="schedule" name="local_persistent_filepath" id="local_persistent_filepath" value="" />
+		</div>
+
+		<div class="clear"></div>
+		<span>
 			<input type="submit" class="button save" value="<?php echo _('Save') ?>" />
 			<input type="reset" class="button clear" value="<?php echo _('Clear') ?>" />
-		</p>
+		</span>
 	</form>
 </div>
