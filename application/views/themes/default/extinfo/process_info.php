7767d5ecfd4638102ce963daaab3ffad61f4f099
Tobias Sjöndin <tsjondin@op5.com>
2012-10-24 12:11:59 +0200

    Fixed style on process info
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/views/themes/default/extinfo/process_info.php
+++ application/views/themes/default/extinfo/process_info.php
@@ -1,5 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-
+<?php
+$shieldon = '<span class="icon-16 x16-shield-ok"></span>';
+$shieldoff = '<span class="icon-16 x16-shield-critical"></span>';
+?>
 <div class="left width-50" id="extinfo_current">
 <table class="ext">
 	<tr><th class="headerNone" colspan="2" style="border-left:0px"><?php echo _('Process Information'); ?></th></tr>
@@ -26,61 +29,61 @@
 	<tr>
 		<td class="dark"><?php echo _('Notifications enabled?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($notifications_str); ?>"><?php echo ucfirst(strtolower($notifications_str)) ?></span>
+			<span><?php echo (strtolower($notifications_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($notifications_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Service checks being executed?') ?> </td>
 		<td>
-			<span class="<?php echo strtolower($servicechecks_str); ?>"><?php echo ucfirst(strtolower($servicechecks_str)) ?></span>
+			<span><?php echo (strtolower($servicechecks_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($servicechecks_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Passive service checks being accepted?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($passive_servicechecks_str); ?>"><?php echo ucfirst(strtolower($passive_servicechecks_str)) ?></span>
+			<span><?php echo (strtolower($passive_servicechecks_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($passive_servicechecks_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Host checks being executed?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($hostchecks_str); ?>"><?php echo ucfirst(strtolower($hostchecks_str)) ?></span>
+			<span><?php echo (strtolower($hostchecks_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($hostchecks_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Passive host checks being accepted?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($passive_hostchecks_str); ?>"><?php echo ucfirst(strtolower($passive_hostchecks_str)) ?></span>
+			<span><?php echo (strtolower($passive_hostchecks_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($passive_hostchecks_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Event handlers enabled?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($eventhandler_str); ?>"><?php echo ucfirst(strtolower($eventhandler_str)) ?></span>
+			<span><?php echo (strtolower($eventhandler_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($eventhandler_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Obsessing over services?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($obsess_services_str); ?>"><?php echo ucfirst(strtolower($obsess_services_str)) ?></span>
+			<span><?php echo (strtolower($obsess_services_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($obsess_services_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Obsessing over hosts?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($obsess_hosts_str); ?>"><?php echo ucfirst(strtolower($obsess_hosts_str)) ?></span>
+			<span><?php echo (strtolower($obsess_hosts_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($obsess_hosts_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Flap detection enabled?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($flap_detection_str); ?>"><?php echo ucfirst(strtolower($flap_detection_str)) ?></span>
+			<span><?php echo (strtolower($flap_detection_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($flap_detection_str)) ?></span>
 		</td>
 	</tr>
 	<tr>
 		<td class="dark"><?php echo _('Performance data being processed?') ?></td>
 		<td>
-			<span class="<?php echo strtolower($performance_data_str); ?>"><?php echo ucfirst(strtolower($performance_data_str)) ?></span>
+			<span><?php echo (strtolower($performance_data_str) == 'yes') ? $shieldon: $shieldoff; ?><?php echo ucfirst(strtolower($performance_data_str)) ?></span>
 		</td>
 	</tr>
 </table>
