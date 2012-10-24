12aa4c41c1b55b39b443fbdbc0eec464aee7973b
Carl Helmertz <chelmertz@op5.com>
2012-10-24 11:14:46 +0200

    report_comments: Add referred-to column comment_timestamp to db schema
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- sql/mysql/ninja_db_v5_to_v6.sql
+++ sql/mysql/ninja_db_v5_to_v6.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS ninja_report_comments (
   event_type int(11) NOT NULL default '0',
   host_name varchar(255) default '',
   service_description varchar(255) default '',
+  comment_timestamp int(11) NOT NULL default '0',
   username varchar(100) NOT NULL,
   user_comment TEXT NOT NULL,
   KEY report_entry (timestamp, host_name, service_description, event_type)
