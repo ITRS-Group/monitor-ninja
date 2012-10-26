ef4694d685711eccf3e7564053bf0569a4c7f502
Carl Helmertz <chelmertz@op5.com>
2012-10-26 13:17:39 +0200

    sql: Fix last commit
    A bit hasty, are we?
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- sql/mysql/avail_v9_to_v10.sql
+++ sql/mysql/avail_v9_to_v10.sql
@@ -3,4 +3,4 @@ ALTER TABLE avail_config ADD COLUMN description TEXT DEFAULT NULL;
 ALTER TABLE avail_config DROP COLUMN assumeinitialstates;
 ALTER TABLE avail_config DROP COLUMN initialassumedhoststate;
 ALTER TABLE avail_config DROP COLUMN initialassumedservicestate;
-ALTER TABLE sla_config ADD COLUMN include_alerts int(1) DEFAULT 0;
+ALTER TABLE avail_config ADD COLUMN include_alerts int(1) DEFAULT 0;
