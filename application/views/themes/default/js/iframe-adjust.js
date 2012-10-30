ff67d674db6f970b2093047d855a9e2e9921f3fe
Tobias Sjöndin <tsjondin@op5.com>
2012-10-30 11:42:08 +0100

    Updated iframe adjust to also contain an android/ipad detection fix, suggesting using a new tab for the content.
    Problems exist with iframe scrolling on safari and other touch scroll browsers, moving the content to a
    separate window work just fine.
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/views/themes/default/js/iframe-adjust.js
+++ application/views/themes/default/js/iframe-adjust.js
@@ -1,5 +1,36 @@
+
 $(document).ready(function() {
 	adjust_height();
+
+	if ($('#iframe') || $('#nagvis') || $('#hypermap')) {
+		var elements = $('#iframe, #nagvis, #hypermap');
+		
+		var ua = navigator.userAgent.toLowerCase();
+
+		if (ua.indexOf('ipad') != -1 || ua.indexOf('android') != -1) {
+			
+			var agent = (ua.indexOf('ipad') != -1) ? 'iPad' : 'Android';
+
+			elements.load(function () {
+
+				var note = $('<div style="position: relative; border: 1px solid #eebb55; padding: 8px; background: #fea; font-size: 9pt;"></div>');
+
+				if (elements.attr('id') === 'iframe') {
+					note.css('top', '28px');
+				}
+
+				note.append('<span>Known issues on ' + agent + ' with iframes </span>');
+				note.append($('<a href="#"></a>').html('Open in a new window').click(function () {
+					window.open('https://' + window.location.hostname + elements.attr('src'), '_blank');
+				}))
+
+				var first = $(elements.contents().find('body')[0].firstChild);
+				first.before(note);
+			});
+		}
+
+	}
+
 });
 
 function adjust_height() {
