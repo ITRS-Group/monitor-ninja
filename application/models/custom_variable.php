8a2dac71ef3fe74b560a14de6041efb3d99e0b79
Henrik Smederod <hsmederod@op5.com>
2012-11-12 16:47:24 +0100

    Custom_variables_model: Added parser function to check for special cases.
    Signed-off-by: Henrik Smederod <hsmederod@op5.com>

--- application/models/custom_variable.php
+++ application/models/custom_variable.php
@@ -24,4 +24,50 @@ class Custom_variable_Model extends Model
 		}
 		return Database::instance()->query("SELECT * FROM custom_vars WHERE obj_type = ?", array($object_type))->as_array(false);
 	}
+
+	/**
+	 * @param $custom_variables array
+	 * @return array
+	 */
+	public static function parse_custom_variables($custom_variables) {
+		$custom_commands = array();
+		if (count($custom_variables) >= 2) {
+			foreach ($custom_variables as $custom_variable) {
+				// Does custom variable name match pattern?
+				if (substr($custom_variable['variable'], 0, 4) === '_OP5') {
+					$parts = explode('__', $custom_variable['variable']);
+					$command_name = $parts[count($parts)-1];
+					if (!isset($custom_commands[$command_name])) {
+						$custom_commands[$command_name] = array();
+					}
+					if (in_array('ACCESS', $parts)) {
+						$custom_commands[$command_name]['access'] = explode(',',$custom_variable['value']);
+					}
+					if (in_array('ACTION', $parts)) {
+						$custom_commands[$command_name]['action'] = $custom_variable['value'];
+					}
+				}
+				// Add additional special cases here.
+			}
+		}
+		if (count($custom_commands) > 0) {
+			foreach ($custom_commands as $command_name => $params) {
+				$authenticated = false;
+				// Check authorization.
+				$members = array();
+				foreach ($custom_commands[$command_name]['access'] as $contactgroup) {
+					$members[$contactgroup] = Contactgroup_Model::is_user_member($contactgroup);
+					if ($members[$contactgroup] === true) {
+						$authenticated = true;
+					}
+				}
+				if ($authenticated) {
+					$custom_commands[$command_name] = $custom_commands[$command_name]['action'];
+				} else {
+					unset($custom_commands[$command_name]);
+				}
+			}
+		}
+		return $custom_commands;
+	}
 }
