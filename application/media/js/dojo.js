7256975893a9c9fb5b8123d9edfbf402fe38154b
Tobias Sjöndin <tsjondin@op5.com>
2012-11-19 09:14:08 +0100

    Update to quicklinks additions
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- application/media/js/dojo.js
+++ application/media/js/dojo.js
@@ -229,10 +229,9 @@
 
 				if (obj['dojo-quicklinks']) {
 					links = JSON.parse(JSON.parse(xhr.responseText)['dojo-quicklinks']);
-				}
-				
-				for (var i = 0; i < links.length; i += 1) {
-					$('#dojo-quicklink-external').append($('<li><a target="_BLANK" class="image-link" href="' + links[i].href + '"><span title="'+links[i].title+'" class="icon-16 x16-'+links[i].icon+'"></span></a></li>'));
+					for (var i = 0; i < links.length; i += 1) {
+						$('#dojo-quicklink-external').append($('<li><a target="_BLANK" class="image-link" href="' + links[i].href + '"><span title="'+links[i].title+'" class="icon-16 x16-'+links[i].icon+'"></span></a></li>'));
+					}
 				}
 
 				global_quicklinks = links;
