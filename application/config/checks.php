841a9464aa08182b0ffb3d2dd795b529a88acdeb
Robin Sonefors <robin.sonefors@op5.com>
2012-11-26 17:22:39 +0100

    config/checks: Make passive_as_active default
    It (usually) makes very little sense to consider passive checks as
    disabled, so don't. We keep the setting around, though, so it'll be
    user-configurable, and then we'll see if anybody notices.
    
    This closes bug #5428 and bug #6016, and possibly others I haven't
    found.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/config/checks.php
+++ application/config/checks.php
@@ -4,12 +4,10 @@
 * Use this flag to change the default behaviour for how
 * to view passive checks.
 *
-* By settings this flag to true, all passive checks will be treated
-* as active in tactical overview.
+* By settings this flag to false, all passive checks will be treated
+* as disabled in tactical overview.
 *
 * Please note that this value is cached as a session variable so
 * you need to logout and login again for changes to take effect
-*
-* Default value: false
 */
-$config['show_passive_as_active'] = false;
+$config['show_passive_as_active'] = true;
