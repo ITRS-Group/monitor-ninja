bb423f9018af86fb101b0e5bd314f31ad4c66491
Robin Sonefors <robin.sonefors@op5.com>
2012-12-10 18:30:05 +0100

    nagvis: Throw away our old nagvis integration
    Because it did little in terms of actually integrating anything, let's
    just replace it with a straight-up iframe, with some fall-back URL
    support.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/nagvis/automap.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-
-<div style="margin-left: 1px;">
-	<iframe name="nagvis" id="nagvis" src="/nagvis/frontend/nagvis-js/index.php?mod=Map&act=view&show=automap<?php echo $querystring; ?>" width="100%" onload="highlight();">
-		Error : Can not load NagVis.
-	</iframe>
-</div>
--- application/views/themes/default/nagvis/edit.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-
-<div style="margin-left: 1px;">
-	<iframe name="nagvis" id="nagvis" src="/nagvis/frontend/wui/index.php?mod=Map&act=edit&show=<?php echo $map;?>" width="100%">
-		Error : Can not load NagVis.
-	</iframe>
-</div>
