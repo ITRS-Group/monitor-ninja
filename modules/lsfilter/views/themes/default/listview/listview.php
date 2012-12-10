9124bd3dd33589dbda83ca0f06fe9891f064a685
Tobias Sjondin <tsjondin@op5.com>
2012-12-10 12:10:35 +0100

    Fixed some visuals and made the input forms fixed to follow when scrolling!
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- modules/lsfilter/views/themes/default/listview/listview.php
+++ modules/lsfilter/views/themes/default/listview/listview.php
@@ -1,14 +1,12 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
 <div class="extra_toolbar">
-	<div class="right">
+	<div class="right lsfilter-edit-bar">
 		<input type="hidden" id="server_name"
 			value="<?php echo $_SERVER['SERVER_NAME']; ?>" /> <span
 			id="filter_visual_result"></span>
-		<button id="show-filter-query-builder-manual-button">Text Query
-			Builder</button>
-		<button id="show-filter-query-builder-graphical-button">Graphical
-			Query Builder</button>
+		<button id="show-filter-query-builder-manual-button" title="Show/Edit Text Filter"><span class="icon-16 x16-edit"></span></button>
+		<button id="show-filter-query-builder-graphical-button" title="Show/Edit Graphical Filter"><span class="icon-16 x16-command"></span></button>
 	</div>
 	<div id="filter_result_totals"></div>
 </div>
