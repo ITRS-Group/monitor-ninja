241a490e6ef69807e8c0b99e033c1412acb5e635
Robin Sonefors <robin.sonefors@op5.com>
2012-12-11 11:21:17 +0100

    Implement nagvis map listing in nagvisconfig helper
    This replaces both the old nagvis_acl model (which was copy-pasted from
    nagvis' ExtAuthorizationModNinja) and the nagvis_maps model with a much
    smaller, leaner bit of code to reproduce the same behavior, without
    going through quite so much trouble, as far as the widget is concerned.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/helpers/nagvisconfig.php
+++ application/helpers/nagvisconfig.php
@@ -3,12 +3,49 @@
  * Nagvis config reader class
  */
 
-Class nagvisconfig_Core {
+class nagvisconfig_Core {
+	/**
+	 * Get a list of maps and automaps the current user is authorized for
+	 */
+	public static function get_map_list() {
+		$maps = array();
+		$auth = Auth::instance();
+		if ($auth->authorized_for('nagvis_view')) {
+			$files = scandir(Kohana::config('nagvis.nagvis_real_path').'/etc/maps');
+			foreach ($files as $file) {
+				if (strpos($file, '.cfg') !== false)
+					$maps[] = substr($file, 0, -4);
+			}
+
+			return $maps;
+		}
+		$cfg = Op5Config::instance();
+		$nagvis_config = $cfg->getConfig('nagvis');
+
+		$groups_per_type = array(
+			'auth_groups'    => $auth->get_user()->groups,
+			'contact_groups' => $auth->get_contact_groups()
+		);
+
+		foreach($groups_per_type as $grouptype => $groups) {
+			if(!isset($nagvis_config[$grouptype]))
+				continue;
+			foreach( $groups as $group ) {
+				if (!isset( $nagvis_config[$grouptype][$group]))
+					continue;
+				foreach( $nagvis_config[$grouptype][$group] as $map => $perms ) {
+					if (in_array('view', $perms))
+						$maps[] = $map;
+				}
+			}
+		}
+		return $maps;
+	}
 
 	/**
 	 * Function to read Nagvis config file and return array with key->values
 	*/
-	public function get($ConfigFile) {
+	public static function get($ConfigFile) {
 		$raw = file_get_contents($ConfigFile);
 		$lines = explode("\n", $raw);
 
