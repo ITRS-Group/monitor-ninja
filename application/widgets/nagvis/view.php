3354cea66eff9a28fd6dfcaf446094fcf574a7b0
Robin Sonefors <robin.sonefors@op5.com>
2012-12-11 15:11:04 +0100

    widgets/nagvis: Don't show nagvis header in widget
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/widgets/nagvis/view.php
+++ application/widgets/nagvis/view.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <div class="nagvis-widget-content">
 	<?php if (isset($arguments['map']) && $arguments['map']) { ?>
-	<iframe name="nagvis" class="nagvis" src="/nagvis/frontend/nagvis-js/index.php?mod=Map&act=view&show=<?php print $arguments['map'] ?>" width="100%" height="<?php print $arguments['height'] ?>" frameborder="0">
+	<iframe name="nagvis" class="nagvis" src="/nagvis/frontend/nagvis-js/index.php?mod=Map&act=view&show=<?php print $arguments['map'] ?>&header_menu=0" width="100%" height="<?php print $arguments['height'] ?>" frameborder="0">
 	Error : Can not load NagVis.
 	</iframe>
 	<?php } else { ?>
