228671212fa35fd163925963b10ca689beaac817
Max Sikström <msikstrom@op5.com>
2012-12-28 16:47:28 +0100

    ORM: operator mapping for livestaus operators in SQL backend
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/orm/libraries/LivestatusSQLBuilderVisitor.php
+++ modules/orm/libraries/LivestatusSQLBuilderVisitor.php
@@ -11,22 +11,50 @@ class LivestatusSQLBuilderVisitor implements LivestatusFilterVisitor {
 		}
 		return '('.implode(" $op ", $result).')';
 	}
-	
+
 	public function visit_and( LivestatusFilterAnd $filt, $data ) {
 		return $this->visit_op( $filt, $data, 'AND', '1=1' );
 	}
-	
+
 	public function visit_or( LivestatusFilterOr $filt, $data ) {
 		return $this->visit_op( $filt, $data, 'OR', '1=0' );
 	}
-	
+
 	public function visit_match( LivestatusFilterMatch $filt, $data ) {
 		$field = str_replace('.','_',$filt->get_field());
-		$op = $filt->get_op();
 		$value = Database::instance()->escape($filt->get_value());
+		$op = $filt->get_op();
+		switch( $filt->get_op() ) {
+			case '!~~':
+				return "NOT ($field REGEXP $value)";
+			case '!~':
+				return "NOT ($field REGEXP BINARY $value)";
+			case '~~':
+				$op = 'REGEXP';
+				break;
+			case '~':
+				$op = 'REGEXP BINARY';
+				break;
+
+			case '!=~':
+				$op = '!=';
+				break;
+
+			case '=~':
+				$op = '=';
+				break;
+
+			case '!=':
+			case '>=':
+			case '<=':
+			case '>':
+			case '<':
+			case '=':
+				break;
+		}
 		return "($field $op $value)";
 	}
-	
+
 	public function visit_not( LivestatusFilterNot $filt, $data ) {
 		$subfilter = $filt->get_filter();
 		$result = $subfilter->visit($this,false);
