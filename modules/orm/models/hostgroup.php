8576ec452b77fa66f5c2607932782c6233c7e3e3
Max Sikström <msikstrom@op5.com>
2013-01-09 14:37:33 +0100

    ORM: Macros in hostgroup and servicegroup (and indentation fix)
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/orm/models/hostgroup.php
+++ modules/orm/models/hostgroup.php
@@ -3,17 +3,21 @@
 require_once( dirname(__FILE__).'/base/basehostgroup.php' );
 
 class HostGroup_Model extends BaseHostGroup_Model {
+	static public $macros =  array(
+		'$HOSTGROUPNAME$' => 'name',
+		'$HOSTGROUPALIAS$' => 'alias'
+	);
 	static public $rewrite_columns = array(
 		'host_stats' => array('name'),
 		'service_stats' => array('name')
-		);
-	
+	);
+
 	public function __construct($values, $prefix) {
 		parent::__construct($values, $prefix);
 		$this->export[] = 'host_stats';
 		$this->export[] = 'service_stats';
 	}
-	
+
 	public function get_host_stats() {
 		$set = HostPool_Model::all()->reduce_by('groups', $this->get_name(), '>=');
 
@@ -36,29 +40,29 @@ class HostGroup_Model extends BaseHostGroup_Model {
 		$scheduled        = ObjectPool_Model::get_by_query('[hosts] scheduled_downtime_depth > 0');
 		$unscheduled      = ObjectPool_Model::get_by_query('[hosts] scheduled_downtime_depth = 0');
 		$unhandled        = ObjectPool_Model::get_by_query('[hosts] acknowledged = 0 and scheduled_downtime_depth = 0')->intersect($active_checks);
-		
+
 		$flapping         = ObjectPool_Model::get_by_query('[hosts] is_flapping = 1' );
-				
+
 		$stats = array(
-				'total'                             => $all,
-				'pending'                           => $pending,
-				'pending_and_disabled'              => $pending->intersect($disabled_checks),
-				'pending_and_scheduled'             => $pending->intersect($scheduled),
-				'up'                                => $up,
-				'up_and_disabled_active'            => $up->intersect($disabled_active),
-				'up_and_scheduled'                  => $up->intersect($scheduled),
-				'down'                              => $down,
-				'down_and_ack'                      => $down->intersect($acknowledged),
-				'down_and_scheduled'                => $down->intersect($scheduled),
-				'down_and_disabled_active'          => $down->intersect($disabled_active),
-				'down_and_unhandled'                => $down->intersect($unscheduled),
-				'unreachable'                       => $unreachable,
-				'unreachable_and_ack'               => $unreachable->intersect($acknowledged),
-				'unreachable_and_scheduled'         => $unreachable->intersect($scheduled),
-				'unreachable_and_disabled_active'   => $unreachable->intersect($disabled_active),
-				'unreachable_and_unhandled'         => $unreachable->intersect($unhandled)
-				);
-		
+			'total'                             => $all,
+			'pending'                           => $pending,
+			'pending_and_disabled'              => $pending->intersect($disabled_checks),
+			'pending_and_scheduled'             => $pending->intersect($scheduled),
+			'up'                                => $up,
+			'up_and_disabled_active'            => $up->intersect($disabled_active),
+			'up_and_scheduled'                  => $up->intersect($scheduled),
+			'down'                              => $down,
+			'down_and_ack'                      => $down->intersect($acknowledged),
+			'down_and_scheduled'                => $down->intersect($scheduled),
+			'down_and_disabled_active'          => $down->intersect($disabled_active),
+			'down_and_unhandled'                => $down->intersect($unscheduled),
+			'unreachable'                       => $unreachable,
+			'unreachable_and_ack'               => $unreachable->intersect($acknowledged),
+			'unreachable_and_scheduled'         => $unreachable->intersect($scheduled),
+			'unreachable_and_disabled_active'   => $unreachable->intersect($disabled_active),
+			'unreachable_and_unhandled'         => $unreachable->intersect($unhandled)
+		);
+
 		$queries = array();
 		foreach( $stats as $name => $stat ) {
 			$queries[$name] = $set->intersect($stat)->get_query();
@@ -88,31 +92,31 @@ class HostGroup_Model extends BaseHostGroup_Model {
 		$scheduled        = ObjectPool_Model::get_by_query('[services] scheduled_downtime_depth > 0');
 		$unscheduled      = ObjectPool_Model::get_by_query('[services] scheduled_downtime_depth = 0');
 		$unhandled        = ObjectPool_Model::get_by_query('[services] acknowledged = 0 and scheduled_downtime_depth = 0')->intersect($active_checks);
-		
+
 		$down_host        = ObjectPool_Model::get_by_query('[services] host.state != 0');
-		
+
 		$stats = array(
-				'ok'                           => $ok,
-				'warning'                      => $warn,
-				'warning_and_ack'              => $warn->intersect($acknowledged),
-				'warning_and_disabled_active'  => $warn->intersect($disabled_active),
-				'warning_and_scheduled'        => $warn->intersect($scheduled),
-				'warning_and_unhandled'        => $warn->intersect($unhandled),
-				'warning_on_down_host'         => $warn->intersect($down_host),
-				'critical'                     => $critical,
-				'critical_and_ack'             => $critical->intersect($acknowledged),
-				'critical_and_disabled_active' => $critical->intersect($disabled_active),
-				'critical_and_scheduled'       => $critical->intersect($scheduled),
-				'critical_and_unhandled'       => $critical->intersect($unhandled),
-				'critical_on_down_host'        => $critical->intersect($down_host),
-				'unknown'                      => $unknown,
-				'unknown_and_ack'              => $unknown->intersect($acknowledged),
-				'unknown_and_disabled_active'  => $unknown->intersect($disabled_active),
-				'unknown_and_scheduled'        => $unknown->intersect($scheduled),
-				'unknown_and_unhandled'        => $unknown->intersect($unhandled),
-				'unknown_on_down_host'         => $unknown->intersect($down_host),
-				'pending'                      => $pending
-				);
+			'ok'                           => $ok,
+			'warning'                      => $warn,
+			'warning_and_ack'              => $warn->intersect($acknowledged),
+			'warning_and_disabled_active'  => $warn->intersect($disabled_active),
+			'warning_and_scheduled'        => $warn->intersect($scheduled),
+			'warning_and_unhandled'        => $warn->intersect($unhandled),
+			'warning_on_down_host'         => $warn->intersect($down_host),
+			'critical'                     => $critical,
+			'critical_and_ack'             => $critical->intersect($acknowledged),
+			'critical_and_disabled_active' => $critical->intersect($disabled_active),
+			'critical_and_scheduled'       => $critical->intersect($scheduled),
+			'critical_and_unhandled'       => $critical->intersect($unhandled),
+			'critical_on_down_host'        => $critical->intersect($down_host),
+			'unknown'                      => $unknown,
+			'unknown_and_ack'              => $unknown->intersect($acknowledged),
+			'unknown_and_disabled_active'  => $unknown->intersect($disabled_active),
+			'unknown_and_scheduled'        => $unknown->intersect($scheduled),
+			'unknown_and_unhandled'        => $unknown->intersect($unhandled),
+			'unknown_on_down_host'         => $unknown->intersect($down_host),
+			'pending'                      => $pending
+		);
 
 
 		$queries = array();
--- modules/orm/models/servicegroup.php
+++ modules/orm/models/servicegroup.php
@@ -3,15 +3,19 @@
 require_once( dirname(__FILE__).'/base/baseservicegroup.php' );
 
 class ServiceGroup_Model extends BaseServiceGroup_Model {
+	static public $macros =  array(
+		'$SERVICEGROUPNAME$' => 'name',
+		'$SERVICEGROUPALIAS$' => 'alias'
+	);
 	static public $rewrite_columns = array(
 		'service_stats' => array('name')
-		);
-	
+	);
+
 	public function __construct($values, $prefix) {
 		parent::__construct($values, $prefix);
 		$this->export[] = 'service_stats';
 	}
-	
+
 	public function get_service_stats() {
 		$set = ServicePool_Model::all()->reduce_by('groups', $this->get_name(), '>=');
 
@@ -35,31 +39,31 @@ class ServiceGroup_Model extends BaseServiceGroup_Model {
 		$scheduled        = ObjectPool_Model::get_by_query('[services] scheduled_downtime_depth > 0');
 		$unscheduled      = ObjectPool_Model::get_by_query('[services] scheduled_downtime_depth = 0');
 		$unhandled        = ObjectPool_Model::get_by_query('[services] acknowledged = 0 and scheduled_downtime_depth = 0')->intersect($active_checks);
-		
+
 		$down_host        = ObjectPool_Model::get_by_query('[services] host.state != 0');
-		
+
 		$stats = array(
-				'ok'                           => $ok,
-				'warning'                      => $warn,
-				'warning_and_ack'              => $warn->intersect($acknowledged),
-				'warning_and_disabled_active'  => $warn->intersect($disabled_active),
-				'warning_and_scheduled'        => $warn->intersect($scheduled),
-				'warning_and_unhandled'        => $warn->intersect($unhandled),
-				'warning_on_down_host'         => $warn->intersect($down_host),
-				'critical'                     => $critical,
-				'critical_and_ack'             => $critical->intersect($acknowledged),
-				'critical_and_disabled_active' => $critical->intersect($disabled_active),
-				'critical_and_scheduled'       => $critical->intersect($scheduled),
-				'critical_and_unhandled'       => $critical->intersect($unhandled),
-				'critical_on_down_host'        => $critical->intersect($down_host),
-				'unknown'                      => $unknown,
-				'unknown_and_ack'              => $unknown->intersect($acknowledged),
-				'unknown_and_disabled_active'  => $unknown->intersect($disabled_active),
-				'unknown_and_scheduled'        => $unknown->intersect($scheduled),
-				'unknown_and_unhandled'        => $unknown->intersect($unhandled),
-				'unknown_on_down_host'         => $unknown->intersect($down_host),
-				'pending'                      => $pending
-				);
+			'ok'                           => $ok,
+			'warning'                      => $warn,
+			'warning_and_ack'              => $warn->intersect($acknowledged),
+			'warning_and_disabled_active'  => $warn->intersect($disabled_active),
+			'warning_and_scheduled'        => $warn->intersect($scheduled),
+			'warning_and_unhandled'        => $warn->intersect($unhandled),
+			'warning_on_down_host'         => $warn->intersect($down_host),
+			'critical'                     => $critical,
+			'critical_and_ack'             => $critical->intersect($acknowledged),
+			'critical_and_disabled_active' => $critical->intersect($disabled_active),
+			'critical_and_scheduled'       => $critical->intersect($scheduled),
+			'critical_and_unhandled'       => $critical->intersect($unhandled),
+			'critical_on_down_host'        => $critical->intersect($down_host),
+			'unknown'                      => $unknown,
+			'unknown_and_ack'              => $unknown->intersect($acknowledged),
+			'unknown_and_disabled_active'  => $unknown->intersect($disabled_active),
+			'unknown_and_scheduled'        => $unknown->intersect($scheduled),
+			'unknown_and_unhandled'        => $unknown->intersect($unhandled),
+			'unknown_on_down_host'         => $unknown->intersect($down_host),
+			'pending'                      => $pending
+		);
 
 
 		$queries = array();
