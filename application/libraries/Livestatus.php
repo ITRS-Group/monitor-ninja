deb5cfc30837ad4ba85b5a0860cc8e7fb29de9ea
Max Sikström <msikstrom@op5.com>
2013-01-14 14:41:30 +0100

    Livestatus: Remove one livestatus connection
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- application/libraries/Livestatus.php
+++ application/libraries/Livestatus.php
@@ -576,16 +576,10 @@ class Livestatus {
  * Livetatus interaface.
  */
 class LivestatusBackend {
-	private $auth            = false;
 	private $connection      = null;
-	private $config          = false;
 
 	/* constructor */
-	public function __construct($config = null) {
-		$config           = $config ? $config : 'livestatus';
-		$this->config     = Kohana::config('database.'.$config);
-		$this->auth       = Auth::instance();
-		$this->connection = new LivestatusConnection(array('path' => $this->config['path']));
+	public function __construct() {
 	}
 
 	/* combineFilter */
@@ -636,15 +630,9 @@ class LivestatusBackend {
 			$options['columns'] = array_unique( array_merge( $options['columns'], $options['extra_columns'] ) );
 		}
 		
-		$columns = array();
-		if( isset( $options['columns'] ) ) {
-			if (is_array($options['columns']))
-				$query  .= "Columns: ".join(" ", $options['columns'])."\n";
-			else
-				$query  .= "Columns: {$options['columns']}\n";
-			$columns = $options['columns'];
-		}
-		
+		$columns = isset($options['columns'])?$options['columns']:array();
+
+		$output_columns = $columns;
 		if( $stats !== false ) {
 			foreach($stats as $key => $filter) {
 				if( is_string( $filter ) ) {
@@ -652,30 +640,15 @@ class LivestatusBackend {
 				} else {
 					$query .= $this->getQueryFilter($filter, true);
 				}
-				array_push($columns, $key);
+				array_push($output_columns, $key);
 			}
 		}
-		
-		$query .= $this->auth($table, $options);
 
 		$this->prepare_pagination($options);
 		list($objects,$count) = $this->query($table, $query, $columns, $options);
 		$this->postprocess_pagination($options, $count);
 		
-		return $this->objects2Assoc($objects, $columns, isset($options['callbacks']) ? $options['callbacks'] : null);
-	}
-
-	private function auth($table, $options = null) {
-		if(isset($options['auth']) && $options['auth'] === true) {
-			return "";
-		}
-		if(strpos($table, 'services') !== false && !$this->auth->authorized_for('service_view_all') ) {
-			return "AuthUser: ".$this->auth->get_user()->username."\n";
-		}
-		elseif(strpos($table, 'hosts') !== false && !$this->auth->authorized_for('host_view_all') ) {
-			return "AuthUser: ".$this->auth->get_user()->username."\n";
-		}
-		return "";
+		return $this->objects2Assoc($objects, $output_columns, isset($options['callbacks']) ? $options['callbacks'] : null);
 	}
 
 	private function prepare_pagination(&$options) {
@@ -724,10 +697,7 @@ class LivestatusBackend {
 	}
 
 	private function query($table, $filter, $columns, $options) {
-		$query  = "GET $table\n";
-		$query .= "OutputFormat: wrapped_json\n";
-		$query .= "KeepAlive: on\n";
-		$query .= "ResponseHeader: fixed16\n";
+		$ls = LivestatusAccess::instance();
 
 		if( isset( $options['order'] ) ) {
 			$order_mappings = !empty($options['order_mappings'])?$options['order_mappings']:array();
@@ -739,47 +709,21 @@ class LivestatusBackend {
 							$column = substr($column,1);
 							$direction = strtolower($direction)=='asc'?'desc':'asc';
 						}
-						$query .= "Sort: $column $direction\n";
+						$filter .= "Sort: $column $direction\n";
 					}
 				} else {
-					$query .= "Sort: $column $direction\n";
+					$filter .= "Sort: $column $direction\n";
 				}
 			}
 		}
 		if( isset( $options['offset'] ) ) {
-			$query .= "Offset: ".$options['offset']."\n";
+			$filter .= "Offset: ".$options['offset']."\n";
 		}
 		if( isset( $options['limit'] ) && $options['limit'] !== false ) {
-			$query .= "Limit: ".$options['limit']."\n";
-		}
-
-		$query .= $filter."\n";
-		
-		$start   = microtime(true);
-		$rc      = $this->connection->writeSocket($query);;
-		$head    = $this->connection->readSocket(16);
-		$status  = substr($head, 0, 3);
-		$len     = intval(trim(substr($head, 4, 15)));
-		$body    = $this->connection->readSocket($len);
-		if(empty($body))
-			throw new Op5LivestatusException("empty body for query to {$this->config['path']}: <pre>".$query."</pre>");
-		if($status != 200)
-			throw new Op5LivestatusException("Invalid request: $body");
-		$result = json_decode(utf8_encode($body));
-		if (!$result) {
-			throw new Op5LivestatusException("Invalid response: $body");
+			$filter .= "Limit: ".$options['limit']."\n";
 		}
 		
-		$objects = $result->data;
-		$count = $result->total_count;
-		
-		$stop = microtime(true);
-		if ($this->config['benchmark'] == TRUE) {
-			Database::$benchmarks[] = array('query' => $this->formatQueryForDebug($query), 'time' => $stop - $start, 'rows' => $count);//count($objects));
-		}
-		if ($objects === null) {
-			throw new Op5LivestatusException("Invalid output");
-		}
+		list($columns, $objects, $count) = $ls->query($table, $filter, $columns);
 		return array($objects,$count);
 	}
 	
@@ -899,142 +843,5 @@ class LivestatusBackend {
 			array_push($result, $n);
 		}
 		return $result;
-	}
-	
-	
-	
-	private function formatQueryForDebug( $query ) {
-		$querylines = explode( "\n", $query );
-		$result = array();
-		$stats = array();
-		$filter = array();
-		
-		$result[] = array_shift( $querylines ); /* GET-line */
-		foreach( $querylines as $line ) {
-			if( empty( $line ) ) continue;
-			$fields = explode( ":", $line, 2 );
-			if( count($fields) != 2 ) {
-				$result[] = $line;
-				continue;
-			}
-			$header = trim($fields[0]);
-			$param  = trim($fields[1]);
-			switch( $header ) {
-				case 'Filter':
-					$filter[] = $param;
-					break;
-				case 'And':
-					$merge = array();
-					for( $i=0; $i<intval($param); $i++ ) {
-						$merge[] = array_pop($filter);
-					}
-					$filter[] = '(' . implode(' and ', $merge) . ')';
-					break;
-				case 'Or':
-					$merge = array();
-					for( $i=0; $i<intval($param); $i++ ) {
-						$merge[] = array_pop($filter);
-					}
-					$filter[] = '(' . implode(' or ', $merge) . ')';
-					break;
-				case 'Stats':
-					$stats[] = $param;
-					break;
-				case 'StatsAnd':
-					$merge = array();
-					for( $i=0; $i<intval($param); $i++ ) {
-						$merge[] = array_pop($stats);
-					}
-					$stats[] = '(' . implode(' and ', $merge) . ')';
-					break;
-				case 'StatsOr':
-					$merge = array();
-					for( $i=0; $i<intval($param); $i++ ) {
-						$merge[] = array_pop($stats);
-					}
-					$stats[] = '(' . implode(' or ', $merge) . ')';
-					break;
-				default:
-					$result[] = "$header: $param";
-			}
-		}
-		if( count($filter) )
-			$result[] = "Filter: ".implode(' and ', $filter);
-		foreach( $stats as $statline ) {
-			$result[] = "Stats: $statline";
-		}
-		return implode("\n", $result);
-	}
-}
-
-/*
- * Livestatus Connection Class
-*/
-class LivestatusConnection {
-	private $connection  = false;
-	private $timeout     = 10;
-
-	public function __construct($options) {
-		$this->connectionString = $options['path'];
-		return $this;
-	}
-
-	public function __destruct() {
-		$this->close();
-	}
-
-	public function connect() {
-		list($type, $address) = explode(':', $this->connectionString, 2);
-
-		if($type == 'tcp') {
-			list($host, $port) = explode(':', $address, 2);
-			$this->connection = fsockopen($address, $port, $errno, $errstr, $this->timeout);
-		}
-		elseif($type == 'unix') {
-			$this->connection = @fsockopen($this->connectionString, NULL, $errno, $errstr, $this->timeout);
-			$i = 0;
-			while ($this->connection === false && $i < $this->timeout * 10) {
-				usleep(100000);
-				$i++;
-				$this->connection = @fsockopen($this->connectionString, NULL, $errno, $errstr, $this->timeout);
-			}
-		}
-		else {
-			throw new Op5LivestatusException("unknown connection type: '$type', valid types are 'tcp' and 'unix'\n");
-		}
-
-		if(!$this->connection) {
-			throw new Op5LivestatusException("connection ".$this->connectionString." failed: ".$errstr);
-		}
-	}
-
-	public function close() {
-		if($this->connection != false) {
-			fclose($this->connection);
-			$this->connection = false;
-		}
-	}
-
-	public function writeSocket($str) {
-		if ($this->connection === false)
-			$this->connect();
-		$out = @fwrite($this->connection, $str);
-		if ($out === false)
-			throw new Op5LivestatusException("Couldn't write to livestatus socket $address");
-	}
-
-	public function readSocket($len) {
-		$socketData = '';
-
-		while($len) {
-			if(($data = fread($this->connection, $len)) == false) {
-				return false;
-			}
-
-			$len -= strlen($data);
-			$socketData .= $data;
-		}
-
-		return $socketData;
 	}
 }
