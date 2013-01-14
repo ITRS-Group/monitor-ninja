9bcf91acff3a4cfe61282e6b7c7ee57144baeb2a
Robin Sonefors <robin.sonefors@op5.com>
2013-01-14 13:57:35 +0100

    reports views: Make code clearer, and consistencify
    To begin with, especially the multiple_service_states was rendered in an
    extremely innovative way, where the summary was printed in different
    places, depending on the position of the sun, which rendered the summary
    in slightly different ways, and where the logical beginning of the loop
    was halfway through it, with many conditionals guarding the first and
    last repetition into and out of that logical start.
    
    The point of all of this was to make sure we render services in
    inconsistent ways, depending on if we're creating a multi-service report
    or a multi-servicegroup report. I don't see the point of trying to be
    inconsistent - we're quite good at that without trying - so drop that
    difference.
    
    This also made me realize the difference between how hosts and services
    are shown - hosts have summaries printed in the regular table, while
    services have an extra table. When trying to make them look the same, I
    decided the extra table is prettier, but I'm open to suggestions.
    
    Wrap the host tables with report_block, so they're properly separated.
    
    Also remove the nonsense-logic to exclude any included object with no
    included state information - versions of this logic have been here for a
    long time, but if this causes anything weird to happen, it's the input
    that is broken. This fixes bug #6435.
    
    (yes, consistencify is totally a word)
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/reports/multiple_host_states.php
+++ application/views/themes/default/reports/multiple_host_states.php
@@ -1,68 +1,70 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <?php foreach ($multiple_states as $data) { ?>
-		<table summary="<?php echo _('Host state breakdown') ?>" id="multiple_host" class="report-block">
-			<tr>
-				<th><?php echo help::render('hostgroup_breakdown').' '.(!empty($data['groupname']) ? str_replace('Hostgroup:','',$data['groupname']) : 'Selected hosts'); ?></th>
-				<th class="headerNone" style="width: 80px"><?php echo _('Up') ?></th>
-				<th class="headerNone" style="width: 80px"><?php echo _('Unreachable') ?></th>
-				<th class="headerNone" style="width: 80px"><?php echo _('Down') ?></th>
-				<th class="headerNone" style="width: 80px"><?php echo _('Undetermined') ?></th>
-			</tr>
-			<?php $no = 0; for ($i=0;$i<$data['nr_of_items'];$i++):
-			if ($data['undetermined'][$i] != 0 ||
-				$data['up'][$i] != 0 ||
-				$data['down'][$i] != 0 ||
-				$data['unreachable'][$i] != 0) { $no++;?>
-			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
-			<?php if (!$options['use_alias']) { ?>
-				<td><?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>' ?></td>
-				<?php } else { ?>
-				<td><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>' ?>)</td>
-				<?php } ?>
-				<td class="data"><?php echo reports::format_report_value($data['up'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['up'][$i]) > 0 ? '' : 'not-').'up.png'),
-					array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px'));
-					if (isset($data['counted_as_up'][$i]) && $data['counted_as_up'][$i] > 0) {
-						echo " (" . reports::format_report_value($data['counted_as_up'][$i]) ."% in other states)";
-					}?></td>
-				<td class="data"><?php echo reports::format_report_value($data['unreachable'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['unreachable'][$i]) > 0 ? '' : 'not-').'unreachable.png'),
-							array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo reports::format_report_value($data['down'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['down'][$i]) > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data"><?php echo reports::format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
-			</tr>
-			<?php } ?>
-			<?php endfor; if ($no > 0): ?>
-			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'; $i++?>">
-				<td><?php echo _('Average'); ?></td>
-				<td class="data_green"><?php echo $data['average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_up'] > 0 ? '' : 'not-').'up.png'),
-							array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
-							array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_down'] > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
-			</tr>
-			<?php endif; if ($options['use_average'] == 0 && $no > 0) { ?>
-			<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
-				<td><?php echo _('Group availability (SLA)') ?></td>
-				<td class="data_green"><?php echo $data['group_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_up'] > 0 ? '' : 'not-').'up.png'),
-							array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['group_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
-							array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['group_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_down'] > 0 ? '' : 'not-').'down.png'),
-							array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
-				<td class="data_red"><?php echo $data['group_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-							array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
-			</tr>
-			<?php } if ($no == 0) { ?>
-			<tr class="even">
-				<td colspan="5">
-					<?php echo _('No data for any of these hosts');?>
-				</td>
-			</tr>
-
-			<?php } ?>
-		</table>
+<div class="state_services report-block">
+	<table summary="<?php echo _('Host state breakdown') ?>" id="multiple_host" class="report-block">
+		<tr>
+			<th><?php echo help::render('hostgroup_breakdown').' '.(!empty($data['groupname']) ? str_replace('Hostgroup:','',$data['groupname']) : 'Selected hosts'); ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Up') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Unreachable') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Down') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Undetermined') ?></th>
+		</tr>
+		<?php for ($i=0;$i<$data['nr_of_items'];$i++) { ?>
+		<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
+		<?php if (!$options['use_alias']) { ?>
+			<td><?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>' ?></td>
+		<?php } else { ?>
+			<td><?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>' ?>)</td>
+		<?php } ?>
+			<td class="data"><?php echo reports::format_report_value($data['up'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['up'][$i]) > 0 ? '' : 'not-').'up.png'),
+				array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px'));
+				if (isset($data['counted_as_up'][$i]) && $data['counted_as_up'][$i] > 0) {
+					echo " (" . reports::format_report_value($data['counted_as_up'][$i]) ."% in other states)";
+				}?></td>
+			<td class="data"><?php echo reports::format_report_value($data['unreachable'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['unreachable'][$i]) > 0 ? '' : 'not-').'unreachable.png'),
+						array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data"><?php echo reports::format_report_value($data['down'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['down'][$i]) > 0 ? '' : 'not-').'down.png'),
+						array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data"><?php echo reports::format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
+						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+		</tr>
+		<?php } ?>
+	</table>
+</div>
+<div class="state_services report-block">
+	<table summary="<?php echo _('Host state breakdown') ?>" id="multiple_host" class="report-block">
+		<tr>
+			<th><?php echo help::render('average_and_sla').' '.('Average and Group availability for ') . ($data['groupname'] ?: _('selected hosts')); ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Up') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Unreachable') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Down') ?></th>
+			<th class="headerNone" style="width: 80px"><?php echo _('Undetermined') ?></th>
+		</tr>
+		<?php ?>
+		<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'; $i++?>">
+			<td><?php echo _('Average'); ?></td>
+			<td class="data_green"><?php echo $data['average_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_up'] > 0 ? '' : 'not-').'up.png'),
+						array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['average_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+						array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['average_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_down'] > 0 ? '' : 'not-').'down.png'),
+						array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+		</tr>
+		<?php if ($options['use_average'] == 0) { ?>
+		<tr class="<?php echo ($i%2 == 0) ? 'even' : 'odd'?>">
+			<td><?php echo _('Group availability (SLA)') ?></td>
+			<td class="data_green"><?php echo $data['group_up'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_up'] > 0 ? '' : 'not-').'up.png'),
+						array( 'alt' => _('Up'), 'title' => _('Up'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['group_unreachable'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_unreachable'] > 0 ? '' : 'not-').'unreachable.png'),
+						array( 'alt' => _('Unreachable'), 'title' => _('Unreachable'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['group_down'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_down'] > 0 ? '' : 'not-').'down.png'),
+						array( 'alt' => _('Down'), 'title' => _('Down'), 'style' => 'height: 12px; width: 12px')) ?></td>
+			<td class="data_red"><?php echo $data['group_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'), 'style' => 'height: 12px; width: 12px')) ?></td>
+		</tr>
+		<?php } ?>
+	</table>
+</div>
 <?php } ?>
