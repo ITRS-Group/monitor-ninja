86647529d863da23117042353188507fae4da568
Robin Sonefors <robin.sonefors@op5.com>
2013-01-14 13:57:35 +0100

    reports views: Make code clearer, and consistencify
    To begin with, especially the multiple_service_states was rendered in an
    extremely innovative way, where the summary was printed in different
    places, depending on the position of the sun, which rendered the summary
    in slightly different ways, and where the logical beginning of the loop
    was halfway through it, with many conditionals guarding the first and
    last repetition into and out of that logical start.
    
    The point of all of this was to make sure we render services in
    inconsistent ways, depending on if we're creating a multi-service report
    or a multi-servicegroup report. I don't see the point of trying to be
    inconsistent - we're quite good at that without trying - so drop that
    difference.
    
    This also made me realize the difference between how hosts and services
    are shown - hosts have summaries printed in the regular table, while
    services have an extra table. When trying to make them look the same, I
    decided the extra table is prettier, but I'm open to suggestions.
    
    Wrap the host tables with report_block, so they're properly separated.
    
    Also remove the nonsense-logic to exclude any included object with no
    included state information - versions of this logic have been here for a
    long time, but if this causes anything weird to happen, it's the input
    that is broken. This fixes bug #6435.
    
    (yes, consistencify is totally a word)
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/themes/default/reports/multiple_service_states.php
+++ application/views/themes/default/reports/multiple_service_states.php
@@ -1,26 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-$sg_no = 0;
-$prev_host = false;
-$prev_group = false;
-$prev_hostname = false;
 foreach ($multiple_states as $data) {
-	echo '<div class="state_services report-block">';
-	for ($i=0;$i<$data['nr_of_items'];$i++) {
-		if (isset($data['ok'][$i])) {
-			$condition = (!empty($data['groupname'])) ? $data['groupname']!= $prev_group : $data['HOST_NAME'][$i]!= $prev_host;
-
-			if ($condition) {
-				$prev_host = $data['HOST_NAME'][$i];
-				$prev_group = $data['groupname'];
-
-				# skip first iteration
-				if ($i != 0) { ?>
-			</table>
-		</div>
-		<div class="state_services report-block">
-			<?php } ?>
-	<table summary="<?php echo _('State breakdown for host services') ?>" class="multiple_services" border="1">
+	$previous_hostname = false;
+?>
+<div class="state_services report-block">
+	<table summary="<?php echo _('State breakdown for services') ?>" class="multiple_services" border="1">
 		<tr>
 			<th>
 			<?php
@@ -28,15 +12,8 @@ foreach ($multiple_states as $data) {
 			if(!empty($data['groupname'])) {
 				echo $data['groupname'];
 			} else {
-				echo '<strong>'._('Services on host') .'</strong>: ';
-				echo '<a href="'.urlencode($data['host_link'][$i]).'">';
-				if (!$options['use_alias']) {
-					echo $data['HOST_NAME'][$i];
-				} else {
-					echo $this->_get_host_alias($data['HOST_NAME'][$i]).' '.$data['HOST_NAME'][$i].')';
-				}
+				echo _('Selected services');
 			}
-			echo '</a>';
 			?>
 			</th>
 			<th class="headerNone" style="width: 80px"><?php echo _('OK') ?></th>
@@ -44,25 +21,22 @@ foreach ($multiple_states as $data) {
 			<th class="headerNone" style="width: 80px"><?php echo _('Unknown') ?></th>
 			<th class="headerNone" style="width: 80px"><?php echo _('Critical') ?></th>
 			<th class="headerNone" style="width: 80px"><?php echo _('Undetermined') ?></th>
-			</tr>
-	<?php }
-			if (!$hide_host && !empty($data['groupname']) && ($data['HOST_NAME'][$i]!= $prev_hostname || $data['groupname']!= $prev_groupname) && $sg_no == 0) { ?>
-		<tr class="even">
-		<?php if (!$options['use_alias'] && $sg_no == 0) { ?>
-			<td colspan="6" class="multiple label"><strong><?php echo _('Services on host') ?></strong>: <?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>'; ?></td>
-		<?php } elseif ($sg_no == 0) { ?>
-			<td colspan="6" class="multiple label"><strong><?php echo _('Services on host') ?></strong>: <?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>'; ?>)</td>
-		<?php } ?>
 		</tr>
-		<?php $prev_hostname = $data['HOST_NAME'][$i]; $prev_groupname = $data['groupname'];
-			}
-			$no = 0;
-			if ($data['ok'][$i] != 0 ||
-				$data['warning'][$i] != 0 ||
-				$data['unknown'][$i] != 0 ||
-				$data['critical'][$i] != 0 ||
-				$data['undetermined'][$i] != 0) {
-				$no++;?>
+	<?php
+	for ($i=0;$i<$data['nr_of_items'];$i++) { ?>
+<?php
+			if (!$hide_host && $previous_hostname != $data['HOST_NAME'][$i]) { ?>
+		<tr class="even">
+		<?php if (!$options['use_alias']) { ?>
+			<td colspan="6" class="multiple label"><strong><?php echo _('Services on host') ?></strong>: <?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>'; ?></td>
+		<?php } else { ?>
+			<td colspan="6" class="multiple label"><strong><?php echo _('Services on host') ?></strong>: <?php echo $this->_get_host_alias($data['HOST_NAME'][$i]) ?> (<?php echo '<a href="'.str_replace('&','&amp;',$data['host_link'][$i]).'">' . $data['HOST_NAME'][$i] . '</a>'; ?>)</td>
+<?php }
+			$previous_hostname = $data['HOST_NAME'][$i];
+?>
+		</tr>
+<?php
+		}?>
 		<tr class="<?php echo ($i%2==0 ? 'even' : 'odd') ?>">
 			<td class="label">
 				<a href="<?php echo str_replace('&','&amp;',$data['service_link'][$i]); ?>"><?php echo $data['SERVICE_DESCRIPTION'][$i]; ?></a>
@@ -81,59 +55,15 @@ foreach ($multiple_states as $data) {
 			<td class="data"><?php echo reports::format_report_value($data['undetermined'][$i]) ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.(reports::format_report_value($data['undetermined'][$i]) > 0 ? '' : 'not-').'pending.png'),
 						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'),'style' => 'height: 12px; width: 12px')) ?></td>
 		</tr>
-	<?php	}
-		}
+<?php
 	} # end for
-
-	$sg_no = $sg_no + $no;
-
-	if (!empty($data['groupname'])) {
-		if ($sg_no == 0) { ?>
-		<tr class="<?php echo (++$i%2==0 ? 'even' : 'odd') ?>">
-			<td><?php echo _('Average') ?></td>
-			<td class="data"><?php echo $data['average_ok'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_ok'] > 0 ? '' : 'not-').'ok.png'),
-						array( 'alt' => _('Ok'), 'title' => _('Ok'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_warning'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_warning'] > 0 ? '' : 'not-').'warning.png'),
-						array( 'alt' => _('Warning'), 'title' => _('Warning'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_unknown'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unknown'] > 0 ? '' : 'not-').'unknown.png'),
-						array( 'alt' => _('Unknown'), 'title' => _('Unknown'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_critical'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_critical'] > 0 ? '' : 'not-').'critical.png'),
-						array( 'alt' => _('Critical'), 'title' => _('Critical'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'),'style' => 'height: 12px; width: 12px')) ?></td>
-		</tr>
-		<?php if (!$options['use_average']) { ?>
-		<tr class="<?php echo (++$i%2==0 ? 'even' : 'odd') ?>">
-			<td><?php if ($options['use_average']==0) { ?><?php echo _('Group availability (SLA)') ?> <?php } else { ?><?php echo _('Average') ?><?php } ?></td>
-			<td class="data_green"><?php echo $data['group_ok'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_ok'] > 0 ? '' : 'not-').'ok.png'),
-						array( 'alt' => _('Ok'), 'title' => _('Ok'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data_red"><?php echo $data['group_warning'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_warning'] > 0 ? '' : 'not-').'warning.png'),
-						array( 'alt' => _('Warning'), 'title' => _('Warning'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data_red"><?php echo $data['group_unknown'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_unknown'] > 0 ? '' : 'not-').'unknown.png'),
-						array( 'alt' => _('Unknown'), 'title' => _('Unknown'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data_red"><?php echo $data['group_critical'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_critical'] > 0 ? '' : 'not-').'critical.png'),
-						array( 'alt' => _('Critical'), 'title' => _('Critical'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data_red"><?php echo $data['group_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['group_undetermined'] > 0 ? '' : 'not-').'pending.png'),
-						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'),'style' => 'height: 12px; width: 12px')) ?></td>
-		</tr>
-<?php	}
-		}
-	}
-
-	if ($sg_no > 0 && $no == 0) { ?>
-		<tr class="even">
-			<td colspan="6">
-				<?php echo _('No data for any of these services');?>
-			</td>
-		</tr>
-<?php } ?>
+?>
 	</table>
 </div>
-
 <div class="state_services report-block">
-	<table summary="<?php echo _('State breakdown for services') ?>" class="multiple_services">
+	<table summary="<?php echo _('State breakdown for services') ?>" class="multiple_services" border="1">
 		<tr>
-			<th><?php echo help::render('average_and_sla').' '.sprintf(_('Average and Group availability for %s'), $data['groupname']) ?></th>
+			<th><?php echo help::render('average_and_sla').' '.sprintf(_('Average and Group availability for %s'), $data['groupname'] ?: _('selected services')) ?></th>
 			<th class="headerNone" style="width: 80px"><?php echo _('OK') ?></th>
 			<th class="headerNone" style="width: 80px"><?php echo _('Warning') ?></th>
 			<th class="headerNone" style="width: 80px"><?php echo _('Unknown') ?></th>
@@ -142,15 +72,15 @@ foreach ($multiple_states as $data) {
 		</tr>
 		<tr class="<?php echo (++$i%2 == 0) ? 'even' : 'odd'; ?>">
 			<td><?php echo _('Average');?></td>
-			<td class="data"><?php echo $data['average_ok'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_ok'] > 0 ? '' : 'not-').'ok.png'),
+			<td class="data_green"><?php echo $data['average_ok'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_ok'] > 0 ? '' : 'not-').'ok.png'),
 						array( 'alt' => _('OK'), 'title' => _('OK'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_warning'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_warning'] > 0 ? '' : 'not-').'warning.png'),
+			<td class="data_red"><?php echo $data['average_warning'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_warning'] > 0 ? '' : 'not-').'warning.png'),
 						array( 'alt' => _('Warning'), 'title' => _('Warning'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_unknown'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unknown'] > 0 ? '' : 'not-').'unknown.png'),
+			<td class="data_red"><?php echo $data['average_unknown'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_unknown'] > 0 ? '' : 'not-').'unknown.png'),
 						array( 'alt' => _('Unknown'), 'title' => _('Unknown'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_critical'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_critical'] > 0 ? '' : 'not-').'critical.png'),
+			<td class="data_red"><?php echo $data['average_critical'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_critical'] > 0 ? '' : 'not-').'critical.png'),
 						array( 'alt' => _('Critical'), 'title' => _('Critical'),'style' => 'height: 12px; width: 12px')) ?></td>
-			<td class="data"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
+			<td class="data_red"><?php echo $data['average_undetermined'] ?> % <?php echo html::image($this->add_path('icons/12x12/shield-'.($data['average_undetermined'] > 0 ? '' : 'not-').'pending.png'),
 						array( 'alt' => _('Undetermined'), 'title' => _('Undetermined'),'style' => 'height: 12px; width: 12px')) ?></td>
 		</tr>
 		<?php if (!$options['use_average']) { ?>
@@ -171,5 +101,4 @@ foreach ($multiple_states as $data) {
 	</table>
 </div>
 <?php
-		$sg_no = 0;
 } # end foreach  ?>
