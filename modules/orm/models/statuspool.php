b80f50e86b60924ac9cb08fee2c7aec2b8951524
Max Sikström <msikstrom@op5.com>
2013-01-28 15:21:22 +0100

    ORM: StatusPool gives direct access to status
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/orm/models/statuspool.php
+++ modules/orm/models/statuspool.php
@@ -3,4 +3,7 @@
 require_once( dirname(__FILE__).'/base/basestatuspool.php' );
 
 class StatusPool_Model extends BaseStatusPool_Model {
+	public static function status() {
+		return self::all()->getIterator()->current();
+	}
 }
