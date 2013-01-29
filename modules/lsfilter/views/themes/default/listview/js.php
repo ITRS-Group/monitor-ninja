a910cef30e40a919417877312afe7dacc9290e45
Max Sikström <msikstrom@op5.com>
2013-01-29 10:19:49 +0100

    ListView: correct mime-type of column config javascript
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/lsfilter/views/themes/default/listview/js.php
+++ modules/lsfilter/views/themes/default/listview/js.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
-
+header('Content-Type: text/javascript');
 foreach( $vars as $var => $value ) {
 	printf( "var %s = %s;\n", $var, json_encode($value));
 }
