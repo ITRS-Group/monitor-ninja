339db1677f77971f423f3540a6b78698d7c83c75
Max Sikström <msikstrom@op5.com>
2013-02-07 19:28:00 +0100

    ListView: Fix totals for hosts and services listings
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/orm/models/hostset.php
+++ modules/orm/models/hostset.php
@@ -11,11 +11,11 @@ class HostSet_Model extends BaseHostSet_Model {
 	public function get_totals() {
 		$pool = new HostPool_Model();
 		$stats = array(
-				'host_state_up'          => $pool->get_by_name('std host state up'),
-				'host_state_down'        => $pool->get_by_name('std host state down'),
-				'host_state_unreachable' => $pool->get_by_name('std host state unreachable'),
-				'host_pending'           => $pool->get_by_name('std host pending'),
-				'host_all'               => $pool->get_by_name('std host all')
+				'host_state_up'          => $pool->get_by_query('[hosts] state = 0 and has_been_checked=1'),
+				'host_state_down'        => $pool->get_by_query('[hosts] state = 1 and has_been_checked=1'),
+				'host_state_unreachable' => $pool->get_by_query('[hosts] state = 2 and has_been_checked=1'),
+				'host_pending'           => $pool->get_by_query('[hosts] has_been_checked=0'),
+				'host_all'               => $pool->get_by_query('[hosts] all')
 		);
 		
 		$stats_result = $this->stats($stats);
--- modules/orm/models/serviceset.php
+++ modules/orm/models/serviceset.php
@@ -10,12 +10,12 @@ class ServiceSet_Model extends BaseServiceSet_Model {
 	public function get_totals() {
 		$pool = new ServicePool_Model();
 		$stats = array(
-				'service_state_ok'       => $pool->get_by_name('std service state ok'),
-				'service_state_warning'  => $pool->get_by_name('std service state warning'),
-				'service_state_critical' => $pool->get_by_name('std service state critical'),
-				'service_state_unknown'  => $pool->get_by_name('std service state unknown'),
-				'service_pending'        => $pool->get_by_name('std service pending'),
-				'service_all'            => $pool->get_by_name('std service all')
+				'service_state_ok'       => $pool->get_by_query('[services] state = 0 and has_been_checked=1'),
+				'service_state_warning'  => $pool->get_by_query('[services] state = 1 and has_been_checked=1'),
+				'service_state_critical' => $pool->get_by_query('[services] state = 2 and has_been_checked=1'),
+				'service_state_unknown'  => $pool->get_by_query('[services] state = 3 and has_been_checked=1'),
+				'service_pending'        => $pool->get_by_query('[services] has_been_checked=0'),
+				'service_all'            => $pool->get_by_query('[services] all')
 		);
 		$stats_result = $this->stats($stats);
 		$totals = array();
