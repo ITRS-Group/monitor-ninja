dfefe6eb53c21986899c4ff84042f5ca3e65cc6c
Max Sikström <msikstrom@op5.com>
2013-02-14 11:42:00 +0100

    Documentation: Module_Manifest_Model documented
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- application/models/module_manifest.php
+++ application/models/module_manifest.php
@@ -1,10 +1,19 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
+/**
+ * Handle manifest files from modules
+ */
 class Module_Manifest_Model {
 	private static $manifests = array();
 	
+	/**
+	 * Load manifest files from modules manifest directories
+	 */
 	private static function load_manifest( $name ) {
 		$manifest = array();
+		if(!preg_match('/^[a-z_]+$/',$name)) {
+			return array();
+		}
 		$files = glob(MODPATH . '*/manifest/'.$name.'.php' );
 		foreach( $files as $file ) {
 			require( $file );
@@ -12,6 +21,9 @@ class Module_Manifest_Model {
 		return $manifest;
 	}
 	
+	/**
+	 * Load manifest parameters from modules
+	 */
 	public static function get( $name ) {
 		if( !isset( self::$manifests[$name] ) ) {
 			self::$manifests[$name] = self::load_manifest( $name ); 
