780d6b216d940eee973beeebc2f2b7416262621c
Max Sikström <msikstrom@op5.com>
2013-02-26 10:08:07 +0100

    ListView: Make config not to overwrite config.
    It's needed to be able to add new configuration parameters from module-configs.
    
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- application/config/listview.php
+++ application/config/listview.php
@@ -3,13 +3,14 @@
 /*
  * This defined which columns is visible in each table, as a default setting for all users in the system.
 */
-$config['columns'] = array(
-	'hosts' => 'all',
-	'services' => 'all',
-	'hostgroups' => 'all',
-	'servicegroups' => 'all',
-	'comments' => 'all',
-	'downtimes' => 'all',
-	'contacts' => 'all',
-	'notifications' => 'all'
-);
+if( !isset( $config['columns'] ) )
+	$config['columns'] = array();
+
+$config['columns']['hosts'] = 'all';
+$config['columns']['services'] = 'all';
+$config['columns']['hostgroups'] = 'all';
+$config['columns']['servicegroups'] = 'all';
+$config['columns']['comments'] = 'all';
+$config['columns']['downtimes'] = 'all';
+$config['columns']['contacts'] = 'all';
+$config['columns']['notifications'] = 'all';
