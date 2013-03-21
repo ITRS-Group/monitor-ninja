9297a299b9d9c950615c63dccb55ee26de020f21
Carl Helmertz <chelmertz@op5.com>
2013-03-21 09:53:30 +0100

    lsfilter/textarea.js: Don't reload unchanged query on keyup
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/lsfilter/media/js/LSFilterTextarea.js
+++ modules/lsfilter/media/js/LSFilterTextarea.js
@@ -16,6 +16,9 @@ var lsfilter_textarea = {
 		this.orderelement = $('#filter_query_order');
 		this.element.bind('keyup paste cut', function(evt)
 		{
+			if(self.element.val() === query) {
+				return;
+			}
 			query = self.element.val();
 			self.handle_propertychange(query);
 		});
