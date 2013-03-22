0597f1c1b417e779eae53c7548d7b58541240fbe
Max Sikström <msikstrom@op5.com>
2013-03-22 11:58:31 +0100

    ORM: documented exception
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- modules/orm/libraries/ORMException.php
+++ modules/orm/libraries/ORMException.php
@@ -1,9 +1,19 @@
 <?php
 
+/**
+ * Exception for ORM
+ */
 class ORMException extends Exception {
 	private $table = false;
 	private $field = false;
 	
+	/**
+	 * initialization of the ORMExcetpion
+	 * 
+	 * @param $msg message to print
+	 * @param $table related table, or false
+	 * @param $field related field, or false
+	 */
 	public function __construct( $msg, $table = false, $field = false ) {
 		$message = $msg;
 		$this->table = $table;
@@ -15,10 +25,18 @@ class ORMException extends Exception {
 		parent::__construct($msg);
 	}
 	
+	/**
+	 * Get the table name related to the exception, or false
+	 * @return table name, or false
+	 */
 	public function getTable() {
 		return $this->table;
 	}
 	
+	/**
+	 * Get the field name related to the exception, or false
+	 * @return field name, or false
+	 */
 	public function getField() {
 		return $this->field;
 	}
