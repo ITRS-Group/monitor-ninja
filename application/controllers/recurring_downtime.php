8e373407aa58037e31496daa281c6122a1791d29
Robin Sonefors <robin.sonefors@op5.com>
2013-03-27 17:56:56 +0100

    recurring_downtime: Copy back strings we do in fact need here
    They were only in the report controller, meaning the report loading
    would fail to load if an object in it was gone.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/controllers/recurring_downtime.php
+++ application/controllers/recurring_downtime.php
@@ -152,6 +152,9 @@ class recurring_downtime_Controller extends Authenticated_Controller {
 		$this->js_strings .= "var _confirm_delete_schedule = '"._('Are you sure that you would like to delete this schedule.\nPlease note that already scheuled downtime won\"t be affected by this and will have to be deleted manually.\nThis action can\"t be undone.')."';\n";
 		$this->js_strings .= "var _form_field_time = '"._("time")."';\n";
 		$this->js_strings .= "var _form_field_duration = '"._("duration")."';\n";
+		$this->js_strings .= "var _reports_missing_objects = \""._("Some items in your saved report do not exist anymore and have been removed")."\";\n";
+		$this->js_strings .= "var _reports_missing_objects_pleaseremove = '"._('Please modify the objects to include in your report below and then save it.')."';\n";
+
 
 		$template->day_names = date::day_names();
 		$template->day_index = array(1, 2, 3, 4, 5, 6, 0);
