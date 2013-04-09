dfcf93e3abe72384119a0d128d61ab87718325e3
Robin Sonefors <robin.sonefors@op5.com>
2013-04-09 17:34:36 +0200

    reports/options: Add more labels
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/reports/options.php
+++ application/views/reports/options.php
@@ -23,13 +23,13 @@ if($options['report_id']) { ?>
 		<td><?php echo form::dropdown(array('name' => 'rpttimeperiod'), $options->get_alternatives('rpttimeperiod'), $options['rpttimeperiod']); ?></td>
 	</tr>
 	<tr id="display" style="display: none; clear: both;">
-		<td <?php if ($type == 'sla') { ?> style="display:none"<?php } ?>><?php echo help::render('start-date').' '._('Start date') ?> (<em id="start_time_tmp"><?php echo _('Click calendar to select date') ?></em>)<br />
+		<td <?php if ($type == 'sla') { ?> style="display:none"<?php } ?>><label for="cal_start"><?php echo help::render('start-date').' '._('Start date') ?></label> (<em id="start_time_tmp"><?php echo _('Click calendar to select date') ?></em>)<br />
 			<input type="text" id="cal_start" name="cal_start" maxlength="10" autocomplete="off" value="<?php echo $options->get_date('start_time') ?>" class="date-pick datepick-start" title="<?php echo _('Date Start selector') ?>" />
 			<input type="text" maxlength="5" name="time_start" id="time_start" class="time_start" value="<?php echo $options->get_time('start_time') ?>">
 			<input type="hidden" name="start_time" id="start_time" value="<?php echo $options['start_time'] ?>" />
 		</td>
 		<td<?php if ($type == 'sla') { ?> style="display:none"<?php } ?>>&nbsp;</td>
-		<td<?php if ($type == 'sla') { ?> style="display:none"<?php } ?>><?php echo help::render('end-date').' '._('End date') ?> (<em id="end_time_tmp"><?php echo _('Click calendar to select date') ?></em>)<br />
+		<td<?php if ($type == 'sla') { ?> style="display:none"<?php } ?>><label for="cal_end"><?php echo help::render('end-date').' '._('End date') ?></label> (<em id="end_time_tmp"><?php echo _('Click calendar to select date') ?></em>)<br />
 			<input type="text" id="cal_end" name="cal_end" maxlength="10" autocomplete="off" value="<?php echo $options->get_date('end_time') ?>" class="date-pick datepick-end" title="<?php echo _('Date End selector') ?>" />
 			<input type="text" maxlength="5" name="time_end" id="time_end" class="time_end" value="<?php echo $options->get_time('end_time') ?>">
 			<input type="hidden" name="end_time" id="end_time" value="<?php echo $options['end_time'] ?>" />
