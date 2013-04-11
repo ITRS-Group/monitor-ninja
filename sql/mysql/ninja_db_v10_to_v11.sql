f88e231dc5885bf97871176872f046020538dbda
Mattias Ryrlén <mattiasr@op5.com>
2013-04-11 17:44:56 +0200

    sql: fix systems up to 6.1.0-beta6 that has wrong saved filters for listview
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

---
+++ sql/mysql/ninja_db_v10_to_v11.sql
@@ -0,0 +1,7 @@
+UPDATE `ninja_saved_filters` SET `filter_name` = 'acknowledged hosts', `filter_description` = 'acknowledged hosts' WHERE `filter_name` = 'Acknowledged hosts';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'acknowledged services', `filter_description` = 'acknowledged services' WHERE `filter_name` = 'Acknowledged services';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'host groups with problems', `filter_description` = 'host groups with problems' WHERE `filter_name` = 'Host groups with problems';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'service groups with problems', `filter_description` = 'service groups with problems' WHERE `filter_name` = 'Service groups with problems';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'unhandled host problems', `filter_description` = 'unhandled host problems' WHERE `filter_name` = 'Unhandled host problems';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'unhandled problems', `filter_description` = 'unhandled problems' WHERE `filter_name` = 'Unhandled problems';
+UPDATE `ninja_saved_filters` SET `filter_name` = 'unhandled service problems', `filter_description` = 'unhandled service problems' WHERE `filter_name` = 'Unhandled service problems';
