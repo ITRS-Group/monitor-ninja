f3346db642d1986107d7aa3500727d19603184da
Max Sikström <msikstrom@op5.com>
2013-04-15 15:00:56 +0200

    LSFilter: test generation of livestatus and SQL queries
    This uses the visitor interface to do unit tests of generation of queries from
    LSFilter, through ORM layer, to query the database.
    
    It doesn't actually run them though...
    
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- test/unit_test/tests/lsfilter_Test.php
+++ test/unit_test/tests/lsfilter_Test.php
@@ -122,14 +122,234 @@ class LSFilter_Test extends TapUnit {
 			$this->run_test_query("[$table] all");
 		}
 	}
-	
+
 	public function test_nonexisting_tables() {
-			$this->run_parse_fail("[nonexisting] all");
+		$this->run_parse_fail("[nonexisting] all");
 	}
+
+
+	/*
+	 * Test generation of SQL query
+	 */
+	public function test_sql_simple_all() {
+		$this->run_visitor(
+			"[notifications] all",
+			new LivestatusSQLBuilderVisitor(),
+			"(1=1)"
+			);
+	}
+	public function test_sql_op_eq_str() {
+		$this->run_visitor(
+			"[notifications] contact_name=\"kaka\"",
+			new LivestatusSQLBuilderVisitor(),
+			"((contact_name = 'kaka'))"
+			);
+	}
+	public function test_sql_op_regexp_str() {
+		$this->run_visitor(
+			"[notifications] contact_name~~\"kaka\"",
+			new LivestatusSQLBuilderVisitor(),
+			"((contact_name REGEXP 'kaka'))"
+			);
+	}
+	public function test_sql_op_not_regexp_str() {
+		$this->run_visitor(
+			"[notifications] contact_name!~~\"kaka\"",
+			new LivestatusSQLBuilderVisitor(),
+			"(NOT (contact_name REGEXP 'kaka'))"
+			);
+	}
+	public function test_sql_op_eq_integer() {
+		$this->run_visitor(
+			"[notifications] id=3",
+			new LivestatusSQLBuilderVisitor(),
+			"((id = '3'))" /* FIXME: should be integer, but works as this */
+			);
+	}
+	public function test_sql_andor() {
+		$this->run_visitor(
+			"[notifications] output=\"a\" and output=\"b\" or output=\"c\" and output=\"d\"",
+			new LivestatusSQLBuilderVisitor(),
+			"(((output = 'a') AND (output = 'b')) OR ((output = 'c') AND (output = 'd')))"
+			);
+	}
+	public function test_sql_orand() {
+		$this->run_visitor(
+			"[notifications] (output=\"a\" or output=\"b\") and (output=\"c\" or output=\"d\")",
+			new LivestatusSQLBuilderVisitor(),
+			"((((output = 'a')) OR ((output = 'b'))) AND (((output = 'c')) OR ((output = 'd'))))"
+			);
+	}
+
+
+	/*
+	 * Test generation of Livestaus query
+	*/
+	public function test_ls_all() {
+		$this->run_visitor(
+				"[hosts] all",
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"And: 0"
+						)
+		);
+	}
+	public function test_ls_not_all() {
+		$this->run_visitor(
+				"[hosts] not all",
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"And: 0",
+						"Negate:"
+						)
+		);
+	}
+	public function test_ls_simple_and() {
+		$this->run_visitor(
+				'[hosts] name="a" and name="b"',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"And: 2"
+						)
+		);
+	}
+	public function test_ls_simple_or() {
+		$this->run_visitor(
+				'[hosts] name="a" or name="b"',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Or: 2"
+						)
+		);
+	}
+	public function test_ls_andor() {
+		$this->run_visitor(
+				'[hosts] (name="a" or name="b") and (name="c" or name="d")',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Or: 2",
+						"Filter: name = c",
+						"Filter: name = d",
+						"Or: 2",
+						"And: 2"
+						)
+		);
+	}
+	public function test_ls_orand() {
+		$this->run_visitor(
+				'[hosts] (name="a" and name="b") or (name="c" and name="d")',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"And: 2",
+						"Filter: name = c",
+						"Filter: name = d",
+						"And: 2",
+						"Or: 2"
+						)
+		);
+	}
+	public function test_ls_orandnot() {
+		$this->run_visitor(
+				'[hosts] (name="a" and name="b") or not (name="c" and name="d")',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"And: 2",
+						"Filter: name = c",
+						"Filter: name = d",
+						"And: 2",
+						"Negate:",
+						"Or: 2"
+						)
+		);
+	}
+	public function test_ls_trippleand_l() {
+		$this->run_visitor(
+				'[hosts] (name="a" and name="b") and name="c"',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Filter: name = c",
+						"And: 3"
+						)
+		);
+	}
+	public function test_ls_trippleand_r() {
+		$this->run_visitor(
+				'[hosts] name="a" and (name="b" and name="c")',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Filter: name = c",
+						"And: 3"
+						)
+		);
+	}
+	public function test_ls_trippleor_l() {
+		$this->run_visitor(
+				'[hosts] (name="a" or name="b") or name="c"',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Filter: name = c",
+						"Or: 3"
+						)
+		);
+	}
+	public function test_ls_trippleor_r() {
+		$this->run_visitor(
+				'[hosts] name="a" or (name="b" or name="c")',
+				new LivestatusFilterBuilderVisitor(),
+				array(
+						"Filter: name = a",
+						"Filter: name = b",
+						"Filter: name = c",
+						"Or: 3"
+						)
+		);
+	}
+	public function test_lsstats_orandnot() {
+		$this->run_visitor(
+				'[hosts] (name="a" and name="b") or not (name="c" and name="d")',
+				new LivestatusStatsBuilderVisitor(),
+				array(
+						"Stats: name = a",
+						"Stats: name = b",
+						"StatsAnd: 2",
+						"Stats: name = c",
+						"Stats: name = d",
+						"StatsAnd: 2",
+						"StatsNegate:",
+						"StatsOr: 2"
+						)
+		);
+	}
+
 
 	/*
 	 * Internal methods to run a test
 	*/
+
+	private function run_visitor( $query, $visitor, $goal ) {
+		if(is_array($goal)) {
+			$goal = implode("\n",$goal)."\n";
+		}
+		$set = ObjectPool_Model::get_by_query($query);
+		$converted = $set->test_visit_filter($visitor, false);
+		$this->ok_eq($converted, $goal, 'Query missmatch');
+	}
 
 	private function run_test_query( $query, $match_query = false ) {
 		if( $match_query === false )
