ITRS-Group/monitor-ninja

commit e16775688b3813f18e5bf4c9c782dc81f74804ea

Macro processing rewritten to match livestaus namings (and therefore most cerainly fixed). Resolves #6175.
Review plx?

Signed-off-by: Max Sikström <msikstrom@op5.com>

application/helpers/nagstat.php
@@ -144,31 +144,58 @@ class nagstat_Core {
 	/**
 	 * Process macros for host- or service objects
 	 */
-	public static function process_macros($string=false, &$obj=false)
+	public static function process_macros($string=false, $obj=false, $objtype=false)
 	{
-		if (empty($string) || empty($obj)) {
+		if (empty($string) || empty($obj) || empty($objtype)) {
 			return false;
 		}
-
+		
+				
 		$macros = array(
-			'$HOSTNAME$' => 'host_name',
-			'$HOSTADDRESS$' => 'address',
-			'$HOSTDISPLAYNAME$' => 'display_name',
-			'$HOSTALIAS$' => 'alias',
-			'$HOSTSTATE$' => array("status_text[%s, host]", 'current_state'), /* UP/DOWN/UNREACHABLE - callback */
-			'$HOSTSTATEID$' => 'current_state',
-			'$HOSTSTATETYPE$' => array('array[%s,SOFT,HARD]', 'state_type'), /* HARD/SOFT - callback */
-			'$HOSTATTEMPT$' => 'current_attempt',
-			'$MAXHOSTATTEMPTS$' => 'max_check_attempts',
-			'$HOSTGROUPNAME$' => 'hostgroup_name',
-			'$HOSTGROUPALIAS$' => 'alias',
-			'$SERVICEDESC$' => 'description',
-			'$SERVICEDISPLAYNAME$' => 'display_name',
-			'$SERVICEGROUPNAME$' => 'servicegroup_name',
-			'$SERVICESTATE$' => array("status_text[%s, service]", 'current_state'),
-			'$SERVICEGROUPALIAS$' => 'alias',
-			'$CURRENT_USER$' => array('current_user', 'host_name')
+				'host' => array(
+					'$HOSTNAME$' => 'name',
+					'$HOSTADDRESS$' => 'address',
+					'$HOSTDISPLAYNAME$' => 'display_name',
+					'$HOSTALIAS$' => 'alias',
+					'$HOSTSTATE$' => array(array('UP','DOWN','UNREACHABLE'), 'state'), /* UP/DOWN/UNREACHABLE - callback */
+					'$HOSTSTATEID$' => 'state',
+					'$HOSTSTATETYPE$' => array(array('SOFT','HARD'), 'state_type'), /* HARD/SOFT - callback */
+					'$HOSTATTEMPT$' => 'current_attempt',
+					'$MAXHOSTATTEMPTS$' => 'max_check_attempts',
+					'$HOSTGROUPNAME$' => array('array_first','groups'),
+					'$CURRENT_USER$' => array('current_user')
+				),
+				'service' => array(
+					'$HOSTNAME$' => 'host_name',
+					'$HOSTADDRESS$' => 'host_address',
+					'$HOSTDISPLAYNAME$' => 'host_display_name',
+					'$HOSTALIAS$' => 'host_alias',
+					'$HOSTSTATE$' => array(array('UP','DOWN','UNREACHABLE'), 'host_state'), /* UP/DOWN/UNREACHABLE - callback */
+					'$HOSTSTATEID$' => 'host_state',
+					'$HOSTSTATETYPE$' => array(array('SOFT','HARD'), 'host_state_type'), /* HARD/SOFT - callback */
+					'$HOSTATTEMPT$' => 'host_current_attempt',
+					'$MAXHOSTATTEMPTS$' => 'host_max_check_attempts',
+					'$HOSTGROUPNAME$' => array('array_first','host_groups'),
+					'$SERVICEDESC$' => 'description',
+					'$SERVICEDISPLAYNAME$' => 'display_name',
+					'$SERVICEGROUPNAME$' => array('array_first','groups'),
+					'$SERVICESTATE$' => array(array('OK','WARNING','CRITICAL','UNKNOWN'), 'state'),
+					'$CURRENT_USER$' => array('current_user')
+				),
+				'hostgroup' => array(
+					'$HOSTGROUPNAME$' => 'name',
+					'$HOSTGROUPALIAS$' => 'alias',
+				),
+				'servicegroup' => array(
+					'$SERVICEGROUPNAME$' => 'name',
+					'$SERVICEGROUPALIAS$' => 'alias',
+				)
 		);
+		
+		if( !isset( $macros[$objtype] ) ) {
+			return $string; /* No macros defined for object type, no macros can be replaced */
+		}
+		$macros = $macros[$objtype];
 
 		$regexp = '/\$[A-Z_]*\$/';
 		$hits = preg_match_all($regexp, $string, $res);
@@ -180,9 +207,15 @@ class nagstat_Core {
 						$field = $macros[$match];
 
 						if (is_array($field)) {
-							$val = isset($obj->{$field[1]}) ? self::do_callback(sprintf($field[0], $obj->{$field[1]})) : false;
-							if ($val !== false) {
-								$string = str_replace($match, $val, $string);
+							if( is_array($field[0]) ) {
+								if( isset($obj->{$field[1]}) ) {
+									$string = str_replace($match, $field[0][$obj->{$field[1]}], $string);
+								}
+							} else {
+								$func = array_shift( $field );
+								$replace = call_user_func( $func, $obj, $field );
+								if( $replace !== false )
+									$string = str_replace($match, $replace, $string);
 							}
 						} else {
 							if (isset($obj->{$field})) {
@@ -195,64 +228,6 @@ class nagstat_Core {
 		}
 
 		return $string;
-	}
-
-	/**
-	* Try to figure out what (and how) to call methods/functions
-	* from the callbacks passed from process_macros()
-	*/
-	public function do_callback(&$callback)
-	{
-		$args = false;
-		if (is_string($callback)) {
-			if (preg_match('/^([^\[]++)\[(.+)\]$/', $callback, $matches)) {
-				// Split the function and args
-				$callback = $matches[1];
-				$args = preg_split('/(?<!\\\\),\s*/', $matches[2]);
-			}
-		}
-
-		if (is_string($callback)) {
-			if (strpos($callback, '::') !== FALSE) {
-				$callback = explode('::', $callback);
-			} elseif (function_exists($callback)) {
-				// No need to check if the callback is a method
-				$callback = $callback;
-			}
-		}
-
-		if ($callback === 'array' && is_array($args) && !empty($args)) {
-			$val = $args[0];
-			# remove first element which is the value to be "translated"
-			array_shift($args);
-			if (array_key_exists($val, $args)) {
-				return $args[$val];
-			} else {
-				return false;
-			}
-		}
-
-		$value = false;
-		$name = false;
-		if (is_callable($callback)) {
-			if (is_array($callback)) {
-				if (is_object($callback[0])) {
-					// Object instance syntax
-					$name = get_class($callback[0]).'->'.$callback[1];
-				} else {
-					// Static class syntax
-					$name = $callback[0].'::'.$callback[1];
-				}
-			} else {
-				// Function syntax
-				$name = $callback;
-			}
-		}
-
-		if (function_exists($name)) {
-			$value = call_user_func_array($name, $args);
-		}
-		return $value;
 	}
 
 
@@ -320,14 +295,16 @@ class nagstat_Core {
 	}
 }
 
-
 /**
-* Helper function to get status text from current status model
-* Used in callbacks in process_macros
+*	Callback to return first of a list field
 */
-function status_text($db_status=false, $type='host')
+function array_first( $obj, $fields )
 {
-	return Current_status_Model::status_text($db_status, 1, $type);
+	if( !isset( $obj->{$fields[0]} ) || !is_array( $obj->{$fields[0]} ) )
+		return false;
+	$arr = $obj->{$fields[0]};
+	if( count($arr) == 0 ) return '';
+	return $arr[0];
 }
 
 /**

application/controllers/extinfo.php
@@ -139,7 +139,7 @@ class Extinfo_Controller extends Authenticated_Controller {
 			$content->lable_next_scheduled_check = _('Next scheduled active check');
 			$content->lable_flapping = _('Is this host flapping?');
 			$obsessing = $result->obsess;
-			$content->notes = $result->notes !='' ? nagstat::process_macros($result->notes, $result) : false;
+			$content->notes = $result->notes !='' ? nagstat::process_macros($result->notes, $result, 'host') : false;
 
 			$content->parents = $result->parents;
 
@@ -795,9 +795,9 @@ class Extinfo_Controller extends Authenticated_Controller {
 		$content->cmd_disable_active_host_checks = nagioscmd::command_id('DISABLE_'.strtoupper($grouptype).'_HOST_CHECKS');
 		$content->cmd_enable_active_host_checks = nagioscmd::command_id('ENABLE_'.strtoupper($grouptype).'_HOST_CHECKS');
 
-		$content->notes_url = $group_info_res->notes_url !='' ? nagstat::process_macros($group_info_res->notes_url, $group_info_res) : false;
-		$content->action_url =$group_info_res->action_url !='' ? nagstat::process_macros($group_info_res->action_url, $group_info_res) : false;
-		$content->notes = $group_info_res->notes !='' ? nagstat::process_macros($group_info_res->notes, $group_info_res) : false;
+		$content->notes_url = $group_info_res->notes_url !='' ? nagstat::process_macros($group_info_res->notes_url, $group_info_res, $grouptype) : false;
+		$content->action_url =$group_info_res->action_url !='' ? nagstat::process_macros($group_info_res->action_url, $group_info_res, $grouptype) : false;
+		$content->notes = $group_info_res->notes !='' ? nagstat::process_macros($group_info_res->notes, $group_info_res, $grouptype) : false;
 
 		switch ($grouptype) {
 			case 'servicegroup':

application/libraries/Livestatus.php
@@ -198,7 +198,9 @@ class Livestatus {
 					'host_active_checks_enabled', 'host_address', 'host_alias', 'host_checks_enabled', 'host_check_type',
 					'host_comments', 'host_groups', 'host_has_been_checked', 'host_icon_image_expanded', 'host_icon_image_alt',
 					'host_is_executing', 'host_is_flapping', 'host_name', 'host_notes_url_expanded',
-					'host_notifications_enabled', 'host_scheduled_downtime_depth', 'host_state', 'host_accept_passive_checks',
+					'host_notifications_enabled', 'host_scheduled_downtime_depth', 'host_state', 'host_state_type',
+					'host_accept_passive_checks', 'host_current_attempt', 'host_max_check_attempts',
+					'host_display_name',
 					'icon_image', 'icon_image_alt', 'icon_image_expanded', 'is_executing', 'is_flapping',
 					'last_check', 'last_notification', 'last_state_change', 'latency', 'long_plugin_output',
 					'low_flap_threshold', 'max_check_attempts', 'next_check', 'notes', 'notes_expanded',

application/views/themes/default/search/result.php
@@ -90,12 +90,12 @@ if (isset($host_result) ) {
 				if (Kohana::config('config.pnp4nagios_path')!==false)
 					echo ($host->pnpgraph_present)  ? '<a href="' . url::site() . 'pnp/?host='.urlencode($host->name).'&srv=_HOST_" style="border: 0px">'.html::image($this->add_path('icons/16x16/pnp.png'), array('alt' => _('Show performance graph'), 'title' => _('Show performance graph'), 'class' => 'pnp_graph_icon')).'</a> &nbsp;' : '';
 				if (!empty($host->action_url)) {
-					echo '<a href="'.nagstat::process_macros($host->action_url, $host).'" style="border: 0px" target="'.$action_url_target.'">';
+					echo '<a href="'.nagstat::process_macros($host->action_url, $host, 'host').'" style="border: 0px" target="'.$action_url_target.'">';
 					echo html::image($this->add_path('icons/16x16/host-actions.png'), _('Perform extra host actions'));
 					echo '</a> &nbsp;';
 				}
 				if (!empty($host->notes_url)) {
-					echo '<a href="'.nagstat::process_macros($host->notes_url, $host).'" style="border: 0px" target="'.$notes_url_target.'">';
+					echo '<a href="'.nagstat::process_macros($host->notes_url, $host, 'host').'" style="border: 0px" target="'.$notes_url_target.'">';
 					echo html::image($this->add_path('icons/16x16/host-notes.png'), _('View extra host notes'));
 					echo '</a>';
 				}
@@ -238,12 +238,12 @@ if (isset($service_result) ) {
 						echo '<a href="' . url::site() . 'pnp/?host='.urlencode($service->host_name).'&srv='.urlencode($service->description).'" style="border: 0px">'.html::image($this->add_path('icons/16x16/pnp.png'), array('alt' => _('Show performance graph'), 'title' => _('Show performance graph'), 'class' => 'pnp_graph_icon')).'</a> &nbsp;';
 				}
 				if (!empty($service->action_url)) {
-					echo '<a href="'.nagstat::process_macros($service->action_url, $service).'" style="border: 0px" target="'.$action_url_target.'">';
+					echo '<a href="'.nagstat::process_macros($service->action_url, $service, 'service').'" style="border: 0px" target="'.$action_url_target.'">';
 					echo html::image($this->add_path('icons/16x16/host-actions.png'),array('alt' => _('Perform extra host actions'),'title' => _('Perform extra host actions')));
 					echo '</a> &nbsp;';
 				}
 				if (!empty($service->notes_url)) {
-					echo '<a href="'.nagstat::process_macros($service->notes_url, $service).'" style="border: 0px" target="'.$notes_url_target.'">';
+					echo '<a href="'.nagstat::process_macros($service->notes_url, $service, 'service').'" style="border: 0px" target="'.$notes_url_target.'">';
 					echo html::image($this->add_path('icons/16x16/host-notes.png'),array('alt' => _('View extra host notes'),'title' => _('View extra host notes')));
 					echo '</a> &nbsp;';
 				}

application/views/themes/default/status/group_overview.php
@@ -129,12 +129,12 @@ $action_url_target = config::get('nagdefault.action_url_target', '*');?>
 
 				if (!is_null($host->host_action_url)) {
 					$lable_host_action = _('Perform Extra Host Actions');
-					echo '<a href="'.nagstat::process_macros($host->host_action_url, $host).'" style="border: 0px" target="'.$action_url_target.'"><img src="'.ninja::add_path('icons/16x16/host-actions.png').'" alt="'.$lable_host_action.'" title="'.$lable_host_action.'" /></a> ';
+					echo '<a href="'.nagstat::process_macros($host->host_action_url, $host, 'service').'" style="border: 0px" target="'.$action_url_target.'"><img src="'.ninja::add_path('icons/16x16/host-actions.png').'" alt="'.$lable_host_action.'" title="'.$lable_host_action.'" /></a> ';
 				}
 
 				if (!is_null($host->host_notes_url)) {
 					$lable_host_notes = _('View Extra Host Notes');
-					echo '<a href="'.nagstat::process_macros($host->host_notes_url, $host).'" style="border: 0px" target="'.$notes_url_target.'"><img src="'.ninja::add_path('icons/16x16/host-notes.png').'" alt="'.$lable_host_notes.'" title="'.$lable_host_notes.'" /></a> ';
+					echo '<a href="'.nagstat::process_macros($host->host_notes_url, $host, 'service').'" style="border: 0px" target="'.$notes_url_target.'"><img src="'.ninja::add_path('icons/16x16/host-notes.png').'" alt="'.$lable_host_notes.'" title="'.$lable_host_notes.'" /></a> ';
 				} ?>
 			</td>
 		</tr>

application/views/themes/default/status/host.php
@@ -159,12 +159,12 @@ foreach ($result as $row) {
 								'<span class="pnp_graph_icon icon-16 x16-pnp pnp_graph_icon" title="'._('Show performance graph').'"></span>',
 								array('style' => 'border: 0px')): '';
 						if (!empty($row->action_url)) {
-							echo '<a href="'.nagstat::process_macros($row->action_url, $row).'" style="border: 0px" target="'.$action_url_target.'">'.
+							echo '<a href="'.nagstat::process_macros($row->action_url, $row, 'host').'" style="border: 0px" target="'.$action_url_target.'">'.
 								'<span class="icon-16 x16-host-actions" title="'._('perform extra host actions').'"></span>'.
 								'</a>';
 						}
 						if (!empty($row->notes_url)) {
-							echo '<a href="'.nagstat::process_macros($row->notes_url, $row).'" style="border: 0px" target="'.$notes_url_target.'">'.
+							echo '<a href="'.nagstat::process_macros($row->notes_url, $row, 'host').'" style="border: 0px" target="'.$notes_url_target.'">'.
 								'<span class="icon-16 x16-host-notes" title="'._('View extra host notes').'"></span>'.
 								'</a>';
 						}

application/views/themes/default/status/service.php
@@ -242,12 +242,12 @@ $action_url_target = config::get('nagdefault.action_url_target', '*');?>
 					array('style' => 'border: 0px')) : '';
 				
 				if (!empty($row->action_url)) {
-					echo '<a href="'.nagstat::process_macros($row->action_url, $row).'" style="border: 0px" target="'.$action_url_target.'">'.
+					echo '<a href="'.nagstat::process_macros($row->action_url, $row, 'service').'" style="border: 0px" target="'.$action_url_target.'">'.
 						'<span class="icon-16 x16-host-actions" title="'._('Perform extra host actions').'"></span>'.
 						'</a>';
 				}
 				if (!empty($row->notes_url)) {
-					echo '<a href="'.nagstat::process_macros($row->notes_url, $row).'" style="border: 0px" target="'.$notes_url_target.'">'.
+					echo '<a href="'.nagstat::process_macros($row->notes_url, $row, 'service').'" style="border: 0px" target="'.$notes_url_target.'">'.
 						'<span class="icon-16 x16-host-notes" title="'._('View extra host notes').'"></span>'.
 					'</a>';
 				}

commit 4136112596e8ba22aa56dbe8dd30c35b8b02d5d8

nagstat helper process_macros(): Use heuristics to determine object type
This function was broken for all callers when a new required variable
was introduced in commit e16775688b3813f18e5bf4c9c782dc81f74804ea,
but since both external and op5-supported widgets rely on the old
call-signature (two arguments required), we need to provide some
sort of fallback for the case where we don't get the newly introduced
third argument.

This patch provides that fallback, providing a backend fix for #6764.

Signed-off-by: Andreas Ericsson <ae@op5.se>

application/helpers/nagstat.php
@@ -146,8 +146,19 @@ class nagstat_Core {
 	 */
 	public static function process_macros($string=false, $obj=false, $objtype=false)
 	{
-		if (empty($string) || empty($obj) || empty($objtype)) {
+		if (empty($string) || empty($obj)) {
 			return $string;
+		}
+
+		# try some heuristics to determine object type
+		if (empty($objtype)) {
+			if (!empty($obj->service_description)) {
+				$objtype = 'service';
+			} elseif (!empty($obj->address) || !empty($obj->state)) {
+				$objtype = 'host';
+			} else {
+				$objtype = 'group';
+			}
 		}
 		$macros = array(
 				'host' => array(
@@ -187,9 +198,18 @@ class nagstat_Core {
 				'servicegroup' => array(
 					'$SERVICEGROUPNAME$' => 'name',
 					'$SERVICEGROUPALIAS$' => 'alias',
-				)
+				),
+				# strictly speaking, this is wrong, but the effect is
+				# hopefully negligible and can easily be avoided by
+				# specifying the objtype at the callsite.
+				'group' => array(
+					'$HOSTGROUPNAME$' => 'name',
+					'$HOSTGROUPALIAS$' => 'alias',
+					'$SERVICEGROUPNAME$' => 'name',
+					'$SERVICEGROUPALIAS$' => 'alias',
+				),
 		);
-		
+
 		if( !isset( $macros[$objtype] ) ) {
 			return $string; /* No macros defined for object type, no macros can be replaced */
 		}