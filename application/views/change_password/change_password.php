e2df20ef68171ee77f4f1d4980cff286be05c731
Max Sikström <msikstrom@op5.com>
2013-05-07 09:41:15 +0200

    Change Password: Change label for confirm.
    People might find it unclear that "Confirm password" means repeat the new
    password. To make it really clear, this is now changed.
    
    Also, this makes it possible to translate those labels.
    
    Related ticket #7208
    
    Signed-off-by: Max Sikström <msikstrom@op5.com>

--- application/views/change_password/change_password.php
+++ application/views/change_password/change_password.php
@@ -12,13 +12,19 @@ if (isset($error_msg)) echo $error_msg; ?>
 		'current_password' => form::password(array('name' => 'current_password', 'autocomplete' => 'off')),
 		'new_password' => form::password(array('name' => 'new_password', 'autocomplete' => 'off')),
 		'confirm_password' => form::password(array('name' => 'confirm_password', 'autocomplete' => 'off'))
-	);?>
+	);
+	$labels = array(
+		'current_password' => _('Current password'),
+		'new_password' => _('New password'),
+		'confirm_password' => _('Repeat new password')
+	);
+	?>
 	<table style="border: none;" class="white-table">
 	<?php
 		foreach ($fields as $label => $field) {
 			echo '
 			<tr>
-				<td style="border: none; padding-right: 10px; width: 100px">'.form::label($label).'</td>
+				<td style="border: none; padding-right: 10px; width: 100px">'.form::label($label, $labels[$label]).'</td>
 				<td style="border: none;">'.$field.'</td>
 			</tr>';
 		} ?>
