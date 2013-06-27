8646ff1858284790f3009913d4ffad1ef98cf184
Robin Sonefors <robin.sonefors@op5.com>
2013-06-27 10:42:55 +0200

    trends: Avoid rounding errors when there are many tiny events
    Because the events were sized according to their duration, you could end
    up in a situation where you had hundreds of events, all with a
    ridiculously small width of 0.002% or something, wich was ignored both
    by browsers and our PDF engine.
    
    Fix by setting the start and stop percentages, and letting the rendering
    engine auto-calculate the width as good it can. This means the graph
    always starts at 0% and ends at 100%, and has a full band of colors
    inbetween. Some events will still be rounded away, because we're subject
    to the real world where pixels are discrete, but if a service is
    flapping enough for this to be an issue, you'll likely still get a
    green-red zebra stripy pattern.
    
    This fixes bug #6656.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- application/views/reports/js/tgraph.js
+++ application/views/reports/js/tgraph.js
@@ -146,10 +146,11 @@ TGraph.prototype = {
 			this.formatNumber(date.getDate());
 	},
 
-	createBlock: function (stop) {
+	createBlock: function (stop, starttime) {
 		var block = document.createElement('div');
 
-		block.style.width = ((stop.duration / this.max) * 100) + '%';
+		block.style.left = ((starttime / this.max) * 100) + '%';
+		block.style.right = (100 - ((starttime + stop.duration) / this.max) * 100) + '%';
 		block.style.background = stop.color;
 		block.className = 'tgraph-block';
 
@@ -259,6 +260,7 @@ TGraph.prototype = {
 			lastHost = cHost;
 
 			time = this.start;
+			running_duration = 0;
 			laststate = '';
 
 			this.height += 40;
@@ -268,7 +270,7 @@ TGraph.prototype = {
 				this.stops[y][i].duration = this.stops[y][i].duration * 1000;
 				this.stops[y][i].index = i;
 
-				this.stops[y][i].block = this.createBlock(this.stops[y][i]);
+				this.stops[y][i].block = this.createBlock(this.stops[y][i], running_duration);
 
 				if ((this.stops[y][i].duration / this.max) < 0.03 && this.upscale === true) {
 
@@ -319,6 +321,7 @@ TGraph.prototype = {
 				line.appendChild(this.stops[y][i].block);
 
 				time += this.stops[y][i].duration;
+				running_duration += this.stops[y][i].duration;
 
 			}
 
