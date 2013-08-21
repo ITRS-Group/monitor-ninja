5c6d5c7367249c2ac82c40f1f0e5803a62c19599
Carl Helmertz <chelmertz@op5.com>
2013-08-21 12:10:15 +0200

    global_search.js: Explicitly check element to .autocomplete()
    The autocomplete plugin sucks and fails with an error if the element
    it's applied to doesn't exist.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/views/js/global_search.js
+++ application/views/js/global_search.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
-   $('#query').autocomplete({
-    serviceUrl:_site_domain + _index_page + '/ajax/global_search/',
-    minChars:2,
-    //delimiter: /(,|;)\s*/, // regex or character
-    maxHeight:500,
-    width:269,
-    deferRequestBy: 300, //miliseconds
-    // callback function:
-    onSelect: function(value, data){ do_redirect(value, data); }
-  });
+	var query = $('#query');
+	if(!query.length) {
+		// fix to avoid autocomplete if element is not present.
+		// the autocomplete source is minified and we've got a demo to run..
+		// new-host wizard fails without this
+		return;
+	}
+	query.autocomplete({
+		serviceUrl:_site_domain + _index_page + '/ajax/global_search/',
+		minChars:2,
+		//delimiter: /(,|;)\s*/, // regex or character
+		maxHeight:500,
+		width:269,
+		deferRequestBy: 300, //miliseconds
+		// callback function:
+		onSelect: function(value, data){ do_redirect(value, data); }
+	});
 
 
 	var search_old_refresh = 0;
-	$('#query').focus(function() {
+	query.focus(function() {
 		search_old_refresh = current_interval;
 		ninja_refresh(0);
 		$("#ninja_refresh_control").attr('checked', true);
 		$('#ninja_refresh_lable').css('font-weight', 'bold');
 	});
 
-	$('#query').blur(function() {
+	query.blur(function() {
 		if (current_interval === 0 && search_old_refresh !== 0) {
 			current_interval = search_old_refresh;
 			ninja_refresh(current_interval);
