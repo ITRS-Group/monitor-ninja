c29bffaa3d132caed4fa8d7d5f59925a0b9ac3fd
Robin Sonefors <robin.sonefors@op5.com>
2013-10-08 14:56:55 +0200

    Revert "menu: Remove title attribute on menu item's li"
    This reverts commit 42a6924707c12c708c7bcc6d04626f58bec61229.

--- application/views/template_menu.php
+++ application/views/template_menu.php
@@ -42,7 +42,7 @@ array(4) {
 		foreach ($links as $section => $entry) {
 
 			?>
-				<li class="supermenu-button" id="<?php echo str_replace(' ','-',strtolower($section)); ?>-button">
+				<li class="supermenu-button" id="<?php echo str_replace(' ','-',strtolower($section)); ?>-button" title="<?php echo $section; ?>">
 				<span class="icon-32 x32-<?php echo str_replace(' ','-',strtolower($section)); ?>"></span>
 			<?php
 
