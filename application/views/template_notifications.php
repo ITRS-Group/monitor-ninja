38c9cb61bcbf85bec121c3297385d10430f79637
Tobias Sjondin <tsjondin@op5.com>
2013-11-13 13:48:14 +0100

    Woopsie, forgot the notification Template!
    Sorry about that, to many files to add!
    
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

---
+++ application/views/template_notifications.php
@@ -0,0 +1,45 @@
+
+<div class="jq-notify-zone"></div>
+
+<?php
+
+	require_once( __DIR__ . "/../helpers/notify.php" );
+
+	/* Check to see if there are any global notifications to display */
+
+	$notifications = array();
+
+	$note_config = Ninja_setting_Model::fetch_page_setting( "notifications", "notifications_facility" );
+	$note_config = $note_config->setting;
+
+	echo "<script>" .
+			"$.notify.sessionid = '" . sha1( session_id() ) . "';" .
+			"$.notify.configured = " . $note_config . ";" .
+			"</script>";
+
+	if ( isset( $global_notifications ) && is_array( $global_notifications ) && count( $global_notifications ) >= 1 ) {
+
+		foreach ( $global_notifications as $note )
+			$notifications[] = $note[0];
+
+		if ( count( $notifications ) > 1 ) {
+			$last = array_pop( $notifications );
+			$message = implode( ", ", $notifications ) . " and " . $last;
+		} else {
+			$message = implode( ", ", $notifications );
+		}
+
+		$message .= ". You can enter the process information page to enable/disable process settings.";
+
+		new Notify( $message, array(
+			"type" => "warning",
+			"sticky" => true,
+			"configurable" => "true"
+		) );
+
+	}
+
+	/* Render PHP added notifications if logged in! */
+
+	if ( Auth::instance()->logged_in() )
+		Notify::render();
