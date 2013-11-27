2865b53c3ac1dbd3c895b0bd3e3cc39a252543cc
Robin Sonefors <robin.sonefors@op5.com>
2013-11-27 16:08:49 +0100

    lsfilter: Ensure button links have a href
    Capybara won't look at links without hrefs.
    
    Change-Id: I02323ddf2a8b61d538895b7f5dc0538ce35f1ec4
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- modules/lsfilter/media/js/lsfilter_renderer_buttons.in.js
+++ modules/lsfilter/media/js/lsfilter_renderer_buttons.in.js
@@ -1,4 +1,4 @@
 listview_renderer_buttons.all = [
-	$('<a data-drop-down="filter-query-multi-action"/>').attr('title', _('Send multi action')).append($('<span class="icon-16 x16-check-boxes"/>')),
-	$('<a data-drop-down="filter-query-builder"/>').attr('title', _('Show/Edit Text Filter')).append($('<span class="icon-16 x16-filter"/>'))
+	$('<a href="#" data-drop-down="filter-query-multi-action"/>').attr('title', _('Send multi action')).append($('<span class="icon-16 x16-check-boxes"/>')),
+	$('<a href="#" data-drop-down="filter-query-builder"/>').attr('title', _('Show/Edit Text Filter')).append($('<span class="icon-16 x16-filter"/>'))
 ];
