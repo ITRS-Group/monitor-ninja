0edef00feb704c79ef7bc04d113c7c3d70abda27
Max Sikstrom <msikstrom@op5.com>
2013-12-05 16:39:49 +0100

    AuthDriver: Header auth, read authentication from http headers
    When protecting the op5 system behind an authenticating proxy, and
    the proxy sends out authorization information as HTTP-headers, and
    you can trust that it isn't accessed directly from a client, which
    can fake the headers, this module can be used to read those
    credentials and create a user.
    
    Make sure that the op5 system is well proceted behind a firewall +
    the authenticating proxy before enabling this though.
    
    This also includes unit tests, with a mocked HTTP header setup
    
    (This commit is imported from op5lib)
    
    Change-Id: Ife3ea70ccadec06cf47ee3b640b52a1dca5c5d72
    Tests: #8104
    Resolves: #8104
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

---
+++ src/op5/auth/AuthDriver_Header.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once(__DIR__.'/AuthDriver.php');
+require_once(__DIR__.'/User.php');
+require_once(__DIR__.'/../config.php');
+
+/**
+ * User authentication and authorization library.
+ *
+ * @package    Auth
+ * @author
+ * @copyright
+ * @license
+*/
+class op5AuthDriver_Header extends op5AuthDriver {
+	/* For testing, if headers is mocked, use the mocked headers. Normally, $mocked_headers = false */
+	private $mocked_headers = false;
+
+	/**
+	 * Attempt to log in a user by static configuration, or external infromation.
+	 *
+	 * Useful for example for HTTP-auth.
+	 *
+	 * @return  op5User  User object, or false
+	 */
+	public function auto_login() {
+		$headers = array();
+
+		/* For testing, if headers is mocked, use the mocked headers */
+		if( $this->mocked_headers != false) {
+			$headers = $this->mocked_headers;
+		} else if(function_exists('apache_request_headers')) {
+			$headers = apache_request_headers();
+		}
+
+		$params = array();
+
+		if($this->fetch_header_if( $headers, 'header_username', $value ) ) {
+			$params['username'] = $value;
+		} else {
+			return false;
+		}
+
+		if($this->fetch_header_if( $headers, 'header_realname', $value ) ) {
+			$params['realname'] = $value;
+		} else {
+			$params['realname'] = $params['username'];
+		}
+
+		if($this->fetch_header_if( $headers, 'header_email', $value ) ) {
+			$params['email'] = $value;
+		} else {
+			$params['email'] = '';
+		}
+
+		$group_delimiter = ' ';
+		if( isset($this->config['group_list_delimiter']) ) {
+			$group_delimiter = $this->config['group_list_delimiter'];
+		}
+
+		if($this->fetch_header_if( $headers, 'header_groups', $value ) ) {
+			$params['groups'] = array_filter(array_map( 'trim', explode($group_delimiter, $value )));
+		} else {
+			$params['groups'] = array();
+		}
+
+		return new op5User($params);
+	}
+
+	private function fetch_header_if( $headers, $config_key, &$value ) {
+		if(isset($this->config[$config_key]) && isset($headers[$this->config[$config_key]])) {
+			$value = $headers[$this->config[$config_key]];
+			return true;
+		}
+		return false;
+	}
+
+	public function test_mock_headers( $headers ) {
+		$this->mocked_headers = $headers;
+	}
+} // End Auth
