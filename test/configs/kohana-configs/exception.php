82f8a6871c4d033897d15f70d373f8f7a64cd0d3
Mattias Ryrlén <mattiasr@op5.com>
2013-12-10 22:38:05 +0100

    testing: ship exception.php with config for CI env
    Ship the config file needed for our CI env to put ninja-stacktraces in
    the /mnt/logs dir.
    
    this fixes #7420
    
    Change-Id: I085c96236a8e635050bc9b4334b4237a51a26989
    Signed-off-by: Mattias Ryrlén <mattiasr@op5.com>

---
+++ test/configs/kohana-configs/exception.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * path to put ninja-stacktraces in default: /tmp/ninja-stacktraces/
+ */
+$config['tmp_dir'] = '/mnt/logs/ninja-stacktraces/';
+
+/**
+ * permissions for tmp_dir, default: 0700
+ */
+$config['tmp_dir_perm'] = 0777;
