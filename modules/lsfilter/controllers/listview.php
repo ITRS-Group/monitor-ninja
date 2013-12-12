f5e2d4e67118631f502cae79b87856c1c7d7fbf8
Carl Helmertz <chelmertz@op5.com>
2013-12-12 14:26:04 +0100

    listview: Move logic from controller to model
    This makes said model available to the HTTP API.
    
    Change-Id: Iaa66161e5123e7ff337e81f967302b0998a6a659
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/lsfilter/controllers/listview.php
+++ modules/lsfilter/controllers/listview.php
@@ -123,65 +123,9 @@ class ListView_Controller extends Authenticated_Controller {
 			return json::fail( array( 'data' => _("No limit specified")) );
 		}
 
-		/* TODO: Fix sorting better sometime
-		 * Do it though ORM more orm-ly
-		 * Check if columns exists and so on...
-		 */
-		$sort = array_map(function($el){return str_replace('.','_',$el);},$sort);
-
+		$model = new LSFilter_Data_Model();
 		try {
-			$result_set = ObjectPool_Model::get_by_query( $query );
-
-			/*
-			 * Some magic column filtering
-			 *
-			 * We need to strip away requested columns that isn't available.
-			 * The ORM layer isn't, and shouldn't, be forgiving about columns
-			 * taht doesn't exist. But due to custom columns, that (yet) doesn't
-			 * know about "virtual" columns (columns defined as methods in the
-			 * ORM models), we need to expect that those exist, try to request
-			 * them, and then handle them as undefined if not defined in the
-			 * result, instead of handling them as undefined already in the
-			 * column definition.
-			 */
-			$structure = Module_Manifest_Model::get('orm_structure');
-
-			/* Extract virtual columns, so we can filter against structure */
-			$raw_columns = $result_set->validate_columns($columns);
-
-			/* Check each column against structure */
-			$columns = array();
-			foreach($raw_columns as $column) {
-				$parts = explode('.',$column);
-				$table = $result_set->get_table();
-				$accept = true;
-				/* Columns can be object.field, so iterate over each part */
-				foreach($parts as $part) {
-					if( isset($structure[$table][$part]) ) {
-						if($structure[$table][$part][0] == 'object') {
-							$table = $structure[$table][$part][1];
-						}
-					} else {
-						$accept = false;
-					}
-				}
-				/* Write back columns we accept */
-				if( $accept ) {
-					$columns[] = $column;
-				}
-			}
-
-			$data = array();
-			foreach( $result_set->it($columns,$sort,$limit,$offset) as $elem ) {
-				$data[] = $elem->export();
-			}
-
-			return json::ok( array(
-				'totals' => $result_set->get_totals(),
-				'data' => $data,
-				'table' => $result_set->get_table(),
-				'count' => count($result_set)
-			) );
+			return json::ok($model->query($query, $limit, $offset, $columns, $sort));
 		} catch( LSFilterException $e ) {
 			return json::fail( array(
 				'data' => $e->getMessage().' at "'.substr($e->get_query(), $e->get_position()).'"',
