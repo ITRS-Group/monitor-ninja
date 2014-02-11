a7f5ea91536007487d727aa4469ee890f5e6da8a
Henrik Smederod <hsmederod@op5.com>
2014-02-11 14:38:35 +0100

    objstore: janitor + spelling
    Fixed some typos and cleaned up formatting
    
    (This commit is imported from op5lib)
    
    Change-Id: Iac1d97d3035d9c0b1c3e67ff59f72a514e227d73
    Signed-off-by: Henrik Smederod <hsmederod@op5.com>

--- src/op5/objstore.php
+++ src/op5/objstore.php
@@ -4,8 +4,8 @@
  * This is a provider for singleton objects in op5lib. This tracks the loaded
  * singletons, and makes it possible to request a module of one kind.
  *
- * This is mostly useful to make the objects testable, because it is possilbe
- * through this interface to preload objects, and mock objects if nessecary.
+ * This is mostly useful to make the objects testable, because it is possible
+ * through this interface to preload objects, and mock objects if necessary.
  *
  * This class is the entrypoint for singletons in op5lib, which makes this, by
  * obvious reasons, a singleton itself.
@@ -23,7 +23,7 @@ class op5objstore {
 	 * @return op5objstore
 	 */
 	public static function instance() {
-		if( self::$instance === false ) {
+		if (self::$instance === false) {
 			self::$instance = self::factory();
 		}
 		return self::$instance;
@@ -50,14 +50,14 @@ class op5objstore {
 		$this->clear();
 	}
 
-	public function mock_add( $name, $object ) {
-		$name = strtolower( $name );
+	public function mock_add($name, $object) {
+		$name = strtolower($name);
 		$this->mock_objects[$name] = $object;
 	}
 
-	public function mock_del( $name ) {
-		$name = strtolower( $name );
-		unset( $this->mock_objects[$name] );
+	public function mock_del($name) {
+		$name = strtolower($name);
+		unset($this->mock_objects[$name]);
 	}
 
 	/**********************************
@@ -70,27 +70,27 @@ class op5objstore {
 		$this->objects = array();
 	}
 
-	public function unload( $name ) {
-		unset( $this->objects[$name] );
+	public function unload($name) {
+		unset($this->objects[$name]);
 	}
 
-	public function obj_instance( $name, $arg=false ) {
-		$name = strtolower( $name );
+	public function obj_instance($name, $arg=false) {
+		$name = strtolower($name);
 		return $this->obj_instance_callback($name,
 				function() use ($name, $arg) {
-					return new $name( $arg );
+					return new $name($arg);
 				}
 		);
 	}
 
-	public function obj_instance_callback( $name, $callback ) {
-		$name = strtolower( $name );
+	public function obj_instance_callback($name, $callback) {
+		$name = strtolower($name);
 		/* If mocking an object, use that instead */
-		if( isset($this->mock_objects[$name]))
+		if (isset($this->mock_objects[$name]))
 			return $this->mock_objects[$name];
 
 		/* If not loaded, load an instance */
-		if( !isset($this->objects[$name]) )
+		if (!isset($this->objects[$name]))
 			$this->objects[$name] = $callback();
 
 		/* Return the instance */
