4e98f6d4ec7e12693931a9cefec6e37ad667f795
Robin Sonefors <robin.sonefors@op5.com>
2014-02-28 11:12:17 +0100

    lsfilter_rend_ext_obj: Only initialize multiaction once
    There's an older API that manages the multiaction dropdown, and the
    extra objects code interfered badly with it, causing bug #8369. Stop
    fighting with it over control, and the bug is fixed.
    
    Change-Id: I613c88251f8ad004079413a90074c3c0a52afe20
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- modules/lsfilter/media/js/lsfilter_renderer_extra_objects.in.js
+++ modules/lsfilter/media/js/lsfilter_renderer_extra_objects.in.js
@@ -1,16 +1,21 @@
 listview_renderer_extra_objects.all = [
 	function() {
-		var menu = $('<div id="filter-query-multi-action"/>').append(
-			$('<h2/>').text(_('Multi action')),
-			$('<ul id="multi-action-list"/>').append(
-				$('<li/>').text(_('Table doesn\'t support multi-select'))
-			),
-			$('<div id="multi-action-message"/>'));
-		lsfilter_multiselect.init(menu.find('#multi-action-list'));
-		lsfilter_multiselect.update({
-			'source': false,
-			'metadata': {'table': lsfilter_storage.list.request_metadata.table}
-		});
+		var menu;
+		if (!lsfilter_multiselect.elem_objtype) {
+			menu = $('<div id="filter-query-multi-action"/>').append(
+				$('<h2/>').text(_('Multi action')),
+				$('<ul id="multi-action-list"/>').append(
+					$('<li/>').text(_('Table doesn\'t support multi-select'))
+				),
+				$('<div id="multi-action-message"/>'));
+			lsfilter_multiselect.init(menu.find('#multi-action-list'));
+			lsfilter_multiselect.update({
+				'source': false,
+				'metadata': {'table': lsfilter_storage.list.request_metadata.table}
+			});
+		} else {
+			menu = $('#filter-query-multi-action')
+		}
 		return menu;
 	}
 ];
