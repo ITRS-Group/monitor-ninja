1486826b6644cc72ae07a79d53bdcb5e1daa8216
Max Sikstrom <msikstrom@op5.com>
2014-03-07 14:53:27 +0100

    ORM: Remove the middle-classes for Object(SQL|LS)Set, and update run_mod
    Since we don't use BaseObject(LS|SQL)Set anymore, we don't need
    Object(LS|SQL)Set either. Remove those.
    
    That is because the backend-specific code is moved to the generated
    template files. Therefore, the run_mod.php must also be updated to not
    generate the backend-specific classes.
    
    Change-Id: If466897aae9b261bf0423df2660a5e1242e2bfc4
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- modules/orm/models/objectlsset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once( dirname(__FILE__).'/base/baseobjectlsset.php' );
-
-/**
- * Describes a set of objects from livestatus
- */
-abstract class ObjectLSSet_Model extends BaseObjectLSSet_Model {
-}
