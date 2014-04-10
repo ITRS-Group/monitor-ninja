00cf83ba0f858d69ca1609c3b873b89dbde12780
Max Sikstrom <msikstrom@op5.com>
2014-04-10 15:22:23 +0200

    Replace Scheduled Downtime widget with lsfilter/gridstat widget
    The gridstat just needs some configuration to be exactly like the
    scheduled downtime widget, so just use that instead.
    
    This approach means a much more generic widget is needed.
    
    Resolves: #7695
    
    Change-Id: I92c8e9efe27b1851c865871715d3c3aeb8223c6f
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- application/widgets/tac_scheduled/tac_scheduled.php
+++ application/widgets/tac_scheduled/tac_scheduled.php
@@ -1,71 +1,108 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
 /**
  * Hosts widget for tactical overview
  *
- * @author     op5 AB
+ * We use the gridstat_Widget from the module lsfilter as template, with just a
+ * simple configuraiton. That widget just needs a configuration to work.
+ *
+ * @author op5 AB
  */
-class Tac_scheduled_Widget extends widget_Base {
-	protected $duplicatable = true;
-
-	public function index()
-	{
-		# fetch widget view path
-		$view_path = $this->view_path('view');
-
-		# HOSTS DOWN / problems
-		$problem = array();
-		$i = 0;
-
-		$current_status = $this->get_current_status();
-
-		if ($current_status->hst->down_and_scheduled) {
-			$problem[$i]['type'] = _('Host');
-			$problem[$i]['status'] = _('Down');
-			$problem[$i]['url'] = 'status/host/all/'.nagstat::HOST_DOWN.'/'.nagstat::HOST_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->hst->down_and_scheduled.' '._('Scheduled hosts');
-			$i++;
-		}
-
-		if ($current_status->hst->unreachable_and_scheduled) {
-			$problem[$i]['type'] = _('Host');
-			$problem[$i]['status'] = _('Unreachable');
-			$problem[$i]['url'] = 'status/host/all/'.nagstat::HOST_UNREACHABLE.'/'.nagstat::HOST_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->hst->unreachable_and_scheduled.' '._('Scheduled hosts');
-			$i++;
-		}
-
-		if ($current_status->hst->up_and_scheduled) {
-			$problem[$i]['type'] = _('Host');
-			$problem[$i]['status'] = _('Up');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::HOST_UP.'/'.nagstat::HOST_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->hst->up_and_scheduled.' '._('Scheduled hosts');
-			$i++;
-		}
-
-		if ($current_status->svc->critical_and_scheduled) {
-			$problem[$i]['type'] = _('Service');
-			$problem[$i]['status'] = _('Critical');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_CRITICAL.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->svc->critical_and_scheduled.' '._('Scheduled services');
-			$i++;
-		}
-
-		if ($current_status->svc->warning_and_scheduled) {
-			$problem[$i]['type'] = _('Service');
-			$problem[$i]['status'] = _('Warning');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_WARNING.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->svc->warning_and_scheduled.' '._('Scheduled services');
-			$i++;
-		}
-
-		if ($current_status->svc->unknown_and_scheduled) {
-			$problem[$i]['type'] = _('Service');
-			$problem[$i]['status'] = _('Unknown');
-			$problem[$i]['url'] = 'status/service/all/0/'.nagstat::SERVICE_UNKNOWN.'/'.nagstat::SERVICE_SCHEDULED_DOWNTIME;
-			$problem[$i]['title'] = $current_status->svc->unknown_and_scheduled.' '._('Scheduled services');
-			$i++;
-		}
-
-		require($view_path);
+class Tac_scheduled_Widget extends gridstat_Widget {
+	public function __construct($widget_model) {
+		parent::__construct($widget_model);
+		$this->settings = array (
+			array (
+				'title' => _('Down'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[hosts] (has_been_checked = 1 and state = 1) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled hosts')
+					)
+				)
+			),
+			array (
+				'title' => _('Unreachable'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[hosts] (has_been_checked = 1 and state = 2) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled hosts')
+					)
+				)
+			),
+			array (
+				'title' => _('Up'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[hosts] (has_been_checked = 1 and state = 0) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled hosts')
+					)
+				)
+			),
+			array (
+				'title' => _('Pending'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[hosts] has_been_checked = 0 and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled hosts')
+					)
+				)
+			),
+			array (
+				'title' => _('Critical'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[services] (has_been_checked = 1 and state = 2) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled services')
+					)
+				)
+			),
+			array (
+				'title' => _('Warning'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[services] (has_been_checked = 1 and state = 1) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled services')
+					)
+				)
+			),
+			array (
+				'title' => _('Unknown'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[services] (has_been_checked = 1 and state = 3) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled services')
+					)
+				)
+			),
+			array (
+				'title' => _('Ok'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[services] (has_been_checked = 1 and state = 0) and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled services')
+					)
+				)
+			),
+			array (
+				'title' => _('Pending'),
+				'icon' => 'scheduled-downtime',
+				'fields' => array(
+					array(
+						'filter' => '[services] has_been_checked = 0 and scheduled_downtime_depth > 0',
+						'text' => '%d '._('Scheduled services')
+					)
+				)
+			)
+		);
 	}
 }
