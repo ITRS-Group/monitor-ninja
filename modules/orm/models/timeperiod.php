8b64984b5538f70079cc61554ee22d68f732e420
Max Sikstrom <msikstrom@op5.com>
2014-04-28 11:42:34 +0200

    ORM: Rename timeperiod field "in" to "is_active"
    since, "in" is a reservced keyword in the filter language, we can't use
    it as a column name for filtering, and thus none could ever have used
    it.
    
    "[timeperiods] in = 1" would end up with a parse error at =, since it
    expects a saved filter name there, not an operator.
    
    The only way this coulumn would be possible to use is in the new filter
    api, which is still possible since the rewrite method in the timeperiod
    model.
    
    Change-Id: Id766bbf54caeee701c04f1d17c13ea9a6c29b6ec
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- modules/orm/models/timeperiod.php
+++ modules/orm/models/timeperiod.php
@@ -1,9 +1,27 @@
 <?php
-
-require_once( dirname(__FILE__).'/base/basetimeperiod.php' );
+require_once (dirname(__FILE__) . '/base/basetimeperiod.php');
 
 /**
  * Describes a single object from livestatus
  */
 class TimePeriod_Model extends BaseTimePeriod_Model {
+	/**
+	 * An array containing the custom column dependencies
+	 */
+	public static $rewrite_columns = array ('in' => array ('is_active'));
+
+	/**
+	 * For backward compatibility with the filter API
+	 *
+	 * We can't use the name 'in', since it conflicts with the lsfilter-operator
+	 * 'in'.
+	 *
+	 * In all cases, use the is_active instead.
+	 *
+	 * @deprecated
+	 * @return boolean
+	 */
+	public function get_in() {
+		return $this->get_is_active();
+	}
 }
