34f4fcd16f6b543b1eaa7ab260d48f3f43128dcc
Max Sikstrom <msikstrom@op5.com>
2014-06-19 13:03:26 +0200

    Add PostgreSQL visitor for ORM, again..
    There are some incompatibilities with mysql, such as regexp searching.
    
    The visitor itself is half boilerplate to identify the different
    operators and nodes, and the other half is just configuration for the
    syntax, and it's interleaved. Using the same class would just be
    messier than treating it as configuration and copy everything. Also,
    fixing bugs in mysql would possibly end up with creating bugs in
    postgre if not copied
    
    This is part of the solution of fixing regexp searching in
    logserver-table
    
    By some reason, this visitor become removed when refactoring from 7.0
    to 7.1, and also the reference to the visitor got in the wrong way.
    
    Without this patch, logger won't have regexp support.
    
    Change-Id: I24223b05139c7a55e7feeaae7f451f51d80fbe5e
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- src/op5/ninja_sdk/orm/driver_PgSQL/ORMPgSQLObjectPoolGenerator.php
+++ src/op5/ninja_sdk/orm/driver_PgSQL/ORMPgSQLObjectPoolGenerator.php
@@ -3,4 +3,6 @@
 require_once(__DIR__."/../driver_SQL/ORMSQLObjectPoolGenerator.php");
 
 class ORMPgSQLObjectPoolGenerator extends ORMSQLObjectPoolGenerator {
+	/* We just need another visitor class for this */
+	protected $visitor_class = "LivestatusPgSQLBuilderVisitor";
 }
--- src/op5/ninja_sdk/orm/driver_PgSQL/ORMPgSQLObjectSetGenerator.php
+++ src/op5/ninja_sdk/orm/driver_PgSQL/ORMPgSQLObjectSetGenerator.php
@@ -3,6 +3,4 @@
 require_once(__DIR__."/../driver_SQL/ORMSQLObjectSetGenerator.php");
 
 class ORMPgSQLObjectSetGenerator extends ORMSQLObjectSetGenerator {
-	/* We just need another visitor class for this */
-	protected $visitor_class = "LivestatusPgSQLBuilderVisitor";
 }
