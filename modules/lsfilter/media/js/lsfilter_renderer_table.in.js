163d73689e4ae0887ce177d93edc0a89f34b72f0
Tobias Sjondin <tsjondin@op5.com>
2014-06-27 10:32:58 +0200

    Added restricted output class
    To be used for plugin output and logserver messages
    
    Change-Id: Ifc48117d35f9e309902046d409dc65e3ca1b38fe
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- modules/lsfilter/media/js/lsfilter_renderer_table.in.js
+++ modules/lsfilter/media/js/lsfilter_renderer_table.in.js
@@ -152,7 +152,7 @@ listview_renderer_table.hosts = {
 		"depends" : [ 'plugin_output' ],
 		"sort" : [ 'plugin_output' ],
 		"cell" : function(args) {
-			return $('<td style="min-width: 300px; width: 35%; max-width: 640px; word-wrap: break-word;" />').update_text(
+			return $('<td class="restricted-output" />').update_text(
 					args.obj.plugin_output);
 		}
 	},
@@ -479,7 +479,7 @@ listview_renderer_table.services = {
 		"depends" : [ 'plugin_output' ],
 		"sort" : [ 'plugin_output' ],
 		"cell" : function(args) {
-			return $('<td style="min-width: 300px; width: 35%; max-width: 640px; word-wrap: break-word;" />').update_text(
+			return $('<td class="restricted-output" />').update_text(
 					args.obj.plugin_output);
 		}
 	}
@@ -1023,7 +1023,7 @@ listview_renderer_table.notifications = {
 		"depends" : [ 'output' ],
 		"sort" : [ 'output' ],
 		"cell" : function(args) {
-			return $('<td />').update_text(args.obj.output);
+			return $('<td class="restricted-output" />').update_text(args.obj.output);
 		}
 	}
 };
