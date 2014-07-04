a1c49639035dd8a2f0d6c70a2b188ebb8c2551bf
Max Sikstrom <msikstrom@op5.com>
2014-07-04 17:10:11 +0200

    Remove generators, moved to ninja (ninja-devel package)
    Since generators only are made to work with ninja, we should keep them
    in the ninja repository. Thus remove them from the op5lib runtime
    library
    
    (This commit is imported from op5lib)
    
    Change-Id: Ib2efa0f74447616541aee3de6053163621ea0bf7
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- src/op5/generators/generator_lib.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-class GeneratorException extends Exception {}
--- src/op5/generators/orm/ORMPgSQLSetGenerator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once("ORMSQLSetGenerator.php");
-
-class ORMPgSQLSetGenerator extends ORMSQLSetGenerator {
-	/* We just need another visitor class for this */
-	protected $visitor_class = "LivestatusPgSQLBuilderVisitor";
-}
