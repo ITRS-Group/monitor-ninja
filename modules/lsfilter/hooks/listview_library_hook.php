eca2f88d8dc21a51bbfe5d746f929b2f3803103b
Robin Sonefors <robin.sonefors@op5.com>
2014-07-23 13:48:55 +0200

    Make xdoc happy
    Change-Id: I1996f56904c6a3625841eaaacd6b9a7ab262742b
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- modules/lsfilter/hooks/listview_library_hook.php
+++ modules/lsfilter/hooks/listview_library_hook.php
@@ -1,11 +1,20 @@
 <?php
 
+/**
+ * Hook that imports all livestatus javascript everywhere, because we need it, well, everywhere.
+ */
 class listview_library_hook {
+	/**
+	 * hook setup
+	 */
 	public function __construct()
 	{
 		Event::add('system.post_controller_constructor', array($this, 'add_files'));
 	}
 
+	/**
+	 * hook callback
+	 */
 	public function add_files()
 	{
 		$controller = Event::$data;
--- modules/reports/models/savedreportpool.php
+++ modules/reports/models/savedreportpool.php
@@ -10,7 +10,6 @@ require_once( dirname(__FILE__).'/base/basesavedreportpool.php' );
 class SavedReportPool_Model extends BaseSavedReportPool_Model {
 	/**
 	 * List which places this object is available for.
-	 * @var array
 	 */
 	protected static $available_for = array(
 		// We can't guarantee the integrety of the api for this table in upcoming versions at the moment. Don't expose it to the API
