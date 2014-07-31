39787d4e6d9a462ebaa297fca1297979c33efe7a
Max Sikstrom <msikstrom@op5.com>
2014-07-31 10:22:41 +0200

    Tests: Mock Session class for unit tests
    There is no way in Kohana to disable session cookies using the Session
    driver. That means, we should mock it up for testing
    
    Easiest is just to make sure the mock-driver is in place before loading
    kohana, which means it simply just won't autoload, because it is
    believed already to be loaded.
    
    The mock-driver doesn't do anything, but return the default value every
    time a variable is meant to be recieved.
    
    Change-Id: Ia375dc16f4d577961462349a76233ee363902e3c
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- test/unit_test/bootstrap.php
+++ test/unit_test/bootstrap.php
@@ -1,3 +1,28 @@
 <?php
 define('SKIP_KOHANA', true);
+
+/**
+ * Dummy Session module for tests
+ *
+ * Since Session is autoloaded by Kohana, and always generates a cookie, which
+ * works quite well in normal Kohana usage, we need to mock this up in unit
+ * tests, since we get the problem otherwise of "Headers already sent". And
+ * since we never use a session in the unit tests, it shouldn't bother us
+ * either.
+ */
+class Session {
+	public static function instance() { return new self(); }
+	public function id() { return 'sessionid'; }
+	public function create($vars = NULL) {}
+	public function destroy() {}
+	public function write_close() {}
+	public function set($keys, $val = FALSE) {}
+	public function set_flash($keys, $val = FALSE) {}
+	public function keep_flash($keys = NULL) {}
+	public function expire_flash() {}
+	public function get($key = FALSE, $default = FALSE) { return $default; }
+	public function get_once($key, $default = FALSE) { return $default; }
+	public function delete($keys) {}
+}
+
 require_once(__DIR__.'/../../index.php');
