519b87316d32d5e6208229b44ecd828d0d5b82d9
Max Sikstrom <msikstrom@op5.com>
2014-08-25 12:07:19 +0200

    Global search: Make search tables configurable through manifests
    Make it possible to plug in logserver search tables to global search
    through manifests. This isn't a finished rewrite, but just makes it
    possible to have the dependencies regarding searching in 7.0 in the
    correct direction.
    
    Change-Id: I9d793e5251a6b884f67cf5402b0474730a5e2949
    Signed-off-by: Max Sikstrom <msikstrom@op5.com>

--- modules/exp/libraries/ExpParser_SearchFilter.php
+++ modules/exp/libraries/ExpParser_SearchFilter.php
@@ -7,39 +7,43 @@ class ExpParser_SearchFilter extends ExpParser {
 	/**
 	 * List of table shortcuts for searching
 	 */
-	protected $objects = array(
-			'h'  => 'hosts',
-			's'  => 'services',
-			'c'  => 'comments',
-			'hg' => 'hostgroups',
-			'sg' => 'servicegroups',
-			'si' => '_si',
-			'l'  => 'syslog'
-			);
-	
+	protected $objects = array();
+
 	/**
 	 * Last object found in the search string
 	 */
 	protected $last_object = false;
-	
+
 	/**
 	 * Last string in the search query, for auto-complete
 	 */
 	protected $last_string = false;
-	
+
+	/**
+	 * Create an expression parser, given a set of prefixes/object types available
+	 * to search for
+	 *
+	 * Use the format as array( shortcut => object type )
+	 *
+	 * @param $objects array of object types
+	 */
+	public function __construct( $objects ) {
+		$this->objects = $objects;
+	}
+
 	/**
 	 * Entrypoint to start the parsing
 	 */
 	protected function run() {
 		$filters = array();
-		
+
 		do {
 			list( $object, $criteria ) = $this->criteria();
 			if( !isset( $filters[$object] ) )
 				$filters[$object] = array();
 			$filters[$object][] = $criteria;
 		} while( $this->acceptKeyword(array('and'), true) );
-		
+
 		$params = array(
 				'filters' => $filters
 				);
@@ -47,15 +51,15 @@ class ExpParser_SearchFilter extends ExpParser {
 			/* For auto-complete */
 			$this->last_string = false;
 			$this->last_object = false;
-			
+
 			$this->expectSym(array('='));
 			$value = $this->expectNum();
 			$params[$arg] = $value;
 		}
-		
+
 		return $params;
 	}
-	
+
 	/**
 	 * Parse a criteria (table:string or string or string)
 	 */
@@ -63,26 +67,26 @@ class ExpParser_SearchFilter extends ExpParser {
 		/* For auto-complete */
 		$this->last_string = false;
 		$this->last_object = false;
-		
+
 		$object = $this->expectKeyword(
 				array_merge(
 						array_keys($this->objects),
 						array_values($this->objects)
 						)
 				);
-		
+
 		/* If short form is used, expand to long form */
 		if( array_key_exists($object, $this->objects) )
 			$object = $this->objects[$object];
-		
+
 		$this->expectSym( array(':') );
-		
+
 		$args = array();
-		
+
 		do {
 			$objstr = $this->expectUnquotedUntil( array( 'and', 'or', 'limit', false ) );
 			$args[] = $objstr;
-			
+
 			if( trim($objstr) != "" ) {
 				/* For auto-complete */
 				$this->last_string = $objstr;
@@ -92,10 +96,10 @@ class ExpParser_SearchFilter extends ExpParser {
 				$this->last_object = false;
 			}
 		} while( $this->acceptKeyword(array('or'), true) );
-		
+
 		return array( $object, $args );
 	}
-	
+
 	/**
 	 * Return the type of last string/name specified in the query.
 	 *
@@ -106,25 +110,25 @@ class ExpParser_SearchFilter extends ExpParser {
 	public function getLastString() {
 		return $this->last_string;
 	}
-	
+
 	/**
 	 * Return the type of last object specified in the query.
-	 * 
+	 *
 	 * Useful for autocomplete
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getLastObject() {
 		return $this->last_object;
 	}
-	
+
 	/**
 	 * Get everything to a given token.
 	 */
 	protected function acceptUnquotedUntil( $keywordlist = false ) {
 		/* Peek at next keyword */
 		$minpos = false;
-		
+
 		foreach( $keywordlist as $keyword ) {
 			if( $keyword === false ) {
 				$pos = strlen($this->expr);
@@ -135,7 +139,7 @@ class ExpParser_SearchFilter extends ExpParser {
 					$pos=$matches[1][1]; /* Second match (the keyword), second index (=position, not match) */
 				}
 			}
-			
+
 			if( $pos !== false ) {
 				if( $minpos === false )
 					$minpos = $pos;
@@ -151,7 +155,7 @@ class ExpParser_SearchFilter extends ExpParser {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Expect that there is anything left in the buffer, and get everything up until next keyword.
 	 */
