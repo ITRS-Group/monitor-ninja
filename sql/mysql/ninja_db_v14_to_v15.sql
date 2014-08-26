5c79e9ab042b44dbbc73bc179428d6c6cecc794f
Anton Lofgren <alofgren@op5.com>
2014-08-26 10:52:45 +0200

    sql: Add default log filters
    This change adds a handful of default log filters.
    
    They should serve as good examples for what is possible to do with the
    logfilter functionality, while at the same time providing functionality
    that could be useful for a majority of users.
    
    This fixes #8880.
    
    Change-Id: I41b550c5a37028250f2865bf4515ec30c3d55fae
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

---
+++ sql/mysql/ninja_db_v14_to_v15.sql
@@ -0,0 +1,10 @@
+INSERT INTO `ninja_saved_filters` (`username`, `filter_name`, `filter_table`, `filter`, `filter_description`) VALUES
+(NULL, 'error messages', 'log_messages', '[log_messages] severity <= 3', 'error messages'),
+(NULL, 'warning messages', 'log_messages', '[log_messages] severity = 4', 'warning messages'),
+(NULL, 'failed login', 'log_messages', '[log_messages] (event = 4625) or (facility = 10 and severity = 2)', 'failed login'),
+(NULL, 'windows account locked', 'log_messages', '[log_messages] event = 4740', 'windows account locked'),
+(NULL, 'reboot', 'log_messages', '[log_messages] (event = 4740) or (ident = "shutdown")', 'reboot'),
+(NULL, 'sudo usage', 'log_messages', '[log_messages] ident = "sudo"', 'sudo usage'),
+(NULL, 'kernel messages', 'log_messages', '[log_messages] facility = 0', 'kernel messages'),
+(NULL, 'monitoring events', 'log_messages', '[log_messages] ident = "monitor"', 'monitoring events')
+
