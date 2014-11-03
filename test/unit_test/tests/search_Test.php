eace26109b79bc4371a211a6929ac5d40cb3fcdc
Max Sikstrom <max.sikstrom@op5.com>
2014-11-03 15:30:34 +0100

    Update tests to match changes in global search
    Some local methods were renamed, update those calls in the test too
    
    Change-Id: I5e6d58bedeb20cd70d11cf0d62cde6a89863900a
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- test/unit_test/tests/search_Test.php
+++ test/unit_test/tests/search_Test.php
@@ -6,20 +6,26 @@
  */
 class Search_Test extends PHPUnit_Framework_TestCase {
 	protected $controller = false; /* Controller to test */
-	
+
 	public function setUp() {
 		global $_SESSION;
 		$_SESSION = array();
+
+		/* Make sure our environment is clean */
+		$objstore = op5objstore::instance();
+		$objstore->clear();
+		$objstore->mock_clear();
+
 		$this->controller = new Search_Controller();
 	}
 
 	/*
 	 * Those tests should test how the search from the ExpParser filter is converted to a live status query
-	 * 
+	 *
 	 * Tests handling the syntax of the filter shoudl be in expparser_searchfilter_Test,
 	 * This is about columns and generation oh the query, and wildcard
 	 */
-	
+
 	/* *****
 	 * Test simple table access
 	 */
@@ -38,22 +44,22 @@ class Search_Test extends PHPUnit_Framework_TestCase {
 	public function test_status_info() {
 		$this->run_test('si:kaka', array('hosts'=>'[hosts] (plugin_output ~~ "kaka" or long_plugin_output ~~ "kaka")','services'=>'[services] (plugin_output ~~ "kaka" or long_plugin_output ~~ "kaka")') );
 	}
-	
+
 	/* ******
 	 * Test wildcard search
 	 */
 	public function test_wildcard() {
 		$this->run_test('h:aaa%bbb', array('hosts'=>'[hosts] (name ~~ "aaa.*bbb" or display_name ~~ "aaa.*bbb" or address ~~ "aaa.*bbb" or alias ~~ "aaa.*bbb" or notes ~~ "aaa.*bbb")') );
 	}
-	
-	
+
+
 	/* ******
 	 * Test combined host/service (services by hosts)
 	 */
 	public function test_host_serivce() {
 		$this->run_test('h:kaka and s:pong', array('services'=>'[services] (description ~~ "pong" or display_name ~~ "pong" or notes ~~ "pong") and (host.name ~~ "kaka" or host.display_name ~~ "kaka" or host.address ~~ "kaka" or host.alias ~~ "kaka" or host.notes ~~ "kaka")') );
 	}
-	
+
 	/* ******
 	 * Test limit
 	 */
@@ -62,7 +68,7 @@ class Search_Test extends PHPUnit_Framework_TestCase {
 	}
 
 	protected function run_test( $query, $expect ) {
-		$result = $this->controller->queryToLSFilter( $query );
+		$result = $this->controller->_queryToLSFilter( $query );
 		$this->assertEquals( $result, $expect, "SearchFilter query '$query' doesn't match expected result." );
 	}
 }
