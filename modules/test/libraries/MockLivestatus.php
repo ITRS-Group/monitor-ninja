f0c56e564d9b15914314c85b78e4ca34151949e1
Max Sikstrom <max.sikstrom@op5.com>
2014-11-10 13:50:05 +0100

    Outages: Implement tests
    This controller needs refactoring, and refactoring needs tests.
    Implement tests.
    
    To implement tests for outages, the livestatus mock library needs to be
    better at calculating stats, so stats needs to be added to the
    MockLivestatus library.
    
    Change-Id: Ib18da15eb6c9ff7720ec1671290fd3ce8b024422
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- modules/test/libraries/MockLivestatus.php
+++ modules/test/libraries/MockLivestatus.php
@@ -6,6 +6,127 @@
 class MockLivestatus_Exception extends Exception {}
 
 /**
+ * Interface for differnent types of stats aggregators.
+ * Takes register for each object, and gets the return value when finished by
+ * calling ->calculate()
+ */
+interface MockLivestatus_StatsAggregator {
+	/**
+	 * Register an object to the aggregator, represented by an associative array
+	 *
+	 * @param $obj object
+	 */
+	public function register($obj);
+	/**
+	 * Get the result of the aggregator
+	 */
+	public function calculate();
+}
+
+/**
+ * A stats aggregator, which counts matches to a stats search
+ */
+class MockLivestatus_FilterCountStats implements MockLivestatus_StatsAggregator {
+	private $count = 0;
+	private $filterfunc = false;
+
+	/**
+	 * Initialize the filter count stats aggregator with a filter function.
+	 *
+	 * @param $filterfunc The
+	 *        	filter function, should be callable and take an object as
+	 *        	argument and return a boolean
+	 */
+	public function __construct($filterfunc) {
+		$this->filterfunc = $filterfunc;
+	}
+	/**
+	 * Register an object to the aggregator, represented by an associative array
+	 *
+	 * @param $obj object
+	 */
+	public function register($obj) {
+		$filterfunc = $this->filterfunc;
+		if ($filterfunc( $obj ))
+			$this->count ++;
+	}
+	/**
+	 * Get the result of the aggregator
+	 */
+	public function calculate() {
+		return $count;
+	}
+}
+/**
+ * Calculated stats aggregator, to aggregate columns to calculate average, sum,
+ * min max value and so on.
+ */
+class MockLivestatus_CalculatedStats implements MockLivestatus_StatsAggregator {
+	private $min = false;
+	private $max = false;
+	private $sum = 0;
+	// private $suminv = 0;
+	private $count = 0;
+	private $column = false;
+	private $type = false;
+	/**
+	 * Initialize the caluculated stats aggregator
+	 *
+	 * @param $column Name
+	 *        	of the column to calculate
+	 * @param $type Type
+	 *        	of stats as string (min, max, sum, avg)
+	 */
+	public function __construct($column, $type) {
+		$this->column = $column;
+		$this->type = $type;
+	}
+	/**
+	 * Register an object to the aggregator, represented by an associative array
+	 *
+	 * @param $obj object
+	 */
+	public function register($obj) {
+		$value = 0;
+		if (isset( $obj[$this->column] )) {
+			$value = $obj[$this->column];
+		}
+
+		if ($this->count == 0) {
+			$this->min = $value;
+			$this->max = $value;
+		} else {
+			if ($this->min > $value)
+				$this->min = $value;
+			if ($this->max < $value)
+				$this->max = $value;
+		}
+		$this->sum += $value;
+		// $this->suminv += 1.0 / $value;
+		$this->count ++;
+	}
+	/**
+	 * Get the result of the aggregator
+	 */
+	public function calculate() {
+		switch ($this->type) {
+			case 'min' :
+				return $this->min;
+			case 'max' :
+				return $this->max;
+			case 'sum' :
+				return $this->sum;
+			case 'avg' :
+				return ( float ) $this->sum / $this->count;
+			/* case 'std' TODO: to be implmemented when needed */
+			/* case 'suminv': TODO: to be implmemented when needed */
+			/* case 'avginv' : TODO: to be implmemented when needed */
+		}
+		return false;
+	}
+}
+
+/**
  * Local helper for MockLivestatus-library.
  * Handles the stack for resolving livestatus filters on a given object.
  */
@@ -14,21 +135,12 @@ class MockLivestatus_StateMachine {
 	 * Stack for the state of the state machine
 	 */
 	public $stack;
-	/**
-	 * The object to use as source for tests
-	 */
-	public $object;
 
 	/**
-	 * Initialize the state machine, with an empty stack and a object to work
-	 * with
-	 *
-	 * @param $object an
-	 *        	array with retrieved parameters
+	 * Initialize the state machine to generate a filter function.
 	 */
-	public function __construct($object) {
+	public function __construct() {
 		$this->stack = array ();
-		$this->object = $object;
 	}
 
 	/**
@@ -39,59 +151,66 @@ class MockLivestatus_StateMachine {
 	 * @throws MockLivestatus_Exception
 	 */
 	public function process_Filter($args) {
-		if (preg_match('/^\s*([a-zA-Z_]+)\s+(!?)([<>=~]+)\s+(.*)$/', $args,
-			$matches)) {
-			$match_var = $matches[1];
-			$match_negate = $matches[2];
-			$match_op = $matches[3];
-			$match_value = $matches[4];
-
-			if (!isset($this->object[$match_var])) {
-				throw new MockLivestatus_Exception('Unknown field ' . $match_var);
-			}
-
-			$value = $this->object[$match_var];
-
-			$result = false;
-			switch ($match_op) {
-			case '=':/* equality */
-				$result = ($match_value == $value);
-				break;
-			case '~': /* match regular expression (substring match) */
-				$result = (false != preg_match('/' . $match_value . '/', $value));
-				break;
-			case '=~': /* equality ignoring case */
-				$result = (strtolower($match_value) == strtolower($value));
-				break;
-			case '~~': /* regular expression ignoring case */
-				$result = (false != preg_match('/' . $match_value . '/i',
-					$value));
-				break;
-			case '<': /* less than */
-				$result = ($match_value < $value);
-				break;
-			case '>': /* greater than */
-				$result = ($match_value > $value);
-				break;
-			case '<=': /* less or equal */
-				$result = ($match_value <= $value);
-				break;
-			case '>=': /* greater or equal */
-				$result = ($match_value >= $value);
-				break;
-			default:
-				throw new MockLivestatus_Exception(
-					'Unknown filter operator ' . $match_op);
-			}
-
-			if ($match_negate == '!') {
-				$result = !$result;
-			}
-
-			$this->stack[] = $result;
-		} else {
-			throw new MockLivestatus_Exception("Malformed filter: " . $args);
+		if (preg_match( '/^(min|max|avg|sum|std|suminv|avginv)\s*(.*)$/', $args, $matches )) {
+			$this->stack[] = array (
+					$matches[2],
+					$matches[1]
+			);
+			return;
 		}
+
+		if (preg_match( '/^([a-zA-Z_]+)\s+(!?)([<>=~]+)\s*(.*)$/', $args, $matches )) {
+			$this->stack[] = function ($obj) use($matches) {
+				$match_var = $matches[1];
+				$match_negate = $matches[2];
+				$match_op = $matches[3];
+				$match_value = $matches[4];
+
+				if (! isset( $obj[$match_var] )) {
+					throw new MockLivestatus_Exception( 'Unknown field ' . $match_var );
+				}
+
+				$value = $obj[$match_var];
+
+				$result = false;
+				switch ($match_op) {
+					case '=':/* equality */
+					$result = ($match_value == $value);
+						break;
+					case '~': /* match regular expression (substring match) */
+					$result = (false != preg_match( '/' . $match_value . '/', $value ));
+						break;
+					case '=~': /* equality ignoring case */
+					$result = (strtolower( $match_value ) == strtolower( $value ));
+						break;
+					case '~~': /* regular expression ignoring case */
+					$result = (false != preg_match( '/' . $match_value . '/i', $value ));
+						break;
+					case '<': /* less than */
+					$result = ($match_value < $value);
+						break;
+					case '>': /* greater than */
+					$result = ($match_value > $value);
+						break;
+					case '<=': /* less or equal */
+					$result = ($match_value <= $value);
+						break;
+					case '>=': /* greater or equal */
+					$result = ($match_value >= $value);
+						break;
+					default :
+						throw new MockLivestatus_Exception( 'Unknown filter operator ' . $match_op );
+				}
+
+				if ($match_negate == '!') {
+					$result = ! $result;
+				}
+
+				return $result;
+			};
+			return;
+		}
+		throw new MockLivestatus_Exception( "Malformed filter: " . $args );
 	}
 	/**
 	 * Process a "And: N" livestatus filter line, manipulates the stack
@@ -103,17 +222,21 @@ class MockLivestatus_StateMachine {
 	 */
 	public function process_And($args) {
 		$result = true;
-		if (!is_numeric($args)) {
-			throw new MockLivestatus_Exception(
-				"And statement isn't numeric: " . $args);
+		if (! is_numeric( $args )) {
+			throw new MockLivestatus_Exception( "And statement isn't numeric: " . $args );
 		}
-		for ($i = 0; $i < intval($args); $i++) {
-			$val = array_pop($this->stack);
-			if (!$val) {
-				$result = false;
+		$subfilters = array ();
+		for($i = 0; $i < intval( $args ); $i ++) {
+			$subfilters[] = array_pop( $this->stack );
+		}
+		$this->stack[] = function ($obj) use($subfilters) {
+			foreach ( $subfilters as $subf ) {
+				if (! $subf( $obj )) {
+					return false;
+				}
 			}
-		}
-		$this->stack[] = $result;
+			return true;
+		};
 	}
 	/**
 	 * Process a "Or: N" livestatus filter line, manipulates the stack
@@ -124,18 +247,22 @@ class MockLivestatus_StateMachine {
 	 * @throws MockLivestatus_Exception
 	 */
 	public function process_Or($args) {
-		$result = false;
-		if (!is_numeric($args)) {
-			throw new MockLivestatus_Exception(
-				"Or statement isn't numeric: " . $args);
+		$result = true;
+		if (! is_numeric( $args )) {
+			throw new MockLivestatus_Exception( "Or statement isn't numeric: " . $args );
 		}
-		for ($i = 0; $i < intval($args); $i++) {
-			$val = array_pop($this->stack);
-			if ($val) {
-				$result = true;
+		$subfilters = array ();
+		for($i = 0; $i < intval( $args ); $i ++) {
+			$subfilters[] = array_pop( $this->stack );
+		}
+		$this->stack[] = function ($obj) use($subfilters) {
+			foreach ( $subfilters as $subf ) {
+				if ($subf( $obj )) {
+					return true;
+				}
 			}
-		}
-		$this->stack[] = $result;
+			return false;
+		};
 	}
 	/**
 	 * Process a "Negate:" livestatus filter line, negates the top of the stack
@@ -145,12 +272,21 @@ class MockLivestatus_StateMachine {
 	 * @throws MockLivestatus_Exception
 	 */
 	public function process_Negate($args) {
-		if (!empty($args)) {
-			throw new MockLivestatus_Exception(
-				'"Negate:" line with arguments isn\'t allowed');
+		if (! empty( $args )) {
+			throw new MockLivestatus_Exception( '"Negate:" line with arguments isn\'t allowed' );
 		}
-		$result = !array_pop($this->stack);
-		$this->stack[] = $result;
+		$subfilter = ! array_pop( $this->stack );
+		$this->stack[] = function ($obj) use($subfilter) {
+			return ! $subfilter( $obj );
+		};
+	}
+
+	/**
+	 * Finish up all posts in stack with an implicit And filter, so the stack
+	 * is guaranteed to contain one element.
+	 */
+	public function finish_And() {
+		$this->process_And( count( $this->stack ) );
 	}
 
 	/**
@@ -158,13 +294,8 @@ class MockLivestatus_StateMachine {
 	 *
 	 * @return boolean
 	 */
-	public function get_result() {
-		foreach ($this->stack as $val) {
-			if (!$val) {
-				return false;
-			}
-		}
-		return true;
+	public function get_stack() {
+		return $this->stack;
 	}
 }
 
@@ -177,7 +308,17 @@ class MockLivestatus {
 	/**
 	 * Storage for the mocked environment.
 	 */
-	protected $data;
+	public $data;
+
+	/**
+	 * Options for behaviour of mock environment
+	 */
+	protected $options = array (
+
+			// Requesting undefined columns ends up with an empty string instead
+			// of error
+			'allow_undefined_columns' => false
+	);
 
 	/**
 	 * An array of the previously requested columns
@@ -189,9 +330,16 @@ class MockLivestatus {
 	 * @param $data Data
 	 *        	to be available in the mocked environemnt, indexed by table,
 	 *        	structure as livestatus result (but with names as keys)
+	 * @param $options Attributes to change behaviour of MockLivestatus. An
+	 *              array containing flags, if not set, use defaults. set
+	 *              'allow_undefined_columns' to true to make all undefined
+	 *              columns get the value of an empty string.
 	 */
-	public function __construct($data) {
+	public function __construct($data, $options = array()) {
 		$this->data = $data;
+		foreach ( $options as $k => $v ) {
+			$this->options[$k] = $v;
+		}
 	}
 
 	/**
@@ -211,55 +359,128 @@ class MockLivestatus {
 	public function query($table, $filter, $columns, $options = array()) {
 
 		/* Strip down $filter-var to make sure it's an array */
-		if (is_string($filter))
-			$filter = explode("\n", $filter);
-		if (empty($filter))
+		if (is_string( $filter )) {
+			$filter = explode( "\n", $filter );
+		}
+		if (empty( $filter )) {
 			$filter = array ();
+		}
 
-		$processed_filter = array ();
-		foreach ($filter as $filterline) {
-			if (!empty($filterline)) {
-				$filterop = explode(':', $filterline, 2);
-				if (count($filterop) == 2) {
-					$processed_filter[] = array_map('trim', $filterop);
+		/* Parse the filter */
+		$filter_sm = new MockLivestatus_StateMachine();
+		$stats_sm = new MockLivestatus_StateMachine();
+		foreach ( $filter as $filterline ) {
+			if (! empty( $filterline )) {
+				$filterop = explode( ':', $filterline, 2 );
+				if (count( $filterop ) == 2) {
+					list ( $op, $args ) = array_map( 'trim', $filterop );
+					switch ($op) {
+						case 'Filter' :
+							$filter_sm->process_Filter( $args );
+							break;
+						case 'And' :
+							$filter_sm->process_And( $args );
+							break;
+						case 'Or' :
+							$filter_sm->process_Or( $args );
+							break;
+						case 'Negate' :
+							$filter_sm->process_Negate( $args );
+							break;
+						case 'Stats' :
+							$stats_sm->process_Filter( $args );
+							break;
+						case 'StatsAnd' :
+							$stats_sm->process_And( $args );
+							break;
+						case 'StatsOr' :
+							$stats_sm->process_Or( $args );
+							break;
+						case 'StatsNegate' :
+							$stats_sm->process_Negate( $args );
+							break;
+					}
 				}
 			}
 		}
+
+		/*
+		 * Filter should always at the end be one function in the stack (apply
+		 * implcit AND in livestatus), and retreive the function
+		 */
+		$filter_sm->finish_And();
+		list ( $filter_func ) = $filter_sm->get_stack();
+
+		/*
+		 * Identify stats columns, some is filters, some is aggregated columns
+		 */
+		$stats_accumelators = array ();
+		foreach ( $stats_sm->get_stack() as $col ) {
+			if (is_array( $col )) {
+				$stats_accumelators[] = new MockLivestatus_CalculatedStats( $col[0], $col[1] );
+			} else {
+				$stats_accumelators[] = new MockLivestatus_FilterCountStats( $col );
+			}
+		}
+
+		/* Fetch the data in storage */
 		$table_data = $this->data[$table];
 
 		$this->last_columns = $columns;
-		if(is_array($this->last_columns)) {
-			sort($this->last_columns);
+		if (is_array( $this->last_columns )) {
+			sort( $this->last_columns );
 		}
 
-		if (empty($columns)) {
-			$columns = array_keys($table_data[0]);
+		if (empty( $columns )) {
+			$columns = array_keys( $table_data[0] );
 		}
-		if (!is_array($columns)) {
-			throw new MockLivestatus_Exception(
-				'Unknown column definition: ' . var_dump($columns, false));
+		if (! is_array( $columns )) {
+			throw new MockLivestatus_Exception( 'Unknown column definition: ' . var_dump( $columns, false ) );
 		}
 
-		$objects = array ();
-		foreach ($table_data as $obj) {
-			$filter_sm = new MockLivestatus_StateMachine($obj);
-			foreach ($processed_filter as $fop) {
-				$filter_sm->{'process_' . $fop[0]}($fop[1]);
+		if (count( $stats_accumelators ) == 0) {
+			/* Do a non-stats search */
+			$objects = array ();
+			foreach ( $table_data as $obj ) {
+				if ($filter_func( $obj )) {
+					$this_obj = array ();
+					foreach ( $columns as $col ) {
+						if (array_key_exists( $col, $obj )) {
+							$this_obj[] = $obj[$col];
+						} else {
+							if ($this->options['allow_undefined_columns']) {
+								$this_obj[] = '';
+							} else {
+								throw new MockLivestatus_Exception( 'Unknown column ' . $col . ' for table ' . $table );
+							}
+						}
+					}
+					$objects[] = $this_obj;
+				}
 			}
-			if ($filter_sm->get_result()) {
-				$this_obj = array ();
-				foreach ($columns as $col) {
-					if (array_key_exists($col, $obj)) {
-						$this_obj[] = $obj[$col];
-					} else {
-						throw new MockLivestatus_Exception(
-							'Unknown column ' . $col . ' for table ' . $table);
+		} else {
+			/*
+			 * Do a stats search, (Note: doesn't support bucketed stats for now)
+			 */
+			foreach ( $table_data as $obj ) {
+				if ($filter_func( $obj )) {
+					foreach ( $stats_accumelators as $statcol ) {
+						$statcol->register( $obj );
 					}
 				}
-				$objects[] = $this_obj;
 			}
+			$this_obj = array ();
+			$columns = array ();
+			foreach ( $stats_accumelators as $i => $statcol ) {
+				$this_obj[] = $statcol->calculate();
+				$columns[] = 'statscol_' . $i;
+			}
+			$objects[] = $this_obj;
 		}
-
-		return array ($columns,$objects,count($objects));
+		return array (
+				$columns,
+				$objects,
+				count( $objects )
+		);
 	}
 }
---
+++ test/unit_test/tests/outages_Test.php
@@ -0,0 +1,181 @@
+<?php
+require_once ('op5/objstore.php');
+require_once ('op5/livestatus.php');
+class outages_Test extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * The virtual environment this test is using
+	 *
+	 * @var array
+	 */
+	protected $objects = array (
+			"services" => array (
+					array (
+							'description' => 's_a',
+							'host_name' => 'h_a',
+							'hourly_value' => 1
+					),
+					array (
+							'description' => 's_b',
+							'host_name' => 'h_b',
+							'hourly_value' => 2
+					),
+					array (
+							'description' => 's_c',
+							'host_name' => 'h_c',
+							'hourly_value' => 4
+					),
+					array (
+							'description' => 's_d',
+							'host_name' => 'h_c',
+							'hourly_value' => 8
+					),
+					array (
+							'description' => 's_e',
+							'host_name' => 'h_f',
+							'hourly_value' => 16
+					)
+			),
+			"hosts" => array (
+					array (
+							'name' => 'h_a',
+							'childs' => array (
+									'h_b',
+									'h_c'
+							),
+							'hourly_value' => 32,
+							'num_services' => 0,
+							'state' => 0
+					),
+					array (
+							'name' => 'h_b',
+							'childs' => array (),
+							'hourly_value' => 64,
+							'num_services' => 0,
+							'state' => 0
+					),
+					array (
+							'name' => 'h_c',
+							'childs' => array (
+									'h_d',
+									'h_e'
+							),
+							'hourly_value' => 128,
+							'num_services' => 0,
+							'state' => 0
+					),
+					array (
+							'name' => 'h_d',
+							'childs' => array (),
+							'hourly_value' => 256,
+							'num_services' => 0,
+							'state' => 0
+					),
+					array (
+							'name' => 'h_e',
+							'childs' => array (
+									'h_f'
+							),
+							'hourly_value' => 512,
+							'num_services' => 0,
+							'state' => 0
+					),
+					array (
+							'name' => 'h_f',
+							'childs' => array (),
+							'hourly_value' => 1024,
+							'num_services' => 0,
+							'state' => 0
+					)
+			)
+	);
+	/**
+	 * Contains a reference to the mocked livestatus, so we can see some debug
+	 * variables
+	 *
+	 * @var MockLivestatus
+	 */
+	protected $ls = false;
+	/**
+	 * The controller under test
+	 */
+	protected $sut = false;
+	/**
+	 * Make sure the enviornment is clean, and livestatus is mocked
+	 */
+	public function setUp() {
+		op5objstore::instance()->mock_clear();
+		op5objstore::instance()->clear();
+		$this->ls = new MockLivestatus( $this->objects, array (
+				'allow_undefined_columns' => true
+		) );
+		op5objstore::instance()->mock_add( 'op5Livestatus', $this->ls );
+		$this->sut = new Outages_Controller();
+	}
+	/**
+	 * Remove mock environment
+	 */
+	public function tearDown() {
+		op5objstore::instance()->mock_clear();
+		op5objstore::instance()->clear();
+	}
+	/**
+	 * Test that when you fetch all columns, you get all columns exported,
+	 * including virtual columns
+	 */
+	public function test_no_outages() {
+		$this->sut->index();
+		$this->assertEquals( array (), $this->sut->template->content->outage_data );
+	}
+	/**
+	 * Test that when you fetch all columns, you get all columns exported,
+	 * including virtual columns
+	 */
+	public function test_root_outage() {
+		/* h_a - root */
+		$this->ls->data['hosts'][0]['state'] = 1;
+
+		$this->sut->index();
+
+		$outage_data = $this->sut->template->content->outage_data;
+
+		$this->assertCount( 1, $outage_data );
+		$this->assertEquals( 'h_a', $outage_data[0]['name'] );
+		$this->assertEquals( 2047, $outage_data[0]['severity'] );
+	}
+	/**
+	 * Test that when you fetch all columns, you get all columns exported,
+	 * including virtual columns
+	 */
+	public function test_subtree_outage() {
+		/* h_c => h_a and h_b available */
+		$this->ls->data['hosts'][2]['state'] = 1;
+
+		$this->sut->index();
+
+		$outage_data = $this->sut->template->content->outage_data;
+
+		$this->assertCount( 1, $outage_data );
+		$this->assertEquals( 'h_c', $outage_data[0]['name'] );
+		$this->assertEquals( 2047 - 1 - 2 - 32 - 64, $outage_data[0]['severity'] );
+	}
+	/**
+	 * Test that when you fetch all columns, you get all columns exported,
+	 * including virtual columns
+	 */
+	public function test_multiple_outage() {
+		$this->ls->data['hosts'][1]['state'] = 1; /* h_a */
+		$this->ls->data['hosts'][2]['state'] = 1; /* h_c */
+
+		$this->sut->index();
+
+		$outage_data = $this->sut->template->content->outage_data;
+		$this->assertCount( 2, $outage_data );
+
+		$this->assertEquals( 'h_b', $outage_data[0]['name'] );
+		$this->assertEquals( 64 + 2, $outage_data[0]['severity'] );
+
+		$this->assertEquals( 'h_c', $outage_data[1]['name'] );
+		$this->assertEquals( 2047 - 1 - 2 - 32 - 64, $outage_data[1]['severity'] );
+	}
+}
