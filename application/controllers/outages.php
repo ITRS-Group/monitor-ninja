26ba4fd3098a5df2b331188092bfcc99fe2808ab
Max Sikstrom <max.sikstrom@op5.com>
2014-11-10 14:44:21 +0100

    Outages: Convert to ORM and use MayI
    Network outages is a strange algorithm, which actually would be best to
    have in ORM with support from backend. But since the backend doesn't
    support it, and it needs to be generalized before added to ORM, the
    least bad place to have the calculation of the outage is still in this
    controller.
    
    The best way to refactor this to a nice implementation, afaik today, is
    to write a SQL backend instead of livestatus with support for
    subqueries. Which we don't have.
    
    Change-Id: I4ee6de3f721dac093a37542f3d5059ef3af2c51a
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- application/controllers/outages.php
+++ application/controllers/outages.php
@@ -1,109 +1,102 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php
+defined( 'SYSPATH' ) or die( 'No direct access allowed.' );
 /**
- * Status controller
+ * Outages controller
  *
- *  op5, and the op5 logo are trademarks, servicemarks, registered servicemarks
- *  or registered trademarks of op5 AB.
- *  All other trademarks, servicemarks, registered trademarks, and registered
- *  servicemarks mentioned herein may be the property of their respective owner(s).
- *  The information contained herein is provided AS IS with NO WARRANTY OF ANY
- *  KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY, AND FITNESS FOR A
- *  PARTICULAR PURPOSE.
+ * Calculate the value of network outages
+ *
+ * op5, and the op5 logo are trademarks, servicemarks, registered servicemarks
+ * or registered trademarks of op5 AB.
+ * All other trademarks, servicemarks, registered trademarks, and registered
+ * servicemarks mentioned herein may be the property of their respective
+ * owner(s).
+ * The information contained herein is provided AS IS with NO WARRANTY OF ANY
+ * KIND, INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY, AND FITNESS FOR A
+ * PARTICULAR PURPOSE.
  */
-class Outages_Controller extends Authenticated_Controller
-{
-	const SERVICE_SEVERITY_DIVISOR = 4; /**< Magical constant that tells us how many times less interesting a service is compared to a host */
-
+class Outages_Controller extends Ninja_Controller {
 	/**
-	*	@name	index
-	*	@desc	default method
-	*
-	*/
-	public function index()
-	{
-		return $this->display_network_outages();
-	}
+	 * default method
+	 *
+	 * @name index
+	 */
+	public function index() {
+		$outages_objs = HostPool_Model::all()->reduce_by( 'state', 1, '=' )->reduce_by( 'childs', '', '!=' );
+		/* @var $outages_objs HostSet_Model */
 
-	/**
-	*	shows all hosts that are causing network outages
-	*/
-	public function display_network_outages()
-	{
-		$ls = Livestatus::instance();
-		$outages = $ls->getHosts( array(
-			'filter' => array(
-				'state'  => 1,
-				'childs' => array(
-					'!=' => ''
-				)
-			)
-		));
+		$this->_verify_access($outages_objs->mayi_resource().':view.list.outages');
 
+		$outages = array ();
+		foreach ( $outages_objs as $host_obj ) {
+			$host = $host_obj->export();
 
-		if(count($outages) > 0) {
-			foreach($outages as &$host) {
+			// count number of affected hosts / services
 
-				# count number of affected hosts / services
+			list ( $affected_hosts, $affected_services, $severity ) = $this->count_affected_hosts_and_services( $host_obj );
 
-				list($affected_hosts,$affected_services,$severity) = $this->count_affected_hosts_and_services($host['name']);
-				$host['affected_hosts']    = $affected_hosts;
-				$host['affected_services'] = $affected_services;
-				$host['severity']          = $severity;
+			$host['affected_hosts'] = $affected_hosts;
+			$host['affected_services'] = $affected_services;
+			$host['severity'] = $severity;
 
-			}
+			$outages[] = $host;
 		}
 
+		$this->template->toolbar = new Toolbar_Controller( _( "Network outages" ), _( "Blocking outages" ) );
 
-		$this->template->toolbar = new Toolbar_Controller( _("Network outages"), _("Blocking outages") );
-
-		$this->template->content = $this->add_view('outages/network_outages');
+		$this->template->content = $this->add_view( 'outages/network_outages' );
 		$content = $this->template->content;
-		$content->title = _('Blocking Outages');
+		$content->title = _( 'Blocking Outages' );
 
 		$content->outage_data = $outages;
-		$this->template->title = _('Monitoring » Network outages');
+		$this->template->title = _( 'Monitoring » Network outages' );
 	}
-
-
 	private function count_affected_hosts_and_services($host) {
-		/* FIXME: This method needs to be partly implemented in livestatus.
+		/*
+		 * TODO: This method needs to be partly implemented in livestatus.
 		 * That can be done by allowing the recursion in childs/parents-column
-		 * to be handled within livestatus, and do a simple Stats-request instead
+		 * to be handled within livestatus, and do a simple Stats-request
+		 * instead
+		 *
+		 * But until then, this is needed...
 		 */
-		$affected_hosts    = 0;
+		$affected_hosts = 0;
 		$affected_services = 0;
 
-		$hosts_to_test = array($host);
-		$hosts_services = array();
-
-		$ls = Livestatus::instance();
-		$lsb = $ls->getBackend();
+		$hosts_to_test = array (
+				$host->get_name()
+		);
+		$hosts_services = array ();
 
 		$severity_value = 0;
 
 		/* Iterate through all hosts with children. */
-		while( !empty( $hosts_to_test ) ) {
-			$host_name = array_shift($hosts_to_test);
+		while ( ! empty( $hosts_to_test ) ) {
+			$host_name = array_shift( $hosts_to_test );
+			$host = HostPool_Model::fetch_by_key( $host_name );
+			/* @var $host Host_Model */
 
 			/* Skip if already tested... */
-			if(isset($hosts_services[$host_name]))
+			if (isset( $hosts_services[$host_name] ))
 				continue;
 
-			$host = $ls->getHosts(array(
-					'filter'=>array('name'=>$host_name),
-					'columns' => array('name', 'childs', 'hourly_value', 'num_services')
-			));
-			$service_value = $lsb->getStats('services', array('value' => 'sum hourly_value'), array(
-					'filter' => array('host_name'=>$host[0]['name'])
-					));
+				/* Fetch sum of hourly_values */
+			$service_value = 0;
+			foreach ( $host->get_services_set()->it( array (
+					'hourly_value'
+			), array () ) as $service ) {
+				$service_value += $service->get_hourly_value();
+			}
 
-			$hosts_services[$host_name] = $host[0]['num_services'];
-			$hosts_to_test += $host[0]['childs'];
+			$hosts_services[$host_name] = $host->get_num_services();
+			$hosts_to_test += $host->get_childs();
 
-			$severity_value += $host[0]['hourly_value'] + $service_value[0]['value'];
+			$severity_value += $host->get_hourly_value() + $service_value;
 		}
 
-		return array(count($hosts_services), array_sum($hosts_services), $severity_value);
+		return array (
+				count( $hosts_services ),
+				array_sum( $hosts_services ),
+				$severity_value
+		);
 	}
-
 }
