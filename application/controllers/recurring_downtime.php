ITRS-Group/monitor-ninja

commit 5c975b65e3d60864f36dfcd12b2980cac104761a

Updated filterable for reports and recurring downtime
Using the new filterable jquery module.
The module depends on the lib.set.js which
handles diff, intersects and unions of larger
sets of data.

I hope I didn't miss anything in this commit :)

Change-Id: I8831773d9aef6361011fc1a9d562c1d0ef3ea515
Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

application/controllers/recurring_downtime.php
@@ -62,7 +62,11 @@ class recurring_downtime_Controller extends Authenticated_Controller {
 		$this->template->js[] = $this->add_path('reports/js/common.js');
 		$this->template->js[] = $this->add_path('recurring_downtime/js/recurring_downtime.js');
 
+		$this->template->js[] = 'application/media/js/lib.set.js';
+		$this->template->js[] = 'application/media/js/jquery.filterable.js';
+
 		$this->template->css[] = $this->add_path('reports/css/datePicker.css');
+		$this->template->css[] = 'application/media/css/jquery.filterable.css';
 
 		$date_format = cal::get_calendar_format(true);
 

application/views/recurring_downtime/setup.php
@@ -20,7 +20,28 @@
 		if (isset($schedule_id) && !empty($schedule_id)) {?>
 		<input type="hidden" name="schedule_id" value="<?php echo $schedule_id ?>" />
 		<?php }?>
-		<table summary="Select object type" class="setup-tbl">
+		<table summary="Select object type" class="setup-tbl obj_selector">
+			<tr>
+				<td colspan="3">
+					<select id="report_type" name="report_type">
+						<option value="hostgroups" <?php echo $schedule_info->get_downtime_type() === 'hostgroups' ? 'selected="selected"' : ''; ?>><?php echo _('Hostgroups') ?></option>
+						<option value="hosts" <?php echo $schedule_info->get_downtime_type() === 'hosts' ? 'selected="selected"' : ''; ?>><?php echo _('Hosts') ?></option>
+						<option value="servicegroups" <?php echo $schedule_info->get_downtime_type() === 'servicegroups' ? 'selected="selected"' : ''; ?>><?php echo _('Servicegroups') ?></option>
+						<option value="services" <?php echo $schedule_info->get_downtime_type() === 'services' ? 'selected="selected"' : ''; ?>><?php echo _('Services') ?></option>
+					</select>
+					<input type="button" id="sel_report_type" class="button select20" value="<?php echo _('Select') ?>" />
+					&nbsp;
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3">
+					<select data-filterable data-type="hostgroup" name="objects[]" id="objects" multiple="multiple">
+					</select>
+				</td>
+			</tr>
+		</table>
+
+		<!--table summary="Select object type" class="setup-tbl">
 
 			<tr>
 				<td colspan="3">
@@ -62,7 +83,7 @@
 				<div class="clear"></div>
 				</td>
 			</tr>
-		</table>
+		</table-->
 	</div>
 
 	<div class="report-block">

application/media/css/jquery.filterable.css
@@ -1,0 +1,130 @@
+
+.jq-filterable-box {
+	display: inline-block;
+	vertical-align: 45%;
+	position: relative;
+}
+
+.jq-filterable-right {
+	display: inline-block;
+	position: relative;
+	vertical-align: top;
+}
+
+.jq-filterable-left {
+	display: inline-block;
+	position: relative;
+	vertical-align: top;
+}
+
+.jq-filterable-filter[type="text"] {
+	margin: 0;
+	width: 150px;
+	padding: 6px 2px 5px 2px;
+	border: 1px solid #bbb;
+	vertical-align: top;
+}
+
+.jq-filterable-larger {
+	width: 250px !important;
+}
+
+.jq-filterable-list {
+	margin-top: 0;
+	border: 1px solid #bbb;
+	width: 150px;
+	height: 25px;
+	vertical-align: top;
+	padding: 4px;
+}
+
+.jq-filterable-list[multiple],
+.jq-filterable-results {
+	margin-top: 0;
+	height: auto;
+	border: 1px solid #bbb;
+	width: 256px !important;
+	min-width: 256px !important;
+	height: 100px;
+}
+
+.jq-filterable-list[multiple] {
+	border-top: none;
+}
+
+.jq-filterable-results {
+	height: 127px;
+}
+
+.jq-filterable-list option,
+.jq-filterable-results option {
+	//padding: 4px 4px;
+	font-size: 110%;
+}
+
+.jq-filterable-results option[data-matching="no-match"] {
+	color: #888;
+}
+
+ul.jq-filterable-dragbox {
+	position: fixed;
+	list-style: none;
+
+	border: 1px solid #999;
+	background: #fefefe;
+	font-size: 90%;
+	color: #666;
+
+	margin: 0;
+	padding: 0;
+	width: 254px;
+}
+
+ul.jq-filterable-dragbox li {
+	margin: 0;
+	padding: 4px 4px;
+	border-top: 1px dotted #ccc;
+}
+
+.jq-filterable-move {
+	position: absolute;
+	right: 0px;
+	top: 4px;
+	padding: 0px 2px !important;
+	margin: 0 !important;
+	background: transparent !important;
+	border: 1px solid #ccc;
+}
+
+.jq-filterable-stats,
+.jq-filterable-stats-multi,
+.jq-filterable-result-stats {
+
+	position: relative;
+	text-align: right;
+
+	width: 150px;
+	padding: 2px 2px 1px 2px;
+	border: none;
+
+	font-size: 8pt;
+	color: #999;
+}
+
+.jq-filterable-stats[data-state="info"] {
+	color: #999;
+}
+
+.jq-filterable-stats[data-state="warning"] {
+	color: #ffcc00;
+}
+
+.jq-filterable-stats[data-state="error"] {
+	color: #ff4400;
+}
+
+.jq-filterable-working select,
+.jq-filterable-working input,
+.jq-filterable-working button {
+	cursor: wait !important;
+}

application/media/js/jquery.filterable.js
@@ -1,0 +1,438 @@
+
+( function ( jQuery, global ) {
+
+	var settings = {
+
+		"limit": 1000,
+		"selector": "select[data-filterable]",
+		"host": window.location.protocol + "//" + window.location.host,
+
+		"datasource": function ( select ) {
+			var type = select.attr( 'data-type' );
+			return settings.host + '/api/status/' + type
+		},
+
+		"collector": function ( select, data ) {
+			var names = [];
+			for ( var i = 0; i < data.length; i++ )
+				names.push( data[ i ].name );
+			select.filterable( names );
+		},
+
+		"ajax": {
+			data: { 'format': 'json', csrf_token: _csrf_token, 'auth_method': 'ninja' },
+			dataType: 'json',
+			error: function( xhr ) {
+				console.log( xhr.responseText );
+			}
+		}
+
+	};
+
+	var getBoxing = function ( filtered, multi ) {
+
+		if ( multi ) {
+			return $( '<div class="jq-filterable-box">' ).append(
+				$( '<div class="jq-filterable-left">' ).append(
+					$( '<input type="text" class="jq-filterable-filter jq-filterable-larger" placeholder="Search...">' ),
+					$( '<input type="button" value="➤" class="jq-filterable-move" title="Use objects matching search">' ),
+					"<br>",
+					filtered.clone()
+						.addClass( "jq-filterable-list" ),
+					"<br>",
+					$( '<div class="jq-filterable-stats jq-filterable-larger">' )
+				),$( '<div class="jq-filterable-right">' ).append(
+					$( '<select multiple class="jq-filterable-results">' ),
+					"<br>",
+					$( '<div class="jq-filterable-result-stats jq-filterable-larger">' ).append( "No items selected..." )
+				)
+			);
+		} else {
+			return $( '<div class="jq-filterable-box">' ).append(
+				$( '<div class="jq-filterable-left">' ).append(
+					$( '<input type="text" class="jq-filterable-filter" placeholder="Search...">' ),
+					filtered.clone()
+						.addClass( "jq-filterable-list" ),
+					"<br>",
+					$( '<div class="jq-filterable-stats">' )
+				)
+			);
+		}
+
+	}
+
+	var Filterable = function Filterable ( filtered, data ) {
+
+		var self = this;
+		this.box = null;
+
+		if ( filtered.attr( "multiple" ) ) {
+			this.box = getBoxing( filtered, true );
+			this.multiple = true;
+		} else {
+			this.box = getBoxing( filtered, false );
+			this.multiple = false;
+		}
+
+		if (!this.multiple && !filtered.attr('data-required')) {
+			data.unshift('');
+		}
+
+		this.results =			new Set();
+		this.memory =				new Set();
+		this.data =					new Set( data );
+
+		this.filter =				this.box.find( ".jq-filterable-filter" );
+		this.filtered =			this.box.find( ".jq-filterable-list" );
+
+		this.statusbar =		this.box.find( '.jq-filterable-stats' );
+		this.selected =			this.box.find( '.jq-filterable-results' );
+		this.resultstats =	this.box.find( '.jq-filterable-result-stats' );
+		this.mover =				this.box.find( '.jq-filterable-move' );
+
+		if ( this.multiple ) {
+
+			this.selected.attr( "id", this.filtered.attr( "id" ) );
+			this.selected.attr( "name", this.filtered.attr( "name" ) );
+
+			this.filtered.attr("id", this.selected.attr("id").replace('[', '_tmp['));
+			this.filtered.removeAttr( "name" );
+
+		}
+
+		filtered.replaceWith( this.box );
+
+
+		var _default = filtered.find( ':selected' );
+		if ( _default ) {
+			this.filter.val( _default.val() );
+			this.search( _default.val() );
+		}
+
+		/*if ( filtered.val() ) {
+
+			var value = filtered.val();
+			if ( typeof( value ) == "string" )
+				value = [ value ];
+
+			this.add( new Set( value ) );
+
+		}*/
+
+		// Add relevant events
+
+		var key_timeout = null;
+		this.box.on( "keypress", ".jq-filterable-filter", function ( e ) {
+
+			if ( $.inArray( e.which, [ 32 ] ) >= 0 ) return;
+			else if ( e.which == 13 ) {
+
+				e.preventDefault();
+				clearTimeout( key_timeout );
+				self.search( self.filter.val() );
+
+			} else {
+
+				clearTimeout( key_timeout );
+				key_timeout = setTimeout( function () {
+					self.search( self.filter.val() );
+				}, 250 );
+
+			}
+
+		} );
+
+		this.selected.closest("form").on( "submit", function ( e ) {
+			e.preventDefault();
+			self.selected.find( "option" ).attr( "selected", true );
+		} );
+
+		this.box.on('click', '.deselect_all', function( e ) {
+			e.preventDefault();
+			self.reset();
+		});
+
+		if ( this.multiple ) {
+
+			this.box.on( "change", ".jq-filterable-list, .jq-filterable-results", function ( e ) {
+
+				var parent = $( e.target ),
+					values = new Set( parent.val() );
+
+				if ( parent[0] == self.selected[0] )
+					self.remove( values );
+				else self.add( values );
+
+			} );
+
+			this.mover.on( "click", function () {
+
+				var values = self.search( self.filter.val(), null, true );
+				self.add( values );
+
+			} );
+
+		}
+
+	};
+
+	Filterable.prototype.batcher = function batcher ( set ) {
+
+		var iterator = new SetIterator( set ),
+			self = this;
+
+		this.selected.empty();
+
+		return function () {
+
+			var fragment = document.createDocumentFragment(),
+				counter = 0,
+				index = null,
+				opt = null;
+
+			while ( index = iterator.next() ) {
+
+				opt = document.createElement( 'option' );
+				opt.innerHTML = index;
+				opt.value = index;
+
+				fragment.appendChild( opt );
+
+				counter++;
+				if ( counter > 1000 ) break;
+
+			}
+
+			self.selected.append( fragment );
+			return ( counter < 1000 );
+
+		}
+
+	};
+
+	Filterable.prototype.add = function add ( set ) {
+
+		var self = this;
+
+		this.memory.reset();
+		this.memory = set.union( this.memory );
+
+		this.filtered.attr( 'disabled', 'disabled' );
+		this.box.addClass( 'jq-filterable-working' );
+
+		var batch = this.batcher( this.memory );
+			interval = setInterval( function () {
+			var completed = batch();
+			if ( completed ) {
+
+				clearInterval( interval );
+
+				self.filtered.attr( 'disabled', false );
+				self.box.removeClass( 'jq-filterable-working' );
+				self.search( self.filter.val() );
+
+			}
+		}, 10 );
+
+	};
+
+	Filterable.prototype.remove = function remove ( set ) {
+
+		var iterator = new SetIterator( set ),
+			index = null, i = null;
+
+		while ( index = iterator.next() ) {
+			i = this.memory.find( index );
+			if ( i >= 0 ) this.memory.remove( i );
+			this.selected.find( 'option[value="' + index + '"]' ).remove();
+		}
+
+		this.search( this.filter.val() );
+
+	};
+
+	Filterable.prototype.note = function note ( message, type ) {
+
+		this.statusbar.html( message );
+
+		if ( type && type == "error" )
+			this.statusbar.attr( "data-state", "error" );
+		else if ( type && type == "warning" )
+			this.statusbar.attr( "data-state", "warning" );
+		else this.statusbar.attr( "data-state", "info" );
+
+	};
+
+	Filterable.prototype.error = function error ( message ) {
+
+		this.filter.css( { "border-color": "#f40" } );
+		this.note( message, "error" );
+
+	};
+
+	Filterable.prototype.reset = function reset () {
+
+		this.memory.empty();
+		this.selected.empty();
+		this.search( this.filter.val() );
+
+	};
+
+	Filterable.prototype.update_labels = function update_labels ( ) {
+
+		if ( this.results.size() >= settings.limit ) {
+			this.note( "Not all items shown; " + settings.limit + "/" + this.data.size() );
+		} else {
+			this.note( this.results.size() + " Items" );
+		}
+
+		this.selected.css( 'width', '0px' );
+		this.selected.css( 'width', '' );
+
+		if( this.memory.size() > 0 ) {
+			this.resultstats.html( this.memory.size() + " items selected. <a href='#' class='deselect_all'>Deselect all</a>" );
+		} else {
+			this.resultstats.text( "No items selected..." );
+		}
+
+	};
+
+	/** method search ( string term )
+	  *
+	  * Searches the data array for regexp matches
+	  * against term, then runs method populate.
+	  *
+	  * @param string term
+	  * @param boolean respond
+	  * @return void
+	  */
+	Filterable.prototype.search = function search ( term, source, respond ) {
+
+		var memresult = [];
+		this.results = new Set();
+
+		try {
+			term = new RegExp( term, "i" );
+		} catch ( e ) {
+			this.error( "Invalid search ( " + e.message + " ) " );
+			return;
+		}
+
+		var iterator = new SetIterator( this.data ),
+			index = null;
+
+		while ( index = iterator.next() ) {
+			if ( index.match( term ) )
+				this.results.push( index );
+		}
+
+
+		this.memory.reset();
+		this.results.reset();
+
+		this.results = this.results.diff( this.memory );
+
+		if ( respond ) {
+			this.results.reset();
+			return this.results;
+		} else {
+			this.results.shrink( 0, settings.limit );
+			this.populate();
+		}
+
+	};
+
+	/** method populate ( string array data )
+	  *
+	  * Searches the data array for regexp matches
+	  * against term, then runs method populate.
+	  *
+	  * @param string term
+	  * @return void
+	  */
+	Filterable.prototype.populate = function populate () {
+
+		var fragment = document.createDocumentFragment(),
+			iterator = null,
+			opt = null,
+			index = 0;
+
+		iterator = new SetIterator( this.results );
+
+		while ( index = iterator.next() ) {
+
+			opt = document.createElement( 'option' );
+			opt.innerHTML = index;
+			opt.value = index;
+
+			fragment.appendChild( opt );
+
+		}
+
+		this.filtered.empty();
+		this.filtered.append( fragment );
+		this.update_labels();
+
+		if ( this.multiple ) {
+			this.filtered.val([]);
+		}
+
+	};
+
+	var Filterables = [];
+	var FilterableFactory = function FilterableFactory ( data ) {
+
+		var F = ( new Filterable( this, data ) );
+		Filterables.push( F );
+		return F;
+
+	};
+
+	jQuery.fn.filterable = FilterableFactory;
+	jQuery.fn.filterable.find = function ( element ) {
+
+		for ( var i = 0; i < Filterables.length; i++ ) {
+			if ( Filterables[ i ].selected[ 0 ] == element[ 0 ] ) {
+				return Filterables[ i ];
+			}
+		}
+
+		return null;
+
+	};
+
+	function selectload ( index, element ) {
+
+		var select = $( element );
+
+		if ( select.attr( 'data-type' ) ) {
+
+			settings.ajax.success = function ( data ) {
+				settings.collector( select, data );
+			};
+
+			settings.ajax.url = settings.datasource( select );
+			$.ajax( settings.ajax );
+
+		} else if (select.length) {
+
+			var options = $.map(select.children(), function(option) {return option.text;});
+
+			select.children().each( function() {
+				if (!$(this).attr('selected')) {
+					select.removeOption(this.text);
+				}
+			} );
+
+			select.filterable( options );
+
+		}
+
+	}
+
+	$( document ).ready( function () {
+		var selects = $( settings.selector );
+		selects.each( selectload );
+	} );
+
+} ) ( jQuery, window );

application/media/js/lib.set.js
@@ -1,0 +1,294 @@
+
+var SetIterator = function ( set ) {
+
+	set.reset();
+
+	this.next = function () {
+
+		var next = set.back();
+		set.increase();
+
+		if ( next ) return next;
+		return null;
+
+	}
+
+};
+
+var Set = function ( data ) {
+
+	var iterator = 0;
+
+	if ( typeof( data ) == "number" ) {
+		data = new Array( data );
+	} else if ( !data ) {
+		data = [];
+	} else data = data.sort();
+
+	/**
+	 * Increases the iterator position by 1
+	 * @return {void}
+	 */
+	this.increase = function () {
+		iterator++;
+	};
+
+	/**
+	 * Returns the Set data array
+	 * @return {array}   The Set data array
+	 */
+	this.data = function () {
+		return data;
+	}
+
+	/**
+	 * Returns the element of the iterators
+	 * current position
+	 *
+	 * @return {mixed}   The current value
+	 */
+	this.back = function () {
+
+		if ( data[ iterator ] ) {
+			return data[ iterator ];
+		} return null;
+
+	}
+
+	/**
+	 * Sets the value of the current iterator
+	 * position and increases the iterator.
+	 *
+	 * @param  {mixed} value   Value to add
+	 * @return {void}
+	 */
+	this.push = function ( value ) {
+
+		data[ iterator ] = value;
+		this.increase();
+
+	};
+
+	/**
+	 * Resets the iterator position
+	 * @return {void}
+	 */
+	this.reset = function () {
+
+		iterator = 0;
+
+	};
+
+	/**
+	 * Slices the Set data, without arguments it
+	 * slices from 0 to iterator position.
+	 *
+	 * This method also resets the iterator position.
+	 *
+	 * @param  {offset} offset   optional, slices from offset with size iterator
+	 * @param  {length} length   optional, slices from offset with size length
+	 * @return {void}
+	 */
+	this.shrink = function ( offset, length ) {
+
+		if ( typeof(offset) == "number" && typeof(length) == "number" ) {
+			data = data.slice( offset, length );
+		} else if ( typeof( offset ) == "number" ) {
+			data = data.slice( offset, iterator );
+		} else {
+			data = data.slice( 0, iterator );
+		}
+
+		this.reset();
+
+	};
+
+	/**
+	 * Public getter of the Set length
+	 * @return {number} Size   The Set length
+	 */
+	this.size = function () {
+
+		return data.length;
+
+	};
+
+	/**
+	 * Iterates over the Set and searches for
+	 * input value, if found the value is returned,
+	 * otherwise it returns null.
+	 *
+	 * @param  {mixed} value   The value to search for
+	 * @return {mixed} value | null
+	 */
+	this.find = function ( value ) {
+
+		var itt = new SetIterator( this ),
+			index = null;
+
+		while ( index = itt.next() ) {
+			if ( index === value ) {
+				return iterator - 1;
+			}
+		}
+
+		return -1;
+
+	};
+
+	this.empty = function () {
+		data = [];
+		iterator = 0;
+	};
+
+	this.insert = function ( value ) {
+
+		//console.log( value );
+
+		var it = new SetIterator( this ),
+			index = null, inserted = false;
+
+		while ( index = it.next() ) {
+
+			comp = Set.compare( index, value );
+			if ( comp == -1 ) {
+				data.splice( iterator, 0, value );
+				inserted = true;
+				break;
+			}
+
+		}
+
+		if ( !inserted ) {
+			data.push( value );
+		}
+
+		iterator = 0;
+
+	};
+
+	this.remove = function ( index ) {
+
+		data.splice( index, 1 );
+		iterator = 0;
+
+	};
+
+	/**
+	 * Merges two sorted Sets into one sorted Set
+	 * of unique values
+	 *
+	 * @param  {Set} target   The Set to merge this with
+	 * @return {Set} merged   The resulting merged set
+	 */
+	this.union = function ( target ) {
+
+		var merged = new Set( this.size() + target.size() );
+
+		while ( this.back() || target.back() ) {
+
+			comp = Set.compare( this.back(), target.back() );
+
+			switch ( comp ) {
+				case 0:
+					target.increase();
+					break;
+				case 1:
+					merged.push( target.back() );
+					target.increase();
+					break;
+				case -1:
+					merged.push( this.back() );
+					this.increase();
+					break;
+			};
+
+		}
+
+		merged.shrink();
+		return merged;
+
+	};
+
+	/**
+	 * Merges two sorted Sets into one sorted Set
+	 * of unique values only where both Sets have the value
+	 *
+	 * @param  {Set} target   The Set to merge this with
+	 * @return {Set} merged   The resulting merged set
+	 */
+	this.intersect = function ( target ) {
+
+		var intersect = new Set( this.size() + target.size() );
+
+		while ( this.back() || target.back() ) {
+
+			comp = Set.compare( this.back(), target.back() );
+
+			switch ( comp ) {
+				case 0:
+					intersect.push( this.back() );
+					target.increase();
+					this.increase();
+					break;
+				case -1:
+					this.increase();
+					break;
+				case 1:
+					target.increase();
+					break;
+			};
+
+		}
+
+		intersect.shrink();
+		return intersect;
+
+	};
+
+	/**
+	 * Returns a new Set of items that are
+	 * in this Set but not in the target Set.
+	 *
+	 * @param  {Set} target   The Set to diff against
+	 * @return {Set}          The diff Set
+	 */
+	this.diff = function ( target ) {
+
+		var diff = new Set( this.size() ),
+			comp = null;
+
+		while ( this.back() ) {
+
+			comp = Set.compare( this.back(), target.back() );
+
+			switch ( comp ) {
+				case 0:
+					this.increase();
+					break;
+				case -1:
+					diff.push( this.back() );
+					this.increase();
+					break;
+				case 1:
+					target.increase();
+					break;
+			};
+
+		}
+
+		diff.shrink();
+		return diff;
+
+	};
+
+	this.reset();
+	return this;
+
+};
+
+Set.compare = function ( v1, v2 ) {
+	if ( v1 == v2 ) return 0;
+	if ( !v2 || v1 < v2 ) return -1;
+	if ( !v1 || v1 > v2 ) return 1;
+};

application/views/reports/js/common.js
@@ -3,46 +3,6 @@ var sla_month_disabled_color = '#cdcdcd';
 var sla_month_enabled_color  = '#fafafa';
 var current_obj_type = false; // keep track of what we are viewing
 $(document).ready(function() {
-	// handle the move-between-lists-button (> + <) and double click events
-	function move_right() {
-		var selects = $(this).parent().parent().find('select');
-		moveAndSort(selects.filter(':first'), selects.filter(':last'));
-	}
-	function move_left() {
-		var selects = $(this).parent().parent().find('select');
-		moveAndSort(selects.filter(':last'), selects.filter(':first'));
-	}
-	$('.arrow-right').click(move_right);
-	$('.arrow-left').click(move_left);
-	$('#hostgroup_tmp, #servicegroup_tmp, #host_tmp, #service_tmp, #objects_tmp').dblclick(move_right);
-	$('#hostgroup, #servicegroup, #host_name, #service_description, #objects').dblclick(move_left);
-
-	$('#response').on('click', "#hide_response", function() {
-		$('#response').hide('slow');
-	});
-
-	$(".fancybox").fancybox({
-		'overlayOpacity'	:	0.7,
-		'overlayColor'		:	'#ffffff',
-		'hideOnContentClick' : false,
-		'autoScale':true,
-		'autoDimensions': true,
-	});
-
-	init_regexpfilter();
-	$('#filter_field').keyup(function() {
-		if ($(this).attr('value') == '') {
-			MyRegexp.resetFilter($("select[id$=_tmp]").filter(":visible").attr('id'));
-			return;
-		}
-		MyRegexp.selectFilter($("select[id$=_tmp]").filter(":visible").attr('id'), this.value);
-	});
-
-	$('#clear_filter').click(function() {
-		$('#filter_field').attr('value', '');
-		MyRegexp.resetFilter($("select[id$=_tmp]").filter(":visible").attr('id'));
-		$('#filter_field').focus();
-	});
 
 	var direct_link_visible = false;
 	$('#current_report_params').click(function() {
@@ -135,33 +95,35 @@ $(document).ready(function() {
 		});
 	});
 
-	$('select[name=report_type]').on('change', function() {
-		var value = this.value;
-		set_selection(value);
-		get_members(value, function(all_names) {
-			populate_options($('#objects_tmp'), $('#objects'), all_names);
-		});
-	}).each(function() {
-		var value = this.value;
-		set_selection(value);
-		get_members(value, function(all_names) {
-			populate_options($('#objects_tmp'), $(), all_names);
-			var tmp = $('#objects_tmp');
-			var mo = new missing_objects();
-			var objs = $('#objects');
-			var elems = objs.children();
-			for (var i = 0; i < elems.length; i++) {
-				var prop = elems[i];
-				if (tmp.containsOption(prop.value)) {
-					tmp.removeOption(prop.value);
-				} else {
-					mo.add(prop.value);
-					objs.removeOption(prop.value);
+	var hostname = window.location.protocol + "//" + window.location.host;
+	$('select[name=report_type]').on('change', function( e ) {
+
+		var filterable = jQuery.fn.filterable.find( $('select[name="objects[]"]') ),
+			type = e.target.value.replace( /s$/, "" );
+
+		if ( filterable ) {
+			$.ajax({
+				url: hostname + '/api/status/' + type,
+				data: { 'format': 'json', csrf_token: _csrf_token, 'auth_method': 'ninja' },
+				dataType: 'json',
+				error: function( xhr ) {
+					console.log( xhr.responseText );
+				},
+				success: function( data ) {
+
+					var names = [];
+					for ( var i = 0; i < data.length; i++ )
+						names.push( data[ i ].name );
+
+					filterable.data = new Set( names );
+					filterable.reset();
+
 				}
-			}
-			mo.display_if_any();
-		});
+			});
+		}
+
 	});
+
 	$('#sel_report_type').on('click', function() {
 		var value = this.form.report_type.value;
 		set_selection(value);
@@ -692,42 +654,3 @@ jQuery.extend(
 		}
 	}
 );
-
-/**
-*	Regexp filter that (hopefully) works for all browsers
-*	and not just FF
-*/
-function init_regexpfilter() {
-	MyRegexp = new Object();
-	MyRegexp.selectFilterData = new Object();
-	MyRegexp.selectFilter = function(selectId, filter) {
-		var list = document.getElementById(selectId);
-		if(!MyRegexp.selectFilterData[selectId]) {
-			//if we don't have a list of all the options, cache them now'
-			MyRegexp.selectFilterData[selectId] = new Array();
-			for(var i = 0; i < list.options.length; i++)
-				MyRegexp.selectFilterData[selectId][i] = list.options[i];
-		}
-		list.options.length = 0;   //remove all elements from the list
-		var r = new RegExp(filter, 'i');
-		for(var i = 0; i < MyRegexp.selectFilterData[selectId].length; i++) {
-			//add elements from cache if they match filter
-			var o = MyRegexp.selectFilterData[selectId][i];
-			//if(o.text.toLowerCase().indexOf(filter.toLowerCase()) >= 0) list.add(o, null);
-			if(!o.parentNode && r.test(o.text)) list.add(o, null);
-		}
-	}
-	MyRegexp.resetFilter = function(selectId) {
-		if (typeof MyRegexp.selectFilterData[selectId] == 'undefined' || !MyRegexp.selectFilterData[selectId].length)
-			return;
-		var list = document.getElementById(selectId);
-		list.options.length = 0;   //remove all elements from the list
-		for(var i = 0; i < MyRegexp.selectFilterData[selectId].length; i++) {
-			//add elements from cache if they match filter
-			var o = MyRegexp.selectFilterData[selectId][i];
-			if (!o.parentNode)
-				list.add(o, null);
-		}
-
-	};
-}

modules/reports/controllers/reports.php
@@ -57,6 +57,9 @@ class Reports_Controller extends Base_reports_Controller
 		$this->template->js[] = $this->add_path('reports/js/common.js');
 		$this->template->js[] = $this->add_path('reports/js/reports.js');
 
+		$this->template->js[] = 'application/media/js/lib.set.js';
+		$this->template->js[] = 'application/media/js/jquery.filterable.js';
+
 		# this makes anything in application/media be imported before
 		# application/views before modules/whatever, so op5reports can
 		# put random crap here as well.
@@ -65,6 +68,7 @@ class Reports_Controller extends Base_reports_Controller
 		sort($this->template->js);
 		$this->template->js = array_unique($this->template->js);
 
+		$this->template->css[] = 'application/media/css/jquery.filterable.css';
 		$this->template->css[] = $this->add_path('reports/css/tgraph.css');
 		$this->template->css[] = $this->add_path('reports/css/datePicker.css');
 

commit 6d87eaff5343c5c945faec42b4dc15639d823ab0

recurring_downtime: Load saved objects and their type when editing
This fixes #9304 (object type) and #9305 (objects) by preparing the form
server side with selected values. "Editing a recurring downtime" was
broken since 5c975b65 which made the switch to the new object selectors.

Change-Id: Id2fec7c9a7e8feece41d3a160b486f3dc319ffc5
Signed-off-by: Carl Helmertz <chelmertz@op5.com>

application/controllers/recurring_downtime.php
@@ -99,9 +99,6 @@ class recurring_downtime_Controller extends Authenticated_Controller {
 			$schedule_info = new RecurringDowntime_Model($schedule_info, '', false);
 		}
 
-		if ($schedule_id) {
-			$this->js_strings .= "var _report_data = " . json_encode(array('objects' => $schedule_info->get_objects(), 'downtime_type' => $schedule_info->get_downtime_type())) . "\n";
-		}
 		$this->js_strings .= reports::js_strings();
 
 		$this->js_strings .= "var _reports_err_str_noobjects = '".sprintf(_("Please select objects by moving them from %s the left selectbox to the right selectbox"), '<br />')."';\n";

application/views/recurring_downtime/js/recurring_downtime.js
@@ -186,30 +186,3 @@ function check_setup()
 	}
 	return true;
 }
-
-/**
-*	Receive params as JSON object
-*	Parse fields and populate corresponding fields in form
-*	with values.
-*/
-function expand_and_populate(reportObj)
-{
-	var field_str = reportObj.downtime_type;
-	get_members(field_str, function(all_names) {
-		var mo = new missing_objects();
-		var from = $('#objects_tmp');
-		var to = $('#objects');
-		populate_options(from, to, all_names);
-		// select report objects
-		for (prop in reportObj.objects) {
-			if (!from.containsOption(reportObj.objects[prop])) {
-				mo.add(reportObj.objects[prop])
-			} else {
-				from.selectOptions(reportObj.objects[prop]);
-			}
-		}
-		mo.display_if_any();
-		// move selected options from left -> right
-		moveAndSort(from, to);
-	});
-}

application/views/recurring_downtime/setup.php
@@ -20,7 +20,7 @@
 		if (isset($schedule_id) && !empty($schedule_id)) {?>
 		<input type="hidden" name="schedule_id" value="<?php echo $schedule_id ?>" />
 		<?php }?>
-		<table summary="Select object type" class="setup-tbl obj_selector">
+		<table class="setup-tbl obj_selector">
 			<tr>
 				<td colspan="3">
 					<select id="report_type" name="downtime_type">
@@ -34,7 +34,10 @@
 			</tr>
 			<tr>
 				<td colspan="3">
-					<select data-filterable data-type="hostgroup" name="objects[]" id="objects" multiple="multiple">
+					<select data-filterable data-type="<?php echo $schedule_info->get_downtime_type() ? rtrim($schedule_info->get_downtime_type(), 's') : 'hostgroup' // first from select above ?>" name="objects[]" id="objects" multiple="multiple">
+					<?php foreach($schedule_info->get_objects() as $object) { ?>
+						<option value="<?php echo $object ?>"><?php echo $object ?></option>
+					<?php } ?>
 					</select>
 				</td>
 			</tr>