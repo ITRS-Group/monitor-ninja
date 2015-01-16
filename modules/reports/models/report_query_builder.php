e05c095bd5deb897c41c95ea71cfadea18425754
Robin Sonefors <robin.sonefors@op5.com>
2015-01-16 11:59:05 +0100

    helpers/sql: Replace generic "combine" helper with an OR and an AND helper
    When there are 0 non-false cases, you'd expect an OR to exclude
    everything (an OR is a list of valid cases - no valid cases, no
    results), while an AND should include everything (an AND introduces
    further restrictions - no restrictions means everything goes), but we
    used AND's behavior for ORs as well.
    
    Now there are two functions, and they both have the correct behavior
    for the empty case. With this in place, bug #8149 should be resolved,
    for real this time.
    
    Change-Id: Ibdaac7a24147c0fe304da3b930cc8f77435aed12
    Signed-off-by: Robin Sonefors <robin.sonefors@op5.com>

--- modules/reports/models/report_query_builder.php
+++ modules/reports/models/report_query_builder.php
@@ -179,7 +179,7 @@ class Report_query_builder_Model extends Model
 			$service_states_sql = '(event_type = ' . Reports_Model::SERVICECHECK .
 				' AND state IN('.implode(', ', $included_states).'))';
 
-		$alert_types = sql::combine('or', $host_states_sql, $service_states_sql);
+		$alert_types = sql::sqlor($host_states_sql, $service_states_sql);
 
 		if (isset($this->options['include_downtime']) && $this->options['include_downtime'])
 			$downtime = 'event_type < 1200 AND event_type > 1100';
@@ -219,19 +219,19 @@ class Report_query_builder_Model extends Model
 
 		$query = "SELECT " . $fields . "\nFROM " . $this->db_table;
 		$query .= ' WHERE '.
-			sql::combine('and',
+			sql::sqland(
 				$time_first,
 				$time_last,
 				$id_first,
 				$id_last,
-				sql::combine('or',
+				sql::sqlor(
 					$process,
-					sql::combine('and',
+					sql::sqland(
 						$object_selection,
-						sql::combine('or',
+						sql::sqlor(
 							$downtime,
 							$flapping,
-							sql::combine('and',
+							sql::sqland(
 								$softorhard,
 								$alert_types)))),
 				$wildcard_filter
@@ -249,8 +249,7 @@ class Report_query_builder_Model extends Model
 							return $db->escape(current($e));
 						};
 			if (!empty($hosts[1])) {
-				$extra_sql[] = sql::combine(
-						"AND",
+				$extra_sql[] = sql::sqland(
 						"host_name IN (".
 						implode(", ",array_map($objtosql,$hosts[1])).")",
 						"service_description = ''"
