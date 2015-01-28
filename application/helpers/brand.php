61c1d39a894305e832d7bee99b52f29f4c6ab80b
Tobias Sjondin <tsjondin@op5.com>
2015-01-28 16:50:26 +0100

    Documentation required
    Failed to document brand helper, so there it is.
    And mayi takes an array as second argument
    
    Change-Id: Iacfcea809d9e200227d1315d1da7c1df02c04393
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- application/helpers/brand.php
+++ application/helpers/brand.php
@@ -1,18 +1,30 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 /**
- * Helper to get the proper complete URL to the server
+ * Helper to get the branding HTML
  */
 class brand {
 
+  /**
+   * Default image to use when unlicensed or license
+   * without an icon
+   */
   const DEFAULT_IMAGE = 'default.png';
 
+  /**
+   * Generates brading based based on license, if any, and
+   * if the image exists, otherwise return generic branding
+   * blob
+   *
+   * @return string A HTML blob to use in branding
+   */
   public static function get() {
 
+    // Default branding, ninja in community, op5 banana otherwise
     $image = ninja::add_path('brands/' . brand::DEFAULT_IMAGE, false, true);
     $label = "";
 
-    op5MayI::instance()->run("monitor.system.brand:info.icon", false, $messages, $perfdata);
+    op5MayI::instance()->run("monitor.system.brand:info.icon", array(), $messages, $perfdata);
 
     if (!empty($messages)) {
 
@@ -22,7 +34,10 @@ class brand {
       try {
         $image = ninja::add_path(sprintf($format, $label), false, false);
         $label = "";
-      } catch (FileLookupErrorException $e) {}
+      } catch (FileLookupErrorException $e) {
+        // If there is no image, use default image already set
+        // and the message as a label for that image.
+      }
 
     }
 
