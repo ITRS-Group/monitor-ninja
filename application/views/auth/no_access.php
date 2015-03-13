10d79f0aacd5b04cceb42e0b83cc431c630b370a
Anton Lofgren <alofgren@op5.com>
2015-03-13 15:00:51 +0100

    views/auth: Tell the user why they're not allowed
    "You don't have permission to view this page" gives me absolutely
    no hint of how I'm supposed to gain access to <this page>. This patch
    makes sure that the user is informed of just that, and if no such reason
    is provided urges them to file a bug report! All in big, friendly letters
    and a polite, personal demeanor.
    
    This fixes #9789
    
    Change-Id: I636b6c3262e957a951825aed7539e38790773829
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

--- application/views/auth/no_access.php
+++ application/views/auth/no_access.php
@@ -1,3 +1,20 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct access allowed.'); ?>
+<div class="alert error" >
+<?php
+echo '<h1><b>'._("I'm sorry, you don't have permission to view this page ...").'</b></h1>';
+if (count($messages) > 0) {
+	echo '<p>' . array_shift($messages) . "</p>";
+}
+else {
+	echo "<p>And I don't know why! This could be a bug, and we'd really appreciate it if you'd <a target=\"_blank\" href=\"http://bugs.op5.com\">report it</a> or <a target=\"_blank\" href=\"http://www.op5.com/services/support/\">contact support</a>.</p>";
+	echo "<p>Action was: <b>" . $action . "</b>, please include this information in your bug report/support ticket.</p>";
+}
 
-echo '<h1>'._('You don\'t have permission to view this page').'</h1>';
+if (count($messages) > 0) {
+	echo 'In addition';
+	foreach ($messages as $message) {
+		echo ',<p>' . $message; '</p>';
+	}
+}
+?>
+</div>
