8abb74f6d14ae7d3a716dba2b3485966e15869bf
Anton Lofgren <alofgren@op5.com>
2015-05-29 16:07:39 +0200

    csrf: Validate CSRF token on all POST'y requests
    We communicate denial by 403'ing, which should send a pretty clear
    message that the user's doing something she's not supposed to.
    
    Now, the 403/404 pages look pretty bad, so we might want to do something
    about those as well, but that's out of scope for this particular patch.
    
    This fixes MON-8082.
    
    Change-Id: I4f5a46e20989a2118420295014837b49d9b773fe
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

---
+++ modules/csrf/hooks/csrf_validate.php
@@ -0,0 +1,17 @@
+<?php
+
+Event::add('system.pre_controller', function() {
+
+	if ((PHP_SAPI === 'cli') || (!in_array($_SERVER['REQUEST_METHOD'], array('POST', 'PUT', 'DELETE')))) {
+		return;
+	}
+
+	$log = op5log::instance('ninja');
+	$log->log('debug', 'Validating CSRF token');
+	if (!isset($_REQUEST['csrf_token']) || !csrf::valid($_REQUEST['csrf_token'])) {
+		$log->log('warning', 'CSRF token validation failed');
+		Event::run('system.403');
+	}
+
+	$log->log('debug', 'CSRF token validation successful');
+});
