8f3c1b91949c5786cfc14d2d9a868120e49802dc
Tobias Sjondin <tsjondin@op5.com>
2015-06-15 15:09:15 +0200

    navigation: Missed a testing variables value
    The dynamic menus become very limited if they only ever
    can show max 1 item
    
    This fixes MON-8180
    
    Change-Id: I213b1f34433a95e2a4650d7de0aef053dc0c35ae
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- modules/lsfilter/hooks/listview_menu_hook.php
+++ modules/lsfilter/hooks/listview_menu_hook.php
@@ -8,7 +8,7 @@
 
     $menu = Event::$data;
     $mayi = op5MayI::instance();
-    $max_filters = 1;
+    $max_filters = 6;
 
     $section = $menu->get('Monitor');
     $menu->set('Manage.Manage filters', listview::querylink('[saved_filters] all'), 3, 'icon-16 x16-eventlog');
