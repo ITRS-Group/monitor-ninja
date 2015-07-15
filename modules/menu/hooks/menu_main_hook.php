6f4e2d126c72a08ff56e3d2539f32108a47220cf
Tobias Sjondin <tsjondin@op5.com>
2015-07-15 11:01:33 +0200

    navigation: removed op5 related items
    They are added from the license module instead
    since only licensed customers should have them
    available
    
    Change-Id: Ife01acff52b72b48788bac64f060e2eb8292785e
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- modules/menu/hooks/menu_main_hook.php
+++ modules/menu/hooks/menu_main_hook.php
@@ -7,9 +7,6 @@ Event::add('ninja.menu.setup', function () {
   $menu->set('Branding', null, 0, null, array('style' => 'padding-top: 2px'))
     ->get('Branding')->set_label(brand::get())
     ->set('About', null, 0, 'icon-16 x16-op5', array('id' => 'open-about-button'))
-    ->set('op5 Portal', '/', 0, 'icon-16 x16-op5', array('target' => '_blank'))
-    ->set('op5 Manual', Kohana::config('menu.manual_url'), 1, 'icon-16 x16-manual', array('target' => '_blank'))
-    ->set('op5 Support', 'http://www.op5.com/support', 2, 'icon-16 x16-support', array('target' => '_blank'))
     ->set('HTTP API', '/api/help', 3, 'icon-16 x16-op5', array('target' => '_blank'));
 
   Event::$data = $menu;
