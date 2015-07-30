e309b8caaef0397baa7f4436a41b544e30b705ca
Max Sikstrom <max.sikstrom@op5.com>
2015-07-30 18:07:20 +0200

    Add response to command submission
    A user reads natural languages better than arrays, so output the result
    in a user friendly way
    
    Part of MON-6761
    
    Change-Id: Ibb4ef78d229ce4917972ffcbd29afed7d3feb1e4
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- modules/lsfilter/views/cmd/exec.php
+++ modules/lsfilter/views/cmd/exec.php
@@ -1,2 +1,11 @@
 <?php
-print_r(get_defined_vars());
+if ($result === true) {
+	echo '<div class="alert notice">'.sprintf(_('Your command was successfully submitted to %s.'), Kohana::config('config.product_name')).'</div>'.
+			 '<input style="margin-left: 12px" type="button" value="Done" onclick="history.go(-2)" />'."\n";
+} else {
+	echo '<div class="alert error">'.sprintf(_('There was an error submitting your command to %s.'), Kohana::config('config.product_name'));
+	if (!empty($error)) {
+		echo '<br /><br />'._('ERROR').': '.$error;
+	}
+	echo "</div>\n";
+}
