aa3d30109d8592e247a6aba652d9d6ee767219e4
Max Sikstrom <max.sikstrom@op5.com>
2015-09-29 08:49:28 +0200

    Put saved_filters back to lsfilter module.
    It isn't monitoring specific, so we should keep the table renderer in
    lsfilter module. Was migrated out by mistake.
    
    Part of MON-8357
    
    Change-Id: Ibf68917086618e14bbc0936e4e38e50d287fea41
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

---
+++ modules/lsfilter/media/js/lsfilter_renderer_buttons.in.js
@@ -0,0 +1,4 @@
+listview_renderer_buttons.all = [
+	$('<a href="#" data-drop-down="filter-query-multi-action"/>').attr('title', _('Send multi action')).append($('<span class="icon-16 x16-check-boxes"/>')),
+	$('<a href="#" data-drop-down="filter-query-builder"/>').attr('title', _('Show/Edit Text Filter')).append($('<span class="icon-16 x16-filter"/>'))
+];
--- modules/lsfilter/media/js/lsfilter_renderer_table.in.js
+++ modules/lsfilter/media/js/lsfilter_renderer_table.in.js
@@ -40,3 +40,114 @@ var listview_renderer_table_all = {
 		}
 	},
 }
+
+listview_renderer_table.saved_filters = {
+	"icon" : {
+		"header" : '',
+		"depends" : [ 'filter_table' ],
+		"sort" : false,
+		"cell" : function(args) {
+			var cell = $('<td class="icon" />');
+
+			var icon = false;
+			var base = 'menu';
+			switch (args.obj.filter_table) {
+			case 'hosts':
+				icon = 'host';
+				break;
+			case 'services':
+				icon = 'service';
+				break;
+			case 'hostgroups':
+				icon = 'hostgroup';
+				break;
+			case 'servicegroups':
+				icon = 'servicegroup';
+				break;
+			case 'comments':
+				icon = 'comments';
+				break;
+			case 'notifications':
+				icon = 'notifications';
+				break;
+			default:
+				icon = 'eventlog';
+			}
+			if (icon) {
+				cell.append(icon16(icon, false, false, base));
+			}
+			return cell;
+
+		}
+	},
+	"scope" : {
+		"header" : _('Scope'),
+		"depends" : [ 'scope' ],
+		"sort" : false,
+		"cell" : function(args) {
+			return $('<td />').text(args.obj.scope);
+		}
+	},
+	"name" : {
+		"header" : _('Name'),
+		"depends" : [ 'filter_name', 'filter' ],
+		"sort" : [ 'filter_name' ],
+		"cell" : function(args) {
+			return $('<td />').append(
+					link_query(args.obj.filter).text(args.obj.filter_name));
+
+		}
+	},
+	"filter" : {
+		"header" : _('filter string'),
+		"depends" : [ 'filter' ],
+		"sort" : [ 'filter' ],
+		"cell" : function(args) {
+			return $('<td />').append(
+					link_query(args.obj.filter).text(args.obj.filter));
+		}
+	},
+	"owner" : {
+		"header" : _('Owner'),
+		"depends" : [ 'username' ],
+		"sort" : [ 'username' ],
+		"cell" : function(args) {
+			var cell = $('<td />');
+			if (args.obj.username) {
+				cell.text(args.obj.username);
+			}
+			return cell;
+
+		}
+	},
+	"actions" : {
+		"header" : _('Actions'),
+		"depends" : [ 'id', 'deletable' ],
+		"sort" : false,
+		"cell" : function(args) {
+			var cell = $('<td />');
+
+			if (args.obj.deletable) {
+				// Delete
+				var del_icon = icon16('delete',
+						_("Delete/cancel this saved filter"));
+				var del_link = _site_domain + _index_page
+						+ '/listview/delete_saved_filter?id=' + args.obj.id;
+
+				var del_elem = $('<a />')
+						.attr('href', del_link)
+						.addClass('link_ajax_refresh')
+						.click(
+								function(ev) {
+									if (!confirm(_("Are you sure you want to delete the filter '")
+											+ args.obj.filter_name + "' ?")) {
+										return false;
+									}
+								}).append(del_icon);
+
+				cell.append(del_elem);
+			}
+			return cell;
+		}
+	}
+};
