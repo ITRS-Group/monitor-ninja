e97ab29ab78d7a1ed558c512fffb29843b3ef2b8
Max Sikstrom <max.sikstrom@op5.com>
2015-10-02 17:09:50 +0200

    Don't add basepath if what we added is a protocol
    This makes it possible to load external javascripts throguh external
    sources
    
    Change-Id: I8e0ee3e5ff412638785686375b93e04f8f669ad7
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- application/views/js_header.php
+++ application/views/js_header.php
@@ -6,6 +6,9 @@ if(empty($js))
 	$js = array();
 
 foreach ($js as $js_file) {
-	$path = ninja::add_version_to_uri($basepath.$js_file);
+	if(!preg_match('/^[a-z]+:\/\//', $js_file)) {
+		$js_file = $basepath . $js_file;
+	}
+	$path = ninja::add_version_to_uri($js_file);
 	echo "<script type=\"text/javascript\" src=\"".html::specialchars($path)."\"></script>\n";
 }
