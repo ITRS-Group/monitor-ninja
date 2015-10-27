c3f6bea3e1be24d3101e2dc20f915d2a808d2fc9
Tobias Sjondin <tsjondin@op5.com>
2015-10-27 17:21:27 +0100

    Required change to make nacoma filterable available
    From within the new ninja nacoma landing page
    
    Change-Id: Ic6d86e6b5030abee26cb7d164a961b7d64a69206
    Signed-off-by: Tobias Sjondin <tsjondin@op5.com>

--- application/views/js_header.php
+++ application/views/js_header.php
@@ -6,6 +6,10 @@ if(empty($js))
 	$js = array();
 
 foreach ($js as $js_file) {
+	if (preg_match('/^\/monitor\/op5\/nacoma/', $js_file)) {
+		echo "<script type=\"text/javascript\" src=\"".html::specialchars($js_file)."\"></script>\n";
+		continue;
+	}
 	if(!preg_match('/^[a-z]+:\/\//', $js_file)) {
 		$js_file = $basepath . $js_file;
 	}
