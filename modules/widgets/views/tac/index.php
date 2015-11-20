129a9a4a8c759d85a5a96832404ff8ece8773d85
Tobias Sjöndin <tsjondin@op5.com>
2015-11-20 15:02:28 +0100

    janitor: Fix widget widths when divided by an uneven number
    One-third of 100 is not 33 and its ugly to pretend so, make it slightly more
    accurate by allowing two decimals in the width style attribute so the widgets
    fall in line with the other rows widgets.
    
    This is only a slight graphical issue that has not been reported, reporting it
    is more overhead than it is worth.
    
    Change-Id: I36a81a7a910ce6189d104b400ed4d38cddc6c68e
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- modules/widgets/views/tac/index.php
+++ modules/widgets/views/tac/index.php
@@ -8,7 +8,7 @@ foreach( $tac_column_count as $count ) {
 	for( $j=0; $j<$count; $j++ ) {
 		$widget_list = isset($widgets[$i])?$widgets[$i]:array();
 		$placeholder_id = 'widget-placeholder' . ($i>0 ? $i : '');
-		echo '<div class="widget-place" id="'.$placeholder_id.'" style="width: '.intval(100/$count).'%;">';
+		echo '<div class="widget-place" id="'.$placeholder_id.'" style="width: '.number_format(100/$count, 2).'%;">';
 		foreach ($widget_list as $idx => $widget) {
 			echo $widget->render('index', true);
 		}
