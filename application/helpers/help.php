353a78e99c83ead279d17f5568d83643cc00e3c4
Carl Helmertz <chelmertz@op5.com>
2016-05-18 13:22:05 +0200

    help: Cleanup code a bit, make $key argument required
    I looked, there seems to be no place where help::render() is called without
    arguments.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/helpers/help.php
+++ application/helpers/help.php
@@ -5,30 +5,27 @@
 class help
 {
 	/**
-	*	Render the help text calls to fetch
-	*	translated help texts vis ajax calls.
-	*
-	* 	Example usage:
-	* 		Entering <?php help::render('edit'); ?> somewhere on a page (view) will
-	* 		create a clickable icon that fetches the help text from the current
-	* 		controller.
-	*
-	* 		By adding a second parameter to the call:
-	* 		<?php help::render('edit', 'extinfo'); ?> the information is instead
-	* 		fetched from the extinfo controller.
-	*/
-	public static function render($key=false, $controller=false)
+ 	 * Render the help text calls to fetch
+ 	 * translated help texts vis ajax calls.
+	 *
+	 * Example usage:
+	 *
+	 * Entering <?php help::render('edit'); ?> somewhere on a page (view) will
+	 * create a clickable icon that fetches the help text from the current
+	 * controller.
+	 *
+	 * By adding a second parameter to the call:
+	 * <?php help::render('edit', 'extinfo'); ?> the information is instead
+	 * fetched from the extinfo controller.
+	 *
+	 * @param $key string
+	 * @param $controller string = null
+	 * @return string
+	 */
+	public static function render($key, $controller = null)
 	{
-		if (empty($key)) {
-			return false;
-		}
-
 		$img_path = url::base(false).'application/views/icons/12x12/help.png';
-
-		$controller = !empty($controller) ? $controller : Router::$controller;
-
+		$controller = $controller !== null ? $controller : Router::$controller;
 		return '<a data-popover="help:'.$controller.'.'.$key.'"><img src="'.$img_path.'" style="width: 12px; height: 12px; margin-bottom: -1px" /></a>';
-		/*return '<a class="helptext_target" style="border:0" data-helptext-controller="'.$controller.'" data-helptext-key="'.$key.'" href="#">'.
-		'<img src="'.$img_path.'" style="width: 12px; height: 12px; margin-bottom: -1px" /></a>';*/
 	}
 }
