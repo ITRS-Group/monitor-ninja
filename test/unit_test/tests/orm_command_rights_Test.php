d7a0dbb01412c963fc96751911179804ee1a91c8
Carl Helmertz <chelmertz@op5.com>
2016-05-31 12:13:59 +0200

    mayi: Let {host,service}_edit_all delete downtimes/comments etc again
    The fix for MON-9376 in 868edb4ac2d was a bit overzealous. The fix
    for that issue correctly removed the monitor.monitoring.hosts:delete
    right for the host_edit_all permission, but it also removed related
    rights such as monitor.monitoring.comments:delete. That meant that a
    user could for example not delete any comments or downtimes
    anylonger, although they previously could do that with only
    host_edit_all. There is no reason to change that behaviour.
    
    This fixes MON-9413.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- test/unit_test/tests/orm_command_rights_Test.php
+++ test/unit_test/tests/orm_command_rights_Test.php
@@ -3,16 +3,32 @@ require_once('op5/objstore.php');
 
 class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
-	function setup() {
-
+	protected function setup() {
+		op5objstore::instance()->mock_clear();
 		$mayi_auth = new mayi_auth_hooks();
-		$acl_auth = new user_mayi_authorization();
 		$this->user = new User_AlwaysAuth_Model();
 
-		Op5Auth::instance()->force_user($this->user, true);
-		op5MayI::instance()->be('user', Op5Auth::instance());
-		op5MayI::instance()->act_upon($acl_auth, 10);
+		$auth = new op5auth(array(
+			'common' => array (
+				'default_auth' => 'mydefault',
+				'session_key' => 'testkey'
+			),
+			'mydefault' => array (
+				'driver' => 'Default'
+			)
+		));
 
+		op5objstore::instance()->mock_add('Op5Auth', $auth);
+		$auth->force_user($this->user);
+		$mayi = new op5MayI();
+
+		$mayi->be('user', $auth);
+		$mayi->act_upon(new user_mayi_authorization(), 10);
+		op5objstore::instance()->mock_add('op5MayI', $mayi);
+	}
+
+	protected function tearDown() {
+		op5objstore::instance()->mock_clear();
 	}
 
 	private function set_authorization ($authdata) {
@@ -21,7 +37,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 		}
 	}
 
-	function test_host_command_list_without_right_to_delete () {
+	public function test_host_command_list_without_right_to_delete () {
 
 		$this->set_authorization(array(
 			'host_add_delete' => false
@@ -32,7 +48,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_host_command_list_without_rights () {
+	public function test_host_command_list_without_rights () {
 
 		$this->user = new User_NoAuth_Model();
 		Op5Auth::instance()->force_user($this->user);
@@ -42,7 +58,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_service_command_list_without_right_to_delete () {
+	public function test_service_command_list_without_right_to_delete () {
 
 		$this->set_authorization(array(
 			'service_add_delete' => false
@@ -53,7 +69,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_service_command_list_without_rights () {
+	public function test_service_command_list_without_rights () {
 
 		$this->user = new User_NoAuth_Model();
 		Op5Auth::instance()->force_user($this->user);
@@ -63,7 +79,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_hostgroup_command_list_without_right_to_delete () {
+	public function test_hostgroup_command_list_without_right_to_delete () {
 
 		$this->set_authorization(array(
 			'hostgroup_add_delete' => false
@@ -74,7 +90,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_hostgroup_command_list_without_rights () {
+	public function test_hostgroup_command_list_without_rights () {
 
 		$this->user = new User_NoAuth_Model();
 		Op5Auth::instance()->force_user($this->user);
@@ -84,7 +100,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_servicegroup_command_list_without_right_to_delete () {
+	public function test_servicegroup_command_list_without_right_to_delete () {
 
 		$this->set_authorization(array(
 			'servicegroup_add_delete' => false
@@ -95,7 +111,7 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 
 	}
 
-	function test_servicegroup_command_list_without_rights () {
+	public function test_servicegroup_command_list_without_rights () {
 
 		$this->user = new User_NoAuth_Model();
 		Op5Auth::instance()->force_user($this->user);
@@ -103,6 +119,54 @@ class Orm_Command_Rights_Test extends PHPUnit_Framework_TestCase {
 		$commands = ServiceGroup_Model::list_commands_static();
 		$this->assertTrue((count($commands) === 0));
 
+	}
+
+	/**
+	 * @group MON-9413
+	 */
+	public function test_user_allowed_to_cancel_downtime_if_allowed_to_edit_hosts () {
+		$user = new User_Model(array(
+			'username' => 'a_forced_user',
+			'realname' => 'This is forced',
+			'email' => 'i_do_have@n.email',
+			'auth_data' => array (
+				'host_edit_all' => true,
+			),
+		));
+
+		Op5Auth::instance()->force_user($user);
+		op5MayI::instance()->be('user', Op5Auth::instance());
+
+		$acl_auth = new user_mayi_authorization();
+		op5MayI::instance()->act_upon($acl_auth, 10);
+
+		$commands = Downtime_Model::list_commands_static();
+
+		$this->assertArrayHasKey('delete', $commands);
+	}
+
+	/**
+	 * @group MON-9413
+	 */
+	public function test_user_allowed_to_delete_comment_if_allowed_to_edit_hosts () {
+		$user = new User_Model(array(
+			'username' => 'a_forced_user',
+			'realname' => 'This is forced',
+			'email' => 'i_do_have@n.email',
+			'auth_data' => array (
+				'host_edit_all' => true,
+			),
+		));
+
+		Op5Auth::instance()->force_user($user);
+		op5MayI::instance()->be('user', Op5Auth::instance());
+
+		$acl_auth = new user_mayi_authorization();
+		op5MayI::instance()->act_upon($acl_auth, 10);
+
+		$commands = Comment_Model::list_commands_static();
+
+		$this->assertArrayHasKey('delete', $commands);
 	}
 
 }
