d1ed61a91b6da8149416ba260c2a6ec19732567c
Carl Helmertz <chelmertz@op5.com>
2016-06-14 18:08:17 +0200

    form/views: Render real <label> elements
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/form/views/form/integer.php
+++ modules/form/views/form/integer.php
@@ -3,10 +3,12 @@
 /* @var $field Form_Field_Text_Model */
 
 $default = $form->get_value($field->get_name(), 0);
+$element_id = 'element_id_'.uniqid();
 
 echo '<div class="nj-form-field">';
 echo '<label>';
 echo '<div class="nj-form-label">' . html::specialchars($field->get_pretty_name()) . '</div>';
-echo '<input type="number" patter="^\d+$" class="nj-form-option" name="'.$field->get_name().'" value="'.html::specialchars($default).'" />';
+echo '<div class="nj-form-label"><label for="'.$element_id.'">' . html::specialchars($field->get_pretty_name()) . '</label></div>';
+echo '<input type="number" id="'.$element_id.'" pattern="^\d+$" class="nj-form-option" name="'.$field->get_name().'" value="'.html::specialchars($default).'" />';
 echo '</label>';
 echo '</div>';
--- modules/form/views/form/options.php
+++ modules/form/views/form/options.php
@@ -7,10 +7,11 @@ $render = $field->get_force_render();
 $options = $field->get_options();
 
 if ($render === "select" || ($render === false && count($options) > 3)) {
+	$element_id = 'element_id_'.uniqid();
 	echo '<div class="nj-form-field">';
 	echo '<label>';
-	echo '<div class="nj-form-label">' . html::specialchars($field->get_pretty_name()) . '</div>';
-	echo '<select class="nj-form-option" name="' . $field->get_name() . '" />';
+	echo '<div class="nj-form-label"><label for="'.$element_id.'">' . html::specialchars($field->get_pretty_name()) . '</label></div>';
+	echo '<select class="nj-form-option" id="'.$element_id.'" name="' . $field->get_name() . '" />';
 	foreach ( $field->get_options() as $value => $label ) {
 		$selectstr = ($default == $value) ? ' selected="selected"' : '';
 		echo '<option value="' . html::specialchars( $value ) . '"'.$selectstr.'>' . html::specialchars( $label ) . '</option>';
@@ -22,12 +23,13 @@ if ($render === "select" || ($render === false && count($options) > 3)) {
 	echo '<div class="nj-form-field">';
 	echo '<div class="nj-form-label">' . html::specialchars($field->get_pretty_name()) . '</div>';
 	foreach ( $field->get_options() as $value => $label ) {
+		$element_id = 'element_id_'.uniqid();
 		echo '<div class="nj-form-field-radio">';
 		echo '<label>';
 		$selectstr = ($default == $value) ? ' checked' : '';
 		/* Order of input and nj-form-label matters! */
-		echo '<input name="' . $field->get_name() . '" type="radio" value="' . html::specialchars($value) . '"'.$selectstr.'>';
-		echo '<div class="nj-form-label">' . html::specialchars($label) . '</div>';
+		echo '<input id="'.$element_id.'" name="' . $field->get_name() . '" type="radio" value="' . html::specialchars($value) . '"'.$selectstr.'>';
+		echo '<div class="nj-form-label"><label for="'.$element_id.'">' . html::specialchars($label) . '</label></div>';
 		echo '</label>';
 		echo '</div>';
 	}
