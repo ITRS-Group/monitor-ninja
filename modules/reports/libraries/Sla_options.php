b23450a8096c4cb1076e100c634e92d9ac5528a7
Carl Helmertz <chelmertz@op5.com>
2016-06-15 15:19:59 +0200

    sla: We want all months now
    Before, we wanted to make sure that all months in the input was in between the
    report's start- and end dates. Now, we want all of the values, because of
    MON-6154.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/reports/libraries/Sla_options.php
+++ modules/reports/libraries/Sla_options.php
@@ -61,45 +61,17 @@ class Sla_options extends Report_options {
 		}
 	}
 
-	/**
-	 * Because this if condition is too long for my pretty little mind to worry about,
-	 * I'm'a make a predicate out of it!
-	 */
-	private static function is_valid_month($month, $start_month, $end_month)
-	{
-		if ($start_month > $end_month && ($month >= $start_month || $month <= $end_month)) # e.g. report is dec(12)->feb(2), only 12, 1, 2 valid
-			return true;
-		if ($start_month <= $end_month && ($month >= $start_month && $month <= $end_month)) # e.g. report is feb(2)->apr(4), only 2, 3, 4 valid
-			return true;
-		return false;
-	}
-
 	private static function validate_months($start_time, $end_time, $months)
 	{
-		$start_month = date('n', $start_time);
-		$end_month = date('n', $end_time - 1);
 		$res = array();
 		if (!is_array($months))
 			$months = array();
-		if (($end_month - $start_month == 11) || ($end_month - $start_month == -1)) {
-			$start = 1;
-			$stop = 12;
-		}
-		else {
-			$start = $start_month;
-			$stop = $end_month;
-		}
-		// about the weird modulo: the valid range is 1->12, not 0->11
-		for ($i = $start; true; $i = ($i % 12) + 1)
-		{
-			if (static::is_valid_month($i, $start_month, $end_month)) {
-				if (!isset($months[$i]))
-					$res[$i] = 0.0;
-				else
-					$res[$i] = $months[$i];
+
+		for ($i = 1; $i <= 12; $i++) {
+			$res[$i] = 0.0;
+			if (isset($months[$i])) {
+				$res[$i] = $months[$i];
 			}
-			if ($i == $stop)
-				break;
 		}
 		return $res;
 	}
@@ -107,24 +79,26 @@ class Sla_options extends Report_options {
 	public function set($name, $value)
 	{
 		if ($name == 'months') {
-			$value = static::validate_months($this['start_time'], $this['end_time'], $value);
+			$value = self::validate_months($this['start_time'], $this['end_time'], $value);
 		}
 		$resp = parent::set($name, $value);
 		if ($resp === false && preg_match('/^month/', trim($name))) {
 			$id = (int)str_replace('month_', '', $name);
 
-			if (!$this->is_valid_month($id, date('n', $this['start_time']), date('n', $this['end_time'] - 1)))
-				return false;
-
 			if (trim($value) == '')
 				return;
+
 			// Because fuck locales, amiright?
 			$value = str_replace(',', '.', $value);
-			$value = (float)$value;
-			// values greater than 100 doesn't make sense
-			if ($value>100)
+
+			// values are percentages
+			if ($value>100) {
 				$value = 100;
-			$this->options['months'][$id] = $value;
+			} elseif($value < 0) {
+				$value = 0;
+			}
+
+			$this->options['months'][$id] = (float)$value;
 			return true;
 		}
 		return $resp;
@@ -134,7 +108,7 @@ class Sla_options extends Report_options {
 	{
 		$res = parent::calculate_time($report_period);
 		if ($res && isset($this->options['start_time']) && isset($this->options['end_time'])) {
-			$this->options['months'] = static::validate_months($this['start_time'], $this['end_time'], $this['months']);
+			$this->options['months'] = self::validate_months($this['start_time'], $this['end_time'], $this['months']);
 		}
 		return $res;
 	}
