6a3d287a4d35d980c3df30563bb6112192369e71
Max Sikstrom <max.sikstrom@op5.com>
2016-06-17 10:44:24 +0200

    Builder: build.php specify targets to build
    If we're going to increase the usage of builders (for example for js),
    we should also be able to build what we want for targets.
    
    php build.php module1 module2   => builds all targets in modules
    php build.php module:           => builds all targets
    php build.php :target1 :target2 => builds targets in all modules
    php build.php module:target     => builds target on module
    
    Signed-off-by: Max Sikstrom <max.sikstrom@op5.com>

--- build.php
+++ build.php
@@ -12,18 +12,41 @@ $modules = false;
 
 /* First, get module names from command line */
 if($modules === false && count($argv) > 1) {
-	$modules = array_slice($argv, 1);
+	$rules = array_slice($argv, 1);
+}
+
+/* If no rules is specified, load everything */
+if(empty($rules)) {
+	$rules = array(':');
 }
 
 /* If no modules found on command line, scan modules dir */
-if($modules === false) {
-	$modules = scandir( "modules" );
-}
+$all_modules = array_filter(
+	scandir( "modules" ),
+	function($line) {
+		return $line[0] != '.';
+	}
+);
 
 
-foreach ( $modules as $module ) {
-	if ($module[0] == '.')
-		continue;
-	$builder->add_module( "modules/$module" );
+foreach ( $rules as $rule ) {
+	$parts = explode(':', $rule);
+
+	/* If module is specified, only use that, otherwise all */
+	if(!empty($parts[0])) {
+		$modules = array($parts[0]);
+	} else {
+		$modules = $all_modules;
+	}
+
+	/* If target is specified, only use that */
+	$target = null;
+	if(count($parts) >= 2)
+		$target = $parts[1];
+
+	/* Add rule to builder */
+	foreach($modules as $module) {
+		$builder->add_module( "modules/".$module, $target );
+	}
 }
 $builder->generate();
--- src/op5/ninja_sdk/Ninja_Builder.php
+++ src/op5/ninja_sdk/Ninja_Builder.php
@@ -2,6 +2,26 @@
 class Ninja_Builder {
 	protected $build_targets = array ();
 	protected $builders_basedir = __DIR__;
+	protected $all_targets = array();
+	protected $builders = array();
+
+	public function __construct() {
+		$this->all_targets = array();
+		foreach( scandir( $this->builders_basedir ) as $target ) {
+			if( $target[0] === '.' )
+				continue;
+			if( !is_dir($this->builders_basedir . DIRECTORY_SEPARATOR . $target) )
+				continue;
+			$this->all_targets[] = $target;
+		}
+
+		$this->builders = array();
+		foreach ( $this->all_targets as $target ) {
+			$builderclass = $target . "_Builder";
+			require_once ($this->builders_basedir . DIRECTORY_SEPARATOR . $target . DIRECTORY_SEPARATOR . "builder.php");
+			$this->builders[$target] = new $builderclass();
+		}
+	}
 
 	/**
 	 * Add a module to the builder
@@ -14,13 +34,20 @@ class Ninja_Builder {
 	 *
 	 * @param string $moduledir
 	 */
-	public function add_module($moduledir) {
+	public function add_module($moduledir = null, $target = null) {
 		$srcdir = $moduledir . DIRECTORY_SEPARATOR . "src";
 		if (! is_dir( $srcdir ))
 			return;
-		foreach ( scandir( $srcdir ) as $target ) {
-			if ($target[0] == '.')
-				continue;
+
+		/* If a target is specified, use that, otherwise list */
+		if(empty($target)) {
+			$targets = $this->all_targets;
+		} else {
+			$targets = array( $target );
+		}
+
+		/* Register all targets for this module */
+		foreach ( $targets as $target ) {
 			if (! array_key_exists( $target, $this->build_targets ))
 				$this->build_targets[$target] = array ();
 			$this->build_targets[$target][$moduledir] = $srcdir . DIRECTORY_SEPARATOR . $target;
@@ -33,17 +60,12 @@ class Ninja_Builder {
 	 * called.
 	 */
 	public function generate() {
-		$builders = array ();
-
-		foreach ( $this->build_targets as $target => $modules ) {
-			$builderclass = $target . "_Builder";
-			require_once ($this->builders_basedir . DIRECTORY_SEPARATOR . $target . DIRECTORY_SEPARATOR . "builder.php");
-			$builders[$target] = new $builderclass();
-		}
-
-		foreach ( $this->build_targets as $target => $modules ) {
-			$builder = $builders[$target];
+		foreach ( $this->all_targets as $target ) {
+			$builder = $this->builders[$target];
+			$modules = isset($this->build_targets[$target]) ? $this->build_targets[$target] : array();
 			foreach ( $modules as $moduledir => $confdir ) {
+				if( !is_dir($moduledir . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $target) )
+					continue;
 				$builder->generate( $moduledir, $confdir );
 			}
 		}
