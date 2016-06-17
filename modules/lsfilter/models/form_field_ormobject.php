59e0cf71d3d3ce758a502c978abe3f97cfddd413
Carl Helmertz <chelmertz@op5.com>
2016-06-17 09:42:18 +0200

    Document Form_Field_ORMObject_Model + validate $table array
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/lsfilter/models/form_field_ormobject.php
+++ modules/lsfilter/models/form_field_ormobject.php
@@ -1,24 +1,51 @@
 <?php
+
+/**
+ * Let the user select a single object with this field model. Autocompletion of
+ * object names should be supported by frontend code. By submitting multiple
+ * tables to the constructor, you can let the user get autocomplete options for
+ * several object types (as long as they are registered with
+ * @see autocomplete::add_table()).
+ */
 class Form_Field_ORMObject_Model extends Form_Field_Model {
 
+	/**
+	 * @param $name string
+	 * @param $pretty_name string
+	 * @param $tables array Front end names of tables, such as saved_filters or hosts
+	 */
 	public function __construct($name, $pretty_name, array $tables) {
 		parent::__construct($name, $pretty_name);
 		$this->tables = $tables;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function get_type() {
 		return 'ormobject';
 	}
 
+	/**
+	 * @return array
+	 */
 	public function get_tables () {
 		return $this->tables;
 	}
 
+	/**
+	 * @param $raw_data array
+	 * @param $result Form_Result_Model
+	 * @throws Form_Exception
+	 */
 	public function process_data(array $raw_data, Form_Result_Model $result) {
 		$name = $this->get_name();
 		if (!isset($raw_data[$name]))
 			throw new FormException("Unknown field $name");
-		if (!is_array($raw_data[$name])) {
+		if (!is_array($raw_data[$name]) ||
+			!array_key_exists('table', $raw_data[$name]) ||
+			!array_key_exists('value', $raw_data[$name])
+		) {
 			throw new FormException("$name does not point at a valid object (".var_export($raw_data[$name], true).")");
 		}
 
