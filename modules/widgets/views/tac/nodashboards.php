d87dd433967d930177f7e9ffd9b0d58887d3a55e
Tobias Sjöndin <tsjondin@op5.com>
2016-08-01 10:16:50 +0200

    Update appearance of No dashboard prompt
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- modules/widgets/views/tac/nodashboards.php
+++ modules/widgets/views/tac/nodashboards.php
@@ -1,8 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
-<h1>No dashboards</h1>
+<div class="center">
+	<h1 class="emote"><b>:(</b> No dashboard..</h1>
 <p>There are no dashboards available.</p>
 <p>
     <a class="menuitem_dashboard_option" href=<?php
         echo LinkProvider::factory()->get_url('tac', 'new_dashboard_dialog');
     ?>>Click here to add dashboard!</a>
 </p>
+</div>
