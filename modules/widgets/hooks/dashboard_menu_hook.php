1e7f5155a65904bac9b0cfebebc84ea1111dd0cf
Tobias Sjöndin <tsjondin@op5.com>
2016-08-02 09:11:08 +0200

    Use id in menu namespace instead of name
    This allows multiple dashboards with the same name to be rendered as multiple
    items in the dashboard menu.
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- modules/widgets/hooks/dashboard_menu_hook.php
+++ modules/widgets/hooks/dashboard_menu_hook.php
@@ -6,12 +6,15 @@ Event::add ( 'ninja.menu.setup', function () {
 	/* @var $menu Menu_Model */
 
 	$db_menu = $menu->set('Dashboards', null, 1, 'icon-16 x16-tac', array('style' => 'padding-top: 8px'))->get('Dashboards');
-
 	$idx = 0;
+
 	foreach($dashboards->it(array('name'), array('name')) as $dashboard) {
-			$db_menu->set ( $dashboard->get_name(), 'tac/index/'.$dashboard->get_key(), $idx++ );
+		$item = $db_menu->set($dashboard->get_id(), 'tac/index/'.$dashboard->get_key(), $idx++)->get($dashboard->get_id());
+		$item->set_label($dashboard->get_name());
 	}
-	$db_menu->set ( "New dashboard", 'tac/new_dashboard_dialog', $idx++, 'icon-16 x16-sign-add', array(
-			'class' => "menuitem_dashboard_option" /* Popup as fancybox */
+
+	$db_menu->set("New dashboard", 'tac/new_dashboard_dialog', $idx++, 'icon-16 x16-sign-add', array(
+		'class' => "menuitem_dashboard_option" /* Popup as fancybox */
 	));
+
 });
