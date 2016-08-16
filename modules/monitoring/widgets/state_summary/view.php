9db33c1d7779aebd1bf8f66a6f67f193a30fd14c
Tobias Sjöndin <tsjondin@op5.com>
2016-08-16 10:59:02 +0200

    Update view to not show decimals if the number is lower than 1000
    The update to clipped_number did not take state summary widget display into
    account
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- modules/monitoring/widgets/state_summary/view.php
+++ modules/monitoring/widgets/state_summary/view.php
@@ -7,13 +7,20 @@ foreach($state_definitions['states'] as $silly_counter => $state) {
 		$stats[$silly_counter]
 	);
 
+	$display_number = $stats[$silly_counter];
+	if ($display_number >= 1000) {
+		$display_number = text::clipped_number($display_number, 2);
+	} else {
+		$display_number = text::clipped_number($display_number, 0);
+	}
+
 	printf("<a title=\"Go to the listview for these " . $stats[$silly_counter] . " $object_type\" href=\"%s\"><div class='state_summary-state state_summary-%s'>
 		<span class='" . strtolower($state['label']) . " state_summary-figure'>%s</span><br>
 		<span class='" . strtolower($state['label']) . " supplementary state_summary-description'>%s</span>
 		</div></a>",
 		listview::querylink($queries[$silly_counter]),
 		$css_class,
-		text::clipped_number($stats[$silly_counter], 2),
+		$display_number,
 		$state['label']
 	);
 
