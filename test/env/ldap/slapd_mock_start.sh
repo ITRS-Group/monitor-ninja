1ace92a6045a8929fddfc851880f0e391437c400
Carl Helmertz <chelmertz@op5.com>
2016-08-19 10:22:35 +0200

    spec/test: Remove references to suse/sles
    This is just a change that was overdue, but having less dead code/text
    in the way is nice when you are trying to debug things (in my case,
    extraneous output from the installation process).
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- test/env/ldap/slapd_mock_start.sh
+++ test/env/ldap/slapd_mock_start.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-SLAPD=false
-# Hack because SLES is wierd...
-if test -x /usr/lib/openldap/slapd; then
-	SLAPD=/usr/lib/openldap/slapd
-else
-	SLAPD=$(which slapd)
-fi
-
 pushd "$(dirname "$0")"
-$SLAPD -f slapd.conf -h ldap://0.0.0.0:13389
+$(which slapd) -f slapd.conf -h ldap://0.0.0.0:13389
 sleep 0.5
 popd
