f20cfb28d9b6d03508edaf0e49c5a94f1e9aa4b0
Carl Helmertz <chelmertz@op5.com>
2016-09-26 16:15:12 +0200

    controllers/error: Remove empty construct()
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/controllers/error.php
+++ application/controllers/error.php
@@ -13,10 +13,6 @@
  */
 class Error_Controller extends Ninja_Controller  {
 
-	public function __construct () {
-		parent::__construct();
-	}
-
 	public function show_403() {
 		if (PHP_SAPI !== 'cli')
 			header('HTTP/1.1 403 Forbidden');
