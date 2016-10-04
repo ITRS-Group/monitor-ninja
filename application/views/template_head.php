6f33c03e667a3204d9d2896fb6d8ad8e9df8cd9e
Carl Helmertz <chelmertz@op5.com>
2016-10-04 17:29:16 +0200

    Upgrade jQuery from v1.7 to 3.1.1 and jQuery UI from 1.7.1 to 1.12.1
    This is part of the investigation that is logged in MON-9627
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- application/views/template_head.php
+++ application/views/template_head.php
@@ -24,7 +24,7 @@ if (!empty($base_href)) {
 	<link href="<?php echo ninja::add_path('css/ninja-icons.css'); ?>" type="text/css" rel="stylesheet" media="all" />
 	<link href="<?php echo ninja::add_path('css/'.$current_skin.'common.css'); ?>" type="text/css" rel="stylesheet" media="all" />
 	<link href="<?php echo ninja::add_path('css/'.$current_skin.'print.css'); ?>" type="text/css" rel="stylesheet" media="print" />
-	<link type="text/css" rel="stylesheet" href="<?php echo ninja::add_path('css/'.$current_skin.'jquery-ui-custom.css') ?>" media="screen" />
+	<link type="text/css" rel="stylesheet" href="<?php echo ninja::add_path('css/'.$current_skin.'jquery-ui.min.css') ?>" media="screen" />
 	<?php
 		$v = new View('css_header', array('css' => isset($css)?$css:array()));
 		$v->render(true);
