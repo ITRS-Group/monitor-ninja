3c8cda98997a538059bdee56548800ea2ad06f8f
Carl Helmertz <chelmertz@op5.com>
2016-10-28 16:27:35 +0200

    views/naemon_command: Escape output from Naemon
    There is no known issue of bad output ending up here, but there is no
    reason to be trusty around here.
    
    Signed-off-by: Carl Helmertz <chelmertz@op5.com>

--- modules/monitoring/views/monitoring/naemon_command.php
+++ modules/monitoring/views/monitoring/naemon_command.php
@@ -1,13 +1,13 @@
 <?php
 print "<h2>" . $object->get_readable_name() . "</h2>";
 if ($result['status']) {
-	echo '<div class="alert notice">'.$result['output'];
+	echo '<div class="alert notice">'.html::specialchars($result['output']);
 	echo "</div>\n";
 	$this->footer = '<input style="margin-left: 12px" type="button" value="Done" onclick="history.go(-2)" />'."\n";
 } else {
 	echo '<div class="alert error">'.sprintf(_('There was an error submitting your command to %s.'), Kohana::config('config.product_name'));
 	if (!empty($result['output'])) {
-		echo '<br /><br />'._('ERROR').': '.$result['output'];
+		echo '<br /><br />'._('ERROR').': '.html::specialchars($result['output']);
 	}
 	echo "</div>\n";
 	$this->footer = '<input style="margin-left: 12px" type="button" value="Back" onclick="history.go(-1)" />'."\n";
