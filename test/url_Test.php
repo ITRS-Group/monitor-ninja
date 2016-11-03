57988e55f7423ca81e6b81a9eaf5f5186015d083
Aron Lander <aron.lander@op5.com>
2016-11-03 14:17:45 +0100

    UrlTest: Added a test for hostgroups
    Related to: MON-9817
    
    Signed-off-by: Aron Lander <aron.lander@op5.com>

--- test/url_Test.php
+++ test/url_Test.php
@@ -8,7 +8,10 @@ class Url_Test extends PHPUnit_Framework_TestCase {
 			// this is the cheap version of checking whether or not we
 			// are running the test against an installed version of
 			// Monitor, or from the checked out code (meaning Ninja)
-			array("dokuwiki/hello", "/(ninja|monitor)/index.php/dokuwiki/hello")
+			array("dokuwiki/hello", "/(ninja|monitor)/index.php/dokuwiki/hello"),
+
+            //To make sure that the cmd.php class returns the proper url, related to: MON-9817
+            array("configuration/configure/hostgroup/test:1", "/monitor/index.php/configuration/configure/hostgroup/test:1")
 		);
 	}
 
@@ -23,4 +26,23 @@ class Url_Test extends PHPUnit_Framework_TestCase {
 			"debugging, look for how it handles leading '/'."
 		);
 	}
+
+	public function site_url_provider() {
+        return array(
+            array("configuration/configure/hostgroup/test:1", "/monitor/index.php/configuration/configure/hostgroup/test:1")
+        );
+    }
+
+	/**
+     * @dataProvider site_url_provider
+     * @group MON-9817
+     */
+	public function test_site_url($uri, $expected) {
+        $url = url::site($uri);
+        $this->assertRegExp('~^'.$expected.'$~', $url,
+            "The url helper treated the input incorrectly. When ".
+            "debugging, look for how it handles leading '/'."
+        );
+    }
+
 }
