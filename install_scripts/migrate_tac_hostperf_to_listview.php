8cdf25bc92de3d15033f5c65df09a80e8ac10a8b
Tobias Sjöndin <tsjondin@op5.com>
2017-01-11 13:10:45 +0100

    Update broken test and add test for tac_hostperf migration
    Updated test broken by updated User_AlwaysAuth, AlwaysAuth should ALWAYS auth,
    it doesn't matter it the authorization is through rights or permission quarks.
    
    The Dashboard test assumed that AlwaysAuth did not have access to
    permission_quarked items.
    
    Signed-off-by: Tobias Sjöndin <tsjondin@op5.com>

--- install_scripts/migrate_tac_hostperf_to_listview.php
+++ install_scripts/migrate_tac_hostperf_to_listview.php
@@ -5,47 +5,67 @@ if(PHP_SAPI !== 'cli') {
 	die("This script should never be run from a browser, execute it from commmand-line instead.");
 }
 
-/**
- * Used to migrate tac_hostperf widgets to listview widgets so that
- * tac_hostperf can be removed.
- */
 if(!class_exists('Kohana')) {
 	define('SKIP_KOHANA', true);
 	require_once(realpath(__DIR__.'/../index.php'));
 }
 
-$user = new User_AlwaysAuth_Model();
-op5auth::instance()->force_user($user);
-$widgets = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'tac_hostperf', '=');
-$first_host = HostPool_Model::all()->one()->get_name();
+/**
+ * Used to migrate tac_hostperf widgets to listview widgets so that
+ * tac_hostperf can be removed.
+ */
+function migrate_tac_hostperf_widgets () {
 
-foreach ($widgets as $widget) {
+	$widgets = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'tac_hostperf', '=');
+	$first_host = HostPool_Model::all()->one();
 
-	$widget->set_name('listview');
-	$new_settings = array(
-		"columns" => "state,description,status_information",
-		"limit" => "100"
-	);
-
-	$old_settings = $widget->get_setting();
-	$host_name = isset($old_settings['host_name']) ? $old_settings['host_name'] : $first_host;
-	$new_settings['title'] = 'Host performance - ' . $host_name;
-
-	$hidden = isset($old_settings['hidden']) ? $old_settings['hidden'] : '';
-	$hidden = explode(";", $hidden);
-
-	$query_set = ServicePool_Model::all();
-	$query_set = $query_set->reduce_by('host.name', $host_name, '=');
-
-	if (count($hidden) > 0) {
-		foreach ($hidden as $description) {
-			$query_set = $query_set->reduce_by('description', $description, '!=');
-		}
+	if ($first_host) {
+		$first_host = $first_host->get_name();
 	}
 
-	$new_settings['query'] = $query_set->get_query();
-	$widget->set_setting($new_settings);
-	$widget->save();
+	foreach ($widgets as $widget) {
+
+		$widget->set_name('listview');
+		$new_settings = array(
+			"columns" => "state,description,status_information",
+			"limit" => "100"
+		);
+
+		$old_settings = $widget->get_setting();
+		$host_name = isset($old_settings['host_name']) ? $old_settings['host_name'] : $first_host;
+		$new_settings['title'] = 'Host performance - ' . $host_name;
+
+		$hidden = isset($old_settings['hidden']) ? $old_settings['hidden'] : '';
+		$hidden = explode(";", $hidden);
+
+		if ($host_name) {
+
+			$query_set = ServicePool_Model::all();
+			$query_set = $query_set->reduce_by('host.name', $host_name, '=');
+
+			if (count($hidden) > 0) {
+				foreach ($hidden as $description) {
+					$query_set = $query_set->reduce_by('description', $description, '!=');
+				}
+			}
+
+			$new_settings['query'] = $query_set->get_query();
+
+		} else {
+
+			$new_settings['query'] = '[services] description=""';
+
+		}
+
+		$widget->set_setting($new_settings);
+		$widget->save();
+
+	}
 
 }
 
+if (isset($argv, $argv[0]) && realpath($argv[0]) === __FILE__) {
+	/** This script is the entrypoint **/
+	op5auth::instance()->force_user(new User_AlwaysAuth_Model());
+	migrate_tac_hostperf_widgets();
+}
---
+++ test/migrate_tac_hostperf_Test.php
@@ -0,0 +1,195 @@
+<?php
+
+require('install_scripts/migrate_tac_hostperf_to_listview.php');
+
+/**
+ * Migrate Tac Hostperf Test
+ *
+ * @package    Unit_Test
+ * @author     op5
+ */
+class Migrate_Tac_Hostperf_Test extends PHPUnit_Framework_TestCase {
+
+	public function setUp () {
+
+		op5auth::instance()->force_user(new User_AlwaysAuth_Model());
+		$this->mock_data(array(
+            'ORMDriverMySQL default' => array(
+				'dashboards' => array(
+					array(
+						'name' => 'dummy',
+						'id' => 1
+					)
+				),
+				'dashboard_widgets' => array(
+					array(
+						'id' => 1,
+						'name' => 'tac_hostperf',
+						'dashboard_id' => 1,
+						'setting' => '{}'
+					),
+					array(
+						'id' => 2,
+						'name' => 'tac_hostperf',
+						'dashboard_id' => 1,
+						'setting' => '{"host_name":"Obaeron"}'
+					),
+					array(
+						'id' => 3,
+						'name' => 'tac_hostperf',
+						'dashboard_id' => 1,
+						'setting' => '{"host_name":"Fellion","hidden":"Burp;Zek"}'
+					)
+				)
+			),
+			'ORMDriverLS default' => array(
+				'hosts' => array(
+					array(
+						'name' => 'Intini'
+					),
+					array(
+						'name' => 'Obaeron'
+					),
+					array(
+						'name' => 'Fellion'
+					)
+
+				),
+				'services' => array(
+					array(
+						'description' => 'Servicon',
+						'host' => 'Intini'
+					),
+					array(
+						'description' => 'Betazoid',
+						'host' => 'Obaeron'
+					),
+					array(
+						'description' => 'Deltar',
+						'host' => 'Fellion'
+					),
+					array(
+						'description' => 'Burp',
+						'host' => 'Fellion'
+					),
+					array(
+						'description' => 'Zek',
+						'host' => 'Fellion'
+					)
+				)
+            )
+		));
+
+	}
+
+    private function mock_data($tables) {
+        foreach($tables as $driver => $tables) {
+            op5objstore::instance()->mock_add(
+                $driver,
+                new ORMDriverNative($tables, null, $driver)
+            );
+        }
+    }
+
+	public function test_migration_tac_hostperf_are_migrated () {
+
+		$all = Dashboard_WidgetPool_Model::all();
+
+		$listviews = $all->reduce_by('name', 'listview', '=');
+		$hostperfs = $all->reduce_by('name', 'tac_hostperf', '=');
+
+		$this->assertEquals(3, $hostperfs->count());
+		$this->assertEquals(0, $listviews->count());
+
+		migrate_tac_hostperf_widgets();
+
+		$listviews = $all->reduce_by('name', 'listview', '=');
+		$hostperfs = $all->reduce_by('name', 'tac_hostperf', '=');
+
+		$this->assertEquals(0, $hostperfs->count());
+		$this->assertEquals(3, $listviews->count());
+
+	}
+
+	public function test_migration_tac_hostperf_becomes_listview_widget () {
+
+		migrate_tac_hostperf_widgets();
+
+		$widget = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'listview', '=')->one();
+		$this->assertInstanceOf('Dashboard_Widget_Model', $widget);
+
+	}
+
+	public function test_migration_tac_hostperf_gets_first_host_if_no_setting () {
+
+		migrate_tac_hostperf_widgets();
+
+		$widget = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'listview', '=')->one();
+		$setting = $widget->get_setting();
+		$service = ObjectPool_Model::get_by_query($setting['query'])->one();
+		$this->assertEquals("Intini", $service->get_host()->get_name());
+
+	}
+
+	public function test_migration_tac_hostperf_with_host_setting () {
+
+		migrate_tac_hostperf_widgets();
+
+		$widget = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'listview', '=')->reduce_by('id', 2, '=')->one();
+		$setting = $widget->get_setting();
+		$service = ObjectPool_Model::get_by_query($setting['query'])->one();
+		$this->assertEquals("Obaeron", $service->get_host()->get_name());
+
+	}
+
+	public function test_migration_tac_hostperf_with_hidden_setting_converted_to_filter_only_contains_one_service () {
+
+		migrate_tac_hostperf_widgets();
+
+		$widget = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'listview', '=')->reduce_by('id', 3, '=')->one();
+		$setting = $widget->get_setting();
+
+		$services = ObjectPool_Model::get_by_query($setting['query']);
+		$this->assertEquals(1, $services->count());
+		$this->assertEquals("Deltar", $services->one()->get_description());
+
+	}
+
+	public function test_migration_tac_hostperf_with_no_hosts_in_system_and_no_setting_host_becomes_nonematching_service_filter () {
+
+		$this->mock_data(array(
+            'ORMDriverMySQL default' => array(
+				'dashboards' => array(
+					array(
+						'name' => 'dummy',
+						'id' => 1
+					)
+				),
+				'dashboard_widgets' => array(
+					array(
+						'id' => 1,
+						'name' => 'tac_hostperf',
+						'dashboard_id' => 1,
+						'setting' => '{}'
+					)
+				)
+			),
+			'ORMDriverLS default' => array(
+				'hosts' => array(
+				),
+				'services' => array(
+				)
+            )
+		));
+
+		migrate_tac_hostperf_widgets();
+
+		$widget = Dashboard_WidgetPool_Model::all()->reduce_by('name', 'listview', '=')->one();
+		$setting = $widget->get_setting();
+		$this->assertEquals('[services] description=""', $setting['query']);
+		$services = ObjectPool_Model::get_by_query($setting['query']);
+		$this->assertEquals(0, $services->count());
+
+	}
+
+}
