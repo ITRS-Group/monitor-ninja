27c14a2d11240c96eff90f17bf36d010dd98d8f2
Ramesh T <ramesht@op5.com>
2018-11-13 12:59:39 +0530

    Fixed XSS Security Hole in the Quickbar
    Previously Quickbar form data send as json string(no validation for URI
    field) and added in merlin db "ninja_settings" table.
    
    Now Quickbar form validation updated for URI field and test case
    updated.
    
    This fixes issue MON-11283.
    
    Signed-off-by: Ramesh T ramesht@op5.com

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -31,8 +31,8 @@ class Ajax_Controller extends Authenticated_Controller {
 	{
 		$setting_info = json_decode($setting, true);
 		foreach ($setting_info as $setting_data) {
-		$href = htmlspecialchars($setting_data['href'], ENT_QUOTES, 'UTF-8');
-		$validate_href = "(^/)";
+			$href = htmlspecialchars($setting_data['href'], ENT_QUOTES, 'UTF-8');
+			$validate_href = "(^/)";
 			if(preg_match($validate_href, $href)) {
 				$setting_href[] = array('href' => $href);
 			}else {
