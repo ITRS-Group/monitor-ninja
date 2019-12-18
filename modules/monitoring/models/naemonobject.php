ITRS-Group/monitor-ninja

commit 0182856827d205a459fb78eb5996043930ec447d

Add del_downtime_by_host_name to host/service models
For `del_downtime_by_host_name()`, send the Naemon external command
`DEL_DOWNTIME_BY_HOST_NAME`.

This is part of MON-11872.

Signed-off-by: Aksel Sjögren <asjogren@itrsgroup.com>

modules/monitoring/models/naemonobject.php
@@ -36,11 +36,15 @@ class NaemonObject_Model extends Object_Model {
 		}
 		$key = ($key===false) ? "" : ";".$key;
 
-		$raw_command = sprintf("[%d] %s%s", time(), $cmd, $key);
+		// Don't use $key for DEL_DOWNTIME_BY_HOST_NAME since the required keys
+		// are passed as function parameters.
+		$raw_command = sprintf("[%d] %s%s", time(), $cmd,
+			($cmd == 'DEL_DOWNTIME_BY_HOST_NAME') ? '' : $key);
 		foreach($args as $arg) {
 			$raw_command .= ";".$arg;
 		}
 
+		op5log::instance("ninja")->log("debug", "submit_naemon_command raw_command: $raw_command");
 		$output = false;
 		try {
 			$qh = op5queryhandler::instance();

modules/monitoring/models/host.php
@@ -1086,4 +1086,45 @@ class Host_Model extends BaseHost_Model {
 
 	}
 
+	/**
+	 * @param host_name
+	 * @param service_description = ""
+	 * @param downtime_start_time = ""
+	 * @param comment = true
+	 *
+	 * @ninja orm_command name Delete downtime
+	 * @ninja orm_command category Actions
+	 * @ninja orm_command icon scheduled-downtime
+	 *
+	 * @ninja orm_command params.host_name.id 0
+	 * @ninja orm_command params.host_name.type string
+	 * @ninja orm_command params.host_name.name Host_name
+	 *
+	 * @ninja orm_command params.service_description.id 1
+	 * @ninja orm_command params.service_description.type string
+	 * @ninja orm_command params.service_description.name Service description
+	 * @ninja orm_command params.service_description.default ""
+	 *
+	 * @ninja orm_command params.downtime_start_time.id 2
+	 * @ninja orm_command params.downtime_start_time.type time
+	 * @ninja orm_command params.downtime_start_time.name Downtime start time
+	 * @ninja orm_command params.downtime_start_time.default ""
+	 *
+	 * @ninja orm_command params.comment.id 3
+	 * @ninja orm_command params.comment.type bool
+	 * @ninja orm_command params.comment.name Comment
+	 * @ninja orm_command params.comment.default ""
+	 *
+	 * @ninja orm_command mayi_method update.command.downtime
+	 * @ninja orm_command description
+	 *     This command is used to delete a host or service downtime.
+	 *     The command will delete all downtimes which matches the criteria.
+	 *     Only the host_name is required, in which case all downtimes for
+	 *     for that host is deleted.
+	 * @ninja orm_command view monitoring/naemon_command
+	 */
+	public function del_downtime_by_host_name($host_name, $service_description="", $downtime_start_time="", $comment="") {
+		return $this->submit_naemon_command("DEL_DOWNTIME_BY_HOST_NAME", $host_name, $service_description, $downtime_start_time, $comment);
+	}
+
 }

modules/monitoring/models/service.php
@@ -968,4 +968,43 @@ class Service_Model extends BaseService_Model {
 
     }
 
+	/**
+	 * @param host_name
+	 * @param service_description
+	 * @param downtime_start_time = ""
+	 * @param comment = true
+	 *
+	 * @ninja orm_command name Delete downtime
+	 * @ninja orm_command category Actions
+	 * @ninja orm_command icon scheduled-downtime
+	 *
+	 * @ninja orm_command params.host_name.id 0
+	 * @ninja orm_command params.host_name.type string
+	 * @ninja orm_command params.host_name.name Host_name
+	 *
+	 * @ninja orm_command params.service_description.id 1
+	 * @ninja orm_command params.service_description.type string
+	 * @ninja orm_command params.service_description.name Service description
+	 *
+	 * @ninja orm_command params.downtime_start_time.id 2
+	 * @ninja orm_command params.downtime_start_time.type time
+	 * @ninja orm_command params.downtime_start_time.name Downtime start time
+	 * @ninja orm_command params.downtime_start_time.default ""
+	 *
+	 * @ninja orm_command params.comment.id 3
+	 * @ninja orm_command params.comment.type bool
+	 * @ninja orm_command params.comment.name Comment
+	 * @ninja orm_command params.comment.default ""
+	 *
+	 * @ninja orm_command mayi_method update.command.downtime
+	 * @ninja orm_command description
+	 *     This command is used to delete a host or service downtime.
+	 *     The command will delete all downtimes which matches the criteria.
+	 *     Only the host_name is required, in which case all downtimes for
+	 *     for that host is deleted.
+	 * @ninja orm_command view monitoring/naemon_command
+	 */
+	public function del_downtime_by_host_name($host_name, $service_description, $downtime_start_time="", $comment="") {
+		return $this->submit_naemon_command("DEL_DOWNTIME_BY_HOST_NAME", $host_name, $service_description, $downtime_start_time, $comment);
+	}
 }

commit e46e4dae80322f73e44963a65306d82cda71d7c5

Correct downtime deletion on downtime objects
0182856827d205a459fb78eb5996043930ec447d introduced a regression making
it the external command for downtime deletion incorrect for downtime
objects. With this commit we ensure that the correct keys are using in
all scenarios.

Signed-off-by: Jacob Hansen <jhansen@op5.com>

modules/monitoring/models/naemonobject.php
@@ -31,15 +31,16 @@ class NaemonObject_Model extends Object_Model {
 				// keys, which does not correspond to the Neamon cmd
 				$key = $this->get_id();
 			}
+		} else if ($cmd == 'DEL_DOWNTIME_BY_HOST_NAME') {
+			// Don't use $key for DEL_DOWNTIME_BY_HOST_NAME since the required keys
+			// are passed as function parameters on non downtime/comment objects.
+			$key=false;
 		} else {
 			$key = $this->get_key();
 		}
 		$key = ($key===false) ? "" : ";".$key;
 
-		// Don't use $key for DEL_DOWNTIME_BY_HOST_NAME since the required keys
-		// are passed as function parameters.
-		$raw_command = sprintf("[%d] %s%s", time(), $cmd,
-			($cmd == 'DEL_DOWNTIME_BY_HOST_NAME') ? '' : $key);
+		$raw_command = sprintf("[%d] %s%s", time(), $cmd, $key);
 		foreach($args as $arg) {
 			$raw_command .= ";".$arg;
 		}