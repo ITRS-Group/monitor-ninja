14589d4e6211e2a16d4d7c48dee047b9ccd9aaa8
Jacob Hansen <jhansen@op5.com>
2020-02-18 16:38:42 +0100

    Fix deletion of all downtimes
    Previously, all downtimes were deleted by using the
    DEL_HOST/SVC_DOWNTIME external command. However this was changed in
    937db15e27c413da00d7564744a386227307b556 to use `DEL_DOWNTIME_BY_HOSTNAME`
    instead.
    
    This, seemed to have caused a regression for deleting all downtimes of a
    host/service.
    
    This commit the downtimes can be deleted correctly when calling the
    cancel all function.
    
    Fixes: MON-12040
    
    Signed-off-by: Jacob Hansen <jhansen@op5.com>

--- modules/monitoring/models/host.php
+++ modules/monitoring/models/host.php
@@ -633,16 +633,7 @@ class Host_Model extends BaseHost_Model {
 	 * @return array ['status' => boolean, 'output' => string]
 	 */
 	public function cancel_all_downtimes() {
-		$downtime_set = ObjectPool_Model::get_by_query('[downtimes] host.name = "'.$this->get_name().'"');
-		foreach($downtime_set->it(array(),array(), false, 0) as $downtime) {
-			$output = $downtime->delete();
-			if (!$output['status'])
-				return $output;
-		}
-		return array(
-			'status' => True,
-			'output' => sprintf(_('Your command was successfully submitted to %s.'), Kohana::config('config.product_name'))
-		);
+		return $this->del_downtime_by_host_name($this->get_name());
 	}
 
 	/**
