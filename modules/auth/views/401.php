595865589106a9a5ffda19ffbf20832c7212ad3e
Linus <linus@agren.cc>
2021-01-29 14:16:06 +0100

    Remove references to op5.com
    These are left over references to op5.com which no longer serves a web
    site, so I have replaced the links with the ITRS equivalents.
    
    I have also removed the link on the logo in the about popup, since the
    exact same link is right below it.

--- modules/auth/views/401.php
+++ modules/auth/views/401.php
@@ -6,8 +6,8 @@ if (count($messages) > 0) {
 	echo '<p>' . array_shift($messages) . "</p>";
 }
 else {
-	echo "<p>And I don't know why! This could be a bug, and we'd really appreciate it if you'd <a target=\"_blank\" href=\"http://bugs.op5.com\">report it</a> or <a target=\"_blank\" href=\"http://www.op5.com/services/support/\">contact support</a>.</p>";
-	echo "<p>Action was: <b>" . $action . "</b>, please include this information in your bug report/support ticket.</p>";
+	echo "<p>And I don't know why! This could be a bug, and we'd really appreciate it if you'd <a target=\"_blank\" href=\"https://support.itrsgroup.com\">contact support</a>.</p>";
+	echo "<p>Action was: <b>" . $action . "</b>, please include this information in your support ticket.</p>";
 }
 
 if (count($messages) > 0) {
--- modules/menu/views/about.php
+++ modules/menu/views/about.php
@@ -1,17 +1,15 @@
 <div class="popup-about">
-	<a target="_blank" href="http://www.op5.com">
 		<img class="popup-about-img" src="/ninja/modules/menu/views/itrs-about-logo.png">
-	</a>
   <table class="popup-about-table">
     <tr class="popup-about-row popup-about-row-links">
       <td>
         <a href="https://docs.itrsgroup.com/docs/op5-monitor/">Knowledge Base</a>
       </td>
       <td>
-        <a href="http://www.op5.com">www.op5.com</a>
+        <a href="https://www.itrsgroup.com/products/network-monitoring-op5-monitor">ITRS OP5 Monitor</a>
       </td>
       <td>
-        <a href="http://www.op5.com/support">Support</a>
+        <a href="https://support.itrsgroup.com/">Support</a>
       </td>
     </tr>
   </table>
