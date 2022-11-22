c973f792aa47ca3b8cd4bfc0391883e81ed12baa
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2022-11-22 13:13:51 +0800

    Requested Changes for About Panel
    Provided changes from PR for a better menu row display.
    
    Signed-off-by: Eunice Remoquillo <eremoquillo@itrsgroup.com>

--- modules/menu/views/about.php
+++ modules/menu/views/about.php
@@ -1,21 +1,21 @@
 <div class="popup-about">
 		<img class="popup-about-img" src="/ninja/modules/menu/views/itrs_op5_expanded.png">
-  <table class="popup-about-table">
-    <tr class="popup-about-row popup-about-row-links">
-      <td>
+  <div class="popup-about-table">
+    <div class="popup-about-row popup-about-row-links">
+      <span class="popup-link">
         <a href="<?php echo Kohana::config('product.docs_url'); ?>">Knowledge Base</a>
-      </td>
-      <td>
+      </span>
+      <span class="popup-link">
         <a href="https://www.itrsgroup.com/products/network-monitoring-op5-monitor">ITRS OP5 Monitor</a>
-      </td>
-      <td>
+      </span>
+      <span class="popup-link">
         <a onclick="openLicenseInfo()">License Information</a>
-      </td>
-      <td>
+      </span>
+      <span class="popup-link">
         <a href="<?php echo Kohana::config('product.support_url'); ?>">Support</a>
-      </td>
-    </tr>
-  </table>
+      </span>
+  </div>
+  </div>
 <?php
 $data = $about->get_all();
 echo html::get_definition_list($data);
