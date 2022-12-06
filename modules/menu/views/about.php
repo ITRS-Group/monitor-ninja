8b7a49b347458f22f23d05a2dff4c7abc5bba8f9
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2022-12-07 02:42:33 +0800

    Update about popup display
    This commit updates the about pop up layout.
    
    This resolves MON-12997
    
    Signed-off-by: Eunice Remoquillo <eremoquillo@itrsgroup.com>

--- modules/menu/views/about.php
+++ modules/menu/views/about.php
@@ -1,24 +1,24 @@
 <div class="popup-about">
 		<img class="popup-about-img" src="/ninja/modules/menu/views/itrs_op5_expanded.png">
-  <div class="popup-about-table">
-    <div class="popup-about-row popup-about-row-links">
-      <span class="popup-link">
+  <table class="popup-about-table">
+    <tr class="popup-about-row popup-about-row-links">
+      <td>
         <a href="<?php echo Kohana::config('product.docs_url'); ?>">Knowledge Base</a>
-      </span>
-      <span class="popup-link">
+      </td>
+      <td>
         <a href="https://www.itrsgroup.com/products/network-monitoring-op5-monitor">ITRS OP5 Monitor</a>
-      </span>
-      <span class="popup-link">
+      </td>
+      <td>
         <a onclick="openLicenseInfo()">License Information</a>
-      </span>
-      <span class="popup-link">
+      </td>
+      <td>
         <a href="<?php echo Kohana::config('product.support_url'); ?>">Support</a>
-      </span>
-  </div>
-  </div>
+      </td>
+    </tr>
+  </table>
 <?php
 $data = $about->get_all();
 echo html::get_definition_list($data);
 ?>
-
 </div>
+
