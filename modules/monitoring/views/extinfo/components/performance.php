9b0336706c8457c52c6057fe0c21a6472b5fbb5d
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2023-04-03 10:34:53 +0800

    Display unit of measurement if there is an existing value
    This commit makes the unit of measurement be displayed on the performance table only when value, min and max were set.
    
    This resolves MON-13179
    
    Signed-off-by: Eunice Remoquillo <eremoquillo@itrsgroup.com>

--- modules/monitoring/views/extinfo/components/performance.php
+++ modules/monitoring/views/extinfo/components/performance.php
@@ -125,7 +125,7 @@ if(count($perf_data)) {
 			</td>
 			<td>
 <?php
-		echo (isset($ds['value']) ? $ds['value'] : '') . (isset($ds['unit']) ? $ds['unit'] : '');
+		echo (isset($ds['value']) ? $ds['value'] . (isset($ds['unit']) ? $ds['unit'] : '') : '');
 ?>
 			</td>
 			<td>
@@ -140,12 +140,12 @@ if(count($perf_data)) {
 			</td>
 			<td>
 <?php
-		echo (isset($ds['min']) ? $ds['min'] : '') . (isset($ds['unit']) ? $ds['unit'] : '');
+		echo (isset($ds['min']) ? $ds['min'] . (isset($ds['unit']) ? $ds['unit'] : '') : '');
 ?>
 			</td>
 			<td>
 <?php
-		echo (isset($ds['max']) ? $ds['max'] : '') . (isset($ds['unit']) ? $ds['unit'] : '');
+		echo (isset($ds['max']) ? $ds['max'] . (isset($ds['unit']) ? $ds['unit'] : '') : '');
 ?>
 			</td>
 		</tr>
