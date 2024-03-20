d5e781e82ca35a92258d9610a64f1a5610644d68
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2024-03-20 16:39:49 +0800

    Clean up trendgraphs onload function
    Moved php and javascript commands inside the html head tag to run before the body finish loading its div components.

--- modules/reports/views/trends/new_report.php
+++ modules/reports/views/trends/new_report.php
@@ -1,64 +1,55 @@
-<div id="trends_graphs" style="margin: 20px auto 0 auto;">
-	<div id='tgraph'>Trend graphs loading...</div>
-	<script>
-		<?php
+<html>
+  <head>
+	<?php
+		$rawdata = array();
+		$labels = array();
+		$state_names = array();
+		$outputs = array();
+		$outputs_r = array();
 
-			$rawdata = array();
-			$labels = array();
-			$state_names = array();
-			$outputs = array();
-			$outputs_r = array();
+		foreach ($graph_pure_data as $service => $statechanges) {
 
-			foreach ($graph_pure_data as $service => $statechanges) {
+			$labels[] = $service;
+			$servicerow = array();
 
-				$labels[] = $service;
-
-				$servicerow = array();
-
-				for ($i = 0; $i < count($statechanges); $i++) {
-
-					$cur_out = $statechanges[$i]['output'];
-					if( isset( $outputs_r[$cur_out] ) ) {
-						$output_id = $outputs_r[$cur_out];
-					} else {
-						$output_id = count($outputs);
-						$outputs[] = $cur_out;
-						$outputs_r[$cur_out] = $output_id;
-					}
-
-					$servicerow[] = array(
-						$statechanges[$i]['duration'], /* 0: duration */
-						$statechanges[$i]['state'], /* 1: state */
-						$output_id /* 2: short */
-						);
-
-					switch(strtolower($obj_type)) {
-						case 'host':
-							$state_name = Reports_Model::$host_states[$statechanges[$i]['state']];
-							break;
-						case 'service':
-							$state_name = Reports_Model::$service_states[$statechanges[$i]['state']];
-							break;
-						default:
-							$state_name = 'N/A';
-					}
-					$state_names[$statechanges[$i]['state']] = ucfirst($state_name);
-
+			for ($i = 0; $i < count($statechanges); $i++) {
+				$cur_out = $statechanges[$i]['output'];
+				if( isset( $outputs_r[$cur_out] ) ) {
+					$output_id = $outputs_r[$cur_out];
+				} else {
+					$output_id = count($outputs);
+					$outputs[] = $cur_out;
+					$outputs_r[$cur_out] = $output_id;
 				}
-				$rawdata[] = $servicerow;
 
+				$servicerow[] = array(
+					$statechanges[$i]['duration'], /* 0: duration */
+					$statechanges[$i]['state'], /* 1: state */
+					$output_id /* 2: short */
+					);
+
+				switch(strtolower($obj_type)) {
+					case 'host':
+						$state_name = Reports_Model::$host_states[$statechanges[$i]['state']];
+						break;
+					case 'service':
+						$state_name = Reports_Model::$service_states[$statechanges[$i]['state']];
+						break;
+					default:
+						$state_name = 'N/A';
+				}
+				$state_names[$statechanges[$i]['state']] = ucfirst($state_name);
 			}
-			$colors = reports::_state_color_table($obj_type);
-
-		?>
-
-
+			$rawdata[] = $servicerow;
+		}
+		$colors = reports::_state_color_table($obj_type);
+	?>
+	<script defer>
 		var rawdata = <?php echo json_encode($rawdata); ?>,
 			short_names = <?php echo json_encode($outputs); ?>,
 			labels = <?php echo json_encode($labels); ?>,
 			state_names = <?php echo json_encode($state_names); ?>,
 			colors = <?php echo json_encode($colors); ?>;
-
 
 		var data = [];
 		for( var i=0; i<rawdata.length; i++ ) {
@@ -72,18 +63,22 @@
 				};
 			}
 		}
-
-		$(window).on("load", function () {
+		function displaytgraph() {
 			new TGraph(
 				data, 'timeline',
 				labels,
 				<?php echo $graph_start_date ?>,
 				<?php echo ($use_scaling) ? 'true':'false'; ?>
 			);
-		});
-
+		}
 	</script>
-	<?php if ($included && $skipped) {
-		printf(_("<p>Not showing %d graphs due to being 100%% OK</p>"), $skipped);
-	} ?>
-</div>
+  </head>
+  <body style="border:0; margin: 0" onload="displaytgraph()">
+	<div id="trends_graphs" style="margin: 20px auto 0 auto;">
+		<div id='tgraph'>Trend graphs loading...</div>
+		<?php if ($included && $skipped) {
+			printf(_("<p>Not showing %d graphs due to being 100%% OK</p>"), $skipped);
+		} ?>
+	</div>
+  </body>
+</html>
