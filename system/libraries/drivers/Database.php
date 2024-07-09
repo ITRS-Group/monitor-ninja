d163e64451abe847130ee20aff75d2752d42cc6d
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2024-07-09 11:01:19 +0800

    test add return type

--- system/libraries/drivers/Database.php
+++ system/libraries/drivers/Database.php
@@ -530,7 +530,7 @@ abstract class Database_Result implements ArrayAccess, Iterator, Countable {
 	/**
 	 * Countable: count
 	 */
-	public function count()
+	public function count(): int
 	{
 		return $this->total_rows;
 	}
