fd2e0fe632f883b8111a1e9688583d442fc06a5e
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2024-08-22 14:49:40 +0800

    Update orm_complete_Test.php

--- test/orm_complete_Test.php
+++ test/orm_complete_Test.php
@@ -37,7 +37,7 @@ class ORM_Complete_Test extends \PHPUnit\Framework\TestCase {
 	#[DataProvider('object_manifest_provider')]
 	public function test_set_by_key_always_returns_set($set_model, $pool_model) {
 		$set = StatusPool_Model::set_by_key('');
-		if(isset($set_model)){
+		if(isset($set_model) && !empty($set_model)) {
 			$string_model = (string) $set_model;
 			$this->assertInstanceOf($string_model, $set);
 		}
