5cef39667be8808f37a7f357b5704b680af5a9d4
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2024-08-29 16:08:50 +0800

    Update downtime.php

--- modules/monitoring/includes/downtime.php
+++ modules/monitoring/includes/downtime.php
@@ -481,9 +481,8 @@ class RecurringDowntime extends Downtime {
 	public function match_week_interval($target_date) {
 		$diff = $this->get_days_delta($target_date);
 		$week_diff = (int)(round($diff / 7));
-		$w_int = $this->match_interval($week_diff);
 		var_dump($target_date);
-		var_dump($w_int);
+		var_dump($diff);
 		return $this->match_interval($week_diff);
 	}
 
