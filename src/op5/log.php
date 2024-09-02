d45805713d9f3c9e8ad18cb50d1d20ad7f7e79a2
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2024-09-02 17:15:29 +0800

    debug log (2)

--- src/op5/log.php
+++ src/op5/log.php
@@ -252,6 +252,7 @@ class op5Log {
 			if($res === false) {
 				error_log(implode("\n", $messages));
 				error_log('Could not write to log file: ' . $file);
+				echo "File: $file could not be written";
 			} elseif($new_file) {
 				/* Set read-writable by owner and group.
 				 * both the web server and the monitor user is a member of the web server (apache) group.
@@ -259,6 +260,7 @@ class op5Log {
 				 * access errors.
 				 * */
 				chmod($file, 0664);
+				echo "New File: $file\n";
 			}
 			if ($user === 'root' && posix_getpwuid(fileowner($file)) === 'root') {
 				exec("id monitor -gn", $p_group, $status);
