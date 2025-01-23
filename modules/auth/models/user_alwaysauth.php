9fa2f763508735d465ff7cba07db377c4b278277
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2025-01-23 17:05:14 +0800

    test unserialize auth data

--- modules/auth/models/user_alwaysauth.php
+++ modules/auth/models/user_alwaysauth.php
@@ -32,7 +32,7 @@ class User_AlwaysAuth_Model extends User_Model {
 	 * @param $value bool
 	 */
 	public function set_authorized_for ($type, $value) {
-		$auth_data = $this->get_auth_data();
+		$auth_data = unserialize($this->get_auth_data());
 		if (!isset($auth_data[$type]))
 			throw new Exception(
 				"Unknown authorization type $type: are you sure everything was spelled correctly?");
