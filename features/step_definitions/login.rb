cc64d6df71f878701093d5be02f0cb4caa89439e
Gian Carlo Ilagan <gilagan@itrsgroup.com>
2025-01-28 13:18:56 +0800

    Modified duplicate step definition

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -1,13 +1,13 @@
 Given /^I am real logged in as "([^"]*)" with password "([^"]*)"$/ do |username, password|
 	steps %Q{
 		When I am on the main page
-		And I enter "#{username}" into "username"
-		And I enter "#{password}" into "password"
+		And I enter real "#{username}" into "username"
+		And I enter real "#{password}" into "password"
 		And I click "Log in"
 		Then I should see "Log out"
 	}
 end
 
-When /^I enter "([^"]*)" into "([^"]*)"$/ do |val, sel|
+When /^I enter real "([^"]*)" into "([^"]*)"$/ do |val, sel|
     fill_in(sel % @params, :with => val)
   end
