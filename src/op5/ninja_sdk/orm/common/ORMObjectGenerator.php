96cf9d00bde5b0393105b3d02df03550284ad939
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2025-05-20 12:34:09 +0800

    Update ORMObjectGenerator.php

--- src/op5/ninja_sdk/orm/common/ORMObjectGenerator.php
+++ src/op5/ninja_sdk/orm/common/ORMObjectGenerator.php
@@ -150,8 +150,10 @@ abstract class ORMObjectGenerator extends ORMGenerator {
 		$this->write(         '$subobj_export[$parts[0]][] = $parts[1];');
 		$this->write(         '$obj->export[] = $parts[0];');
 		$this->write(     '} else {');
-		$this->write(         '$part = strstr($parts[0], ` `, true) ?: $parts[0];');
+		$this->write(         'if(!is_null($parts[0])){');
+		$this->write(         '$part = strstr($parts[0], " ", true) ?: $parts[0];');
 		$this->write(         '$obj->export[] = $part;');
+		$this->write(         '}');
 		$this->write(     '}');
 		$this->write( '}');
 		$this->comment('If object fields exists, make sure the object only exists in the export array once');
