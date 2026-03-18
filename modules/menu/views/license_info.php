82f4f65f94f12aa15c633cc1da23832a1d5ec849
Eunice Remoquillo <eremoquillo@itrsgroup.com>
2026-03-18 12:23:20 +0800

    Updates to fix potential reverse tabnabbing attacks
    This commit updates various files to include the rel="noopener noreferrer" attribute on external links, improving security by preventing potential reverse tabnabbing attacks. Affected files include common.js, view.php for the bignumber widget, license_info.php, and list.php for backup management.

--- modules/menu/views/license_info.php
+++ modules/menu/views/license_info.php
@@ -6,13 +6,13 @@ if (($system_version = @file_get_contents('/etc/op5-monitor-release')) !== FALSE
 <h1>Service &amp; Support</h1>
 <p>Detailed information regarding Service and Support is covered in the general mandatory ITRS Service and Support agreement. This needs to be in place prior to contacting ITRS Support.</p>
 <p><strong>Note:</strong> If you have purchased this system trough an ITRS authorized dealer always contact your dealer first.</p>
-<p>For fast support, proceed to <a href="https://support.itrsgroup.com" target="_blank" class="about">support.itrsgroup.com</a>
+<p>For fast support, proceed to <a href="https://support.itrsgroup.com" rel="noopener noreferrer" target="_blank" class="about">support.itrsgroup.com</a>
 and fill out the support form.</p>
 
 <h1>Request for Enhancement (RFE)</h1>
 <p>Every RFE are of interest to us. You might not be alone with an idea or suggestion on a feature or other enhancements to the system. The RFE will be prioritized higher and come at a cheaper per-customer price if many customers have requested similar enhancements.</p>
 <p>You can add your enhancement request at:
-<a href="https://support.itrsgroup.com" target="_blank" class="about">support.itrsgroup.com</a></p>
+<a href="https://support.itrsgroup.com" rel="noopener noreferrer" target="_blank" class="about">support.itrsgroup.com</a></p>
 
 <h1>Software Licensing Information</h1>
 <p>The ITRS OP5 Monitor System and most of the added extra software packages included under the GNU General Public License (GPL) 2.0. The Apache web server is licensed under the Apache Software Foundation License.</p>
@@ -22,4 +22,4 @@ and fill out the support form.</p>
 <p>ITRS retains strict legal copyright on all software, be it scripts, source code or binaries, that are written from scratch by the ITRS development team. All rights are reserved.</p>
 
 <p>For further questions regarding copyright and licensing, get in touch at:
-<a href="https://www.itrsgroup.com/about/contact" target="_blank" class="about">www.itrsgroup.com/about/contact</a>.</p>
+<a href="https://www.itrsgroup.com/about/contact" rel="noopener noreferrer" target="_blank" class="about">www.itrsgroup.com/about/contact</a>.</p>
